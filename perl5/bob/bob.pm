d51ef535a0830c83dec1a658eda09d4df5049538
Erik Schierboom <erik_schierboom@hotmail.com>
2014-10-29 21:15:47 +0100

    Added all languages (with .gitignore files)

---
+++ perl5/bob/bob.pm
@@ -0,0 +1,25 @@
+#
+# This is a SKELETON file and has been provided to enable you to get working on the
+# first exercise more quickly.
+#
+
+package Bob;
+
+use strict;
+use Exporter;
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+
+$VERSION = 1.00;
+@ISA = qw(Exporter);
+@EXPORT = ();
+@EXPORT_OK = qw(hey);
+%EXPORT_TAGS = ( DEFAULT => [qw(&hey)],
+);
+
+sub hey {
+#
+# YOUR CODE GOES HERE
+#
+}
+
+1;
