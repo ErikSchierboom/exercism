dca14e540f5c156a2c472dd1e5a0c57d1bc98530
Erik Schierboom <erik_schierboom@hotmail.com>
2014-10-31 21:51:57 +0100

    Lazy optimization

--- scala/space-age/src/main/scala/SpaceAge.scala
+++ scala/space-age/src/main/scala/SpaceAge.scala
@@ -9,18 +9,18 @@ case class SpaceAge(seconds: Long) {
   val JupiterOrbitalPeriodInEarthYears = 11.862615
   val SaturnOrbitalPeriodInEarthYears = 29.447498
   val UranusOrbitalPeriodInEarthYears = 84.016846
-  val NeptureOrbitalPeriodInEarthYears = 164.79132
+  val NeptuneOrbitalPeriodInEarthYears = 164.79132
 
   def round(number: Double, digits: Int) = Math.round(number * Math.pow(10, digits)) / Math.pow(10, digits)
 
   def getYears(orbitalPeriod: Double): Double = round(seconds.toDouble / (SecondsInEarthYear * orbitalPeriod), 2)
 
-  def onEarth(): Double = getYears(EarthOrbitalPeriodInEarthYears)
-  def onMercury(): Double = getYears(MercuryOrbitalPeriodInEarthYears)
-  def onVenus(): Double = getYears(VenusOrbitalPeriodInEarthYears)
-  def onMars(): Double = getYears(MarsOrbitalPeriodInEarthYears)
-  def onJupiter(): Double = getYears(JupiterOrbitalPeriodInEarthYears)
-  def onSaturn(): Double = getYears(SaturnOrbitalPeriodInEarthYears)
-  def onUranus(): Double = getYears(UranusOrbitalPeriodInEarthYears)
-  def onNeptune(): Double = getYears(NeptureOrbitalPeriodInEarthYears)
+  lazy val onEarth = getYears(EarthOrbitalPeriodInEarthYears)
+  lazy val onMercury = getYears(MercuryOrbitalPeriodInEarthYears)
+  lazy val onVenus = getYears(VenusOrbitalPeriodInEarthYears)
+  lazy val onMars = getYears(MarsOrbitalPeriodInEarthYears)
+  lazy val onJupiter = getYears(JupiterOrbitalPeriodInEarthYears)
+  lazy val onSaturn = getYears(SaturnOrbitalPeriodInEarthYears)
+  lazy val onUranus = getYears(UranusOrbitalPeriodInEarthYears)
+  lazy val onNeptune = getYears(NeptuneOrbitalPeriodInEarthYears)
 }
