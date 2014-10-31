0d8305be8d5655e650457349258802b21a557956
Erik Schierboom <erik_schierboom@hotmail.com>
2014-10-31 21:38:18 +0100

    Use lazy val for optimization

--- scala/grains/src/main/scala/Grains.scala
+++ scala/grains/src/main/scala/Grains.scala
@@ -1,5 +1,5 @@
 object Grains {
   def square(number: Int): BigInt = BigInt(2).pow(number - 1)
 
-  def total: BigInt = (1 to 64).map(square).sum
+  lazy val total: BigInt = (1 to 64).map(square).sum
 }
