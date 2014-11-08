56f8dc6ca198b1113a152ef4750698e99c41be59
Erik Schierboom <erik_schierboom@hotmail.com>
2014-11-08 09:06:08 +0100

    Small improvement to Hamming

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,10 +1,3 @@
 object Hamming {
-  def compute(strand1: String, strand2: String): Int = {
-    val sharedLength = Math.min(strand1.length, strand2.length)
-
-    def compareNucleotide(index: Int) = if (strand1(index) == strand2(index)) 0 else 1
-    def compareNucleotides(sum: Int, index: Int) = sum + compareNucleotide(index)
-
-    (0 until sharedLength).foldLeft(0)(compareNucleotides)
-  }
+  def compute(strand1: String, strand2: String): Int = strand1.zip(strand2).count { case (x, y) => x != y }
 }
