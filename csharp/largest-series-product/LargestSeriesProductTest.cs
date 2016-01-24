1645774c5ba03b0309d9998b77873b9bc138bdc6
Erik Schierboom <erik_schierboom@hotmail.com>
2016-01-24 13:02:36 +0100

    Solve Largest Series Product (C#)

--- csharp/largest-series-product/LargestSeriesProductTest.cs
+++ csharp/largest-series-product/LargestSeriesProductTest.cs
@@ -1,5 +1,6 @@
 using System;
 using NUnit.Framework;
+using Exercism;
 
 [TestFixture]
 public class LargestSeriesProductTest
@@ -10,21 +11,18 @@ public class LargestSeriesProductTest
         Assert.That(new LargestSeriesProduct("0123456789").Digits, Is.EqualTo(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }));
     }
 
-    [Ignore]
     [Test]
     public void Digits_maintain_order()
     {
         Assert.That(new LargestSeriesProduct("9876543210").Digits, Is.EqualTo(new[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }));
     }
 
-    [Ignore]
     [Test]
     public void Digits_is_an_empty_array_for_empty_input()
     {
         Assert.That(new LargestSeriesProduct("").Digits, Is.Empty);
     }
 
-    [Ignore]
     [Test]
     public void Slices_digits_by_limit_given()
     {
@@ -32,7 +30,6 @@ public class LargestSeriesProductTest
             Is.EqualTo(new[] { new[] { 0, 1 }, new[] { 1, 2 }, new[] { 2, 3 }, new[] { 3, 4 } }));
     }
 
-    [Ignore]
     [Test]
     public void Slices_are_as_long_as_the_limit_given()
     {
@@ -40,7 +37,6 @@ public class LargestSeriesProductTest
             Is.EqualTo(new[] { new[] { 9, 8, 2 }, new[] { 8, 2, 3 }, new[] { 2, 3, 4 }, new[] { 3, 4, 7 } }));
     }
 
-    [Ignore]
     [Test]
     public void Cannot_slice_bigger_than_input()
     {
@@ -48,7 +44,6 @@ public class LargestSeriesProductTest
         Assert.That(ex.Message, Is.EqualTo("Slice size is too big"));
     }
 
-    [Ignore]
     [TestCase("01234567890", 2, Result = 72)]
     [TestCase("1027839564", 3, Result = 270)]
     public int Gets_the_largest_product(string digits, int seriesLength)
@@ -56,14 +51,12 @@ public class LargestSeriesProductTest
         return new LargestSeriesProduct(digits).GetLargestProduct(seriesLength);
     }
 
-    [Ignore]
     [Test]
     public void Largest_product_works_for_small_numbers()
     {
         Assert.That(new LargestSeriesProduct("19").GetLargestProduct(2), Is.EqualTo(9));
     }
 
-    [Ignore]
     [Test]
     public void Largest_product_works_for_large_numbers()
     {
@@ -71,7 +64,6 @@ public class LargestSeriesProductTest
         Assert.That(new LargestSeriesProduct(LARGE_NUMBER).GetLargestProduct(6), Is.EqualTo(23520));
     }
 
-    [Ignore]
     [Test]
     public void Largest_product_for_empty_input_is_1()
     {
