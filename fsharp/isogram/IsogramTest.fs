9d9a39b25578f5354d987ddd0617a7d2c44e95c4
Erik Schierboom <erik_schierboom@hotmail.com>
2016-05-14 09:25:01 +0200

    Add more F# exercises

---
+++ fsharp/isogram/IsogramTest.fs
@@ -0,0 +1,17 @@
+﻿module IsogramTest
+    
+open NUnit.Framework
+
+open Isogram
+
+[<TestCase("duplicates", ExpectedResult = true)>]
+[<TestCase("eleven", ExpectedResult = false)>]
+[<TestCase("subdermatoglyphic", ExpectedResult = true)>]
+[<TestCase("Alphabet", ExpectedResult = false)>]
+[<TestCase("thumbscrew-japingly", ExpectedResult = true)>]
+[<TestCase("Hjelmqvist-Gryb-Zock-Pfund-Wax", ExpectedResult = true)>]
+[<TestCase("Heizölrückstoßabdämpfung", ExpectedResult = true)>]
+[<TestCase("the quick brown fox", ExpectedResult = false)>]
+[<TestCase("Emily Jung Schwartzkopf", ExpectedResult = true)>]
+let ``Isogram correctly detects isograms`` (actual: string) =
+    isogram actual
