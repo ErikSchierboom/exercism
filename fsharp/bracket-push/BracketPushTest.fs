d27318d2fbf1bd0f55270c9a8eeb321ae577d70d
Erik Schierboom <erik_schierboom@hotmail.com>
2016-05-19 09:33:02 +0200

    Add F# exercises

---
+++ fsharp/bracket-push/BracketPushTest.fs
@@ -0,0 +1,65 @@
+﻿module BracketPushTest
+
+open NUnit.Framework
+
+open BracketPush
+
+[<Test>]
+let ``Paired square brackets`` () =
+    let actual ="[]"
+    Assert.That(matched actual, Is.True)
+
+[<Test>]
+let ``Empty string`` () =
+    let actual =""
+    Assert.That(matched actual, Is.True)
+
+[<Test>]
+let ``Unpaired brackets`` () =
+    let actual ="[["
+    Assert.That(matched actual, Is.False)
+
+[<Test>]
+let ``Wrong ordered brackets`` () =
+    let actual ="}{"
+    Assert.That(matched actual, Is.False)
+
+[<Test>]
+let ``Paired with whitespace`` () =
+    let actual ="{ }"
+    Assert.That(matched actual, Is.True)
+
+[<Test>]
+let ``Simple nested brackets`` () =
+    let actual ="{[]}"
+    Assert.That(matched actual, Is.True)
+
+[<Test>]
+let ``Several paired brackets`` () =
+    let actual ="{}[]"
+    Assert.That(matched actual, Is.True)
+
+[<Test>]
+let ``Paired and nested brackets`` () =
+    let actual ="([{}({}[])])"
+    Assert.That(matched actual, Is.True)
+
+[<Test>]
+let ``Unpaired and nested brackets`` () =
+    let actual ="([{])"
+    Assert.That(matched actual, Is.False)
+
+[<Test>]
+let ``Paired and wrong nested brackets`` () =
+    let actual ="[({]})"
+    Assert.That(matched actual, Is.False)
+
+[<Test>]
+let ``Math expression`` () =
+    let actual ="(((185 + 223.85) * 15) - 543)/2"
+    Assert.That(matched actual, Is.True)
+
+[<Test>]
+let ``Complex latex expression`` () =
+    let actual ="\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)"
+    Assert.That(matched actual, Is.True)
