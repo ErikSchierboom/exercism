7bb6fa45c7e50448f1eee9053a397a5adc73f12f
Erik Schierboom <erik_schierboom@hotmail.com>
2016-07-28 10:18:04 +0200

    Add more F# exercises
    - Add sgf-parsing exercise
    - Add tree-building exercise
    - Add transpose exercise
    - Add tree building exercise
    - Add grep exercise
    - Add hangman exercise
    - Add error handling exercise
    - Add dot-dsl exercise

---
+++ fsharp/rectangles/RectanglesTest.fs
@@ -0,0 +1,93 @@
+﻿module RectanglesTest
+
+open NUnit.Framework
+
+open System
+
+open Rectangle
+
+[<Test>]
+let ``No rows`` () =
+    let input = []
+    Assert.That(rectangles input, Is.EqualTo(0))
+
+[<Test>]
+let ``No columns`` () =
+    let input = [""]
+    Assert.That(rectangles input, Is.EqualTo(0))
+
+[<Test>]
+let ``No rectangles`` () =
+    let input = [" "]
+    Assert.That(rectangles input, Is.EqualTo(0))
+
+[<Test>]
+let ``One rectangle`` () =
+    let input = 
+        [ "+-+";
+          "| |";
+          "+-+" ]        
+    Assert.That(rectangles input, Is.EqualTo(1))
+
+[<Test>]
+let ``two rectangles without shared parts`` () =
+    let input = 
+        [ "  +-+";
+          "  | |";
+          "+-+-+";
+          "| |  ";
+          "+-+  " ]        
+    Assert.That(rectangles input, Is.EqualTo(2))
+
+[<Test>]
+let ``Five rectangles with shared parts`` () =
+    let input = 
+        [ "  +-+";
+          "  | |";
+          "+-+-+";
+          "| | |";
+          "+-+-+" ]        
+    Assert.That(rectangles input, Is.EqualTo(5))
+
+[<Test>]
+let ``Only complete rectangles are counted`` () =
+    let input = 
+        [ "  +-+";
+          "    |";
+          "+-+-+";
+          "| | -";
+          "+-+-+" ]       
+    Assert.That(rectangles input, Is.EqualTo(1))
+
+[<Test>]
+let ``Rectangles can be of different sizes`` () =
+    let input =
+         ["+------+----+";
+          "|      |    |";
+          "+---+--+    |";
+          "|   |       |";
+          "+---+-------+" ]       
+    Assert.That(rectangles input, Is.EqualTo(3))
+
+[<Test>]
+let ``Corner is required for a rectangle to be complete`` () =
+    let input = 
+        [ "+------+----+";
+          "|      |    |";
+          "+------+    |";
+          "|   |       |";
+          "+---+-------+" ]       
+    Assert.That(rectangles input, Is.EqualTo(2))
+
+[<Test>]
+let ``Large input with many rectangles`` () =
+    let input = 
+        [ "+---+--+----+";
+          "|   +--+----+";
+          "+---+--+    |";
+          "|   +--+----+";
+          "+---+--+--+-+";
+          "+---+--+--+-+";
+          "+------+  | |";
+          "          +-+" ]       
+    Assert.That(rectangles input, Is.EqualTo(60))
