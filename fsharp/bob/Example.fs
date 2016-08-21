a8c4e3f3a606223658194770ab154c75f6828fb6
Erik Schierboom <erik_schierboom@hotmail.com>
2016-08-21 14:49:14 +0200

    F# exercises improvements

--- fsharp/bob/Example.fs
+++ fsharp/bob/Example.fs
@@ -2,14 +2,14 @@
 
 open System
 
-let hey(input: string) = 
+let hey (input: string) = 
     let isEmpty = String.IsNullOrWhiteSpace input
     let isYell = Seq.exists Char.IsLetter input && input = input.ToUpperInvariant()
     let isQuestion = input.EndsWith "?"
 
     match input with 
-        | _ when isEmpty -> "Fine. Be that way!"
-        | _ when isYell -> "Whoa, chill out!"
-        | _ when isQuestion -> "Sure."
-        | _ -> "Whatever."
+    | _ when isEmpty -> "Fine. Be that way!"
+    | _ when isYell -> "Whoa, chill out!"
+    | _ when isQuestion -> "Sure."
+    | _ -> "Whatever."
 
--- fsharp/say/Example.fs
+++ fsharp/say/Example.fs
@@ -30,7 +30,8 @@ let bases n =
     Array.tryItem (n - 1) values
                         
 let tens n = 
-    if n < 20 then bases n
+    if n < 20 then 
+        bases n
     else 
         let values = 
             [| "twenty"
@@ -43,43 +44,42 @@ let tens n =
                "ninety" |]
         
         let (count, remainder) = quotRem (int64 n) 10L
-        let countStr = Array.item ((int count) - 2)  values
+        let countStr = Array.item ((int count) - 2) values
         let basesStr = Option.fold (fun acc item -> "-" + item) "" (bases (int remainder))
         Some (countStr + basesStr)
 
 let hundreds n = 
-    if n < 100L then tens (int n)
+    if n < 100L then 
+        tens (int n)
     else 
         let (count, remainder) = quotRem (int64 n) 100L
         let tensStr = Option.fold (fun acc item -> " " + item) "" (tens (int remainder))
         Option.bind (fun item -> Some (item + " hundred" + tensStr)) (bases (int count))
 
 let chunk str n = Option.bind (fun item -> Some (item + " " + str)) (hundreds n)
-
 let thousands = chunk "thousand"
-
 let millions = chunk "million"
-
 let billions = chunk "billion"
 
-let parts n = 
-    let (billionsCount, billionsRemainder) = quotRem n 1000000000L
-    let (millionsCount, millionsRemainder) = quotRem billionsRemainder 1000000L
-    let (thousandsCount, remainder) = quotRem millionsRemainder 1000L
-    (billionsCount, millionsCount, thousandsCount, remainder)
+let parts number = 
+    let (billionsCount,  billionsRemainder)  = quotRem number            1000000000L
+    let (millionsCount,  millionsRemainder)  = quotRem billionsRemainder 1000000L
+    let (thousandsCount, thousandsRemainder) = quotRem millionsRemainder 1000L
+    (billionsCount, millionsCount, thousandsCount, thousandsRemainder)
     
-let inEnglish n = 
-    match n with
-    | _ when n < 0L || n>= 1000000000000L -> None
-    | 0L -> Some "zero"
+let inEnglish number = 
+    match number with
+    | _ when number < 0L || number>= 1000000000000L -> 
+        None
+    | 0L -> 
+        Some "zero"
     | _ -> 
-        let (billionsCount, millionsCount, thousandsCount, remainder) = parts n
+        let (billionsCount, millionsCount, thousandsCount, remainder) = parts number
 
         [ billions billionsCount; 
           millions millionsCount;
           thousands thousandsCount;
-          hundreds remainder]
-        |> List.filter Option.isSome
-        |> List.map Option.get
+          hundreds remainder ]
+        |> List.choose id
         |> List.reduce (fun x y -> x + " " + y)
         |> Some
