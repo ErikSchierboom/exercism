892fded1d6e9cf2f8d5960e350631a93befe3eba
Erik Schierboom <erik_schierboom@hotmail.com>
2016-10-17 17:13:09 +0200

    Add grains exercise (C#)

---
+++ csharp/grains/Example.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+
+public static class Grains
+{
+    public static double Square(int i) => Math.Pow(2, i - 1);
+
+    public static double Total() => Enumerable.Range(1, 64).Select(Square).Sum();
+}
