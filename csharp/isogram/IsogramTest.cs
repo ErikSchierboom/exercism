d41f11e6253f02d9b8cf2920e3e0b9c80dbc6e87
Erik Schierboom <erik_schierboom@hotmail.com>
2016-10-18 19:29:36 +0200

    Add isogram exercise (C#)

---
+++ csharp/isogram/IsogramTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class IsogramTest
+{
+    [TestCase("duplicates", ExpectedResult = true)]
+    [TestCase("eleven", ExpectedResult = false)]
+    [TestCase("subdermatoglyphic", ExpectedResult = true)]
+    [TestCase("Alphabet", ExpectedResult = false)]
+    [TestCase("thumbscrew-japingly", ExpectedResult = true)]
+    [TestCase("Hjelmqvist-Gryb-Zock-Pfund-Wax", ExpectedResult = true)]
+    [TestCase("Heizölrückstoßabdämpfung", ExpectedResult = true)]
+    [TestCase("the quick brown fox", ExpectedResult = false)]
+    [TestCase("Emily Jung Schwartzkopf", ExpectedResult = true)]
+    [TestCase("éléphant", ExpectedResult = false)]
+    public bool Isogram_correctly_detects_isograms(string input)
+    {
+        return Isogram.IsIsogram(input);
+    }
+}
