a7776678d88ebfc437fc65feff5e2432e9dee78c
Erik Schierboom <erik_schierboom@hotmail.com>
2016-10-18 19:24:26 +0200

    Add perfect-numbers exercise (C#)

---
+++ csharp/perfect-numbers/Example.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+public enum NumberType
+{
+    Deficient,
+    Perfect,
+    Abundant
+}
+
+public class PerfectNumbers
+{
+    public static NumberType Classify(int number)
+    {
+        var sumOfFactors = Enumerable.Range(1, number/2).Where(x => number%x == 0).Sum();
+
+        if (sumOfFactors == number)
+        { 
+            return NumberType.Perfect;
+        }
+
+        return sumOfFactors < number ? NumberType.Deficient : NumberType.Abundant;
+    }
+}
