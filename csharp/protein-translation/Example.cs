37c57b2f03f21fbb42b40b52797a6b9305dc03e8
Erik Schierboom <erik_schierboom@hotmail.com>
2016-10-18 19:21:05 +0200

    Add protein-translation exercise (C#)

---
+++ csharp/protein-translation/Example.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ProteinTranslation
+{
+    public static IEnumerable<string> Translate(string codons)
+    {
+        return codons.Chunks(3).Select(TranslateCodon).TakeWhile(x => x != "STOP").ToList();
+    }
+
+    private static string TranslateCodon(string codon)
+    {
+        switch (codon)
+        {
+            case "AUG":
+                return "Methionine";
+            case "UUC":
+            case "UUU":
+                return "Phenylalanine";
+            case "UUA":
+            case "UUG":
+                return "Leucine";
+            case "UCU":
+            case "UCC":
+            case "UCA":
+            case "UCG":
+                return "Serine";
+            case "UAU":
+            case "UAC":
+                return "Tyrosine";
+            case "UGU":
+            case "UGC":
+                return "Cysteine";
+            case "UGG":
+                return "Tryptophan";
+            case "UAA":
+            case "UAG":
+            case "UGA":
+                return "STOP";
+            default:
+                throw new ArgumentException();
+        }
+    }
+
+    private static IEnumerable<string> Chunks(this string str, int chunkSize)
+    {
+        return Enumerable.Range(0, (int)Math.Ceiling(str.Length / (double)chunkSize))
+                         .Select(i => str.Substring(i * chunkSize, Math.Min(str.Length - i * chunkSize, chunkSize)));
+    }
+}
---
+++ csharp/protein-translation/ProteinTranslationTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class ProteinTranslationTest
+{
+    [TestCase("AUG")]
+    public void Identifies_methionine_codons(string codon)
+    {
+        Assert.That(ProteinTranslation.Translate(codon), Is.EquivalentTo(new[] { "Methionine" }));
+    }
+
+    [TestCase("UUU")]
+    [TestCase("UUC")]
+    public void Identifies_phenylalanine_codons(string codon)
+    {
+        Assert.That(ProteinTranslation.Translate(codon), Is.EquivalentTo(new[] { "Phenylalanine" }));
+    }
+
+    [TestCase("UUA")]
+    [TestCase("UUG")]
+    public void Identifies_leucine_codons(string codon)
+    {
+        Assert.That(ProteinTranslation.Translate(codon), Is.EquivalentTo(new[] { "Leucine" }));
+    }
+
+    [TestCase("UCU")]
+    [TestCase("UCC")]
+    [TestCase("UCA")]
+    [TestCase("UCG")]
+    public void Identifies_serine_codons(string codon)
+    {
+        Assert.That(ProteinTranslation.Translate(codon), Is.EquivalentTo(new[] { "Serine" }));
+    }
+
+    [TestCase("UAU")]
+    [TestCase("UAC")]
+    public void Identifies_tyrosine_codons(string codon)
+    {
+        Assert.That(ProteinTranslation.Translate(codon), Is.EquivalentTo(new[] { "Tyrosine" }));
+    }
+
+    [TestCase("UGU")]
+    [TestCase("UGC")]
+    public void Identifies_cysteine_codons(string codon)
+    {
+        Assert.That(ProteinTranslation.Translate(codon), Is.EquivalentTo(new[] { "Cysteine" }));
+    }
+
+    [TestCase("UGG")]
+    public void Identifies_tryptophan_codons(string codon)
+    {
+        Assert.That(ProteinTranslation.Translate(codon), Is.EquivalentTo(new[] { "Tryptophan" }));
+    }
+
+    [Test]
+    public void Translates_rna_strand_into_correct_protein()
+    {
+        Assert.That(ProteinTranslation.Translate("AUGUUUUGG"), Is.EquivalentTo(new[] { "Methionine", "Phenylalanine", "Tryptophan" }));
+    }
+
+    [Test]
+    public void Stops_translation_if_stop_codon_present()
+    {
+        Assert.That(ProteinTranslation.Translate("AUGUUUUAA"), Is.EquivalentTo(new[] { "Methionine", "Phenylalanine" }));
+    }
+
+    [Test]
+    public void Stops_translation_of_longer_strand()
+    {
+        Assert.That(ProteinTranslation.Translate("UGGUGUUAUUAAUGGUUU"), Is.EquivalentTo(new[] { "Tryptophan", "Cysteine", "Tyrosine" }));
+    }
+
+    [Test]
+    public void Throws_for_invalid_codons()
+    {
+        Assert.That(() => ProteinTranslation.Translate("CARROT"), Throws.Exception);
+    }
+}
