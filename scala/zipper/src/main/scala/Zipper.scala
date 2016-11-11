f125af697520fafc9e13f5398811a840d33a6763
Erik Schierboom <erik_schierboom@hotmail.com>
2016-11-11 18:27:00 +0100

    Improved zipper exercise (Scala)

--- scala/zipper/src/main/scala/Zipper.scala
+++ scala/zipper/src/main/scala/Zipper.scala
@@ -2,7 +2,7 @@ trait Crumb[A]
 case class LeftCrumb[A](value: A, tree: Option[BinTree[A]]) extends Crumb[A]
 case class RightCrumb[A](value: A, tree: Option[BinTree[A]]) extends Crumb[A]
 
-case class Zipper[A](zValue: A, zLeft: Option[BinTree[A]], zRight: Option[BinTree[A]], zCrumbs: List[Crumb[A]])
+case class Zipper[A](value: A, left: Option[BinTree[A]], right: Option[BinTree[A]], crumbs: List[Crumb[A]])
 case class BinTree[A](value: A, left: Option[BinTree[A]], right: Option[BinTree[A]])
 
 object Zipper {
@@ -16,34 +16,28 @@ object Zipper {
         case (RightCrumb(tv, tl)::cs) => loop(cs, BinTree(tv, tl, Some(t)))
       }
 
-    val tree = BinTree(zipper.zValue, zipper.zLeft, zipper.zRight)
-    loop(zipper.zCrumbs, tree)
+    val tree = BinTree(zipper.value, zipper.left, zipper.right)
+    loop(zipper.crumbs, tree)
   }
 
-  def value[A](zipper: Zipper[A]): A = zipper.zValue
+  def value[A](zipper: Zipper[A]): A = zipper.value
 
   def left[A](zipper: Zipper[A]): Option[Zipper[A]] =
-    zipper.zLeft match {
-      case None => None
-      case Some(l) => Some(Zipper(l.value, l.left, l.right, LeftCrumb(zipper.zValue, zipper.zRight) :: zipper.zCrumbs))
-    }
+    zipper.left.map(l => fromTree(l).copy(crumbs = LeftCrumb(zipper.value, zipper.right) :: zipper.crumbs))
 
   def right[A](zipper: Zipper[A]): Option[Zipper[A]] =
-    zipper.zRight match {
-      case None => None
-      case Some(r) => Some(Zipper(r.value, r.left, r.right, RightCrumb(zipper.zValue, zipper.zLeft) :: zipper.zCrumbs))
-    }
+    zipper.right.map(r => fromTree(r).copy(crumbs = RightCrumb(zipper.value, zipper.left) :: zipper.crumbs))
 
   def up[A](zipper: Zipper[A]): Option[Zipper[A]] =
-    zipper.zCrumbs match {
+    zipper.crumbs match {
       case Nil => None
       case LeftCrumb(tv, tr)::cs => Some(Zipper(tv, Some(toTree(zipper)), tr, cs))
       case RightCrumb(tv, tl)::cs => Some(Zipper(tv, tl, Some(toTree(zipper)), cs))
     }
 
-  def setValue[A](v: A, zipper: Zipper[A]): Zipper[A] = Zipper[A](v, zipper.zLeft, zipper.zRight, zipper.zCrumbs)
+  def setValue[A](v: A, zipper: Zipper[A]): Zipper[A] =  zipper.copy(value = v)
 
-  def setLeft[A](l: Option[BinTree[A]], zipper: Zipper[A]): Zipper[A] = Zipper[A](zipper.zValue, l, zipper.zRight, zipper.zCrumbs)
+  def setLeft[A](l: Option[BinTree[A]], zipper: Zipper[A]): Zipper[A] = zipper.copy(left = l)
 
-  def setRight[A](r: Option[BinTree[A]], zipper: Zipper[A]): Zipper[A] = Zipper[A](zipper.zValue, zipper.zLeft, r, zipper.zCrumbs)
+  def setRight[A](r: Option[BinTree[A]], zipper: Zipper[A]): Zipper[A] = zipper.copy(right = r)
 }
