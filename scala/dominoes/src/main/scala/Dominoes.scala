c6bb95a86bb1bce43d77589b78609448489cf154
Erik Schierboom <erik_schierboom@hotmail.com>
2016-11-26 20:07:03 +0100

    Update dominoes exercise (Scala)

--- scala/dominoes/src/main/scala/Dominoes.scala
+++ scala/dominoes/src/main/scala/Dominoes.scala
@@ -1,27 +1,27 @@
 object Dominoes {
 
-  private def validChain(dominoes: List[(Int, Int)]): Boolean =
-    dominoes match {
-      case Nil => true
-      case (x, y)::Nil => x == y
-      case (x1, y1)::(x2, y2)::xs => y1 == x2 && validChain((x1, y2)::xs)
+  private def left(dominoes: List[(Int, Int)]) = dominoes.head._1
+  private def right(dominoes: List[(Int, Int)]) = dominoes.last._2
+
+  private def tryChainTo(path: List[(Int, Int)])(attach: ((Int, Int), List[(Int, Int)])): List[List[(Int, Int)]] =
+    attach match {
+      case ((a, b), rest) if b == left(path) => findChains(rest, (a, b) :: path)
+      case ((a, b), rest) if a == left(path) => findChains(rest, (b, a) :: path)
+      case _ => Nil
     }
 
-  private def rotations(dominoes: List[(Int, Int)]) =
-    dominoes.indices.map(i => dominoes.drop(i) ++ dominoes.take(i))
-
-  private def allPaths(dominoes: List[(Int, Int)]): List[List[(Int, Int)]] = {
-    dominoes match {
-      case Nil => Nil
-      case x::Nil => List(List(x), List(x.swap))
-      case x::xs => allPaths(xs)
-        .flatMap(path => rotations(x::path) ++ rotations(x.swap::path))
+  private def findChains(stones: List[(Int, Int)], path: List[(Int, Int)]): List[List[(Int, Int)]] =
+    stones match {
+      case Nil => if (left(path) == right(path)) List(path) else Nil
+      case _ => rotations(stones).flatMap(tryChainTo(path))
     }
-  }
+
+  private def rotations[A](ls: List[A]): List[(A, List[A])] =
+    ls.indices.map(i => (ls.drop(i).head, ls.take(i) ++ ls.drop(i + 1))).toList
 
   def chain(dominoes: List[(Int, Int)]): Option[List[(Int, Int)]] =
     dominoes match {
       case Nil => Some(Nil)
-      case xs => allPaths(dominoes).find(validChain)
+      case first :: rest => findChains(rest, List(first)).headOption
     }
 }
