869726a4d38c5bf55ffe634a28eee93fbabfd86b
Erik Schierboom <erik_schierboom@hotmail.com>
2016-12-05 09:45:06 +0100

    Update wordy exercise (Scala)

--- scala/wordy/src/main/scala/WordProblem.scala
+++ scala/wordy/src/main/scala/WordProblem.scala
@@ -3,7 +3,7 @@ import scala.util.parsing.combinator._
 sealed trait Expression {
   def solve: Int
 }
-case class Number(n: Int) extends Expression {
+case class Operand(n: Int) extends Expression {
   def solve = n
 }
 case class Sum(e1: Expression, e2: Expression) extends Expression {
@@ -22,13 +22,13 @@ case class Power(e1: Expression, e2: Expression) extends Expression {
   def solve = Math.pow(e1.solve, e2.solve).toInt
 }
 
-object WordProblemParser extends RegexParsers {
-  def number: Parser[Number] = """(0|-?[1-9]\d*)""".r ^^ { str => Number(str.toInt) }
+object WordProblem extends RegexParsers {
+  def operand: Parser[Operand] = """(0|-?[1-9]\d*)""".r ^^ { str => Operand(str.toInt) }
 
-  def operand: Parser[String] = "plus" | "minus" | "multiplied by" | "divided by"  | "raised to the"
+  def operator: Parser[String] = "plus" | "minus" | "multiplied by" | "divided by"  | "raised to the"
 
   def operation: Parser[Expression] =
-    expression ~ (operand ~ expression).* ^^ {
+    expression ~ (operator ~ expression).* ^^ {
       case expr ~ list => list.foldLeft(expr) {
         case (left, "plus" ~ right)          => Sum(left, right)
         case (left, "minus" ~ right)         => Subtract(left, right)
@@ -38,15 +38,13 @@ object WordProblemParser extends RegexParsers {
       }
     }
 
-  def expression: Parser[Expression] = number | operation
+  def expression: Parser[Expression] = operand | operation
 
-  def problem: Parser[Expression] = "What is " ~> operation <~ "?"
-}
+  def equation: Parser[Expression] = "What is " ~> operation <~ "?"
 
-object WordProblem {
   def apply(input: String): Option[Int] = {
-    WordProblemParser.parse(WordProblemParser.problem, input) match {
-      case WordProblemParser.Success(expression, _) => Some(expression.solve)
+    parse(equation, input) match {
+      case Success(expression, _) => Some(expression.solve)
       case _ => None
     }
   }
