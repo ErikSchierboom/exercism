c2143bd2d9ba08639bcba1ce05c9e3e1262564cb
Erik Schierboom <erik_schierboom@hotmail.com>
2016-12-12 09:21:41 +0100

    Add meetup exercise (ES6)

---
+++ ecmascript/meetup/README.md
@@ -0,0 +1,54 @@
+# Meetup
+
+Calculate the date of meetups.
+
+Typically meetups happen on the same day of the week.
+
+Examples are
+
+- the first Monday
+- the third Tuesday
+- the Wednesteenth
+- the last Thursday
+
+Note that "Monteenth", "Tuesteenth", etc are all made up words. There
+was a meetup whose members realised that there are exactly 7 days that
+end in '-teenth'. Therefore, one is guaranteed that each day of the week
+(Monday, Tuesday, ...) will have exactly one date that is named with '-teenth'
+in every month.
+
+## Setup
+
+Go through the setup instructions for ECMAScript to
+install the necessary dependencies:
+
+http://exercism.io/languages/ecmascript
+
+## Requirements
+
+Install assignment dependencies:
+
+```bash
+$ npm install
+```
+
+## Making the test suite pass
+
+Execute the tests with:
+
+```bash
+$ npm test
+```
+
+In many test suites all but the first test have been skipped.
+
+Once you get a test passing, you can unskip the next one by
+changing `xit` to `it`.
+
+## Source
+
+Jeremy Hinegardner mentioned a Boulder meetup that happens on the Wednesteenth of every month [https://twitter.com/copiousfreetime](https://twitter.com/copiousfreetime)
+
+## Submitting Incomplete Problems
+It's possible to submit an incomplete solution so you can see how others have completed the exercise.
+
---
+++ ecmascript/meetup/meetup.spec.js
@@ -0,0 +1,62 @@
+import meetupDay from './meetup';
+
+function MeetupDayException(message) {
+  this.message = message;
+  this.name = 'MeetupDayException';
+}
+
+describe('meetupDay()', () => {
+  it('test monteenth of may 2013', () => {
+    expect(meetupDay(2013, 4, 'Monday', 'teenth')).toEqual(new Date(2013, 4, 13));
+  });
+
+  it('test saturteenth of february 2013', () => {
+    expect(meetupDay(2013, 1, 'Saturday', 'teenth')).toEqual(new Date(2013, 1, 16));
+  });
+
+  it('test first tuesday of may 2013', () => {
+    expect(meetupDay(2013, 4, 'Tuesday', '1st')).toEqual(new Date(2013, 4, 7));
+  });
+
+  it('test second monday of april 2013', () => {
+    expect(meetupDay(2013, 3, 'Monday', '2nd')).toEqual(new Date(2013, 3, 8));
+  });
+
+  it('test third thursday of september 2013', () => {
+    expect(meetupDay(2013, 8, 'Thursday', '3rd')).toEqual(new Date(2013, 8, 19));
+  });
+
+  it('test fourth sunday of march 2013', () => {
+    expect(meetupDay(2013, 2, 'Sunday', '4th')).toEqual(new Date(2013, 2, 24));
+  });
+
+  it('test last thursday of october 2013', () => {
+    expect(meetupDay(2013, 9, 'Thursday', 'last')).toEqual(new Date(2013, 9, 31));
+  });
+
+  it('test last wednesday of february 2012', () => {
+    expect(meetupDay(2012, 1, 'Wednesday', 'last')).toEqual(new Date(2012, 1, 29));
+  });
+
+  it('test last wednesday of december 2014', () => {
+    expect(meetupDay(2014, 11, 'Wednesday', 'last')).toEqual(new Date(2014, 11, 31));
+  });
+
+  it('test last sunday of only four week february 2015', () => {
+    expect(meetupDay(2015, 1, 'Sunday', 'last')).toEqual(new Date(2015, 1, 22));
+  });
+
+  it('test first friday of december 2012', () => {
+    expect(meetupDay(2012, 11, 'Friday', '1st')).toEqual(new Date(2012, 11, 7));
+  });
+
+  it('test fifth monday of march 2015', () => {
+    expect(meetupDay(2015, 2, 'Monday', '5th')).toEqual(new Date(2015, 2, 30));
+  });
+
+  it('test nonexistent fifth monday of february 2015', () => {
+    expect(() => {
+      meetupDay(2015, 1, 'Monday', '5th');
+    }).toThrow();
+  });
+});
