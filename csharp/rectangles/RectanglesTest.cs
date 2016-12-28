73d24b9229173ed50438e13203491e4a72f30751
Erik Schierboom <erik_schierboom@hotmail.com>
2016-12-28 13:35:10 +0100

    Add connect, go-counting and react exercises (C#)

--- csharp/rectangles/RectanglesTest.cs
+++ csharp/rectangles/RectanglesTest.cs
@@ -8,24 +8,21 @@ public class RectanglesTest
         var input = new string[0];
         Assert.That(Rectangles.Count(input), Is.EqualTo(0));
     }
-
-    [Ignore("Remove to run test")]
+    
     [Test]
     public void No_columns()
     {
         var input = new[] { "" };
         Assert.That(Rectangles.Count(input), Is.EqualTo(0));
     }
-
-    [Ignore("Remove to run test")]
+    
     [Test]
     public void No_rectangles()
     {
         var input = new[] { " " };
         Assert.That(Rectangles.Count(input), Is.EqualTo(0));
     }
-
-    [Ignore("Remove to run test")]
+    
     [Test]
     public void One_rectangle()
     {
@@ -37,8 +34,7 @@ public class RectanglesTest
         };
         Assert.That(Rectangles.Count(input), Is.EqualTo(1));
     }
-
-    [Ignore("Remove to run test")]
+    
     [Test]
     public void Two_rectangles_without_shared_parts()
     {
@@ -52,8 +48,7 @@ public class RectanglesTest
         };
         Assert.That(Rectangles.Count(input), Is.EqualTo(2));
     }
-
-    [Ignore("Remove to run test")]
+    
     [Test]
     public void Five_rectangles_with_shared_parts()
     {
@@ -67,8 +62,7 @@ public class RectanglesTest
         };
         Assert.That(Rectangles.Count(input), Is.EqualTo(5));
     }
-
-    [Ignore("Remove to run test")]
+    
     [Test]
     public void Only_complete_rectangles_are_counted()
     {
@@ -82,8 +76,7 @@ public class RectanglesTest
         };
         Assert.That(Rectangles.Count(input), Is.EqualTo(1));
     }
-
-    [Ignore("Remove to run test")]
+    
     [Test]
     public void Rectangles_can_be_of_different_sizes()
     {
@@ -97,8 +90,7 @@ public class RectanglesTest
         };
         Assert.That(Rectangles.Count(input), Is.EqualTo(3));
     }
-
-    [Ignore("Remove to run test")]
+    
     [Test]
     public void Corner_is_required_for_a_rectangle_to_be_complete()
     {
@@ -112,8 +104,7 @@ public class RectanglesTest
         };
         Assert.That(Rectangles.Count(input), Is.EqualTo(2));
     }
-
-    [Ignore("Remove to run test")]
+    
     [Test]
     public void Large_input_with_many_rectangles()
     {
