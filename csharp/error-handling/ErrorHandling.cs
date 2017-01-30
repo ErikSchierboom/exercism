05fada5274f30b1757477870a4800204d61b5483
Erik Schierboom <erik_schierboom@hotmail.com>
2017-01-30 15:07:34 +0100

    Add lens-person exercise (C#)

---
+++ csharp/error-handling/ErrorHandling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException()
+    {
+        throw new Exception();
+    }
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        int number;
+        return int.TryParse(input, out number) ? number : (int?)null;
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result) 
+        => int.TryParse(input, out result);
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableResource)
+    {
+        using (disposableResource)
+            throw new Exception();
+    }
+}
