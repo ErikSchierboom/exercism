a602c190269b324f3e1cad6b95503501a347b5cb
Erik Schierboom <erik_schierboom@hotmail.com>
2017-02-06 10:43:23 +0100

    Update sorting exercise (F#)

--- fsharp/sorting/Example.fs
+++ fsharp/sorting/Example.fs
@@ -1,22 +1,22 @@
 ﻿module Sorting
 
-let swap (input: 'T[]) i j = 
-    let tmp = input.[i]
+let swap input i j = 
+    let tmp = Array.item i input
     input.[i] <- input.[j]
     input.[j] <- tmp
 
 let bubbleSort input = 
-    let sorted = Array.ofList input
+    let sorted = Array.copy input
 
     for i = sorted.Length - 1 downto 0 do
         for j = 1 to i do
             if sorted.[j - 1] > sorted.[j] then
                 swap sorted (j - 1) j
 
-    Array.toList sorted
+    sorted
 
 let insertionSort input = 
-    let sorted = Array.ofList input
+    let sorted = Array.copy input
 
     for i = 1 to sorted.Length - 1 do
         let mutable j = i
@@ -24,10 +24,10 @@ let insertionSort input =
             swap sorted (j - 1) j
             j <- j - 1
 
-    Array.toList sorted
+    sorted
 
 let selectionSort input = 
-    let sorted = Array.ofList input
+    let sorted = Array.copy input
 
     for j = 0 to sorted.Length - 1 do
         let mutable min = j
@@ -39,45 +39,30 @@ let selectionSort input =
         if min <> j then
             swap sorted min j
 
-    Array.toList sorted
+    sorted
 
-let split list =
-    let rec aux left right =
-        function        
-        | []  -> (left, right)
-        | [x] -> (x::left, right)
-        | x::y::tail -> aux (x::left) (y::right) tail
+let partition low high input =
+    let pivot = Array.item high input
+    let mutable i = low
 
-    aux [] [] list
+    for j = low to high - 1 do
+        if input.[j] <= pivot then
+            swap input i j
+            i <- i + 1
 
-let merge left right = 
-    let rec aux left' right' result =
-        match left', right' with
-        | [], [] -> 
-            result
-        | [], h :: t | h :: t, [] ->
-            aux [] t (h :: result)
-        | hl :: tl, hr :: tr ->
-            if hl < hr then
-                aux tl right' (hl :: result)
-            else
-                aux left' tr (hr :: result)
-    
-    aux left right [] |> List.rev
+    swap input i high
+    i
 
-let rec mergeSort = 
-    function
-    | [] -> []
-    | x::[] -> [x]
-    | list -> 
-        let (left, right) = split list
-        merge (mergeSort left) (mergeSort right)
+let rec quicksortRange low high input =
+    if low >= high then
+        input
+    else
+        let p = partition low high input
 
-let rec quicksort = 
-    function
-    | [] -> 
-        []
-    | pivot::tail ->
-        tail
-        |> List.partition (fun x -> x < pivot)
-        ||> (fun l r -> quicksort l @ [pivot] @ quicksort r)
+        input
+        |> quicksortRange low (p - 1) 
+        |> quicksortRange (p + 1) high
+
+let quicksort input = 
+    Array.copy input
+    |> quicksortRange 0 (Array.length input - 1)
