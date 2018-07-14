77923b804c4668309169c4abf3bb536710315680
Erik Schierboom <erik_schierboom@hotmail.com>
2018-07-14 12:27:08 +0200

    csharp: Solve reverse-string exercise

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Linq;
+
+public static class ReverseString
+{
+    public static string Reverse(string input) => new string(input.Reverse().ToArray());
+}
---
+++ csharp/reverse-string/ReverseStringTest.cs
@@ -0,0 +1,36 @@
+// This file was auto-generated based on version 1.1.0 of the canonical data.
+
+using Xunit;
+
+public class ReverseStringTest
+{
+    [Fact]
+    public void An_empty_string()
+    {
+        Assert.Equal("", ReverseString.Reverse(""));
+    }
+
+    [Fact]
+    public void A_word()
+    {
+        Assert.Equal("tobor", ReverseString.Reverse("robot"));
+    }
+
+    [Fact]
+    public void A_capitalized_word()
+    {
+        Assert.Equal("nemaR", ReverseString.Reverse("Ramen"));
+    }
+
+    [Fact]
+    public void A_sentence_with_punctuation()
+    {
+        Assert.Equal("!yrgnuh m'I", ReverseString.Reverse("I'm hungry!"));
+    }
+
+    [Fact]
+    public void A_palindrome()
+    {
+        Assert.Equal("racecar", ReverseString.Reverse("racecar"));
+    }
+}
