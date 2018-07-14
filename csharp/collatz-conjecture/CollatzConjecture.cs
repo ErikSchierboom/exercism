eb10da67bb0c84f67e9b72299853712e7240f6b8
Erik Schierboom <erik_schierboom@hotmail.com>
2018-07-14 12:26:13 +0200

    csharp: Solve collatz-conjecture exercise

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number <= 0)
+        {
+            throw new ArgumentException();
+        }
+
+        return Steps(number, 0);
+
+        int Steps(int remainder, int stepCount)
+        {
+            if (remainder == 1)
+            {
+                return stepCount;
+            }
+
+            if (remainder % 2 == 0)
+            {
+                return Steps(remainder / 2, stepCount + 1);
+            }
+
+            return Steps(remainder * 3 + 1, stepCount + 1);
+        }
+    }
+}
