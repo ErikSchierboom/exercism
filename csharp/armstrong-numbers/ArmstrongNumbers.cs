c7e3ab274446f2751eb7c9ea08a97de49d417acf
Erik Schierboom <e.schierboom@nl.ccv.eu>
2018-07-20 08:29:11 +0200

    csharp: Solve armstrong-numbers exercise

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        var digits = number.ToString().Select(CharToInt).ToArray();
+        var digitsRaisedToPowerSum = digits.Sum(digit => Math.Pow(digit, digits.Length));
+
+        return digitsRaisedToPowerSum == number;
+    }
+
+    private static int CharToInt(char c) => c - '0';
+}
