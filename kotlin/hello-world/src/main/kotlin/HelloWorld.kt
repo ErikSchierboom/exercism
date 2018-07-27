6738605e283dc28c313fbeb6ebef6eb449f8ff25
Erik Schierboom <e.schierboom@nl.ccv.eu>
2018-07-27 13:57:43 +0200

    kotlin: Solve hello-world exercise

--- kotlin/hello-world/src/main/kotlin/HelloWorld.kt
+++ kotlin/hello-world/src/main/kotlin/HelloWorld.kt
@@ -1,6 +1,1 @@
-object HelloWorld {
-    fun hello(name: String? = "World"): String {
-        val normalizedName = if (name.isNullOrBlank()) "World" else name
-        return "Hello, $normalizedName!"
-    }
-}
+fun hello() = "Hello, World!"
