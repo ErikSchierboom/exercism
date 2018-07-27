8f489fe8c065408bcfedc8c896d605cf5c200833
Erik Schierboom <e.schierboom@nl.ccv.eu>
2018-07-27 13:45:41 +0200

    kotlin: Solve bob exercise

--- kotlin/bob/src/main/kotlin/Bob.kt
+++ kotlin/bob/src/main/kotlin/Bob.kt
@@ -1,7 +1,12 @@
 object Bob {
-    fun hey(input: String): String =
-        if (input.isNullOrBlank()) "Fine. Be that way!"
-        else if (input.any { Character.isLetter(it) } && input.toUpperCase().equals(input)) "Whoa, chill out!"
-        else if (input.endsWith('?')) "Sure."
-        else "Whatever."
+    fun hey(input: String): String = when {
+        input.isBlank() -> "Fine. Be that way!"
+        input.isYell() -> "Whoa, chill out!"
+        input.isQuestion() -> "Sure."
+        else -> "Whatever."
+    }
+
+    private fun String.isQuestion() = this.trimEnd().endsWith('?')
+
+    private fun String.isYell() = this.toUpperCase() == this
 }
