feaa80697b1f9267ea4b8a52d67c55cfe369df3d
Erik Schierboom <e.schierboom@nl.ccv.eu>
2018-07-27 13:44:37 +0200

    kotlin: Solve acronym exercise

--- kotlin/acronym/src/main/kotlin/Acronym.kt
+++ kotlin/acronym/src/main/kotlin/Acronym.kt
@@ -1,9 +1,4 @@
 object Acronym {
-    fun acronymChar(word: String) = word[0].toUpperCase()
-
     fun generate(phrase: String): String =
-        Regex("[A-Z]+[a-z]*|[a-z]+")
-             .findAll(phrase)
-             .map { acronymChar(it.value) }
-             .joinToString(separator = "")
+        phrase.split(Regex("""[^\w]+""")).map { it[0].toUpperCase() }.joinToString("")
 }
