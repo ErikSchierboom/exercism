5eb56bacfd84f4be0e180e7faad2c524723cb962
Erik Schierboom <erik_schierboom@hotmail.com>
2018-07-29 21:30:43 +0200

    kotlin: New iteration of secret-handshake exercise

--- kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,11 +1,14 @@
 object HandshakeCalculator {
-    const val REVERSE_SIGNALS_MASK = 0b00010000
+    private const val REVERSE_SIGNALS_MASK = 0b00010000
+    private val signals = Signal.values()
 
     fun calculateHandshake(encodedHandshake: Int): List<Signal> {
-        val signals = Signal.values().filter { matchesMask(encodedHandshake, it.mask) }
+        val encodedSignals = signals.filter { matchesMask(encodedHandshake, signalMask(it) ) }
 
-        return if (reverseSignals(encodedHandshake)) signals.reversed() else signals
+        return if (reverseSignals(encodedHandshake)) encodedSignals.reversed() else encodedSignals
     }
+
+    private fun signalMask(signal: Signal) = 1 shl signals.indexOf(signal)
 
     private fun reverseSignals(encodedHandshake: Int) = matchesMask(encodedHandshake, REVERSE_SIGNALS_MASK)
 
--- kotlin/secret-handshake/src/main/kotlin/Signal.kt
+++ kotlin/secret-handshake/src/main/kotlin/Signal.kt
@@ -1,6 +1,3 @@
-enum class Signal(val mask: Int) {
-    WINK(0b00000001),
-    DOUBLE_BLINK(0b00000010),
-    CLOSE_YOUR_EYES(0b00000100),
-    JUMP(0b00001000)
+enum class Signal() {
+    WINK, DOUBLE_BLINK, CLOSE_YOUR_EYES, JUMP
 }
