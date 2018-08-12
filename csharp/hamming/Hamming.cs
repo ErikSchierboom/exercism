7add24736b78f719c13494a7b7c4adc1a0cdf568
Erik Schierboom <erik_schierboom@hotmail.com>
2018-08-12 11:06:56 +0200

    csharp: Iterate on hamming exercise

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -1,12 +1,15 @@
-﻿namespace hamming
-{
-    using System.Linq;
+﻿using System;
+using System.Linq;
 
-    public static class Hamming
+public static class Hamming
+{
+    public static int Distance(string strand1, string strand2)
     {
-        public static int Compute(string strand1, string strand2)
+        if (strand1.Length != strand2.Length)
         {
-            return strand1.Zip(strand2, (nucleotide1, nucleotide2) => nucleotide1 == nucleotide2 ? 0 : 1).Sum();
+            throw new ArgumentException("Strands have different length");
         }
+
+        return strand1.Zip(strand2, (nucleotide1, nucleotide2) => nucleotide1 == nucleotide2 ? 0 : 1).Sum();
     }
 }
