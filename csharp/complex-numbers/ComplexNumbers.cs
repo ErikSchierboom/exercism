8250718634521b09f12a376cc71d8b3bdbb53356
Erik Schierboom <erik_schierboom@hotmail.com>
2018-08-14 13:25:35 +0200

    csharp: Iterate on complex-numbers exercise

--- csharp/complex-numbers/ComplexNumbers.cs
+++ csharp/complex-numbers/ComplexNumbers.cs
@@ -13,28 +13,28 @@ public struct ComplexNumber
     public double Imaginary() => _imaginary;
 
     public ComplexNumber Mul(ComplexNumber other) 
-        => new ComplexNumber(Real() * other.Real() - Imaginary() * other.Imaginary(), Imaginary() * other.Real() + Real() * other.Imaginary());
+        => new ComplexNumber(_real * other._real - _imaginary * other._imaginary, _imaginary * other._real + _real * other._imaginary);
 
     public ComplexNumber Add(ComplexNumber other)
-        => new ComplexNumber(Real() + other.Real(), Imaginary() + other.Imaginary());
+        => new ComplexNumber(_real + other._real, _imaginary + other._imaginary);
 
     public ComplexNumber Sub(ComplexNumber other)
-        => new ComplexNumber(Real() - other.Real(), Imaginary() - other.Imaginary());
+        => new ComplexNumber(_real - other._real, _imaginary - other._imaginary);
 
     public ComplexNumber Div(ComplexNumber other)
     {
-        var real = (Real() * other.Real() + Imaginary() * other.Imaginary()) / (Math.Pow(other.Real(), 2) + Math.Pow(other.Imaginary(), 2));
-        var imaginary = (Imaginary() * other.Real() - Real() * other.Imaginary()) / (Math.Pow(other.Real(), 2) + Math.Pow(other.Imaginary(), 2));
+        var real = (_real * other._real + _imaginary * other._imaginary) / (Math.Pow(other._real, 2) + Math.Pow(other._imaginary, 2));
+        var imaginary = (_imaginary * other._real - _real * other._imaginary) / (Math.Pow(other._real, 2) + Math.Pow(other._imaginary, 2));
         
         return new ComplexNumber(real, imaginary);
     }
 
     public double Abs()
-        => Math.Sqrt(Math.Pow(Real(), 2) + Math.Pow(Imaginary(), 2));
+        => Math.Sqrt(Math.Pow(_real, 2) + Math.Pow(_imaginary, 2));
 
     public ComplexNumber Conjugate()
-        => new ComplexNumber(Real(), -Imaginary());
+        => new ComplexNumber(_real, -_imaginary);
 
     public ComplexNumber Exp() 
-        => new ComplexNumber(Math.Exp(Real()) * Math.Cos(Imaginary()), Math.Exp(Real()) * Math.Sin(Imaginary()));
+        => new ComplexNumber(Math.Exp(_real) * Math.Cos(_imaginary), Math.Exp(_real) * Math.Sin(_imaginary));
 }
