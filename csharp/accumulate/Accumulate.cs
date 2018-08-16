08dc36ea16e56ff5900a66ec4edcc93de0af45d8
Erik Schierboom <e.schierboom@nl.ccv.eu>
2018-08-16 13:59:27 +0200

    csharp: Iterate of accumulate exercise

--- csharp/accumulate/Accumulate.cs
+++ csharp/accumulate/Accumulate.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 
 public static class AccumulateExtensions
 {
-    public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
+    public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> map)
     {
         foreach (var element in collection)
-        {
-            yield return func(element);
-        }
+            yield return map(element);
     }
 }
