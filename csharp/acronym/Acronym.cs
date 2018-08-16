de18a776e69835ef73efe9dd14e2328a65afd990
Erik Schierboom <e.schierboom@nl.ccv.eu>
2018-08-16 13:59:39 +0200

    csharp: Iterate of acronym exercise

--- csharp/acronym/Acronym.cs
+++ csharp/acronym/Acronym.cs
@@ -4,13 +4,9 @@ using System.Text.RegularExpressions;
 
 public static class Acronym
 {
-    public static string Abbreviate(string phrase)
-    {
-        return Words(phrase).Aggregate("", (abbr, word) => abbr + char.ToUpperInvariant(word[0]));
-    }
+    public static string Abbreviate(string phrase) => new string(Words(phrase).Select(AcronymLetter).ToArray());
 
-    private static IEnumerable<string> Words(string phrase)
-    {
-        return Regex.Matches(phrase, "[A-Z]+[a-z]*|[a-z]+").Cast<Match>().Select(m => m.Value);
-    }
+    private static IEnumerable<string> Words(string phrase) => Regex.Split(phrase, @"[^\w]+");
+
+    private static char AcronymLetter(string word) => char.ToUpper(word[0]);
 }
