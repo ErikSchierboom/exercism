c5ccb7546e35d969936ba73e0235dad71cf4d4c3
Erik Schierboom <erik_schierboom@hotmail.com>
2018-08-17 19:23:25 +0200

    fsharp/clock: add iteration

---
+++ fsharp/clock/Clock.fs
@@ -0,0 +1,18 @@
+module Clock
+
+type Clock = { hours: int; minutes: int }
+
+let private modulo x y = int (((x % y) + y) % y)
+
+let create hours minutes =
+    let totalMinutes = hours * 60 + minutes
+    let normalizedHours = modulo (double totalMinutes / 60.0) 24.0
+    let normalizedMinutes = modulo (double minutes) 60.0
+
+    { hours = normalizedHours; minutes = normalizedMinutes }
+
+let add minutes clock = create clock.hours (clock.minutes + minutes)
+
+let subtract minutes clock = create clock.hours (clock.minutes - minutes)
+
+let display clock = sprintf "%02i:%02i" clock.hours clock.minutes
