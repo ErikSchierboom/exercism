e62d7172a8c89a5a4409ad3520c2dead7cd3dd06
Erik Schierboom <erik_schierboom@hotmail.com>
2018-08-17 18:27:48 +0200

    fsharp/accumulate: add solution

---
+++ fsharp/accumulate/AccumulateTest.fs
@@ -0,0 +1,37 @@
+// This file was created manually and its version is 1.0.0
+
+module AccumulateTest
+
+open System
+open Xunit
+open FsUnit.Xunit
+open Accumulate
+
+let reverse (str:string) = new string(str.ToCharArray() |> Array.rev)
+
+[<Fact>]
+let ``Empty accumulation produces empty accumulation`` () =
+    accumulate (fun x -> x + 1) List.empty |> should be Empty
+
+[<Fact>]
+let ``Identity accumulation returns unmodified list`` () =
+    accumulate id [1; 2; 3] |> should equal [1; 2; 3]
+
+[<Fact>]
+let ``Accumulate squares`` () =
+    accumulate (fun x -> x * x) [1; 2; 3] |> should equal [1; 4; 9]
+
+[<Fact>]
+let ``Accumulate upcases`` () =
+    accumulate (fun (x:string) -> x.ToUpper()) ["hello"; "world"]
+    |> should equal ["HELLO"; "WORLD"]
+
+[<Fact>]
+let ``Accumulate reversed strings`` () =
+    accumulate reverse (List.ofArray ("the quick brown fox etc".Split(' ')))
+    |> should equal (List.ofArray("eht kciuq nworb xof cte".Split(' ')))
+
+[<Fact>]
+let ``Accumulate within accumulate`` () =
+    accumulate (fun (x:string) -> String.concat " " (accumulate (fun y -> x + y) ["1"; "2"; "3"])) ["a"; "b"; "c"]
+    |> should equal ["a1 a2 a3"; "b1 b2 b3"; "c1 c2 c3"]
