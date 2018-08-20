96501eeebc18e2215a7b0d29ff8624eac48a1e1b
Erik Schierboom <erik_schierboom@hotmail.com>
2018-08-20 20:54:12 +0200

    fsharp/armstrong-numbers: add solution

---
+++ fsharp/armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,9 @@
+﻿module ArmstrongNumbers
+
+let isArmstrongNumber number = 
+    let charToDigit char = int char - int '0'
+    let digits = number |> string |> Seq.map charToDigit |> Seq.toList
+    let numberOfDigits = List.length digits
+    let digitToPower digit = pown digit numberOfDigits
+
+    List.sumBy digitToPower digits = number
