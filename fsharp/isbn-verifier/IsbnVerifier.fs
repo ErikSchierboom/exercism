5f366ad897f26cce8cac6f3e0bc282b542863ccd
Erik Schierboom <erik_schierboom@hotmail.com>
2018-08-20 21:17:37 +0200

    fsharp/isbn-verifier: add solution

---
+++ fsharp/isbn-verifier/IsbnVerifier.fs
@@ -0,0 +1,24 @@
+﻿module IsbnVerifier
+
+open System.Text.RegularExpressions
+
+let private normalize (isbn: string) = isbn.Replace("-", "")
+
+let private hasValidCharacters isbn = Regex.IsMatch(isbn, @"^\d{9}[\dX]$")
+
+let private digitToValue index digit =
+    let indexFactor = index + 1
+
+    match digit with
+    | 'X' -> 10 * indexFactor
+    | _   -> (int digit - int '0') * indexFactor
+
+let private checksum isbn =
+    isbn
+    |> Seq.rev
+    |> Seq.mapi digitToValue
+    |> Seq.sum
+
+let isValid isbn = 
+    let normalizedIsbn = normalize isbn
+    hasValidCharacters normalizedIsbn && checksum normalizedIsbn % 11 = 0
