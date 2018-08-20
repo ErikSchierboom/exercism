1c2a773e3cd135e732a183a4c8fa4e378ce8aedb
Erik Schierboom <erik_schierboom@hotmail.com>
2018-08-20 21:32:16 +0200

    fsharp/rotational-cipher: add solution

---
+++ fsharp/rotational-cipher/RotationalCipher.fs
@@ -0,0 +1,24 @@
+﻿module RotationalCipher
+
+open System
+
+let private lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+let private upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+let private keyForLetter letter = if Char.IsLower(letter) then lowerCaseLetters else upperCaseLetters
+
+let private shiftLetter shiftKey (key: string) (letter: char) = 
+    let shiftIndex = key.IndexOf(letter) + shiftKey
+    key.[shiftIndex % key.Length]
+
+let rotateLetter shiftKey letter =
+    if Char.IsLetter(letter) then
+        shiftLetter shiftKey (keyForLetter letter) letter
+    else
+        letter
+
+let rotate shiftKey text =
+    text
+    |> Seq.map (rotateLetter shiftKey)
+    |> Seq.toArray
+    |> String
---
+++ fsharp/rotational-cipher/RotationalCipherTest.fs
@@ -0,0 +1,49 @@
+// This file was auto-generated based on version 1.2.0 of the canonical data.
+
+module RotationalCipherTest
+
+open FsUnit.Xunit
+open Xunit
+
+open RotationalCipher
+
+[<Fact>]
+let ``Rotate a by 0, same output as input`` () =
+    rotate 0 "a" |> should equal "a"
+
+[<Fact>]
+let ``Rotate a by 1`` () =
+    rotate 1 "a" |> should equal "b"
+
+[<Fact>]
+let ``Rotate a by 26, same output as input`` () =
+    rotate 26 "a" |> should equal "a"
+
+[<Fact>]
+let ``Rotate m by 13`` () =
+    rotate 13 "m" |> should equal "z"
+
+[<Fact>]
+let ``Rotate n by 13 with wrap around alphabet`` () =
+    rotate 13 "n" |> should equal "a"
+
+[<Fact>]
+let ``Rotate capital letters`` () =
+    rotate 5 "OMG" |> should equal "TRL"
+
+[<Fact>]
+let ``Rotate spaces`` () =
+    rotate 5 "O M G" |> should equal "T R L"
+
+[<Fact>]
+let ``Rotate numbers`` () =
+    rotate 4 "Testing 1 2 3 testing" |> should equal "Xiwxmrk 1 2 3 xiwxmrk"
+
+[<Fact>]
+let ``Rotate punctuation`` () =
+    rotate 21 "Let's eat, Grandma!" |> should equal "Gzo'n zvo, Bmviyhv!"
+
+[<Fact>]
+let ``Rotate all letters`` () =
+    rotate 13 "The quick brown fox jumps over the lazy dog." |> should equal "Gur dhvpx oebja sbk whzcf bire gur ynml qbt."
+
