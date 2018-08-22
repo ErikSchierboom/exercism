e0ed97d4a5990fe3d2161d3efa57b94a52ea9ffc
Erik Schierboom <erik_schierboom@hotmail.com>
2018-08-22 21:26:17 +0200

    csharp/bank-account: add iteration

--- csharp/bank-account/BankAccount.cs
+++ csharp/bank-account/BankAccount.cs
@@ -4,51 +4,21 @@ public class BankAccount
 {
     private readonly object _lock = new object();
 
-    private float balance;
-    private bool isOpen;
+    private float _balance;
+    private bool _isOpen;
 
-    public void Open()
-    {
-        lock(_lock)
-        {
-            isOpen = true;
-        }
-    }
+    public void Open() => _isOpen = true;
 
-    public void Close()
-    {
-        lock(_lock)
-        {
-            isOpen = false;
-        }
-    }
+    public void Close() => _isOpen = false;
 
-    public float Balance
-    {
-        get
-        {
-            lock (_lock)
-            {
-                if (!isOpen)
-                {
-                    throw new InvalidOperationException("Cannot get balance on an account that isn't open");
-                }
-
-                return balance;
-            }
-        }
-    }
+    public float Balance => _isOpen ? _balance : throw new InvalidOperationException();
 
     public void UpdateBalance(float change)
     {
-        lock(_lock)
-        {
-            if (!isOpen)
-            {
-                throw new InvalidOperationException("Cannot update balance on an account that isn't open");
-            }
+        if (!_isOpen)
+            throw new InvalidOperationException("Cannot update balance on an account that isn't open");
 
-            balance += change;
-        }
+        lock (_lock)
+            _balance += change;
     }
 }
