b52399acda3ec8542e3533ebc7b56bd52a434eee
Erik Schierboom <erik_schierboom@hotmail.com>
2018-09-13 17:12:54 +0200

    kotlin/grade-school: add iteration

--- kotlin/grade-school/src/main/kotlin/School.kt
+++ kotlin/grade-school/src/main/kotlin/School.kt
@@ -1,19 +1,13 @@
-import java.util.*
-import kotlin.comparisons.compareBy
-
 class School {
-    var db = mutableMapOf<Int, MutableList<String>>()
+    private var db = sortedMapOf<Int, List<String>>()
 
     fun db(): Map<Int, List<String>> = db
 
     fun add(student: String, grade: Int) {
-        val students = db.getOrElse(grade) { mutableListOf<String>() }
-        students.add(student)
-
-        db.set(grade, students)
+        db[grade] = grade(grade) + student
     }
 
-    fun grade(grade: Int): List<String> = db.getOrElse(grade) { listOf<String>() }
+    fun grade(grade: Int): List<String> = db.getOrElse(grade) { listOf() }
 
-    fun sort(): Map<Int, List<String>> = db.toSortedMap().mapValues { it.value.sorted() }
+    fun sort(): Map<Int, List<String>> = db.mapValues { it.value.sorted() }
 }
