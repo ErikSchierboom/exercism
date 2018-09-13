b5e2e664f041a67fef455ef3189fc923c54b43d0
Erik Schierboom <erik_schierboom@hotmail.com>
2018-09-13 16:46:58 +0200

    kotlin/robot-simulator: add iteration

--- kotlin/robot-simulator/src/main/kotlin/Robot.kt
+++ kotlin/robot-simulator/src/main/kotlin/Robot.kt
@@ -1,44 +1,45 @@
+import Orientation.*
+
 class Robot(var gridPosition: GridPosition, var orientation: Orientation) {
     fun turnRight() {
         orientation = when (orientation) {
-            Orientation.NORTH -> Orientation.EAST
-            Orientation.EAST -> Orientation.SOUTH
-            Orientation.SOUTH -> Orientation.WEST
-            Orientation.WEST -> Orientation.NORTH
+            NORTH -> EAST
+            EAST -> SOUTH
+            SOUTH -> WEST
+            WEST -> NORTH
         }
     }
 
     fun turnLeft() {
         orientation = when (orientation) {
-            Orientation.NORTH -> Orientation.WEST
-            Orientation.EAST -> Orientation.NORTH
-            Orientation.SOUTH -> Orientation.EAST
-            Orientation.WEST -> Orientation.SOUTH
+            NORTH -> WEST
+            EAST -> NORTH
+            SOUTH -> EAST
+            WEST -> SOUTH
         }
     }
 
     fun advance() {
         gridPosition = when (orientation) {
-            Orientation.NORTH -> gridPosition.copy(y = gridPosition.y + 1)
-            Orientation.EAST -> gridPosition.copy(x = gridPosition.x + 1)
-            Orientation.SOUTH -> gridPosition.copy(y = gridPosition.y - 1)
-            Orientation.WEST -> gridPosition.copy(x = gridPosition.x - 1)
+            NORTH -> gridPosition.copy(y = gridPosition.y + 1)
+            EAST -> gridPosition.copy(x = gridPosition.x + 1)
+            SOUTH -> gridPosition.copy(y = gridPosition.y - 1)
+            WEST -> gridPosition.copy(x = gridPosition.x - 1)
         }
     }
 
     fun simulate(instructions: String) {
         fun simulateInstruction(instruction: Char) {
-            require(instruction in "LRA")
-
             when (instruction) {
                 'L' -> turnLeft()
                 'R' -> turnRight()
                 'A' -> advance()
+                else -> throw IllegalArgumentException("Unknown instruction")
             }
         }
 
         instructions.forEach(::simulateInstruction)
     }
 
-    constructor(): this(GridPosition(0, 0), Orientation.NORTH)
+    constructor(): this(GridPosition(0, 0), NORTH)
 }
