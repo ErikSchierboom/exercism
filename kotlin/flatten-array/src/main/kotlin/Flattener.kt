ca247beaa1bd5f13d57ec7962f5e8be9a5e903ee
Erik Schierboom <erik_schierboom@hotmail.com>
2018-09-13 16:47:07 +0200

    kotlin/flatten-array: add iteration

--- kotlin/flatten-array/src/main/kotlin/Flattener.kt
+++ kotlin/flatten-array/src/main/kotlin/Flattener.kt
@@ -1,14 +1,9 @@
 object Flattener {
-    fun flatten(nestedList: List<Any?>): List<Any> {
-        val results = mutableListOf<Any>()
-
-        for (x in nestedList.filterNotNull()) {
-            when (x) {
-                is List<*> -> results.addAll(flatten(x))
-                else -> results.add(x)
+    fun flatten(nestedList: List<Any?>): List<Any> =
+            nestedList.filterNotNull().flatMap {
+                when (it) {
+                    is List<*> -> flatten(it)
+                    else -> listOf(it)
+                }
             }
-        }
-
-        return results
-    }
 }
