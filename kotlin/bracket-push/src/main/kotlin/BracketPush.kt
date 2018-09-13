0fa70e44250a476540b98ff818c680a4333424ba
Erik Schierboom <erik_schierboom@hotmail.com>
2018-09-13 17:12:46 +0200

    kotlin/bracket-push: add iteration

--- kotlin/bracket-push/src/main/kotlin/BracketPush.kt
+++ kotlin/bracket-push/src/main/kotlin/BracketPush.kt
@@ -1,19 +1,28 @@
 object BracketPush {
-    val brackets = "[]{}()".toSet()
+    private val closingToOpeningBrackets = mapOf(']' to '[', '}' to '{', ')' to '(')
 
-    fun replaceMatchingBrackets(input: String) = input.replace("[]", "").replace("{}", "").replace("()", "")
+    private fun isOpeningBracket(character: Char) = character in closingToOpeningBrackets.values
+
+    private fun isClosingBracket(character: Char) = character in closingToOpeningBrackets.keys
 
     fun isValid(input: String): Boolean {
-        tailrec fun loop(remaining: String): Boolean {
-            if (remaining.isEmpty()) {
-                return true
-            }
-            else {
-                val updated = replaceMatchingBrackets(remaining)
-                return if (updated.length == remaining.length) false else loop(updated)
+        val unpairedOpeningBrackets = mutableListOf<Char>()
+
+        for (character in input) {
+            when {
+                isOpeningBracket(character) -> unpairedOpeningBrackets.add(character)
+                isClosingBracket(character) -> {
+                    val expectedOpeningBracket = closingToOpeningBrackets[character]
+                    val actualOpeningBracket = unpairedOpeningBrackets.lastOrNull()
+
+                    if(expectedOpeningBracket == actualOpeningBracket)
+                        unpairedOpeningBrackets.removeAt(unpairedOpeningBrackets.lastIndex)
+                    else
+                        return false
+                }
             }
         }
 
-        return loop(input.filter { it in brackets })
+        return unpairedOpeningBrackets.isEmpty()
     }
 }
