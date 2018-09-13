cea7c5e3b4bdae140c4352f17ab30be649c76fa4
Erik Schierboom <erik_schierboom@hotmail.com>
2018-09-13 16:27:55 +0200

    kotlin/collatz-conjecture: add iteration

--- kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
+++ kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
@@ -2,15 +2,13 @@ object CollatzCalculator {
     fun computeStepCount(i: Int): Int {
         require(i > 0) { "Only natural numbers are allowed" }
 
-        var steps = 0;
-        var remainder = i;
-
-        while (remainder != 1)
-        {
-            remainder = if (remainder % 2 == 0) remainder / 2 else remainder * 3 +1
-            steps++
+        val collatzSteps = generateSequence(i) {
+            when {
+                it == 1 -> null
+                it % 2 == 0 -> it / 2
+                else -> it * 3 + 1
+            }
         }
-
-        return steps
+        return collatzSteps.count() - 1
     }
 }
