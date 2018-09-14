07fecac9c861717620f916e1a4ba59c5b2befbe4
Erik Schierboom <erik_schierboom@hotmail.com>
2018-09-14 15:39:37 +0200

    kotlin/atbash-cipher: add iteration

--- kotlin/atbash-cipher/src/main/kotlin/Atbash.kt
+++ kotlin/atbash-cipher/src/main/kotlin/Atbash.kt
@@ -1,23 +1,15 @@
-fun <T> List<T>.chunkBySize(size: Int) =
-    (0..lastIndex / size).map {
-        val fromIndex = it * size
-        val toIndex = Math.min(fromIndex + size, this.size)
-        subList(fromIndex, toIndex)
-    }
-
 object Atbash {
-    val letters = 'a' .. 'z'
+    val letters = 'a'..'z'
     val encodingMap = letters.zip(letters.reversed()).toMap()
-
-    fun encodeChar(c: Char) = encodingMap.getOrElse(c) { c }
 
     fun encode(input: String): String =
             input.toLowerCase()
-                 .replace(Regex("[^a-zA-Z0-9]"), "")
-                 .map { encodeChar(it) }
-                 .chunkBySize(5)
-                 .map { it.joinToString(separator = "") }
-                 .joinToString(separator = " ")
+                    .replace(Regex("[^a-z0-9]"), "")
+                    .chunked(5)
+                    .map { it.map(this::encodeChar).joinToString("") }
+                    .joinToString(separator = " ")
 
     fun decode(input: String): String = encode(input).replace(" ", "")
+
+    private fun encodeChar(c: Char): Char = encodingMap.getOrElse(c) { c }
 }
