2b578f5582475f485be28f4fad643a64bb6e8fb4
Erik Schierboom <erik_schierboom@hotmail.com>
2018-09-14 15:35:05 +0200

    kotlin/simple-cipher: add iteration

--- kotlin/simple-cipher/src/main/kotlin/Cipher.kt
+++ kotlin/simple-cipher/src/main/kotlin/Cipher.kt
@@ -3,33 +3,30 @@ import java.util.*
 val random = Random()
 val letters = ('a' .. 'z').toList()
 
-fun <T> List<T>.random() = this[random.nextInt(this.size)]
+private fun <T> List<T>.random() = this[random.nextInt(this.size)]
 
-fun generateKey() = (1..100).map { letters.random() }.joinToString(separator = "")
+private fun modulo (x: Int, y: Int): Int = ((x % y) + y) % y
 
-class Cipher(val key: String) {
+private fun charToInt (c: Char): Int = c - 'a'
 
-    constructor(): this(generateKey()) {}
+private fun intToChar (i: Int): Char = 'a' + modulo(i, 26)
 
+private fun generateKey() = (1..100).map { letters.random() }.joinToString(separator = "")
+
+class Cipher(val key: String = generateKey()) {
     init {
-        if (invalidKey())
-            throw IllegalArgumentException()
+        require(validKey())
     }
 
-    fun invalidKey(): Boolean = key.isEmpty() || key.any { it !in letters }
-
-    fun modulo (x: Int, y: Int): Int = ((x % y) + y) % y
-
-    fun charToInt (c: Char): Int = c - 'a'
-    fun intToChar (i: Int): Char = 'a' + modulo(i, 26)
-
-    fun shiftChar(key: Char, char: Char, operation: (Int, Int) -> Int): Char =
-        intToChar(operation(charToInt(char), charToInt(key)))
-
-    fun shift (key: String, input: String, operation: (Int, Int) -> Int): String =
-        key.zip(input).map { shiftChar(it.first, it.second, operation) }.joinToString(separator = "")
+    private fun validKey(): Boolean = key.isNotEmpty() && key.all { it in letters }
 
     fun encode(input: String): String = shift(key, input) { x, y -> x + y}
 
     fun decode(input: String): String = shift(key, input) { x, y -> x - y}
+
+    private fun shiftChar(key: Char, char: Char, operation: (Int, Int) -> Int): Char =
+        intToChar(operation(charToInt(char), charToInt(key)))
+
+    private fun shift (key: String, input: String, operation: (Int, Int) -> Int): String =
+        key.zip(input).map { shiftChar(it.first, it.second, operation) }.joinToString(separator = "")
 }
