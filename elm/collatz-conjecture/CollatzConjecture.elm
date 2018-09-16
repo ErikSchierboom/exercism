4f3b329615aa2a18841c06b0727d22dbb5e07dda
Erik Schierboom <erik_schierboom@hotmail.com>
2018-09-16 11:06:12 +0200

    elm/collatz-conjecture: add solution

--- elm/collatz-conjecture/CollatzConjecture.elm
+++ elm/collatz-conjecture/CollatzConjecture.elm
@@ -1,6 +1,19 @@
 module CollatzConjecture exposing (collatz)
 
 
+helper : Int -> Int -> Int
+helper steps current =
+    if current == 1 then
+        steps
+    else if current % 2 == 0 then
+        helper (steps + 1) (current // 2)
+    else
+        helper (steps + 1) (current * 3 + 1)
+
+
 collatz : Int -> Result String Int
 collatz start =
-    Debug.crash "Please implement this function"
+    if start < 1 then
+        Err "Only positive numbers are allowed"
+    else
+        Ok (helper 0 start)
--- elm/collatz-conjecture/tests/Tests.elm
+++ elm/collatz-conjecture/tests/Tests.elm
@@ -11,28 +11,22 @@ tests =
         [ test "zero steps for one" <|
             \() ->
                 Expect.equal (Ok 0) (collatz 1)
-
-        -- Once you get the first test passing, remove the
-        -- `skip <|` (just leave the comma) on the next
-        -- lines to continue!
-        , skip <|
-            test "divide if even" <|
-                \() ->
-                    Expect.equal (Ok 4) (collatz 16)
-        , skip <|
-            test "even and odd step" <|
-                \() ->
-                    Expect.equal (Ok 9) (collatz 12)
-        , skip <|
-            test "Large number of even and odd step" <|
-                \() ->
-                    Expect.equal (Ok 152) (collatz 1000000)
-        , skip <|
-            test "zero is an error" <|
-                \() ->
-                    Expect.equal (Err "Only positive numbers are allowed") (collatz 0)
-        , skip <|
-            test "negative values is an error" <|
-                \() ->
-                    Expect.equal (Err "Only positive numbers are allowed") (collatz -15)
+          -- Once you get the first test passing, remove the
+          -- `skip <|` (just leave the comma) on the next
+          -- lines to continue!
+        , test "divide if even" <|
+            \() ->
+                Expect.equal (Ok 4) (collatz 16)
+        , test "even and odd step" <|
+            \() ->
+                Expect.equal (Ok 9) (collatz 12)
+        , test "Large number of even and odd step" <|
+            \() ->
+                Expect.equal (Ok 152) (collatz 1000000)
+        , test "zero is an error" <|
+            \() ->
+                Expect.equal (Err "Only positive numbers are allowed") (collatz 0)
+        , test "negative values is an error" <|
+            \() ->
+                Expect.equal (Err "Only positive numbers are allowed") (collatz -15)
         ]
