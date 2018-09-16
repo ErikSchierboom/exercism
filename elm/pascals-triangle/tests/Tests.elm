717d159ebbe1543490e25ff45d9a20a583b54137
Erik Schierboom <erik_schierboom@hotmail.com>
2018-09-16 11:36:38 +0200

    elm/triangle: add solution

--- elm/pascals-triangle/tests/Tests.elm
+++ elm/pascals-triangle/tests/Tests.elm
@@ -11,24 +11,19 @@ tests =
         [ test "no rows" <|
             \() ->
                 Expect.equal [] (rows 0)
-        , skip <|
-            test "single row" <|
-                \() ->
-                    Expect.equal [ [ 1 ] ] (rows 1)
-        , skip <|
-            test "two rows" <|
-                \() ->
-                    Expect.equal [ [ 1 ], [ 1, 1 ] ] (rows 2)
-        , skip <|
-            test "three rows" <|
-                \() ->
-                    Expect.equal [ [ 1 ], [ 1, 1 ], [ 1, 2, 1 ] ] (rows 3)
-        , skip <|
-            test "four rows" <|
-                \() ->
-                    Expect.equal [ [ 1 ], [ 1, 1 ], [ 1, 2, 1 ], [ 1, 3, 3, 1 ] ] (rows 4)
-        , skip <|
-            test "negative rows" <|
-                \() ->
-                    Expect.equal [] (rows -1)
+        , test "single row" <|
+            \() ->
+                Expect.equal [ [ 1 ] ] (rows 1)
+        , test "two rows" <|
+            \() ->
+                Expect.equal [ [ 1 ], [ 1, 1 ] ] (rows 2)
+        , test "three rows" <|
+            \() ->
+                Expect.equal [ [ 1 ], [ 1, 1 ], [ 1, 2, 1 ] ] (rows 3)
+        , test "four rows" <|
+            \() ->
+                Expect.equal [ [ 1 ], [ 1, 1 ], [ 1, 2, 1 ], [ 1, 3, 3, 1 ] ] (rows 4)
+        , test "negative rows" <|
+            \() ->
+                Expect.equal [] (rows -1)
         ]
