b2c9e75f0777f861ee7be68f26ac2b884388ec74
Erik Schierboom <erik_schierboom@hotmail.com>
2018-09-16 11:06:22 +0200

    elm/etl: add solution

--- elm/etl/Etl.elm
+++ elm/etl/Etl.elm
@@ -1,6 +1,24 @@
 module Etl exposing (transform)
 
+import String exposing (toLower)
+import List exposing (map, concatMap)
+import Dict exposing (Dict, toList, fromList)
+
+
+transformLetter : Int -> String -> ( String, Int )
+transformLetter i letter =
+    ( toLower letter, i )
+
+
+transformLetters : ( Int, List String ) -> List ( String, Int )
+transformLetters ( i, letters ) =
+    letters
+        |> map (transformLetter i)
+
 
 transform : Dict Int (List String) -> Dict String Int
 transform input =
-    Debug.crash "Please implement this function"
+    input
+        |> toList
+        |> concatMap transformLetters
+        |> fromList
