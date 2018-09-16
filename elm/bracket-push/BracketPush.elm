113d714c7e10efde18a782281acef5d3937cf8a5
Erik Schierboom <erik_schierboom@hotmail.com>
2018-09-16 21:34:02 +0200

    elm/bracket-push: add solution

--- elm/bracket-push/BracketPush.elm
+++ elm/bracket-push/BracketPush.elm
@@ -1,6 +1,50 @@
 module BracketPush exposing (isPaired)
 
 
+isOpeningBracket : Char -> Bool
+isOpeningBracket character =
+    character == '(' || character == '[' || character == '{'
+
+
+openingBracketForClosingBracket : Char -> Maybe Char
+openingBracketForClosingBracket character =
+    case character of
+        ')' ->
+            Just '('
+
+        ']' ->
+            Just '['
+
+        '}' ->
+            Just '{'
+
+        _ ->
+            Nothing
+
+
+isPairedHelper : List Char -> List Char -> Bool
+isPairedHelper input openedBrackets =
+    case input of
+        [] ->
+            List.isEmpty openedBrackets
+
+        x :: xs ->
+            if isOpeningBracket x then
+                isPairedHelper xs (x :: openedBrackets)
+
+            else
+                case openingBracketForClosingBracket x of
+                    Just openingBracket ->
+                        if List.head openedBrackets == Just openingBracket then
+                            isPairedHelper xs (List.drop 1 openedBrackets)
+
+                        else
+                            False
+
+                    Nothing ->
+                        isPairedHelper xs openedBrackets
+
+
 isPaired : String -> Bool
 isPaired input =
-    Debug.crash "Please implement this function"
+    isPairedHelper (String.toList input) []
--- elm/bracket-push/tests/Tests.elm
+++ elm/bracket-push/tests/Tests.elm
@@ -1,4 +1,4 @@
-module Tests exposing (..)
+module Tests exposing (tests)
 
 import BracketPush exposing (isPaired)
 import Expect
@@ -12,60 +12,46 @@ tests =
         [ test "paired square brackets" <|
             \_ ->
                 Expect.equal True (isPaired "[]")
-        , skip <|
-            test "empty string" <|
-                \_ ->
-                    Expect.equal True (isPaired "")
-        , skip <|
-            test "unpaired brackets" <|
-                \_ ->
-                    Expect.equal False (isPaired "[[")
-        , skip <|
-            test "wrong ordered brackets" <|
-                \_ ->
-                    Expect.equal False (isPaired "}{")
-        , skip <|
-            test "wrong closing bracket" <|
-                \_ ->
-                    Expect.equal False (isPaired "{]")
-        , skip <|
-            test "paired with whitespace" <|
-                \_ ->
-                    Expect.equal True (isPaired "{ }")
-        , skip <|
-            test "partially paired brackets" <|
-                \_ ->
-                    Expect.equal False (isPaired "{[])")
-        , skip <|
-            test "simple nested brackets" <|
-                \_ ->
-                    Expect.equal True (isPaired "{[]}")
-        , skip <|
-            test "seceral paired brackets" <|
-                \_ ->
-                    Expect.equal True (isPaired "{}[]")
-        , skip <|
-            test "paired and nested brackets" <|
-                \_ ->
-                    Expect.equal True (isPaired "([{}({}[])])")
-        , skip <|
-            test "unopened closing brackets" <|
-                \_ ->
-                    Expect.equal False (isPaired "{[)][]}")
-        , skip <|
-            test "unpaired and nested brackets" <|
-                \_ ->
-                    Expect.equal False (isPaired "([{])")
-        , skip <|
-            test "paired and wrong nested brackets" <|
-                \_ ->
-                    Expect.equal False (isPaired "[({]})")
-        , skip <|
-            test "math expression" <|
-                \_ ->
-                    Expect.equal True (isPaired "(((185 + 223.85) * 15) - 543)/2")
-        , skip <|
-            test "complex latex expression" <|
-                \_ ->
-                    Expect.equal True (isPaired "\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)")
+        , test "empty string" <|
+            \_ ->
+                Expect.equal True (isPaired "")
+        , test "unpaired brackets" <|
+            \_ ->
+                Expect.equal False (isPaired "[[")
+        , test "wrong ordered brackets" <|
+            \_ ->
+                Expect.equal False (isPaired "}{")
+        , test "wrong closing bracket" <|
+            \_ ->
+                Expect.equal False (isPaired "{]")
+        , test "paired with whitespace" <|
+            \_ ->
+                Expect.equal True (isPaired "{ }")
+        , test "partially paired brackets" <|
+            \_ ->
+                Expect.equal False (isPaired "{[])")
+        , test "simple nested brackets" <|
+            \_ ->
+                Expect.equal True (isPaired "{[]}")
+        , test "seceral paired brackets" <|
+            \_ ->
+                Expect.equal True (isPaired "{}[]")
+        , test "paired and nested brackets" <|
+            \_ ->
+                Expect.equal True (isPaired "([{}({}[])])")
+        , test "unopened closing brackets" <|
+            \_ ->
+                Expect.equal False (isPaired "{[)][]}")
+        , test "unpaired and nested brackets" <|
+            \_ ->
+                Expect.equal False (isPaired "([{])")
+        , test "paired and wrong nested brackets" <|
+            \_ ->
+                Expect.equal False (isPaired "[({]})")
+        , test "math expression" <|
+            \_ ->
+                Expect.equal True (isPaired "(((185 + 223.85) * 15) - 543)/2")
+        , test "complex latex expression" <|
+            \_ ->
+                Expect.equal True (isPaired "\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)")
         ]
