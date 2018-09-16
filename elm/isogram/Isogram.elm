e3e191afd2aa886200a2ac986e09c629a409b3c2
Erik Schierboom <erik_schierboom@hotmail.com>
2018-09-16 12:07:49 +0200

    elm/isogram: add solution

--- elm/isogram/Isogram.elm
+++ elm/isogram/Isogram.elm
@@ -1,6 +1,19 @@
 module Isogram exposing (isIsogram)
 
+import String exposing (toLower, filter, toList, toLower, length)
+import Char exposing (isLower)
+import Set exposing (fromList, size)
+
 
 isIsogram : String -> Bool
 isIsogram sentence =
-    Debug.crash "Please implement this function"
+    let
+        letters =
+            sentence
+                |> toLower
+                |> filter isLower
+
+        uniqueLetters =
+            letters |> toList |> fromList
+    in
+        length letters == size uniqueLetters
