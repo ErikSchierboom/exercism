02966077c080660a2cd31ce29242ba0f49c7a0f5
Erik Schierboom <e.schierboom@nl.ccv.eu>
2018-09-20 08:20:16 +0200

    typescript/accumulate: add solution

---
+++ typescript/accumulate/accumulate.ts
@@ -0,0 +1,8 @@
+export default function accumulate<T, K>(input: T[], accumulator: (element: T) => K) {
+    const acc = [];
+
+    for (const element of input)
+        acc.push(accumulator(element));
+
+    return acc;
+}
