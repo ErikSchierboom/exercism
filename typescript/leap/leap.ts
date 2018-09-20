f46e562e181940a3bc7a8d0d63f28b1bef832cdc
Erik Schierboom <ErikSchierboom@nl.ccv.eu>
2018-09-20 08:41:02 +0200

    typescript/leap: add iteration

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,5 +1,5 @@
 function isLeapYear(year: number): boolean {
-    return year % 4 === 0 && year % 100 !== 0 || year % 400 === 0;
+    return year % 4 === 0 && year % 100 !== 0 || year % 400 === 0
 }
 
 export default isLeapYear
