170777a1dfe34af557167632923f50c68c84845a
Erik Schierboom <erik_schierboom@hotmail.com>
2018-09-21 19:20:43 +0200

    javascript/two-fer: add solution

---
+++ javascript/two-fer/two-fer.js
@@ -0,0 +1,3 @@
+export default function twoFer(name) {
+  return `One for ${name || "you"}, one for me.`;
+}
--- javascript/two-fer/two-fer.spec.js
+++ javascript/two-fer/two-fer.spec.js
@@ -1,18 +1,18 @@
-import twoFer from './two-fer';
+import twoFer from "./two-fer";
 
-describe('twoFer()', () => {
-  test('no name given', () => {
-    const name = '';
-    expect(twoFer(name)).toEqual('One for you, one for me.');
+describe("twoFer()", () => {
+  test("no name given", () => {
+    const name = "";
+    expect(twoFer(name)).toEqual("One for you, one for me.");
   });
 
-  xtest('a name given', () => {
-    const name = 'Alice';
-    expect(twoFer(name)).toEqual('One for Alice, one for me.');
+  test("a name given", () => {
+    const name = "Alice";
+    expect(twoFer(name)).toEqual("One for Alice, one for me.");
   });
 
-  xtest('another name given', () => {
-    const name = 'Bob';
-    expect(twoFer(name)).toEqual('One for Bob, one for me.');
+  test("another name given", () => {
+    const name = "Bob";
+    expect(twoFer(name)).toEqual("One for Bob, one for me.");
   });
 });
