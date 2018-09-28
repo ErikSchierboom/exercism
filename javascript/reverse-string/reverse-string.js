018e482b934a23ccf9ea32f4617aa074bd720791
Erik Schierboom <e.schierboom@nl.ccv.eu>
2018-09-28 15:28:20 +0200

    javascript/reverse-string: add solution

---
+++ javascript/reverse-string/reverse-string.js
@@ -0,0 +1,3 @@
+export default function reverseString(input) {
+    return [...input].reverse().join('')
+}
--- javascript/reverse-string/reverse-string.spec.js
+++ javascript/reverse-string/reverse-string.spec.js
@@ -7,25 +7,25 @@ describe('ReverseString', () => {
     expect(actual).toEqual(expected);
   });
 
-  xtest('a word', () => {
+  test('a word', () => {
     const expected = 'tobor';
     const actual = reverseString('robot');
     expect(actual).toEqual(expected);
   });
 
-  xtest('a capitalized word', () => {
+  test('a capitalized word', () => {
     const expected = 'nemaR';
     const actual = reverseString('Ramen');
     expect(actual).toEqual(expected);
   });
 
-  xtest('a sentence with punctuation', () => {
+  test('a sentence with punctuation', () => {
     const expected = '!yrgnuh ma I';
     const actual = reverseString('I am hungry!');
     expect(actual).toEqual(expected);
   });
 
-  xtest('a palindrome', () => {
+  test('a palindrome', () => {
     const expected = 'racecar';
     const actual = reverseString('racecar');
     expect(actual).toEqual(expected);
