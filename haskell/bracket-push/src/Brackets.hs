e282585ce2b42e3687acdf003dfab6e5cf85556c
Erik Schierboom <erik_schierboom@hotmail.com>
2018-09-29 12:46:59 +0200

    haskell/bracket-push: add solution

--- haskell/bracket-push/src/Brackets.hs
+++ haskell/bracket-push/src/Brackets.hs
@@ -1,4 +1,29 @@
 module Brackets (arePaired) where
 
+isOpeningBracket :: Char -> Bool
+isOpeningBracket character = elem character "([{"
+
+openingBracketForClosingBracket :: Char -> Maybe Char
+openingBracketForClosingBracket character =
+    case character of
+        ')' -> Just '('
+        ']' -> Just '['
+        '}' -> Just '{'
+        _ -> Nothing
+
+arePairedHelper :: String -> String -> Bool
+arePairedHelper [] openedBrackets = null openedBrackets
+arePairedHelper (x:xs) openedBrackets
+    | isOpeningBracket x = arePairedHelper xs (x:openedBrackets)
+    | otherwise =
+        case openingBracketForClosingBracket x of
+            Just expectedOpeningBracket ->
+                if null openedBrackets || head openedBrackets /= expectedOpeningBracket then
+                    False
+                else
+                    arePairedHelper xs (tail openedBrackets)
+            Nothing ->
+                arePairedHelper xs openedBrackets
+
 arePaired :: String -> Bool
-arePaired xs = error "You need to implement this function."
+arePaired input = arePairedHelper input []
