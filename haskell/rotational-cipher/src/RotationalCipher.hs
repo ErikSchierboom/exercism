64e4d1e97240111305c63dea63793cdd107fb1e0
Erik Schierboom <erik_schierboom@hotmail.com>
2018-09-29 11:01:31 +0200

    haskell/rotational-cipher: add solution

--- haskell/rotational-cipher/src/RotationalCipher.hs
+++ haskell/rotational-cipher/src/RotationalCipher.hs
@@ -1,4 +1,27 @@
 module RotationalCipher (rotate) where
 
+import Data.List (elemIndex, length)
+import Data.Char (isLower, isLetter)
+import Data.Maybe (fromJust)
+
+lowerCaseLetters :: [Char]
+lowerCaseLetters = ['a' .. 'z']
+
+upperCaseLetters :: [Char]
+upperCaseLetters = ['A' .. 'Z']
+
+keyForLetter :: Char -> [Char]
+keyForLetter letter = if isLower letter then lowerCaseLetters else upperCaseLetters
+
+rotateLetterUsingKey :: Int -> [Char] -> Char -> Char
+rotateLetterUsingKey shiftKey key letter = key !! (shiftIndex `mod` length key)
+    where
+        shiftIndex = shiftKey + (fromJust $ elemIndex letter key)
+
+rotateLetter :: Int -> Char -> Char
+rotateLetter shiftKey letter 
+    | isLetter letter = rotateLetterUsingKey shiftKey (keyForLetter letter) letter
+    | otherwise = letter
+
 rotate :: Int -> String -> String
-rotate = error "You need to implement this function."
+rotate shiftKey text = map (rotateLetter shiftKey) text
