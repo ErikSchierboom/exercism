84a9ffbe42781420d6be409385d8cf04ddc1c003
Erik Schierboom <erik_schierboom@hotmail.com>
2018-09-29 11:21:50 +0200

    haskell/isogram: add solution

--- haskell/isogram/src/Isogram.hs
+++ haskell/isogram/src/Isogram.hs
@@ -1,4 +1,10 @@
 module Isogram (isIsogram) where
 
+import Data.List (length, map, filter, nub)
+import Data.Char (isLower, toLower)
+
 isIsogram :: String -> Bool
-isIsogram = error "You need to implement this function!"
+isIsogram sentence = length letters == length uniqueLetters
+    where
+        letters = filter isLower $ map toLower sentence 
+        uniqueLetters = nub letters  
