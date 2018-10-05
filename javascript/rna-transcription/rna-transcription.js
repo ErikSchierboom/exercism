dc8a8bb3e5cba5a0b6b4c526dedadcc525c74fef
Erik Schierboom <erik_schierboom@hotmail.com>
2018-10-05 19:31:09 +0200

    javascript/rna-transcription: add iteration

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,14 +1,15 @@
 export default class Transcriptor {
-    toRna(dna) {
-        return Array.prototype.map.call(dna, this.dnaToRna).join('');
-    }
+  toRna(dna) {
+    return Array.from(dna, this.dnaToRna).join('')
+  }
 
-    dnaToRna(nucleotide) {
-        switch (nucleotide) {
-            case 'G': return 'C';
-            case 'C': return 'G';
-            case 'T': return 'A';
-            case 'A': return 'U';
-        }
+  dnaToRna(nucleotide) {
+    switch (nucleotide) {
+      case 'G': return 'C'
+      case 'C': return 'G'
+      case 'T': return 'A'
+      case 'A': return 'U'
+      default: throw new Error('Invalid input DNA.')
     }
+  }
 }
