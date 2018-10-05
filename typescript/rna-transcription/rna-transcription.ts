64197440b40073b846ee9ae01aa65e46e38dfccd
Erik Schierboom <e.schierboom@nl.ccv.eu>
2018-10-05 12:31:11 +0200

    typescript/rna-transcription: add solution

---
+++ typescript/rna-transcription/rna-transcription.ts
@@ -0,0 +1,15 @@
+export default class Transcriptor {
+    toRna(dna: string) {
+        return Array.prototype.map.call(dna, this.dnaToRna).join('')
+    }
+
+    dnaToRna(nucleotide: string) {
+        switch (nucleotide) {
+            case 'G': return 'C'
+            case 'C': return 'G'
+            case 'T': return 'A'
+            case 'A': return 'U'
+            default: throw 'Invalid input DNA.'
+        }
+    }
+}
