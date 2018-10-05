e390c654f2988b7baaf0e09ec7506f5aee426cc5
Erik Schierboom <erik_schierboom@hotmail.com>
2018-10-05 19:31:04 +0200

    typescript/rna-transcription: add iteration

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,15 +1,15 @@
 export default class Transcriptor {
-    toRna(dna: string): string {
-        return Array.from(dna).map(this.dnaToRna).join('')
-    }
+  toRna(dna: string): string {
+    return Array.from(dna, this.dnaToRna).join('')
+  }
 
-    private dnaToRna(nucleotide: string): string {
-        switch (nucleotide) {
-            case 'G': return 'C'
-            case 'C': return 'G'
-            case 'T': return 'A'
-            case 'A': return 'U'
-            default: throw new Error('Invalid input DNA.')
-        }
+  private dnaToRna(nucleotide: string): string {
+    switch (nucleotide) {
+      case 'G': return 'C'
+      case 'C': return 'G'
+      case 'T': return 'A'
+      case 'A': return 'U'
+      default: throw new Error('Invalid input DNA.')
     }
+  }
 }
