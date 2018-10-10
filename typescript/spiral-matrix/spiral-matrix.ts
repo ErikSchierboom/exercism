3db330f9574ba47670c9884c7f73b08c6faf3880
Erik Schierboom <erik_schierboom@hotmail.com>
2018-10-10 19:24:02 +0200

    typescript/spiral-matrix: add solution

---
+++ typescript/spiral-matrix/spiral-matrix.ts
@@ -0,0 +1,41 @@
+export default class SpiralMatrix {
+    public static ofSize(size: number) {
+        const totalNumbers = size * size
+        const matrix: number[][] = []
+
+        for (let i = 0; i < size; i += 1) {
+            matrix.push([])
+        }
+
+        let rowOffset = 0
+        let columnOffset = 0
+        let currentNumber = 1
+
+        while (currentNumber <= totalNumbers) {
+            for (let topColumn = columnOffset; topColumn < size - columnOffset; topColumn += 1) {
+                matrix[rowOffset][topColumn] = currentNumber
+                currentNumber += 1
+            }
+
+            for (let rightRow = rowOffset + 1; rightRow < size - rowOffset; rightRow += 1) {
+                matrix[rightRow][size - columnOffset - 1] = currentNumber
+                currentNumber += 1
+            }
+
+            for (let bottomColumn = size - columnOffset - 2; bottomColumn >= columnOffset; bottomColumn -= 1) {
+                matrix[size - rowOffset - 1][bottomColumn] = currentNumber
+                currentNumber += 1
+            }
+
+            for (let leftRow = size - rowOffset - 2; leftRow >= rowOffset + 1; leftRow -= 1) {
+                matrix[leftRow][columnOffset] = currentNumber
+                currentNumber += 1
+            }
+
+            rowOffset += 1
+            columnOffset += 1
+        }
+
+        return matrix
+    }
+}
