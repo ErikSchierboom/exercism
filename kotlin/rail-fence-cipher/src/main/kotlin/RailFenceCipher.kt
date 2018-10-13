437bda46e600704b74a231398f9b04624aa5fae0
Erik Schierboom <erik_schierboom@hotmail.com>
2018-10-13 19:02:03 +0200

    kotlin/rail-fence-cipher: add solution

---
+++ kotlin/rail-fence-cipher/src/main/kotlin/RailFenceCipher.kt
@@ -0,0 +1,30 @@
+import kotlin.coroutines.experimental.buildSequence
+
+class RailFenceCipher(private val rails: Int) {
+    fun getEncryptedData(plainText: String): String =
+        plainText.asSequence().zip(tracks().take(plainText.length))
+                .sortedBy { it.second }
+                .map { it.first }
+                .joinToString("")
+
+    fun getDecryptedData(cipherText: String): String =
+        tracks().take(cipherText.length)
+                .mapIndexed { index, track -> index to track }.toList()
+                .sortedBy { it.second }
+                .map { it.first }
+                .zip(cipherText.asIterable())
+                .sortedBy { it.first }
+                .map { it.second }
+                .joinToString("")
+
+    private fun tracks() = buildSequence {
+        var current = 0
+        var up = true
+
+        while (true) {
+            yield(current)
+            current = if (up) current + 1 else current - 1
+            up = if (up) current < rails - 1 else current == 0
+        }
+    }
+}
