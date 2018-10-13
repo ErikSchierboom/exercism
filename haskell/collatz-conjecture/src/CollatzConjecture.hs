d6bc6cf7f9be2a222e28e09fb5909b82e5d771d1
Erik Schierboom <erik_schierboom@hotmail.com>
2018-10-13 07:54:32 +0200

    haskell/collatz-conjecture: add iteration

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -3,10 +3,8 @@ module CollatzConjecture (collatz) where
 import Data.List (genericLength)
 
 collatz :: Integer -> Maybe Integer
-collatz number = 
-    if number < 1 then
-        Nothing
-    else
-        Just . genericLength . takeWhile (/=1) $ iterate next number
-    where
-        next x = if even x then x `div` 2 else 3 * x + 1
+collatz number 
+    | number < 1 = Nothing
+    | otherwise = Just . genericLength . takeWhile (/=1) $ iterate next number
+        where
+            next x = if even x then x `div` 2 else 3 * x + 1
