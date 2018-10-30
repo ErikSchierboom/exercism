dc4fe39664dd5298caa8e3329c44682a768f0e44
Erik Schierboom <erik_schierboom@hotmail.com>
2018-10-30 21:56:44 +0100

    csharp/darts: add solution

---
+++ csharp/darts/Darts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+        var distance = Math.Sqrt(x * x + y * y);
+
+        if (distance <= 1.0)
+            return 10;
+        if (distance <= 5.0)
+            return 5;
+        if (distance <= 10.0)
+            return 1;
+
+        return 0;
+    }
+}
