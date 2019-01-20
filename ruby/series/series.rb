9832a72cde5f34b38fb4f95852ee99a76a1ef95e
Erik Schierboom <erik_schierboom@hotmail.com>
2019-01-20 15:03:50 +0100

    ruby/series: add iteration

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,11 +1,17 @@
 class Series
-  def initialize(series)
-    @series = series
+
+  attr_reader :numerals
+
+  def initialize(numerals)
+    @numerals = numerals
   end
 
   def slices(length)
-    raise ArgumentError, 'Invalid slice length' if length > @series.length
+    raise ArgumentError, 'Slice length must be less than or equal to input length' unless length <= numerals.length
 
-    (0..@series.length - length).map { |index| @series[index, length] }
+    numerals[0..-length]
+      .each_char
+      .each_with_index
+      .map { |_, index| numerals[index, length] }
   end
 end
