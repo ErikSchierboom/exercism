24698bb66b7624603b379aca129bf38bea650d65
Erik Schierboom <erik_schierboom@hotmail.com>
2019-01-26 11:19:12 +0100

    ruby/phone_number: add solution

---
+++ ruby/phone-number/phone_number_test.rb
@@ -0,0 +1,77 @@
+require 'minitest/autorun'
+require_relative 'phone_number'
+
+# Common test data version: 1.6.1 fc57696
+class PhoneNumberTest < Minitest::Test
+  def test_cleans_the_number
+    assert_equal "2234567890", PhoneNumber.clean("(223) 456-7890")
+  end
+
+  def test_cleans_numbers_with_dots
+    assert_equal "2234567890", PhoneNumber.clean("223.456.7890")
+  end
+
+  def test_cleans_numbers_with_multiple_spaces
+    assert_equal "2234567890", PhoneNumber.clean("223 456   7890   ")
+  end
+
+  def test_invalid_when_9_digits
+    assert_nil PhoneNumber.clean("123456789")
+  end
+
+  def test_invalid_when_11_digits_does_not_start_with_a_1
+    assert_nil PhoneNumber.clean("22234567890")
+  end
+
+  def test_valid_when_11_digits_and_starting_with_1
+    assert_equal "2234567890", PhoneNumber.clean("12234567890")
+  end
+
+  def test_valid_when_11_digits_and_starting_with_1_even_with_punctuation
+    assert_equal "2234567890", PhoneNumber.clean("+1 (223) 456-7890")
+  end
+
+  def test_invalid_when_more_than_11_digits
+    assert_nil PhoneNumber.clean("321234567890")
+  end
+
+  def test_invalid_with_letters
+    assert_nil PhoneNumber.clean("123-abc-7890")
+  end
+
+  def test_invalid_with_punctuations
+    assert_nil PhoneNumber.clean("123-@:!-7890")
+  end
+
+  def test_invalid_if_area_code_starts_with_0
+    assert_nil PhoneNumber.clean("(023) 456-7890")
+  end
+
+  def test_invalid_if_area_code_starts_with_1
+    assert_nil PhoneNumber.clean("(123) 456-7890")
+  end
+
+  def test_invalid_if_exchange_code_starts_with_0
+    assert_nil PhoneNumber.clean("(223) 056-7890")
+  end
+
+  def test_invalid_if_exchange_code_starts_with_1
+    assert_nil PhoneNumber.clean("(223) 156-7890")
+  end
+
+  def test_invalid_if_area_code_starts_with_0_on_valid_11_digit_number
+    assert_nil PhoneNumber.clean("1 (023) 456-7890")
+  end
+
+  def test_invalid_if_area_code_starts_with_1_on_valid_11_digit_number
+    assert_nil PhoneNumber.clean("1 (123) 456-7890")
+  end
+
+  def test_invalid_if_exchange_code_starts_with_0_on_valid_11_digit_number
+    assert_nil PhoneNumber.clean("1 (223) 056-7890")
+  end
+
+  def test_invalid_if_exchange_code_starts_with_1_on_valid_11_digit_number
+    assert_nil PhoneNumber.clean("1 (223) 156-7890")
+  end
+end
