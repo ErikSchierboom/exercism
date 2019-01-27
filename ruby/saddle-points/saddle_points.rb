fd605881528a2e484a78d293c5d5db7caa371ca6
Erik Schierboom <erik_schierboom@hotmail.com>
2019-01-27 20:38:12 +0100

    ruby/saddle-points: add solution

---
+++ ruby/saddle-points/saddle_points.rb
@@ -0,0 +1,21 @@
+class Matrix
+  attr_reader :rows, :columns
+
+  def initialize(matrix)
+    @rows = matrix.each_line.map { |line| line.split.map(&:to_i) }
+    @columns = @rows.transpose
+  end
+
+  def saddle_points
+    rows_maximum = @rows.map(&:max)
+    cols_minimum = @columns.map(&:min)
+
+    coordinates.select { |row, column| rows_maximum[row] == cols_minimum[column] }
+  end
+
+  private
+
+  def coordinates
+    rows.each_index.to_a.product(columns.each_index.to_a)
+  end
+end
