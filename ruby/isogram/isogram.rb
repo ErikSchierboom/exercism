d894436f3cd6fd164426d855356a9766ae9e3d87
Erik Schierboom <erik_schierboom@hotmail.com>
2019-01-29 21:44:48 +0100

    ruby/isogram: add iteration

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,13 +1,6 @@
-require 'set'
-
 module Isogram
   def self.isogram?(word)
-    found_letters = Set.new([])
-
-    word.scan(/\w/).each do |letter|
-      return false if found_letters.include?(letter.downcase)
-
-      found_letters.add(letter.downcase)
-    end
+    letters = word.downcase.scan(/\w/)
+    letters == letters.uniq
   end
 end
