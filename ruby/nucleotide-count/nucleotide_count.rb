99cf7a827cd16065c7e2583d7b89208de38e6a87
Erik Schierboom <erik_schierboom@hotmail.com>
2019-02-09 10:17:21 +0100

    ruby/nucleotide-count: add iteration

--- ruby/nucleotide-count/nucleotide_count.rb
+++ ruby/nucleotide-count/nucleotide_count.rb
@@ -1,27 +1,28 @@
 class Nucleotide
-  attr_reader :histogram
+  EMPTY_HISTOGRAM = { 'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0 }.freeze
+  private_constant :EMPTY_HISTOGRAM
 
   def self.from_dna(dna)
     new(dna)
   end
 
   def initialize(dna)
-    @histogram = to_histogram(dna)
+    raise ArgumentError, 'Unknown nucleotide' unless dna =~ /^[ATCG]*$/
+
+    @dna = dna
   end
 
   def count(nucleotide)
     histogram[nucleotide]
   end
 
+  def histogram
+    dna
+      .each_char
+      .each_with_object(Hash[EMPTY_HISTOGRAM]) { |nucleotide, histogram| histogram[nucleotide] += 1 }
+  end
+
   private
 
-  def to_histogram(dna)
-    empty_histogram = { 'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0 }
-
-    dna.each_char.each_with_object(empty_histogram) do |nucleotide, histogram|
-      raise ArgumentError, 'Unknown nucleotide' unless histogram.include?(nucleotide)
-
-      histogram[nucleotide] += 1
-    end
-  end
+  attr_reader :dna
 end
