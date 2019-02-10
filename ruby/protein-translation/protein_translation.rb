7dc38fd849787f54e948ae5a1a617f3a2a86f03c
Erik Schierboom <erik_schierboom@hotmail.com>
2019-02-10 22:08:43 +0100

    ruby/protein-translation: add solution

---
+++ ruby/protein-translation/protein_translation.rb
@@ -0,0 +1,39 @@
+class InvalidCodonError < StandardError
+end
+
+module Translation
+  CODONS_TO_PROTEINS = {
+    'AUG' => 'Methionine',
+    'UUU' => 'Phenylalanine',
+    'UUC' => 'Phenylalanine',
+    'UUA' => 'Leucine',
+    'UUG' => 'Leucine',
+    'UCU' => 'Serine',
+    'UCC' => 'Serine',
+    'UCA' => 'Serine',
+    'UCG' => 'Serine',
+    'UAU' => 'Tyrosine',
+    'UAC' => 'Tyrosine',
+    'UGU' => 'Cysteine',
+    'UGC' => 'Cysteine',
+    'UGG' => 'Tryptophan',
+    'UAA' => 'STOP',
+    'UAG' => 'STOP',
+    'UGA' => 'STOP'
+  }.freeze
+  private_constant :CODONS_TO_PROTEINS
+
+  def self.of_codon(codon)
+    raise InvalidCodonError, 'Invalid codon' unless CODONS_TO_PROTEINS.key?(codon)
+
+    CODONS_TO_PROTEINS[codon]
+  end
+
+  def self.of_rna(rna)
+    rna
+      .each_char
+      .each_slice(3)
+      .map { |codon| of_codon(codon.join) }
+      .take_while { |protein| protein != 'STOP' }
+  end
+end
