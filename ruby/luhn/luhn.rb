f4524ea72d0fcc54eb78314eb01d5d602d65b5ff
Erik Schierboom <erik_schierboom@hotmail.com>
2019-02-10 15:50:12 +0100

    ruby/luhn: add iteration

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -4,32 +4,34 @@ class Luhn
   end
 
   def initialize(number)
-    @number = number
+    @number = number.delete(' ')
   end
 
   def valid?
-    (checksum % 10).zero? unless digits.nil? || digits.size < 2
+    (luhn_sum % 10).zero? if digits
   end
 
   private
 
   attr_reader :number
 
-  def digits
-    number
-      .delete(' ')
-      .match(/^\d{2,}$/) { |m| m.string.each_char.map(&:to_i) }
-  end
-
-  def checksum
+  def luhn_sum
     digits
       .reverse_each
       .each_slice(2)
-      .sum { |even_digit, odd_digit| even_digit + double(odd_digit.to_i) }
+      .sum { |even, odd| even + double(odd.to_i) }
+  end
+
+  def digits
+    number.each_char.map(&:to_i) if digits_valid?
+  end
+
+  def digits_valid?
+    number =~ /^\d{2,}$/
   end
 
   def double(digit)
     doubled = digit * 2
-    doubled > 9 ? doubled - 9 : doubled
+    doubled < 10 ? doubled : doubled - 9
   end
 end
