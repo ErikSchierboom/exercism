ce9285f36e31f75f8cbe29f735b862cd1940c607
Erik Schierboom <erik_schierboom@hotmail.com>
2019-02-11 21:53:33 +0100

    ruby/etl: add iteration

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -1,9 +1,7 @@
 module ETL
   def self.transform(old)
-    old.each_pair.each_with_object({}) do |(score, letters), new|
-      letters.each_with_object(new) do |letter, new|
-        new[letter.downcase] = score
-      end
-    end
+    old.flat_map do |(score, letters)|
+      letters.map { |letter| [letter.downcase, score] }
+    end.to_h
   end
 end
