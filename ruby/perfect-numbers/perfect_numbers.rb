16c7a84c1e08a744e6d3ca5c3b3043a974359dd8
Erik Schierboom <erik_schierboom@hotmail.com>
2019-02-13 18:02:19 +0100

    ruby/perfect-numbers: add solution

---
+++ ruby/perfect-numbers/perfect_numbers.rb
@@ -0,0 +1,30 @@
+class PerfectNumber
+  def self.classify(number)
+    new(number).classify
+  end
+
+  def initialize(number)
+    @number = number
+  end
+
+  def classify
+    raise 'Number must be positive' unless number >= 0
+
+    return 'deficient' if aliquot_sum < number
+    return 'abundant' if aliquot_sum > number
+
+    'perfect'
+  end
+
+  private
+
+  attr_reader :number
+
+  def aliquot_sum
+    factors.sum
+  end
+
+  def factors
+    (1..number / 2).select { |n| (number % n).zero? }
+  end
+end
---
+++ ruby/perfect-numbers/perfect_numbers_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require_relative 'perfect_numbers'
+
+class PerfectNumberTest < Minitest::Test
+  def test_initialize_perfect_number
+    assert_raises RuntimeError do
+      PerfectNumber.classify(-1)
+    end
+  end
+
+  def test_classify_deficient
+    assert_equal 'deficient', PerfectNumber.classify(13)
+  end
+
+  def test_classify_perfect
+    assert_equal 'perfect', PerfectNumber.classify(28)
+  end
+
+  def test_classify_abundant
+    assert_equal 'abundant', PerfectNumber.classify(12)
+  end
+end
