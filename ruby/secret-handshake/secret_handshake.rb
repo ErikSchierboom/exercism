6fbcbfe47b050716716f6076e2165d1b9467f547
Erik Schierboom <erik_schierboom@hotmail.com>
2019-03-04 21:22:29 +0100

    ruby/secret-handshake: add iteration

--- ruby/secret-handshake/secret_handshake.rb
+++ ruby/secret-handshake/secret_handshake.rb
@@ -4,7 +4,7 @@ class SecretHandshake
     0b00010 => ->(commands) { commands << 'double blink' },
     0b00100 => ->(commands) { commands << 'close your eyes' },
     0b01000 => ->(commands) { commands << 'jump' },
-    0b10000 => ->(commands) { commands.reverse }
+    0b10000 => ->(commands) { commands.reverse! }
   }.freeze
   private_constant :ENCODED_COMMANDS
 
@@ -13,8 +13,8 @@ class SecretHandshake
   end
 
   def commands
-    ENCODED_COMMANDS.inject([]) do |acc, (mask, operation)|
-      encoded_number.anybits?(mask) ? operation.call(acc) : acc
+    ENCODED_COMMANDS.each_with_object([]) do |(mask, operation), acc|
+      operation.call(acc) if encoded_number.anybits?(mask)
     end
   end
 
