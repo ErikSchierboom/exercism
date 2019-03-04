d1c5cef010c700562f78ab51995fd82a833461fe
Erik Schierboom <erik_schierboom@hotmail.com>
2019-03-04 11:36:57 +0100

    ruby/binary-search: add solution

---
+++ ruby/binary-search/binary_search.rb
@@ -0,0 +1,42 @@
+class BinarySearch
+  attr_reader :list
+
+  def initialize(list)
+    raise ArgumentError, 'List must be sorted' unless list.sorted?
+
+    @list = list
+  end
+
+  def middle
+    list.size / 2
+  end
+
+  def search_for(value)
+    left_index = 0
+    right_index = list.size - 1
+
+    while left_index < right_index
+      middle_index = (left_index + right_index) / 2
+      middle_value = list[middle_index]
+
+      return middle_index if middle_value == value
+
+      left_index = middle_index + 1 if middle_value < value
+      right_index = middle_index - 1 if middle_value > value
+    end
+
+    raise 'Could not find value in list'
+  end
+
+  private
+
+  def self.sorted(list)
+    list.each_cons(2).all?(&:<=)
+  end
+end
+
+module Enumerable
+  def sorted?
+    each_cons(2).all? { |a, b| (a <=> b) <= 0 }
+  end
+end
