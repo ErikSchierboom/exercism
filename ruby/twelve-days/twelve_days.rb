09736f8366c74a47cebd23976afa385c9ba665da
Erik Schierboom <erik_schierboom@hotmail.com>
2019-03-08 08:58:33 +0100

    ruby/twelve-days: add iteration

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -1,7 +1,7 @@
 module TwelveDays
-  NUMBERS = %w[a two three four five six seven eight nine ten eleven twelve].freeze
-  COUNTS = %w[first second third fourth fifth sixth seventh eighth ninth tenth eleventh twelfth].freeze
-  SUBJECTS = [
+  DAYS = %w[first second third fourth fifth sixth seventh eighth ninth tenth eleventh twelfth].freeze
+  COUNTS = %w[a two three four five six seven eight nine ten eleven twelve].freeze
+  GIFTS = [
     'Partridge in a Pear Tree',
     'Turtle Doves',
     'French Hens',
@@ -15,33 +15,36 @@ module TwelveDays
     'Pipers Piping',
     'Drummers Drumming'
   ].freeze
-  private_constant :NUMBERS, :COUNTS, :SUBJECTS
+  private_constant :DAYS, :COUNTS, :GIFTS
 
   def self.song
-    1.upto(SUBJECTS.size)
+    1.upto(DAYS.size)
      .map(&method(:verse))
      .join("\n")
   end
 
   private
 
-  def self.verse(verse_number)
-    format("%<begin>s%<end>s.\n", begin: verse_begin(verse_number), end: verse_end(verse_number))
+  def self.verse(n)
+    "On the #{day(n)} day of Christmas my true love gave to me: #{gifts(n)}.\n"
   end
 
-  def self.verse_begin(verse_number)
-    format('On the %<count>s day of Christmas my true love gave to me: ', count: COUNTS[verse_number - 1])
+  def self.gifts(n)
+    n.downto(1)
+     .map { |n| "#{count(n)} #{gift(n)}" }
+     .join(', ')
+     .sub(', a', ', and a')
   end
 
-  def self.verse_end(verse_number)
-    verse_number
-      .downto(1)
-      .map(&method(:subject))
-      .join(', ')
-      .sub(', a', ', and a')
+  def self.day(n)
+    DAYS[n - 1]
   end
 
-  def self.subject(verse_number)
-    format('%<number>s %<subject>s', number: NUMBERS[verse_number - 1], subject: SUBJECTS[verse_number - 1])
+  def self.count(n)
+    COUNTS[n - 1]
+  end
+
+  def self.gift(n)
+    GIFTS[n - 1]
   end
 end
