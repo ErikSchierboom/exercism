9b166ac07449b0ecf0f92e37240026aafb76c264
Erik Schierboom <erik_schierboom@hotmail.com>
2019-03-10 09:25:43 +0100

    ruby/kindergarten-garden: add iteration

--- ruby/kindergarten-garden/kindergarten_garden.rb
+++ ruby/kindergarten-garden/kindergarten_garden.rb
@@ -1,9 +1,9 @@
 class Garden
-  DEFAULT_STUDENTS = %w[Alice Bob Charlie David Eve Fred Ginny Harriet Ileana Joseph Kincaid Larry].freeze
-  PLANT_ENCODING = {'V' => :violets, 'C' => :clover, 'G' => :grass, 'R' => :radishes }
-  private_constant :DEFAULT_STUDENTS, :PLANT_ENCODING
+  STUDENTS = %w[Alice Bob Charlie David Eve Fred Ginny Harriet Ileana Joseph Kincaid Larry].freeze
+  INITIAL_TO_PLANT = { 'V' => :violets, 'C' => :clover, 'G' => :grass, 'R' => :radishes }
+  private_constant :STUDENTS, :INITIAL_TO_PLANT
 
-  def initialize(diagram, students = DEFAULT_STUDENTS)
+  def initialize(diagram, students = STUDENTS)
     top_plants, bottom_plants = top_and_bottom_plants(diagram)
 
     top_plants.zip(bottom_plants).zip(students.sort) do |(top, bottom), student|
@@ -21,8 +21,8 @@ class Garden
 
   def plants(encoded_plants)
     encoded_plants
-      .each_char
-      .map { |encoded_plant| PLANT_ENCODING[encoded_plant] }
+      .chars
+      .map { |initial| INITIAL_TO_PLANT[initial] }
       .each_slice(2)
   end
 end
