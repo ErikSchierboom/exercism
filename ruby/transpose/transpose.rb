0e519ef9ca04edf7ee2f243509922108f12ca531
Erik Schierboom <erik_schierboom@hotmail.com>
2019-04-28 11:32:53 +0200

    ruby/transpose: add iteration

--- ruby/transpose/transpose.rb
+++ ruby/transpose/transpose.rb
@@ -1,18 +1,15 @@
 class Transpose
-  PADDING_CHAR = "\u0080".freeze
-  private_constant :PADDING_CHAR
-
   def self.transpose(*args)
     new(*args).transpose
   end
 
   def initialize(input)
-    @rows = input.split("\n")
+    @rows = input.lines(chomp: true)
   end
 
   def transpose
     transposed_array
-      .map(&method(:normalize_col))
+      .map(&method(:unpad))
       .join("\n")
   end
 
@@ -21,22 +18,27 @@ class Transpose
   attr_reader :rows
 
   def transposed_array
-    rows.map(&method(:pad_row))
+    rows.map(&method(:pad))
         .map(&:chars)
         .transpose
   end
 
-  def pad_row(row)
+  def max_row_length
+    rows.map(&:length).max
+  end
+
+  def pad(row)
     row.ljust(max_row_length, PADDING_CHAR)
   end
 
-  def max_row_length
-    rows.map(&:size).max
-  end
-
-  def normalize_col(col)
+  def unpad(col)
     col.join
-       .gsub(/#{PADDING_CHAR}+$/, '')
+       .gsub(TRAILING_PADDING_CHARS, '')
        .tr(PADDING_CHAR, ' ')
   end
+
+  PADDING_CHAR = "\u0080".freeze
+  TRAILING_PADDING_CHARS = /#{PADDING_CHAR}+$/.freeze
+
+  private_constant :PADDING_CHAR, :TRAILING_PADDING_CHARS
 end
