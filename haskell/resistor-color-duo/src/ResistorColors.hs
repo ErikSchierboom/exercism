f48aae959ced31e9ab2123b383ad4a24acd794aa
Erik Schierboom <erik_schierboom@hotmail.com>
2019-04-29 08:57:06 +0200

    haskell/resistor-colors-duo: add iteration

--- haskell/resistor-color-duo/src/ResistorColors.hs
+++ haskell/resistor-color-duo/src/ResistorColors.hs
@@ -11,19 +11,7 @@ data Color =
   | Violet
   | Grey
   | White
-  deriving (Eq, Show, Read)
+  deriving (Eq, Show, Read, Enum)
 
 value :: [Color] -> Int
-value cs = foldl (\acc color -> acc * 10 + colorValue color) 0 cs
-
-colorValue :: Color -> Int
-colorValue Black  = 0
-colorValue Brown  = 1
-colorValue Red    = 2
-colorValue Orange = 3
-colorValue Yellow = 4
-colorValue Green  = 5
-colorValue Blue   = 6
-colorValue Violet = 7
-colorValue Grey   = 8
-colorValue White  = 9
+value cs = read $ concatMap (show . fromEnum) cs
