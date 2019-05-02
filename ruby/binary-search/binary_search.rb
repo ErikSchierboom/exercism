19f64b4def4546e87f13e189bffd173910e7411d
Erik Schierboom <erik_schierboom@hotmail.com>
2019-05-02 20:53:06 +0200

    ruby/binary-search: add iteration

--- ruby/binary-search/binary_search.rb
+++ ruby/binary-search/binary_search.rb
@@ -1,3 +1,28 @@
+class BinarySearchIndex
+  attr_reader :left_boundary, :right_boundary
+
+  def initialize(left_boundary, right_boundary)
+    @left_boundary = left_boundary
+    @right_boundary = right_boundary
+  end
+
+  def middle
+    (left_boundary + right_boundary) / 2
+  end
+
+  def search_left_of_middle!
+    @right_boundary = middle - 1
+  end
+
+  def search_right_of_middle!
+    @left_boundary = middle + 1
+  end
+
+  def valid?
+    left_boundary < right_boundary
+  end
+end
+
 class BinarySearch
   attr_reader :list
 
@@ -5,27 +30,26 @@ class BinarySearch
     raise ArgumentError, 'List must be sorted' unless list.sorted?
 
     @list = list
+    @search_index = BinarySearchIndex.new(0, list.size - 1)
   end
 
   def middle
-    list.size / 2
+    search_index.middle
   end
 
   def search_for(value)
-    left_index = 0
-    right_index = list.size - 1
+    search_index = BinarySearchIndex.new(0, list.size - 1)
 
-    while left_index < right_index
-      middle_index = (left_index + right_index) / 2
-      middle_value = list[middle_index]
+    while search_index.valid?
+      middle_value = list[search_index.middle]
 
       case compare(middle_value, value)
       when :smaller
-        left_index = middle_index + 1
+        search_index.search_right_of_middle!
       when :larger
-        right_index = middle_index - 1
+        search_index.search_left_of_middle!
       when :equal
-        return middle_index
+        return search_index.middle
       end
     end
 
@@ -33,6 +57,8 @@ class BinarySearch
   end
 
   private
+
+  attr_reader :search_index
 
   def compare(value1, value2)
     case value1 <=> value2
