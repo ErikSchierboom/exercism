a699566d8dc50485ea91e8c25870e529f06020ee
Erik Schierboom <erik_schierboom@hotmail.com>
2019-06-17 10:36:39 +0200

    rust/reverse-string: add iteration

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,8 +1,5 @@
-extern crate unicode_segmentation;
-
-use std::iter::FromIterator;
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    String::from_iter(input.graphemes(true).rev())
+    input.graphemes(true).rev().collect::<String>()
 }
