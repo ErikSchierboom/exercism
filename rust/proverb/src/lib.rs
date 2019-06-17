35cba86272f56cf5f72149e8e970ca43064857af
Erik Schierboom <erik_schierboom@hotmail.com>
2019-06-17 12:41:13 +0200

    rust/proverb: add iteration

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,13 +1,8 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    if list.is_empty() {
-        return String::new();
-    }
-
-    let mut lines = list
+    list
         .windows(2)
         .map(|pair| format!("For want of a {} the {} was lost.", pair[0], pair[1]))
-        .collect::<Vec<_>>();
-
-    lines.push(format!("And all for the want of a {}.", list[0]));
-    lines.join("\n")
+        .chain(list.first().map(|first| format!("And all for the want of a {}.", first)))
+        .collect::<Vec<_>>()
+        .join("\n")
 }
