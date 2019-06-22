1c37d41cce6b4330e600f0e13d97e958755ce9c7
Erik Schierboom <erik_schierboom@hotmail.com>
2019-06-22 12:10:33 +0200

    ruby/diamond: add solution

---
+++ ruby/diamond/diamond_test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require_relative 'diamond'
+
+class DiamondTest < Minitest::Test
+  def test_letter_a
+    answer = Diamond.make_diamond('A')
+    assert_equal "A\n", answer
+  end
+
+  def test_letter_c
+    answer = Diamond.make_diamond('C')
+    string = "  A  \n"\
+             " B B \n"\
+             "C   C\n"\
+             " B B \n"\
+             "  A  \n"
+    assert_equal string, answer
+  end
+
+  def test_letter_e
+    answer = Diamond.make_diamond('E')
+    string = "    A    \n"\
+             "   B B   \n"\
+             "  C   C  \n"\
+             " D     D \n"\
+             "E       E\n"\
+             " D     D \n"\
+             "  C   C  \n"\
+             "   B B   \n"\
+             "    A    \n"
+    assert_equal string, answer
+  end
+end
