83d50f834856e24aabf03e7a9e2d8d49fcacb3b3
Erik Schierboom <erik_schierboom@hotmail.com>
2019-06-22 14:52:35 +0200

    ruby/roman-numerals: add solution

---
+++ ruby/roman-numerals/roman_numerals_test.rb
@@ -0,0 +1,81 @@
+require 'minitest/autorun'
+require_relative 'roman_numerals'
+
+# Common test data version: 1.2.0 3c78ac4
+class RomanNumeralsTest < Minitest::Test
+  def test_1_is_a_single_i
+    assert_equal 'I', 1.to_roman
+  end
+
+  def test_2_is_two_i_s
+    assert_equal 'II', 2.to_roman
+  end
+
+  def test_3_is_three_i_s
+    assert_equal 'III', 3.to_roman
+  end
+
+  def test_4_being_5_1_is_iv
+    assert_equal 'IV', 4.to_roman
+  end
+
+  def test_5_is_a_single_v
+    assert_equal 'V', 5.to_roman
+  end
+
+  def test_6_being_5_1_is_vi
+    assert_equal 'VI', 6.to_roman
+  end
+
+  def test_9_being_10_1_is_ix
+    assert_equal 'IX', 9.to_roman
+  end
+
+  def test_20_is_two_x_s
+    assert_equal 'XXVII', 27.to_roman
+  end
+
+  def test_48_is_not_50_2_but_rather_40_8
+    assert_equal 'XLVIII', 48.to_roman
+  end
+
+  def test_49_is_not_40_5_4_but_rather_50_10_10_1
+    assert_equal 'XLIX', 49.to_roman
+  end
+
+  def test_50_is_a_single_l
+    assert_equal 'LIX', 59.to_roman
+  end
+
+  def test_90_being_100_10_is_xc
+    assert_equal 'XCIII', 93.to_roman
+  end
+
+  def test_100_is_a_single_c
+    assert_equal 'CXLI', 141.to_roman
+  end
+
+  def test_60_being_50_10_is_lx
+    assert_equal 'CLXIII', 163.to_roman
+  end
+
+  def test_400_being_500_100_is_cd
+    assert_equal 'CDII', 402.to_roman
+  end
+
+  def test_500_is_a_single_d
+    assert_equal 'DLXXV', 575.to_roman
+  end
+
+  def test_900_being_1000_100_is_cm
+    assert_equal 'CMXI', 911.to_roman
+  end
+
+  def test_1000_is_a_single_m
+    assert_equal 'MXXIV', 1024.to_roman
+  end
+
+  def test_3000_is_three_m_s
+    assert_equal 'MMM', 3000.to_roman
+  end
+end
