3213ac24be62187c1d3e50f64f5967642ce517b1
Erik Schierboom <erik_schierboom@hotmail.com>
2019-07-05 17:40:06 +0200

    ruby/allergies: add iteration

--- ruby/allergies/allergies.rb
+++ ruby/allergies/allergies.rb
@@ -1,29 +1,33 @@
 class Allergies
   def initialize(coded_allergens)
-    @allergies = ALLERGENS.select { |_, mask| coded_allergens.anybits?(mask) }.keys
+    @coded_allergens = coded_allergens
   end
 
   def allergic_to?(allergen)
-    list.include?(allergen)
+    ALLERGENS[allergen].anybits?(coded_allergens)
   end
 
   def list
-    allergies
+    ALLERGENS.select { |allergen, _| allergic_to?(allergen) }.keys
   end
 
   private
 
-  attr_reader :allergies
+  attr_reader :coded_allergens
+
+  def self.bit(n)
+    1 << (n - 1)
+  end
 
   ALLERGENS = {
-    'eggs'         => 0b00000001,
-    'peanuts'      => 0b00000010,
-    'shellfish'    => 0b00000100,
-    'strawberries' => 0b00001000,
-    'tomatoes'     => 0b00010000,
-    'chocolate'    => 0b00100000,
-    'pollen'       => 0b01000000,
-    'cats'         => 0b10000000,
+      'eggs'         => bit(1),
+      'peanuts'      => bit(2),
+      'shellfish'    => bit(3),
+      'strawberries' => bit(4),
+      'tomatoes'     => bit(5),
+      'chocolate'    => bit(6),
+      'pollen'       => bit(7),
+      'cats'         => bit(8),
   }.freeze
   private_constant :ALLERGENS
 end
