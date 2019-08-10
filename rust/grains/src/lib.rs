1788d565849c176771e2a4b760d5f3e7dfa7ba6a
Erik Schierboom <erik_schierboom@hotmail.com>
2019-08-10 19:28:40 +0200

    rust/grains: add iteration

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,7 +1,5 @@
 pub fn square(s: u32) -> u64 {
-    if s < 1 || s > 64 {
-        panic!("Square must be between 1 and 64");
-    }
+    assert!(s >= 1 && s <= 64, "Square must be between 1 and 64");
 
     2u64.pow(s - 1)
 }
