799c63d89dbf2a8d078a41ba6416975acc9ed945
Erik Schierboom <erik_schierboom@hotmail.com>
2019-08-12 21:16:36 +0200

    rust/grains: add iteration

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn square(s: u32) -> u64 {
     assert!(s >= 1 && s <= 64, "Square must be between 1 and 64");
 
-    2u64.pow(s - 1)
+    1 << s - 1
 }
 
 pub fn total() -> u64 {
-    u64::max_value()
+    (1..=64).map(square).sum()
 }
