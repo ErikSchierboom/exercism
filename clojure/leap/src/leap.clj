1d66e6af7ea8fcd89d68a9b74edd3c54f781fe11
Erik Schierboom <erik_schierboom@hotmail.com>
2019-10-13 14:39:48 +0200

    clojure/leap: add iteration

--- clojure/leap/src/leap.clj
+++ clojure/leap/src/leap.clj
@@ -1,7 +1,9 @@
 (ns leap)
 
 (defn divisible-by? [x y]
-  (= (rem x y) 0))
+  (zero? (rem x y)))
 
 (defn leap-year? [year]
-  (and (divisible-by? year 4) (or (not (divisible-by? year 100)) (divisible-by? year 400))))
+  (and (divisible-by? year 4)
+       (or (not (divisible-by? year 100))
+           (divisible-by? year 400))))
