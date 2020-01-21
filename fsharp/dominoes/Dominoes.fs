d7819255dd701b276fa73701d18bb11c200f8796
Erik Schierboom <erik_schierboom@hotmail.com>
2020-01-21 20:58:41 +0100

    fsharp/dominoes: add iteration

--- fsharp/dominoes/Dominoes.fs
+++ fsharp/dominoes/Dominoes.fs
@@ -2,17 +2,20 @@ module Dominoes
 
 let rotate xs =
     let length = xs |> List.length
-    let perm n = xs |> List.permute (fun index -> (index + n) % length) 
-    [1 .. length] |> List.rev |> List.map perm
+    let perm n = xs |> List.permute (fun index -> (index + n) % length)
+    [ 0 .. length - 1 ] |> List.map perm
 
-let chainPair (x, y) list = 
-    match list with    
-    | (x', y')::xs when y = x' -> (x, y')::xs |> Some
-    | (x', y')::xs when y = y' -> (x, x')::xs |> Some
+let chainPair (x, y) list =
+    match list with
+    | (x', y') :: xs when y = x' -> (x, y') :: xs |> Some
+    | (x', y') :: xs when y = y' -> (x, x') :: xs |> Some
     | _ -> None
-    
-let rec canChain input = 
+
+let rec canChain input =
     match input with
     | [] -> true
-    | (x, y)::[] -> x = y
-    | x::xs -> rotate xs |> List.choose (chainPair x) |> List.exists canChain
+    | (x, y) :: [] -> x = y
+    | x :: xs ->
+        rotate xs
+        |> List.choose (chainPair x)
+        |> List.exists canChain
