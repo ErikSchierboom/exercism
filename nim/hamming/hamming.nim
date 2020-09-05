9f1158ff1df1f0be0c2f98ce65d0f1fd537a9140
Erik Schierboom <erik_schierboom@hotmail.com>
2020-09-05 11:24:23 +0200

    nim/hamming: add iteration

--- nim/hamming/hamming.nim
+++ nim/hamming/hamming.nim
@@ -3,5 +3,5 @@ import sequtils
 proc distance*(strand1: string, strand2: string): int =
     if strand1.len != strand2.len:
         raise newException(ValueError, "Strands have different lengths")
-
-    (strand1.low..strand1.high).countIt(strand1[it] != strand2[it])
+    else:
+        zip(strand1, strand2).countIt(it[0] != it[1])
