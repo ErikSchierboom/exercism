c97cf024cd61692e9bf9046483eb099f720cdc89
Erik Schierboom <erik_schierboom@hotmail.com>
2020-09-05 11:48:30 +0200

    nim/resistor-color: add solution

---
+++ nim/resistor-color/resistor_color.nim
@@ -0,0 +1,10 @@
+import sequtils
+
+type
+  ResistorColor* = enum
+    Black, Brown, Red, Orange, Yellow,
+    Green, Blue, Violet, Grey, White
+
+proc colorCode*(color: ResistorColor): int = color.ord
+
+proc colors*(): seq[ResistorColor] = toSeq(Black .. White)
