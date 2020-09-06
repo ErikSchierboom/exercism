88686222ca07fcce1e6fd9e8575e6da1e30f3972
Erik Schierboom <erik_schierboom@hotmail.com>
2020-09-06 09:41:08 +0200

    nim/reverse-string: add solution

---
+++ nim/reverse-string/reverse_string.nim
@@ -0,0 +1,3 @@
+proc reverse*(str: string): string =
+    for i in countdown(str.high, str.low):
+        result.add(str[i])
---
+++ nim/reverse-string/reverse_string_test.nim
@@ -0,0 +1,23 @@
+import unittest
+import reverse_string
+
+# version 1.2.0
+
+suite "Reverse String":
+  test "an empty string":
+    check reverse("") == ""
+
+  test "a word":
+    check reverse("robot") == "tobor"
+
+  test "a capitalized word":
+    check reverse("Ramen") == "nemaR"
+
+  test "a sentence with punctuation":
+    check reverse("I'm hungry!") == "!yrgnuh m'I"
+
+  test "a palindrome":
+    check reverse("racecar") == "racecar"
+
+  test "an even-sized word":
+    check reverse("drawer") == "reward"
