05585bf6fe7f9f3973c3c19f4a7726fe73fccdfa
Erik Schierboom <erik_schierboom@hotmail.com>
2020-09-06 11:01:34 +0200

    nim/difference-of-squares: add solution

---
+++ nim/difference-of-squares/difference_of_squares.nim
@@ -0,0 +1,11 @@
+import math
+import sequtils
+
+proc squareOfSum*(n: int): int =
+  (1..n).toSeq.sum ^ 2
+
+proc sumOfSquares*(n: int): int =
+  (1..n).toSeq.mapIt(it ^ 2).sum
+
+proc difference*(n: int): int =
+  squareOfSum(n) - sumOfSquares(n)
---
+++ nim/difference-of-squares/difference_of_squares_test.nim
@@ -0,0 +1,32 @@
+import unittest
+import difference_of_squares
+
+# version 1.2.0
+
+suite "Difference of Squares":
+  test "square of sum 1":
+    check squareOfSum(1) == 1
+
+  test "square of sum 5":
+    check squareOfSum(5) == 225
+
+  test "square of sum 100":
+    check squareOfSum(100) == 25_502_500
+
+  test "sum of squares 1":
+    check sumOfSquares(1) == 1
+
+  test "sum of squares 5":
+    check sumOfSquares(5) == 55
+
+  test "sum of squares 100":
+    check sumOfSquares(100) == 338_350
+
+  test "difference of squares 1":
+    check difference(1) == 0
+
+  test "difference of squares 5":
+    check difference(5) == 170
+
+  test "difference of squares 100":
+    check difference(100) == 25_164_150
