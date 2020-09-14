3e442e7e26b1b51979c3ca72d4af34684b48511a
Erik Schierboom <erik_schierboom@hotmail.com>
2020-09-14 14:47:20 +0200

    nim/sum-of-multiples: add solution

---
+++ nim/sum-of-multiples/sum_of_multiples.nim
@@ -0,0 +1,8 @@
+import sequtils
+
+proc sum*(number: int, multiples: seq[int]): int =
+  proc isMultiple(n: int): bool = multiples.anyIt(it != 0 and n mod it == 0)
+
+  for n in 1..<number:
+    if isMultiple(n):
+      result += n
