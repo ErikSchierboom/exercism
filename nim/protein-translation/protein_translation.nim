b85f9cf5583e1e32c1aa1014aebe145e22cdf12d
Erik Schierboom <erik_schierboom@hotmail.com>
2020-09-17 22:04:50 +0200

    nim/protein-translation: add iteration

--- nim/protein-translation/protein_translation.nim
+++ nim/protein-translation/protein_translation.nim
@@ -1,21 +1,33 @@
+import tables
+
+const CodonLen = 3
+
+const CodonToProtein = {
+  "AUG": "Methionine",
+  "UUU": "Phenylalanine",
+  "UUC": "Phenylalanine",
+  "UUA": "Leucine",
+  "UUG": "Leucine",
+  "UCU": "Serine",
+  "UCC": "Serine",
+  "UCA": "Serine",
+  "UCG": "Serine",
+  "UAU": "Tyrosine",
+  "UAC": "Tyrosine",
+  "UGU": "Cysteine",
+  "UGC": "Cysteine",
+  "UGG": "Tryptophan"
+}.toTable
+
+proc toProtein(codon: string): string =
+  CodonToProtein[codon]
+
 proc terminate(codon: string): bool =
   codon in ["UAA", "UAG", "UGA"]
 
-proc toProtein(codon: string): string =
-  case codon
-  of "AUG": "Methionine"
-  of "UUU", "UUC": "Phenylalanine"
-  of "UUA", "UUG": "Leucine"
-  of "UCU", "UCC", "UCA", "UCG": "Serine"
-  of "UAU", "UAC": "Tyrosine"
-  of "UGU", "UGC": "Cysteine"
-  of "UGG": "Tryptophan"
-  of "UAA", "UAG", "UGA": "STOP"
-  else: raise newException(ValueError, "Invalid codon")
-
 iterator codons(rna: string): string =
-  for i in countup(0, rna.len - 1, 3):
-    yield rna[i..i + 2]
+  for i in countup(0, rna.len - CodonLen, CodonLen):
+    yield rna[i ..< i + CodonLen]
 
 proc translate*(rna: string): seq[string] =
   for codon in rna.codons:
