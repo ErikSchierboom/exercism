573d5d281fbaf666e4c29b641f075d5850aa46ee
Erik Schierboom <erik_schierboom@hotmail.com>
2020-09-19 21:17:43 +0200

    nim/pascals-triangle: add solution

---
+++ nim/pascals-triangle/pascals_triangle.nim
@@ -0,0 +1,12 @@
+proc row(n: int): seq[int] =
+  result.add(1)
+
+  var column = 1;
+
+  for j in 1 ..< n:
+    column = column * (n - j) div j
+    result.add(column)
+
+proc pascal*(n: int): seq[seq[int]] =
+  for i in 1..n:
+    result.add(row(i))
