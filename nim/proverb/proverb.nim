f72ae341d61bed339acd14531f852a5915763bc3
Erik Schierboom <erik_schierboom@hotmail.com>
2020-09-21 15:08:29 +0200

    nim/proverb: add iteration

--- nim/proverb/proverb.nim
+++ nim/proverb/proverb.nim
@@ -1,14 +1,8 @@
 import strformat
 
-iterator wordPairs(words: seq[string]): (string, string) =
-  for i in 0 ..< words.len - 1:
-    yield(words[i], words[i + 1])
-
 proc recite*(words: seq[string]): string =
-  if words.len == 0:
-    return
+  for i in 0 ..< words.len - 1:
+    result.add(&"For want of a {words[i]} the {words[i + 1]} was lost.\n")
 
-  for first, second in words.wordPairs:
-    result.add(&"For want of a {first} the {second} was lost.\n")
-
-  result.add(&"And all for the want of a {words[0]}.")
+  if words.len > 0:
+    result.add(&"And all for the want of a {words[0]}.")
