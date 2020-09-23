542a00d7921b0f1ef81cc1d148f39dfd1e7900f5
Erik Schierboom <erik_schierboom@hotmail.com>
2020-09-23 08:58:44 +0200

    nim/all-your-base: add iteration

--- nim/all-your-base/all_your_base.nim
+++ nim/all-your-base/all_your_base.nim
@@ -1,10 +1,7 @@
-import algorithm
+import algorithm, sequtils
 
 proc fromInput(inputDigits: openArray[int], inputBase: int): int =
   for digit in inputDigits:
-    if digit < 0 or digit >= inputBase:
-      raise newException(ValueError, "Invalid input digit")
-
     result = result * inputBase + digit
 
 proc toOutput(value: int, outputBase: int): seq[int] =
@@ -26,7 +23,7 @@ proc toOutput(value: int, outputBase: int): seq[int] =
     result.add(0)
 
 proc convert*(inputDigits: openArray[int], inputBase: int, outputBase: int): seq[int] =
-  if inputBase < 2 or outputBase < 2:
+  if inputBase < 2 or outputBase < 2 or inputDigits.anyIt(it < 0 or it >= inputBase):
     raise newException(ValueError, "Invalid bases")
 
   inputDigits.fromInput(inputBase).toOutput(outputBase)
