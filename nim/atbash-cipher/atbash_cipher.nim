bf5d0783a984cb1d3d8ddad74861912f93ffed65
Erik Schierboom <erik_schierboom@hotmail.com>
2020-09-23 18:20:40 +0200

    nim/atbash-cipher: add solution

---
+++ nim/atbash-cipher/atbash_cipher.nim
@@ -0,0 +1,22 @@
+import algorithm, sequtils, strutils
+
+const groupSize = 5
+const letters = toSeq('a'..'z')
+const digits = toSeq('0'..'9')
+const plain = letters & digits
+const cipher = letters.reversed & digits
+
+proc chunked(phrase: string): string =
+  phrase.reversed.join.insertSep(' ', groupSize).reversed.join
+
+proc encode(letter: char): char =
+  cipher[plain.find(letter)]
+
+proc encode*(phrase: string): string =
+  phrase.toLowerAscii.filter(isAlphaNumeric).map(encode).join.chunked
+
+proc decode(letter: char): char =
+  plain[cipher.find(letter)]
+
+proc decode*(phrase: string): string =
+  phrase.replace(" ").map(decode).join
