c134b9e2d7321fb9a15b3cee2462a485c6e602ae
Erik Schierboom <erik_schierboom@hotmail.com>
2020-09-24 21:29:08 +0200

    nim/sieve: add iteration

--- nim/sieve/sieve.nim
+++ nim/sieve/sieve.nim
@@ -4,9 +4,9 @@ proc primes*(max: int): seq[int] =
   var multiples = initIntSet()
 
   for i in 2 .. max:
-    if multiples.contains(i):
+    if i in multiples:
       continue
-    
+
     result.add(i)
 
     for j in countup(i, max, i):
