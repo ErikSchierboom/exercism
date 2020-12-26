785ce94cdac71b0aefb7918441c2987dfb4befbd
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-26 19:44:15 +0100

    typescript/resistor-color: add solution

---
+++ typescript/resistor-color/resistor-color.ts
@@ -0,0 +1,14 @@
+export const colorCode = (color: string) => COLORS.indexOf(color);
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
