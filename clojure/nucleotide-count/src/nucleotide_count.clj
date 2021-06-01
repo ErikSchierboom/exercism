a8b58a81193063a914252b96a556267fc6900075
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-01 19:17:19 +0200

    clojure/nucleotide-count: add iteration

--- clojure/nucleotide-count/src/nucleotide_count.clj
+++ clojure/nucleotide-count/src/nucleotide_count.clj
@@ -3,9 +3,8 @@
 (def nucleotides [\A \T \C \G])
 
 (defn count-of-nucleotide-in-strand [nucleotide strand]
-  (if (.contains nucleotides nucleotide)
-    (count (filter (partial = nucleotide) strand))
-    (throw (AssertionError. "Invalid nucleotide"))))
+  {:pre [(.contains nucleotides nucleotide)]}
+  (count (filter (partial = nucleotide) strand)))
 
 (defn nucleotide-counts [strand]
   (zipmap nucleotides (map #(count-of-nucleotide-in-strand % strand) nucleotides)))
