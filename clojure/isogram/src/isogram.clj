9f9c295715d777fd119135e10b21d9e034c7f43d
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-07 18:33:12 +0200

    clojure/isogram: add solution

---
+++ clojure/isogram/src/isogram.clj
@@ -0,0 +1,5 @@
+(ns isogram (:require [clojure.string :as str]))
+
+(defn isogram? [sentence]
+  (let [letters (filter #(Character/isLetter %) (str/lower-case sentence))]
+    (= (distinct letters) letters)))
