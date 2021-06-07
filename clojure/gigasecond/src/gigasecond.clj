ee070d4a126811a0344f242ad95f7213a32f5f98
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-07 18:15:44 +0200

    clojure/gigasecond: add solution

---
+++ clojure/gigasecond/src/gigasecond.clj
@@ -0,0 +1,5 @@
+(ns gigasecond (:require [clj-time.core :as t]))
+
+(defn from [year month day]
+  (let [gigasecond-date (t/plus (t/date-time year month day) (t/seconds 1e9))]
+    [(t/year gigasecond-date) (t/month gigasecond-date) (t/day gigasecond-date)]))
