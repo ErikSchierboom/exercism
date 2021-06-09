bd1a7885008539b6675c400517f49bf6f2e42506
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-09 18:14:13 +0200

    clojure/rotational-cipher: add iteration

--- clojure/rotational-cipher/src/rotational_cipher.clj
+++ clojure/rotational-cipher/src/rotational_cipher.clj
@@ -3,11 +3,7 @@
 (defn shift-map [alphabet shift]
   (zipmap alphabet (drop (mod shift (count alphabet)) (cycle alphabet))))
 
-(defn rotate-letter [upper-map lower-map letter]
-  (get (if (Character/isLowerCase letter) lower-map upper-map) letter letter))
-
 (defn rotate [plain-text shift]
-  (let [lower-map (shift-map "abcdefghijklmnopqrstuvwxyz" shift)
-        upper-map (shift-map "ABCDEFGHIJKLMNOPQRSTUVWXYZ" shift)
-        rotate-letter (partial rotate-letter lower-map upper-map)]
-    (apply str (map rotate-letter plain-text))))
+  (let [rotate-letter (merge (shift-map "abcdefghijklmnopqrstuvwxyz" shift)
+                             (shift-map "ABCDEFGHIJKLMNOPQRSTUVWXYZ" shift))]
+    (apply str (map #(get rotate-letter % %) plain-text))))
