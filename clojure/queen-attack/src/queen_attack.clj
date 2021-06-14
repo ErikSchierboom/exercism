1d4c2f45e95e7c85366276a679d82251b04ac004
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-14 15:10:28 +0200

    clojure/queen-attack: add solution

---
+++ clojure/queen-attack/src/queen_attack.clj
@@ -0,0 +1,24 @@
+(ns queen-attack (:require [clojure.string :as str]))
+
+(def rows (range 0 8))
+(def cols (range 0 8))
+
+(defn- board-cell [queens row col]
+  (condp = [row col]
+    (:w queens) "W"
+    (:b queens) "B"
+    "_"))
+
+(defn- board-row [queens row]
+  (str (str/join " " (map #(board-cell queens row %) cols)) "\n"))
+
+(defn board-string [queens]
+  (apply str (map (partial board-row queens) rows)))
+
+(defn can-attack [queens]
+  (let [[white-row white-col] (:w queens)
+        [black-row black-col] (:b queens)]
+    (or
+      (= white-row black-row)
+      (= white-col black-col)
+      (= (Math/abs^int (- white-row black-row)) (Math/abs^int (- white-col black-col))))))
