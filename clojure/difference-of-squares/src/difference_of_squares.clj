47abf148f11dbb64779a5e681827c86779295b7d
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-20 10:49:37 +0200

    clojure/difference-of-squares: add solution

---
+++ clojure/difference-of-squares/src/difference_of_squares.clj
@@ -0,0 +1,13 @@
+(ns difference-of-squares)
+
+(defn- square [n] (* n n))
+
+(defn- natural-numbers [n] (range 1 (inc n)))
+
+(defn sum-of-squares [n] (reduce + (map square (natural-numbers n))))
+
+(defn square-of-sum [n] (square (reduce + (natural-numbers n))))
+
+(defn difference [n] (- (square-of-sum n) (sum-of-squares n)))
+
+
