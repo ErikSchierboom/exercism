a199fb5a898dc5501add647443c5a86d5b2c2da8
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-10 11:26:27 +0200

    clojure/dominoes: add iteration

--- clojure/dominoes/src/dominoes.clj
+++ clojure/dominoes/src/dominoes.clj
@@ -4,14 +4,13 @@
   (lazy-cat (drop n s) (take n s)))
 
 (defn- rotations [s]
-  (for [n (range 0 (count s))]
-    (rotate n s)))
+  (map #(rotate % s) (range (count s))))
 
 (defn- chain-pair [[left right] [[head-left head-right] & rest-stones]]
-  (cond
-    (= right head-left) (cons [left head-right] rest-stones)
-    (= right head-right) (cons [left head-left] rest-stones)
-    :else nil))
+  (condp = right
+    head-left (cons [left head-right] rest-stones)
+    head-right (cons [left head-left] rest-stones)
+    nil))
 
 (defn- possible-chains [stone rest-stones]
   (keep #(chain-pair stone %) (rotations rest-stones)))
