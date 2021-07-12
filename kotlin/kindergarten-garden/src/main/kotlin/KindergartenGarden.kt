464fe24c50b9db9e38b841167f8da82642f82fb0
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-12 17:37:56 +0200

    kotlin/kindergarten-garden: add solution

---
+++ kotlin/kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
@@ -0,0 +1,12 @@
+val PLANTS = arrayOf("violets", "clover", "radishes", "grass")
+val STUDENTS = arrayOf("Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry")
+
+class KindergartenGarden(diagram: String) {
+    val plantsForStudent = diagram.lines()
+        .map { line -> line.map { code -> PLANTS.first { it[0].toUpperCase() == code } } }
+        .let { it[0].chunked(2).zip(it[1].chunked(2)) { top, bottom -> top + bottom } }
+        .mapIndexed { i, plants -> STUDENTS[i] to plants}
+        .toMap()
+
+    fun getPlantsOfStudent(student: String) = plantsForStudent[student].orEmpty()
+}
