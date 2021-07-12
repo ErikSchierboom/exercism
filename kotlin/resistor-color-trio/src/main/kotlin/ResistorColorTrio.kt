55f700b4aa6913dce7541dadbd37df6e87a3e16f
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-12 12:45:13 +0200

    kotlin/resistor-color-trio: add iteration

--- kotlin/resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
+++ kotlin/resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
@@ -2,11 +2,13 @@ import kotlin.math.pow
 
 object ResistorColorTrio {
     fun text(vararg input: Color): String {
-        val ohms = (10 * input[0].ordinal + input[1].ordinal) * (10.0.pow(input[2].ordinal).toInt())
-        return when {
-            ohms >= 1_000_000 -> "${ohms / 1_000_000} megaohms"
-            ohms >= 1_000 -> "${ohms / 1_000} kiloohms"
-            else -> "${ohms} ohms"
-        }
+        val base = (10 * input[0].ordinal + input[1].ordinal)
+        val exponent = 10.0.pow(input[2].ordinal).toInt()
+        val ohms = base * exponent
+
+        val unit = Unit.values().first { exponent < 1000.0.pow(it.ordinal + 1) }
+        val unitModifier = Math.max(1000.0.pow(unit.ordinal - 1).toInt(), 1)
+
+        return "${ohms / unitModifier} ${unit.toString().toLowerCase()}"
     }
 }
