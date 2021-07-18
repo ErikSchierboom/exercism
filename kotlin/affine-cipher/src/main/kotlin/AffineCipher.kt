0fac6aefc681107a6f8f4938034c8fd3bc6f08d9
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-18 16:08:22 +0200

    kotlin/affine-cipher: add solution

---
+++ kotlin/affine-cipher/src/main/kotlin/AffineCipher.kt
@@ -0,0 +1,35 @@
+object AffineCipher {
+    private const val ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+
+    fun encode(input: String, a: Int, b: Int): String {
+        require(a.coprime(ALPHABET.length)) { "a and m must be coprime." }
+
+        return input
+            .transform { a * it + b }
+            .chunked(5)
+            .joinToString(" ")
+    }
+
+    fun decode(input: String, a: Int, b: Int): String {
+        require(a.coprime(ALPHABET.length)) { "a and m must be coprime." }
+
+        val mmi = a.modularInverse(ALPHABET.length)
+        return input.transform { mmi * (it - b) }
+    }
+
+    fun String.transform(map: (Int) -> Int) =
+        filter(Char::isLetterOrDigit)
+            .toLowerCase()
+            .map {
+                val index = ALPHABET.indexOf(it)
+                if (index == -1) it else ALPHABET[map(index).modulo(ALPHABET.length)]
+            }
+            .joinToString("")
+
+}
+
+private fun Int.modulo(m: Int) = (this % m + m) % m
+
+private fun Int.modularInverse(b: Int) = toBigInteger().modInverse(b.toBigInteger()).toInt()
+
+private fun Int.coprime(b: Int) = toBigInteger().gcd(b.toBigInteger()).toInt() == 1
