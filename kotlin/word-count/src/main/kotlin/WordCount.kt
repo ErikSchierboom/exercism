b9f54dfe41dbde596b7ccea7526c37a780171c41
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-19 10:00:33 +0200

    kotlin/word-count: add iteration

--- kotlin/word-count/src/main/kotlin/WordCount.kt
+++ kotlin/word-count/src/main/kotlin/WordCount.kt
@@ -1,12 +1,5 @@
 object WordCount {
-    fun phrase(input: String): Map<String, Int> {
-        return input.toLowerCase()
-                .words()
-                .groupingBy { it }
-                .eachCount()
-    }
+    fun phrase(input: String) = input.toLowerCase().words().groupingBy { it }.eachCount()
 
-    private fun String.words() = Regex("""[\w']+""").findAll(this).map { it.value.unquote() }
-
-    private fun String.unquote() = this.trim('\'')
+    private fun String.words() = Regex("""\w+('?\w)*""").findAll(this).map { it.value }
 }
