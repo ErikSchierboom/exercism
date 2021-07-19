a174de7bf8f71344b87b7b171db43129953d2482
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-19 16:54:22 +0200

    kotlin/largest-series-product: add iteration

---
+++ kotlin/largest-series-product/src/main/kotlin/LargestSeriesProduct.kt
@@ -0,0 +1,21 @@
+class Series(private val series: String) {
+
+    init {
+        require(series.all(Char::isDigit))
+    }
+
+    fun getLargestProduct(span: Int): Long {
+        require(span >= 0)
+        require(span <= series.length)
+
+        return series
+            .map { Character.getNumericValue(it).toLong() }
+            .slices(span)
+            .map { it.product() }
+            .max() ?: 1
+    }
+
+    private fun List<Long>.slices(span: Int) = 0.rangeTo(size - span).map { slice(it until it + span) }
+
+    private fun List<Long>.product() = this.fold(1L, Long::times)
+}
--- kotlin/largest-series-product/src/test/kotlin/SeriesTest.kt
+++ kotlin/largest-series-product/src/test/kotlin/SeriesTest.kt
@@ -5,78 +5,62 @@ import kotlin.test.assertEquals
 class SeriesTest {
 
     @Test
-    fun findsTheLargestProductIfSpanEqualsLength() {
-        assertEquals(18, Series("29").getLargestProduct(2))
+    fun `corner | span equals length`() = assertLSPEquals("29", 2, 18)
+
+    @Test
+    fun `numbers in order | span 2`() = assertLSPEquals("0123456789", 2, 72)
+
+    @Test
+    fun `span 2`() = assertLSPEquals("576802143", 2, 48)
+
+    @Test
+    fun `numbers in order | span 3`() = assertLSPEquals("0123456789", 3, 504)
+
+    @Test
+    fun `span 3`() = assertLSPEquals("1027839564", 3, 270)
+
+    @Test
+    fun `numbers in order | span 5`() = assertLSPEquals("0123456789", 5, 15120)
+
+    @Test
+    fun `corner | long source sequence`() = assertLSPEquals("73167176531330624919225119674426574742355349194934", 6, 23520)
+
+    @Test
+    fun `zeros | only`() = assertLSPEquals("0000", 2, 0)
+
+    @Test
+    fun `zeros | dense`() = assertLSPEquals("99099", 3, 0)
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `reject | span longer than string length`() {
+        lsp("123", 4)
     }
 
     @Test
-    fun findsTheLargestProductOf2WithNumbersInOrder() {
-        assertEquals(72, Series("0123456789").getLargestProduct(2))
-    }
+    fun `corner | zero span on empty sequence`() = assertLSPEquals("", 0, 1)
 
     @Test
-    fun findsTheLargestProductOf2() {
-        assertEquals(48, Series("576802143").getLargestProduct(2))
-    }
+    fun `corner | zero span on not empty sequence`() = assertLSPEquals("123", 0, 1)
 
-    @Test
-    fun findsTheLargestProductOf3WithNumbersInOrder() {
-        assertEquals(504, Series("0123456789").getLargestProduct(3))
-    }
-
-    @Test
-    fun findsTheLargestProductOf3() {
-        assertEquals(270, Series("1027839564").getLargestProduct(3))
-    }
-
-    @Test
-    fun findsTheLargestProductOf5WithNumbersInOrder() {
-        assertEquals(15120, Series("0123456789").getLargestProduct(5))
-    }
-
-    @Test
-    fun findsTheLargestProductWithinABigNumber() {
-        assertEquals(23520, Series("73167176531330624919225119674426574742355349194934").getLargestProduct(6))
-    }
-
-    @Test
-    fun reports0IfAllDigitsAre0() {
-        assertEquals(0, Series("0000").getLargestProduct(2))
-    }
-
-    @Test
-    fun reports0IfAllSpansInclude0() {
-        assertEquals(0, Series("99099").getLargestProduct(3))
+    @Test(expected = IllegalArgumentException::class)
+    fun `reject | empty string and nonzero span`() {
+        lsp("", 1)
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun rejectsSpanLongerThanStringLength() {
-        Series("123").getLargestProduct(4)
-    }
-
-    @Test
-    fun reports1ForEmptyStringAndEmptyProduct() {
-        assertEquals(1, Series("").getLargestProduct(0))
-    }
-
-    @Test
-    fun reports1ForNonEmptyStringAndEmptyProduct() {
-        assertEquals(1, Series("123").getLargestProduct(0))
-    }
-
-    @Test(expected = IllegalArgumentException::class)
-    fun rejectsEmptyStringAndNonZeroSpan() {
-        Series("").getLargestProduct(1)
-    }
-
-    @Test(expected = IllegalArgumentException::class)
-    fun rejectsInvalidCharacterInDigits() {
+    fun `reject | nondigits in source sequence`() {
         Series("1234a5")
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun rejectsNegativeSpan() {
-        Series("12345").getLargestProduct(-1)
+    fun `reject | negative span`() {
+        lsp("12345", -1)
     }
-
 }
+
+private fun assertLSPEquals(input: String, span: Int, lsp: Long) {
+    assertEquals(lsp(input, span), lsp)
+}
+
+private fun lsp(input: String, span: Int) = Series(input).getLargestProduct(span)
+
