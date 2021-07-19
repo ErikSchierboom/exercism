e28d311d1b3d045f18a44ddb87e65f5319632393
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-19 16:11:14 +0200

    kotlin/perfect-numbers: add iteration

--- kotlin/perfect-numbers/src/main/kotlin/NaturalNumber.kt
+++ kotlin/perfect-numbers/src/main/kotlin/NaturalNumber.kt
@@ -2,16 +2,16 @@ enum class Classification {
     DEFICIENT, PERFECT, ABUNDANT
 }
 
-private fun sumOfFactors(naturalNumber: Int) = (1 until naturalNumber).filter { naturalNumber % it == 0 }.sum()
-
 fun classify(naturalNumber: Int): Classification {
     require(naturalNumber > 0)
 
-    val sumOfFactorsForInput = sumOfFactors(naturalNumber)
-
-    return when {
-        sumOfFactorsForInput < naturalNumber -> Classification.DEFICIENT
-        sumOfFactorsForInput == naturalNumber -> Classification.PERFECT
-        else -> Classification.ABUNDANT
+    return naturalNumber.sumOfFactors().let {
+        when {
+            it < naturalNumber -> Classification.DEFICIENT
+            it == naturalNumber -> Classification.PERFECT
+            else -> Classification.ABUNDANT
+        }
     }
 }
+
+fun Int.sumOfFactors() = 1.until(this).filter { this % it == 0 }.sum()
