7ca8488bf531690703a2865914833dff16193498
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-19 10:14:55 +0200

    kotlin/space-age: add iteration

--- kotlin/space-age/src/main/kotlin/SpaceAge.kt
+++ kotlin/space-age/src/main/kotlin/SpaceAge.kt
@@ -1,18 +1,17 @@
-class SpaceAge(val seconds: Long) {
+import kotlin.math.roundToInt
 
-    val secondsOnEarth = 31557600L
+private const val SECONDS_ON_EARTH = 31557600L
 
-    fun yearsUsingPeriod(period: Double): Double {
-        val years = (seconds / period) / secondsOnEarth
-        return Math.round(years * 100.0) / 100.0
-    }
+class SpaceAge(private val seconds: Long) {
+    fun onMercury(): Double = fromEarthYears(0.2408467)
+    fun onVenus(): Double = fromEarthYears(0.61519726)
+    fun onEarth(): Double = fromEarthYears(1.0)
+    fun onMars(): Double = fromEarthYears(1.8808158)
+    fun onJupiter(): Double = fromEarthYears(11.862615)
+    fun onSaturn(): Double = fromEarthYears(29.447498)
+    fun onUranus(): Double = fromEarthYears(84.016846)
+    fun onNeptune(): Double = fromEarthYears(164.79132)
 
-    fun onMercury(): Double = yearsUsingPeriod(0.2408467)
-    fun onVenus(): Double = yearsUsingPeriod(0.61519726)
-    fun onEarth(): Double = yearsUsingPeriod(1.0)
-    fun onMars(): Double = yearsUsingPeriod(1.8808158)
-    fun onJupiter(): Double = yearsUsingPeriod(11.862615)
-    fun onSaturn(): Double = yearsUsingPeriod(29.447498)
-    fun onUranus(): Double = yearsUsingPeriod(84.016846)
-    fun onNeptune(): Double = yearsUsingPeriod(164.79132)
+    private fun fromEarthYears(period: Double) = seconds.div(period).div(SECONDS_ON_EARTH).round()
+    private fun Double.round() = this.times(100.0).roundToInt().div(100.0)
 }
