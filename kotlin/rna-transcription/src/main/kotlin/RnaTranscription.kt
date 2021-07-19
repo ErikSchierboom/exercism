272a92e014b8343ce4d06f8fa492b45e8c333528
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-19 15:53:44 +0200

    kotlin/rna-transcription: add iteration

--- kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,3 +1,3 @@
 val dnaToRna = mapOf('C' to 'G', 'G' to 'C', 'T' to 'A', 'A' to 'U')
 
-fun transcribeToRna(dna: String): String = dna.map { dnaToRna[it] }.joinToString(separator = "")
+fun transcribeToRna(dna: String) = dna.map(dnaToRna::get).joinToString( "")
