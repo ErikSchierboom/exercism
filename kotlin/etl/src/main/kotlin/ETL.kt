e0ca85b45972c4955dc7f88dfeff51bd93eab38c
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-20 11:54:59 +0200

    kotlin/etl: add iteration

--- kotlin/etl/src/main/kotlin/ETL.kt
+++ kotlin/etl/src/main/kotlin/ETL.kt
@@ -1,4 +1,4 @@
 object ETL {
     fun transform(old: Map<Int, List<Char>>) =
-            old.flatMap { it.value.map { str -> str.toLowerCase() to it.key } }.toMap()
+        old.flatMap { it.value.map { str -> str.toLowerCase() to it.key } }.toMap()
 }
