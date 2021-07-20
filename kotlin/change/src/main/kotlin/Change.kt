b563eb9cb27979e82daf0435dc9ee5418e596561
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-20 08:20:55 +0200

    kotlin/change: add iteration

--- kotlin/change/src/main/kotlin/Change.kt
+++ kotlin/change/src/main/kotlin/Change.kt
@@ -1,32 +1,17 @@
-fun <T> List<T>.shift(value: T): List<T> {
-    val list = this.toMutableList()
-    list.add(0, value)
-    return list
-}
+class ChangeCalculator(private val coins: List<Int>) {
+    fun computeMostEfficientChange(amount: Int): List<Int> {
+        require(amount >= 0) { "Negative totals are not allowed." }
 
-class Change(val coins: Set<Int>) {
-    fun findMinimalCoins(amount: Int, map: Map<Int, List<Int>>): List<Int>? =
+        return 1.rangeTo(amount)
+            .fold(mapOf(0 to listOf()), this::calculateMostEfficientChangeForAmount)
+            .getOrElse(amount) { throw IllegalArgumentException("The total $amount cannot be represented in the given currency.\"") }
+    }
+
+    private fun calculateMostEfficientChangeForAmount(map: Map<Int, List<Int>>, amount: Int) =
+        mostEfficientChange(map, amount)?.let { change -> map + (amount to change) } ?: map
+
+    private fun mostEfficientChange(map: Map<Int, List<Int>>, amount: Int) =
         coins.filter { it <= amount }
-             .map { map.getOrElse(amount - it) { emptyList() }.shift(it) }
-             .sortedBy { it.size }
-             .firstOrNull()
-
-    fun minimalCoinsMap(amount: Int): Map<Int, List<Int>> {
-        val minimalCoinsMap = mutableMapOf(0 to emptyList<Int>())
-
-        for (i in 1 .. amount) {
-            val minCoins = findMinimalCoins(i, minimalCoinsMap)
-            if (minCoins != null)
-                minimalCoinsMap.put(i, minCoins)
-        }
-
-        return minimalCoinsMap
-    }
-
-    fun issue(amount: Int): List<Int> {
-        if (amount < 0) throw IllegalArgumentException()
-        if (amount > 0 && amount < coins.min() ?: 0) throw IllegalArgumentException()
-
-        return minimalCoinsMap(amount).getOrElse(amount) { emptyList() }
-    }
+            .mapNotNull { map[amount - it]?.let { change -> listOf(it) + change }  }
+            .minByOrNull { it.size }
 }
