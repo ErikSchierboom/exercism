45fdef9aff19f3a313a75fc4fc86f2f82847bdcf
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-21 19:22:09 +0200

    kotlin/bracket-push: add iteration

--- kotlin/bracket-push/src/main/kotlin/BracketPush.kt
+++ kotlin/bracket-push/src/main/kotlin/BracketPush.kt
@@ -1,28 +1,27 @@
 object BracketPush {
-    private val closingToOpeningBrackets = mapOf(']' to '[', '}' to '{', ')' to '(')
-
-    private fun isOpeningBracket(character: Char) = character in closingToOpeningBrackets.values
-
-    private fun isClosingBracket(character: Char) = character in closingToOpeningBrackets.keys
-
     fun isValid(input: String): Boolean {
-        val unpairedOpeningBrackets = mutableListOf<Char>()
+        val openDelimiters = mutableListOf<Char>()
 
         for (character in input) {
             when {
-                isOpeningBracket(character) -> unpairedOpeningBrackets.add(character)
-                isClosingBracket(character) -> {
-                    val expectedOpeningBracket = closingToOpeningBrackets[character]
-                    val actualOpeningBracket = unpairedOpeningBrackets.lastOrNull()
+                character.isOpenDelimiter() -> openDelimiters.add(character)
+                character.isCloseDelimiter() -> {
+                    val expectedOpenDelimiter = closeToOpenDelimiters[character]
+                    val actualOpenDelimiter = openDelimiters.lastOrNull()
 
-                    if(expectedOpeningBracket == actualOpeningBracket)
-                        unpairedOpeningBrackets.removeAt(unpairedOpeningBrackets.lastIndex)
+                    if (expectedOpenDelimiter == actualOpenDelimiter)
+                        openDelimiters.removeAt(openDelimiters.lastIndex)
                     else
                         return false
                 }
             }
         }
 
-        return unpairedOpeningBrackets.isEmpty()
+        return openDelimiters.isEmpty()
     }
+
+    private val closeToOpenDelimiters = mapOf(']' to '[', '}' to '{', ')' to '(')
+
+    private fun Char.isOpenDelimiter() = this in closeToOpenDelimiters.values
+    private fun Char.isCloseDelimiter() = this in closeToOpenDelimiters.keys
 }
