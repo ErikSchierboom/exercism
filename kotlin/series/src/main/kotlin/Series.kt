378480a7da31385732694caf1e58eb73d530e0a9
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-22 09:03:26 +0200

    kotlin/series: add iteration

--- kotlin/series/src/main/kotlin/Series.kt
+++ kotlin/series/src/main/kotlin/Series.kt
@@ -1,9 +1,7 @@
 object Series {
     fun slices(size: Int, input: String): List<List<Int>> {
-        require(size > 0 && size <= input.length)
+        require(size in 0..input.length)
 
-        return input.map(Character::getNumericValue).let { digits ->
-            0.rangeTo(digits.size - size).map { digits.subList(it, it + size) }
-        }
+        return input.map(Character::getNumericValue).windowed(size)
     }
 }
