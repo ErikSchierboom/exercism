a176be75cdf410034901b4fc8bf47fb4abd1d3e2
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-23 16:45:16 +0200

    kotlin/secret-handshake: add iteration

--- kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,16 +1,12 @@
 object HandshakeCalculator {
-    private const val REVERSE_SIGNALS_MASK = 0b00010000
-    private val signals = Signal.values()
+    private val operations: List<Pair<Int, (List<Signal>) -> List<Signal>>> = listOf(
+        1 shl 0 to { it + Signal.WINK },
+        1 shl 1 to { it + Signal.DOUBLE_BLINK },
+        1 shl 2 to { it + Signal.CLOSE_YOUR_EYES },
+        1 shl 3 to { it + Signal.JUMP },
+        1 shl 4 to { it.reversed() }
+    )
 
-    fun calculateHandshake(encodedHandshake: Int): List<Signal> {
-        val encodedSignals = signals.filter { matchesMask(encodedHandshake, signalMask(it) ) }
-
-        return if (reverseSignals(encodedHandshake)) encodedSignals.reversed() else encodedSignals
-    }
-
-    private fun signalMask(signal: Signal) = 1 shl signals.indexOf(signal)
-
-    private fun reverseSignals(encodedHandshake: Int) = matchesMask(encodedHandshake, REVERSE_SIGNALS_MASK)
-
-    private fun matchesMask(input: Int, mask: Int) = input.and(mask) == mask
+    fun calculateHandshake(code: Int): List<Signal> =
+        operations.fold(emptyList()) { acc, op -> if (op.first.and(code) != 0) op.second(acc) else acc }
 }
