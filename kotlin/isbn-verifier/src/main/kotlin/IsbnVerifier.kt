5f088e911f0ebce7f41e5de67d8ba2b50e79afe4
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-23 19:18:17 +0200

    kotlin/isbn-verifier: add iteration

--- kotlin/isbn-verifier/src/main/kotlin/IsbnVerifier.kt
+++ kotlin/isbn-verifier/src/main/kotlin/IsbnVerifier.kt
@@ -1,17 +1,10 @@
 class IsbnVerifier {
-    fun isValid(isbn: String): Boolean {
-        val normalizedIsbn = isbn.normalize()
-        return isValidNumber(normalizedIsbn) && hasValidChecksum(normalizedIsbn)
-    }
+    fun isValid(isbn: String) = isbn.normalized().let { it.hasValidFormat() && it.hasValidChecksum() }
 
-    private fun String.normalize() = this.replace("-", "")
-
-    private fun isValidNumber(isbn: String) = isbn.matches("""^\d{9}(\d|X)$""".toRegex())
-
-    private fun hasValidChecksum(isbn: String) =
-            isbn.mapIndexed(::checksumDigitValue).sum().rem(11) == 0
-
-    private fun checksumDigitValue(index: Int, char: Char) = char.toDigit() * (10 - index)
+    private fun String.normalized() = replace("-", "")
+    private fun String.hasValidFormat() = matches("""^\d{9}(\d|X)$""".toRegex())
+    private fun String.hasValidChecksum() = checksum().rem(11) == 0
+    private fun String.checksum() = mapIndexed { i, c -> c.toDigit().times(10 - i) }.sum()
 
     private fun Char.toDigit() = if (this == 'X') 10 else Character.getNumericValue(this)
 }
