f1609aa7d9af93fc82ffccbf9a3d0243d6c0f1a7
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-23 19:06:57 +0200

    kotlin/sum-of-multiples: add iteration

--- kotlin/sum-of-multiples/src/main/kotlin/SumOfMultiples.kt
+++ kotlin/sum-of-multiples/src/main/kotlin/SumOfMultiples.kt
@@ -1,3 +1,4 @@
 object SumOfMultiples {
-    fun sum(numbers: Set<Int>, max: Int) = (1 until max).filter { i -> numbers.any { it != 0 && i % it == 0 } }.sum()
+    fun sum(numbers: Set<Int>, max: Int) =
+        1.until(max).filter { i -> numbers.any { it != 0 && i % it == 0 } }.sum()
 }
