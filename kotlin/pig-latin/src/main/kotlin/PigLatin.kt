7dc6097d53ce7438cf9af411901535494a25dcd4
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-23 19:23:22 +0200

    kotlin/pig-latin: add iteration

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,11 +1,11 @@
 object PigLatin {
-    val vowelRegex = Regex("(?<begin>^|\\s+)(?<vowel>a|e|i|o|u|yt|xr)(?<rest>\\w+)")
-    val consonantRegex = Regex("(?<begin>^|\\s+)(?<consonant>ch|qu|thr|th|sch|yt|\\wqu|\\w)(?<rest>\\w+)")
-
-    val vowelReplacement = "\${begin}\${vowel}\${rest}ay"
-    val consonantReplacement = "\${begin}\${rest}\${consonant}ay"
-
-    fun translate(input: String): String =
+    fun translate(input: String) =
         if (vowelRegex.matches(input)) vowelRegex.replace(input, vowelReplacement)
         else consonantRegex.replace(input, consonantReplacement)
+
+    private val vowelRegex = Regex("""(?<begin>^|\s+)(?<vowel>a|e|i|o|u|yt|xr)(?<rest>\w+)""")
+    private val consonantRegex = Regex("""(?<begin>^|\s+)(?<consonant>ch|qu|thr|th|sch|yt|\wqu|\w)(?<rest>\w+)""")
+
+    private const val vowelReplacement = "\${begin}\${vowel}\${rest}ay"
+    private const val consonantReplacement = "\${begin}\${rest}\${consonant}ay"
 }
