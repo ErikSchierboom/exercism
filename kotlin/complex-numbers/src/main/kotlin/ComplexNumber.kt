51ac2be79c39a3a403d8a4e70f7b0fcaeb854303
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-23 08:18:00 +0200

    kotlin/complex-numbers: add iteration

--- kotlin/complex-numbers/src/main/kotlin/ComplexNumber.kt
+++ kotlin/complex-numbers/src/main/kotlin/ComplexNumber.kt
@@ -1,23 +1,30 @@
-fun exponential(complexNumber: ComplexNumber) =
-    ComplexNumber(Math.exp(complexNumber.real) * Math.cos(complexNumber.imaginary), Math.exp(complexNumber.real) * Math.sin(complexNumber.imaginary))
+import kotlin.math.cos
+import kotlin.math.exp
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 data class ComplexNumber(val real: Double = 0.0, val imaginary: Double = 0.0)
 {
-    val abs = Math.sqrt(real * real + imaginary * imaginary)
+    val abs = sqrt(real * real + imaginary * imaginary)
 
-    operator fun times(other: ComplexNumber) = ComplexNumber(real * other.real - imaginary * other.imaginary, imaginary * other.real + real * other.imaginary)
+    operator fun plus(other: ComplexNumber) =
+        ComplexNumber(real + other.real, imaginary + other.imaginary)
 
-    operator fun plus(other: ComplexNumber) = ComplexNumber(real + other.real, imaginary + other.imaginary)
+    operator fun minus(other: ComplexNumber) =
+        ComplexNumber(real - other.real, imaginary - other.imaginary)
 
-    operator fun minus(other: ComplexNumber) = ComplexNumber(real - other.real, imaginary - other.imaginary)
+    operator fun times(other: ComplexNumber) =
+        ComplexNumber(real * other.real - imaginary * other.imaginary, imaginary * other.real + real * other.imaginary)
 
     operator fun div(other: ComplexNumber): ComplexNumber  {
-        val denominator = other.real * other.real + other.imaginary * other.imaginary
-        val divReal = (real * other.real + imaginary * other.imaginary) / denominator
-        val divImaginary = (imaginary * other.real - real * real * other.imaginary) / denominator
+        val numerator = times(other.conjugate())
+        val denominator = other.times(other.conjugate())
 
-        return ComplexNumber(divReal, divImaginary)
+        return ComplexNumber(numerator.real / denominator.real, numerator.imaginary / denominator.real)
     }
 
     fun conjugate() = ComplexNumber(real, -imaginary)
 }
+
+fun exponential(complexNumber: ComplexNumber) =
+    ComplexNumber(exp(complexNumber.real) * cos(complexNumber.imaginary), exp(complexNumber.real) * sin(complexNumber.imaginary))
