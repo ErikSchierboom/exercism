2615c6c395fa8979b2fcdaa95a9ab3fea43db640
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-23 18:45:26 +0200

    kotlin/scale-generator: add iteration

--- kotlin/scale-generator/src/main/kotlin/Scale.kt
+++ kotlin/scale-generator/src/main/kotlin/Scale.kt
@@ -1,19 +1,16 @@
 class Scale(private val tonic: String) {
-    fun chromatic(): List<String> {
-        val scale = if ("#" in tonic) chromaticScale else flatChromaticScale
-        return scale.shift(scale.indexOfFirst { it.equals(tonic, ignoreCase = true) })
-    }
+    fun chromatic() = scale.shift(scale.indexOfFirst { it.equals(tonic, ignoreCase = true) })
 
-    fun interval(intervals: String): List<String> {
-        val scale = chromatic()
-        val indices = intervals.scan(0) { acc, interval -> acc + intervalOffsets[interval]!!}
-        return indices.mapNotNull { scale.getOrNull(it) }
+    fun interval(intervals: String) = chromatic().let { scale ->
+        intervals.scan(0) { acc, interval -> acc + intervalOffsets[interval]!! }
+            .mapNotNull { scale.getOrNull(it) }
     }
-
-    private fun <T> List<T>.shift(n: Int) = drop(n) + take(n)
 
     private val chromaticScale = listOf("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
     private val flatChromaticScale = listOf("C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B")
+    private val scale = if ("#" in tonic) chromaticScale else flatChromaticScale
 
     private val intervalOffsets = mapOf('m' to 1, 'M' to 2, 'A' to 3)
+
+    private fun <T> List<T>.shift(n: Int) = drop(n) + take(n)
 }
