5fe3be52b1d17357cccd209d2a419b1467c77e6a
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-23 07:54:40 +0200

    kotlin/crypto-square: add iteration

--- kotlin/crypto-square/src/main/kotlin/CryptoSquare.kt
+++ kotlin/crypto-square/src/main/kotlin/CryptoSquare.kt
@@ -1,9 +1,13 @@
+import java.util.*
+import kotlin.math.ceil
+import kotlin.math.sqrt
+
 object CryptoSquare {
     fun ciphertext(plaintext: String) =
         if (plaintext.isEmpty()) "" else plaintext.normalize().rows().columns().joinToString(" ")
 
-    private fun String.normalize() = filter { it.isLetterOrDigit() }.toLowerCase()
-    private fun String.size() = Math.ceil(Math.sqrt(length.toDouble())).toInt()
-    private fun String.rows(): List<String> = size().let { size -> chunked(size).map { it.padEnd(size) } }
-    private fun List<String>.columns(): List<String> = this[0].indices.map { i -> this.map { it[i] }.joinToString("") }
+    private fun String.normalize() = filter(Char::isLetterOrDigit).lowercase(Locale.getDefault())
+    private fun String.size() = ceil(sqrt(length.toDouble())).toInt()
+    private fun String.rows() = size().let { size -> chunked(size).map { it.padEnd(size) } }
+    private fun List<String>.columns() = this[0].indices.map { i -> this.map { it[i] }.joinToString("") }
 }
