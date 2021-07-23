8074989935dbc254a988b06fc287fb39a291d18f
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-23 16:26:32 +0200

    kotlin/nth-prime: add iteration

--- kotlin/nth-prime/src/main/kotlin/Prime.kt
+++ kotlin/nth-prime/src/main/kotlin/Prime.kt
@@ -1,17 +1,19 @@
+import kotlin.math.floor
+import kotlin.math.sqrt
+
 object Prime {
     fun nth(n: Int): Int {
-        if (n < 1)
-            throw IllegalArgumentException()
+        require(n > 0) { "There is no zeroth prime." }
 
         return primes().elementAt(n - 1)
     }
 
-    fun isPrime(n: Int): Boolean {
-        val r = Math.floor(Math.sqrt(n.toDouble())).toInt()
+    private fun primes() = sequenceOf(2, 3).plus(possiblePrimes().filter { isPrime(it) })
+
+    private fun possiblePrimes() = generateSequence(1) { it + 1 }.flatMap { sequenceOf(it * 6 - 1, it * 6 + 1) }
+
+    private fun isPrime(n: Int): Boolean {
+        val r = floor(sqrt(n.toDouble())).toInt()
         return r < 5 || (0 .. r - 4).all { n % (5 + it) != 0 }
     }
-
-    fun possiblePrimes(): Sequence<Int> = generateSequence(1) { it + 1 }.flatMap { sequenceOf(it * 6 - 1, it * 6 + 1) }
-
-    fun primes(): Sequence<Int> = sequenceOf(2, 3).plus(possiblePrimes().filter { isPrime(it) })
 }
