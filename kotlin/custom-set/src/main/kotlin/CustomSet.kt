cf1e0cc4468262785e3f44c8a766602ffdff0423
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-02 18:02:55 +0200

    kotlin/custom-set: add iteration

--- kotlin/custom-set/src/main/kotlin/CustomSet.kt
+++ kotlin/custom-set/src/main/kotlin/CustomSet.kt
@@ -1,34 +1,32 @@
 class CustomSet(vararg elements: Int) {
-    val hashToElement = elements.associateBy { it.hashCode() }.toMutableMap()
+    private val hashToElement = elements.associateBy { it.hashCode() }.toMutableMap()
 
     fun isEmpty(): Boolean = hashToElement.isEmpty()
 
-    fun isSubset(other: CustomSet): Boolean = hashToElement.keys.all { it in other.hashToElement }
+    fun isSubset(other: CustomSet) = hashToElement.keys.all { it in other.hashToElement }
 
-    fun isDisjoint(other: CustomSet): Boolean = hashToElement.keys.all { it !in other.hashToElement }
+    fun isDisjoint(other: CustomSet) = hashToElement.keys.all { it !in other.hashToElement }
 
-    fun contains(other: Int): Boolean = other.hashCode() in hashToElement
+    fun contains(other: Int) = other.hashCode() in hashToElement
 
     fun add(other: Int) {
         hashToElement.putIfAbsent(other.hashCode(), other)
     }
 
-    override fun equals(other: Any?): Boolean {
-        return when(other) {
-            is CustomSet -> other.isSubset(this) && this.isSubset(other)
-            else -> false
-        }
+    override fun equals(other: Any?) = when(other) {
+        is CustomSet -> other.isSubset(this) && this.isSubset(other)
+        else -> false
     }
 
-    fun intersection(other: CustomSet): CustomSet = CustomSet().also { newSet ->
+    fun intersection(other: CustomSet) = CustomSet().also { newSet ->
         hashToElement.filter { it.key in other.hashToElement }.values.forEach { newSet.add(it) } }
 
-    operator fun plus(other: CustomSet): CustomSet = CustomSet().also { newSet ->
+    operator fun plus(other: CustomSet) = CustomSet().also { newSet ->
         hashToElement.values.forEach { newSet.add(it) }
         other.hashToElement.values.forEach { newSet.add(it) }
     }
 
-    operator fun minus(other: CustomSet): CustomSet = CustomSet().also { newSet ->
+    operator fun minus(other: CustomSet) = CustomSet().also { newSet ->
         hashToElement.filter { it.key !in other.hashToElement }.values.forEach { newSet.add(it) }
     }
 }
