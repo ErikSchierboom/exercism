dc85811d268e90ac82ade63bfac291c5841a1689
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-07 15:41:23 +0200

    fsharp/secret-handshake: add iteration

--- fsharp/secret-handshake/SecretHandshake.fs
+++ fsharp/secret-handshake/SecretHandshake.fs
@@ -1,14 +1,14 @@
 ﻿module SecretHandshake
 
-let commandsMapping = 
-    [(1 <<< 0, (fun acc -> acc @ ["wink"]));
-     (1 <<< 1, (fun acc -> acc @ ["double blink"]));
-     (1 <<< 2, (fun acc -> acc @ ["close your eyes"]));
-     (1 <<< 3, (fun acc -> acc @ ["jump"]))
-     (1 <<< 4, (fun acc -> acc |> List.rev))]
+let private operations =
+    [fun acc -> acc @ ["wink"]
+     fun acc -> acc @ ["double blink"]
+     fun acc -> acc @ ["close your eyes"]
+     fun acc -> acc @ ["jump"]
+     List.rev]
 
-let applyCommand number acc (mask, apply) = 
-    if number &&& mask <> 0 then apply acc
-    else acc
-
-let commands number = commandsMapping |> List.fold (applyCommand number) []
+let commands number =
+  operations
+  |> List.indexed
+  |> List.fold (fun acc (shiftLeft, operation) -> 
+      if number &&& (1 <<< shiftLeft) <> 0 then operation acc else acc) []
