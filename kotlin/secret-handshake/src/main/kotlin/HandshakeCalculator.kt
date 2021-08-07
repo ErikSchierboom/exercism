de2703070c524f499e461187d35ca0a1a8cc246b
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-07 13:51:35 +0200

    kotlin/secret-handshake: add iteration

--- kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,16 +1,14 @@
-data class Operation(val mask: Int, val map: (List<Signal>) -> List<Signal>)
-
 object HandshakeCalculator {
-    private val operations = listOf(
-        Operation(1 shl 0) { it + Signal.WINK },
-        Operation(1 shl 1) { it + Signal.DOUBLE_BLINK },
-        Operation(1 shl 2) { it + Signal.CLOSE_YOUR_EYES },
-        Operation(1 shl 3) { it + Signal.JUMP },
-        Operation(1 shl 4) { it.reversed() }
+    private val operations = listOf<(List<Signal>) -> List<Signal>>(
+        { it + Signal.WINK },
+        { it + Signal.DOUBLE_BLINK },
+        { it + Signal.CLOSE_YOUR_EYES },
+        { it + Signal.JUMP },
+        { it.reversed() }
     )
 
     fun calculateHandshake(code: Int) =
-        operations.fold(emptyList<Signal>()) { handshake, operation ->
-            if (operation.mask.and(code) != 0) operation.map(handshake) else handshake
+        operations.foldIndexed(emptyList<Signal>()) { leftShift, handshake, operation ->
+            if (1.shl(leftShift).and(code) == 0) handshake else operation(handshake)
         }
 }
