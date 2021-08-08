3c40e1ce81f50bf17f4656b41cb7e9274a3b3bf5
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-08 18:24:59 +0200

    csharp/diffie-hellman: add iteration

--- csharp/diffie-hellman/DiffieHellman.cs
+++ csharp/diffie-hellman/DiffieHellman.cs
@@ -3,11 +3,13 @@ using System.Numerics;
 
 public static class DiffieHellman
 {
-    private static readonly Random Random = new Random();
+    private static readonly Random Random = new();
 
-    public static BigInteger PrivateKey(BigInteger primeP) => new BigInteger(Random.Next(1, (int) primeP - 1));
+    public static BigInteger PrivateKey(BigInteger p) => new(Random.Next(1, (int) p - 1));
 
-    public static BigInteger PublicKey(BigInteger primeP, BigInteger primeG, BigInteger privateKey) => BigInteger.ModPow(primeG, privateKey, primeP);
+    public static BigInteger PublicKey(BigInteger p, BigInteger g, BigInteger privateKey) =>
+        BigInteger.ModPow(g, privateKey, p);
 
-    public static BigInteger Secret(BigInteger primeP, BigInteger publicKey, BigInteger privateKey) => BigInteger.ModPow(publicKey, privateKey, primeP);
+    public static BigInteger Secret(BigInteger p, BigInteger publicKey, BigInteger privateKey) =>
+        BigInteger.ModPow(publicKey, privateKey, p);
 }
