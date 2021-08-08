ca13fcef669067226dabcc730bfd06c5d525292c
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-08 18:20:03 +0200

    csharp/difference-of-squares: add iteration

--- csharp/difference-of-squares/DifferenceOfSquares.cs
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -1,12 +1,15 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 public static class DifferenceOfSquares
 {
-    public static int CalculateSquareOfSum(int max) => Square(Enumerable.Range(1, max).Sum());
+    public static int CalculateSquareOfSum(int max) => Square(NaturalNumbers(max).Sum());
 
-    public static int CalculateSumOfSquares(int max) => Enumerable.Range(1, max).Select(Square).Sum();
+    public static int CalculateSumOfSquares(int max) => NaturalNumbers(max).Sum(Square);
 
     public static int CalculateDifferenceOfSquares(int max) => CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
 
+    private static IEnumerable<int> NaturalNumbers(int max) => Enumerable.Range(1, max);
+    
     private static int Square(int number) => number * number;
 }
