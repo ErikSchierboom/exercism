c9555ef2051d6c4107e568829cd60a8512817078
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-08 17:16:06 +0200

    csharp/custom-set: add iteration

--- csharp/custom-set/CustomSet.cs
+++ csharp/custom-set/CustomSet.cs
@@ -3,60 +3,41 @@ using System.Linq;
 
 public class CustomSet
 {
-    private readonly SortedDictionary<int, int> items = new SortedDictionary<int, int>();
+    private readonly SortedDictionary<int, int> _items = new();
 
     public CustomSet(params int[] values)
     {
-        foreach (var value in values.Where(value => !items.ContainsKey(value.GetHashCode())))
-        {
-            items.Add(value.GetHashCode(), value);
-        }
+        foreach (var value in values.Where(value => !_items.ContainsKey(value.GetHashCode())))
+            _items.Add(value.GetHashCode(), value);
     }
 
-    public CustomSet Add(int value)
-    {
-        return new CustomSet(items.Values.Append(value).ToArray());
-    }
+    public CustomSet Add(int value) => new(_items.Values.Append(value).ToArray());
 
-    public bool Empty() => items.Count == 0;
+    public bool Empty() => _items.Count == 0;
 
-    public bool Contains(int value) => items.ContainsKey(value.GetHashCode());
+    public bool Contains(int value) => _items.ContainsKey(value.GetHashCode());
 
-    public bool Subset(CustomSet right) => items.Keys.All(key => right.items.ContainsKey(key));
+    public bool Subset(CustomSet right) => _items.Keys.All(key => right._items.ContainsKey(key));
 
-    public bool Disjoint(CustomSet right) => !items.Keys.Any(key => right.items.ContainsKey(key));
+    public bool Disjoint(CustomSet right) => !_items.Keys.Any(key => right._items.ContainsKey(key));
 
     public CustomSet Intersection(CustomSet right)
     {
-        var intersectionKeys = items.Keys.Intersect(right.items.Keys);
+        var intersectionKeys = _items.Keys.Intersect(right._items.Keys);
         return new CustomSet(GetValuesFromKeys(intersectionKeys));
     }
 
     public CustomSet Difference(CustomSet right)
     {
-        var differenceKeys = items.Keys.Except(right.items.Keys);
+        var differenceKeys = _items.Keys.Except(right._items.Keys);
         return new CustomSet(GetValuesFromKeys(differenceKeys));
     }
 
-    public CustomSet Union(CustomSet right)
-    {
-        return new CustomSet(items.Values.Concat(right.items.Values).ToArray());
-    }
+    public CustomSet Union(CustomSet right) => new(_items.Values.Concat(right._items.Values).ToArray());
 
-    private int[] GetValuesFromKeys(IEnumerable<int> keys) => keys.Select(key => items[key]).ToArray();
+    private int[] GetValuesFromKeys(IEnumerable<int> keys) => keys.Select(key => _items[key]).ToArray();
 
-    public override bool Equals(object obj)
-    {
-        if (!(obj is CustomSet other))
-        {
-            return false;
-        }
-        
-        return items.Keys.SequenceEqual(other.items.Keys);
-    }
+    public override bool Equals(object obj) => obj is CustomSet other && _items.Keys.SequenceEqual(other._items.Keys);
 
-    public override int GetHashCode()
-    {
-        return items.GetHashCode();
-    }
+    public override int GetHashCode() => _items.GetHashCode();
 }
