08af49716fef5d3869c85bd00bcabd9b823cbdd3
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-08 18:40:28 +0200

    csharp/dnd-character: add iteration

--- csharp/dnd-character/DndCharacter.cs
+++ csharp/dnd-character/DndCharacter.cs
@@ -1,40 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
-public class DndCharacter
+public record DndCharacter(int Strength, int Dexterity, int Constitution, int Intelligence, int Wisdom, int Charisma)
 {
-    private static readonly Random Random = new Random();
-
-    public DndCharacter(int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma)
-    {
-        Strength = strength;
-        Dexterity = dexterity;
-        Constitution = constitution;
-        Intelligence = intelligence;
-        Wisdom = wisdom;
-        Charisma = charisma;
-        Hitpoints = 10 + Modifier(constitution);
-    }
-
-    public int Strength { get; }
-    public int Dexterity { get; }
-    public int Constitution { get; }
-    public int Intelligence { get; }
-    public int Wisdom { get; }
-    public int Charisma { get; }
-    public int Hitpoints { get; }
-
+    public int Hitpoints { get; } = 10 + Modifier(Constitution);
+    
     public static int Modifier(int score) => (int)Math.Floor((score - 10) / 2.0);
 
-    public static int Ability() 
-        => Enumerable
-            .Range(0, 4)
-            .Select(_ => RollDie())
+    public static int Ability() =>
+        Enumerable.Range(0, 4)
+            .Select(_ => Die.Roll())
             .OrderByDescending(score => score).Take(3).Sum();
 
-    private static int RollDie() => Random.Next(1, 7);
+    public static DndCharacter Generate() =>
+        new(Ability(), Ability(), Ability(), Ability(), Ability(), Ability());
+}
 
-    public static DndCharacter Generate() 
-        => new DndCharacter(Ability(), Ability(), Ability(), Ability(), Ability(), Ability());
+public static class Die
+{
+    private static readonly Random Random = new();
+
+    public static int Roll() => Random.Next(1, 7);
 }
