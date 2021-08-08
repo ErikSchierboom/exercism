fcc0620c86816c95e5eac8a00845177cce43ff8f
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-08 14:42:23 +0200

    csharp/binary-search-tree: add iteration

--- csharp/binary-search-tree/BinarySearchTree.cs
+++ csharp/binary-search-tree/BinarySearchTree.cs
@@ -5,26 +5,14 @@ using System.Linq;
 
 public class BinarySearchTree : IEnumerable<int>
 {
-    public BinarySearchTree(int value)
+    public BinarySearchTree(params int[] values)
     {
-        Value = value;
-    }
+        if (values.Length == 0) throw new ArgumentException("Cannot create tree from empty list");
 
-    public BinarySearchTree(IEnumerable<int> values)
-    {
-        var array = values.ToArray();
+        Value = values[0];
 
-        if (array.Length == 0)
-        {
-            throw new ArgumentException("Cannot create tree from empty list");
-        }
-
-        Value = array[0];
-
-        foreach (var value in array.Skip(1))
-        {
+        foreach (var value in values.Skip(1))
             Add(value);
-        }
     }
 
     public int Value { get; }
@@ -33,16 +21,12 @@ public class BinarySearchTree : IEnumerable<int>
 
     public BinarySearchTree Right { get; private set; }
 
-    public BinarySearchTree Add(int value)
+    private BinarySearchTree Add(int value)
     {
         if (value <= Value)
-        {
             Left = Add(value, Left);
-        }
         else
-        {
             Right = Add(value, Right);
-        }
 
         return this;
     }
@@ -50,9 +34,7 @@ public class BinarySearchTree : IEnumerable<int>
     private static BinarySearchTree Add(int value, BinarySearchTree tree)
     {
         if (tree == null)
-        {
             return new BinarySearchTree(value);
-        }
 
         return tree.Add(value);
     }
@@ -60,20 +42,13 @@ public class BinarySearchTree : IEnumerable<int>
     public IEnumerator<int> GetEnumerator()
     {
         foreach (var left in Left?.AsEnumerable() ?? Enumerable.Empty<int>())
-        {
             yield return left;
-        }
 
         yield return Value;
 
         foreach (var right in Right?.AsEnumerable() ?? Enumerable.Empty<int>())
-        {
             yield return right;
-        }
     }
 
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
