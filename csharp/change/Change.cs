62ee3d33e21d174f5b2ccfbc3c8ed0c265c20f18
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-08 15:39:22 +0200

    csharp/change: add iteration

--- csharp/change/Change.cs
+++ csharp/change/Change.cs
@@ -4,49 +4,29 @@ using System.Linq;
 
 public static class Change
 {
-    public static int[] FindFewestCoins(int[] coins, int target)
+    public static int[] FindFewestCoins(int[] coins, int change)
     {
-        if (target < 0)
-        {
-            throw new ArgumentException("Target amount cannot be negative.");
-        }
-        if (target > 0 && target < coins.Min())
-        {
-            throw new ArgumentException("Target amount cannot be less than minimal coin value.");
-        }
-
-        var minimalCoinsMapSeed = new Dictionary<int, List<int>>
-        {
-            [0] = new List<int>(0)
-        };
-
-        var minimalCoinsMap = Enumerable.Range(1, target)
-            .Aggregate(minimalCoinsMapSeed, UpdateMinimalCoinsMap);
-
-        if (minimalCoinsMap.TryGetValue(target, out var minimalCoins))
-            return minimalCoins.OrderBy(x => x).ToArray();
+        if (change < 0) throw new ArgumentException("Change cannot be negative.");
+        if (change > 0 && change < coins.Min()) throw new ArgumentException("Change cannot be less than minimal coin value.");
+ 
+        return Enumerable.Range(1, change)
+            .Aggregate(new Dictionary<int, int[]> {[0] = Array.Empty<int>()}, UpdateFewestCoinsForChange)
+            .GetValueOrDefault(change) ?? throw new ArgumentException(nameof(change));
         
-        throw new ArgumentException();
-
-        Dictionary<int, List<int>> UpdateMinimalCoinsMap(Dictionary<int, List<int>> current, int subTarget)
+        Dictionary<int, int[]> UpdateFewestCoinsForChange(Dictionary<int, int[]> current, int subChange)
         {
-            var subTargetMinimalCoins = MinimalCoins(current, subTarget);
-            if (subTargetMinimalCoins != null)
-                current.Add(subTarget, subTargetMinimalCoins);
+            var fewestCoins = FewestCoinsForChange(current, subChange);
+            if (fewestCoins != null)
+                current.Add(subChange, fewestCoins);
 
             return current;
         }
-
-        List<int> MinimalCoins(Dictionary<int, List<int>> current, int subTarget)
-        {
-            return coins
-                .Where(coin => coin <= subTarget)
-                .Select(coin => current.TryGetValue(subTarget - coin, out var subTargetMinimalCoins)
-                    ? subTargetMinimalCoins.Append(coin).ToList()
-                    : null)
-                .Where(subTargetMinimalCoins => subTargetMinimalCoins != null)
-                .OrderBy(subTargetMinimalCoins => subTargetMinimalCoins.Count)
+        
+        int[] FewestCoinsForChange(Dictionary<int, int[]> current, int subChange) =>
+            coins.Where(coin => coin <= subChange)
+                .Select(coin => current.GetValueOrDefault(subChange - coin)?.Prepend(coin).ToArray())
+                .Where(fewestCoins => fewestCoins != null)
+                .OrderBy(fewestCoins => fewestCoins.Length)
                 .FirstOrDefault();
-        }
     }
 }
