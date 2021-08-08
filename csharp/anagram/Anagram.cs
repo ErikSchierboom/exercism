da05abcc535d1518ecff5de6ef23eb23d513c3e7
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-08 13:57:26 +0200

    csharp/anagram: add iteration

--- csharp/anagram/Anagram.cs
+++ csharp/anagram/Anagram.cs
@@ -5,23 +5,18 @@ using System.Linq;
 public class Anagram
 {
     private readonly string _word;
-    private readonly IEnumerable<char> _normalizedWord;
 
-    public Anagram(string word)
-        => (_word, _normalizedWord) = (word, Normalize(word));
+    public Anagram(string word) => _word = word;
 
-    public IEnumerable<string> Anagrams(IEnumerable<string> sources) 
-        => sources.Where(IsAnagram).ToArray();
+    public IEnumerable<string> Anagrams(IEnumerable<string> candidates) =>
+        candidates.Where(IsAnagram).ToArray();
 
-    private bool IsAnagram(string possibleAnagram) 
-        => HasMatchingCharacters(possibleAnagram) && IsDifferentFromWord(possibleAnagram);
+    private bool IsAnagram(string candidate) =>
+        SortedLowerCase(candidate) == SortedLowerCase(_word) && IsDifferentFromWord(candidate);
 
-    private bool HasMatchingCharacters(string possibleAnagram) 
-        => Normalize(possibleAnagram).SequenceEqual(_normalizedWord);
+    private bool IsDifferentFromWord(string candidate) =>
+        !string.Equals(candidate, _word, StringComparison.OrdinalIgnoreCase);
 
-    private bool IsDifferentFromWord(string possibleAnagram) 
-        => !string.Equals(possibleAnagram, _word, StringComparison.InvariantCultureIgnoreCase);
-
-    private static IEnumerable<char> Normalize(string word) 
-        => word.ToLowerInvariant().OrderBy(c => c);
+    private static string SortedLowerCase(string word) =>
+        new(word.ToLower().OrderBy(c => c).ToArray());
 }
