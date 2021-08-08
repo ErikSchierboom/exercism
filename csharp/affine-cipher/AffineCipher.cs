9e2ebcbeb4e5eeb64c7e34db6d5c103a66a9759a
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-08 13:19:27 +0200

    csharp/affine-cipher: add iteration

--- csharp/affine-cipher/AffineCipher.cs
+++ csharp/affine-cipher/AffineCipher.cs
@@ -11,54 +11,48 @@ public static class AffineCipher
 
     public static string Encode(string plainText, int a, int b)
     {
-        if (!Coprime(a, Alphabet.Length))
-            throw new ArgumentException();
+        if (!Math.Coprime(a, Alphabet.Length)) throw new ArgumentException(nameof(a));
 
-        bool ShouldEncode(char c) => char.IsLetterOrDigit(c);
-        char Encode(char c) => char.IsDigit(c) ? c : IndexToChar(a * CharToIndex(c) + b);
-
-        return plainText
-            .Where(ShouldEncode)
-            .Select(Encode)
-            .Chunk();
+        return plainText.Translate(i => a * i + b).Chunk();
     }
 
-    private static int CharToIndex(char c) => Alphabet.IndexOf(char.ToLower(c));
-    
-    private static char IndexToChar(int i) => Alphabet[Mod(i, Alphabet.Length)];
-
-    private static string Chunk(this IEnumerable<char> encodedLetters) 
-        => string.Join(" ", encodedLetters.Chunks(ChunkSize).Select(chunk => new string(chunk.ToArray())));
-
-    private static IEnumerable<IEnumerable<T>> Chunks<T>(this IEnumerable<T> source, int size)
+    public static string Decode(string cipherText, int a, int b)
     {
-        while (source.Any())
-        {
-            yield return source.Take(size);
-            source = source.Skip(size);
-        }
+        if (!Math.Coprime(a, Alphabet.Length)) throw new ArgumentException(nameof(a));
+
+        var modularInverse = Math.ModularInverse(a, Alphabet.Length);
+
+        return cipherText.Translate(i => modularInverse * (i - b));
     }
 
-    public static string Decode(string cipheredText, int a, int b)
+    private static string Translate(this string str, Func<int, int> mapIndex) =>
+        new(str.Where(char.IsLetterOrDigit).Select(c => c.Translate(mapIndex)).ToArray());
+
+    private static char Translate(this char c, Func<int, int> mapIndex)
     {
-        if (!Coprime(a, Alphabet.Length))
-            throw new ArgumentException();
-
-        var modularInverse = ModularInverse(a, Alphabet.Length);
-
-        bool ShouldDecode(char c) => char.IsLetterOrDigit(c);
-        char Decode(char c) => char.IsDigit(c) ? c : IndexToChar(modularInverse * (CharToIndex(c) - b));
-
-        return new string(cipheredText.Where(ShouldDecode).Select(Decode).ToArray());
+        var index = Alphabet.IndexOf(char.ToLower(c));
+        return index == -1 ? c : Alphabet[Math.Mod(mapIndex(index), Alphabet.Length)];
     }
 
-    private static bool Coprime(int a, int m) => Gcd(a, m) == 1;
+    private static string Chunk(this string encodedLetters) =>
+        string.Join(" ", encodedLetters.Chunks(ChunkSize));
+
+    private static IEnumerable<string> Chunks(this string str, int size)
+    {
+        for (var i = 0; i < str.Length; i += size)
+            yield return str.Substring(i, System.Math.Min(size, str.Length - i));
+    }
+}
+
+public static class Math
+{
+    public static bool Coprime(int a, int m) => Gcd(a, m) == 1;
 
     private static int Gcd(int a, int m) => a == 0 ? m : Gcd(m % a, a);
     
-    private static int Mod(int a, int m) => (a % m + m) % m;
+    public static int Mod(int a, int m) => (a % m + m) % m;
 
-    private static int ModularInverse(int a, int m)
+    public static int ModularInverse(int a, int m)
     {
         if (m == 1)
             return 0;
