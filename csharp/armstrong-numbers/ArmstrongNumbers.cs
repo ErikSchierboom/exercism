cfc7dc39234790cfc32fa0eeb73a6eed6e3421e5
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-08 14:06:41 +0200

    csharp/armstrong-numbers: add iteration

--- csharp/armstrong-numbers/ArmstrongNumbers.cs
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 
 public static class ArmstrongNumbers
 {
-    public static bool IsArmstrongNumber(int number)
-    {
-        var numberAsString = number.ToString();
-        return numberAsString.Select(CharToInt).Sum(IntRaisedToPower) == number;
+    public static bool IsArmstrongNumber(int n) => ArmstrongSum(n) == n;
 
-        int IntRaisedToPower(int i) => (int) Math.Pow(i, numberAsString.Length);
-        int CharToInt(char c) => c - '0';
+    private static int ArmstrongSum(int n)
+    {
+        var digits = Digits(n);
+        return digits.Sum(digit => Pow(digit, digits.Length));
     }
+
+    private static int[] Digits(int n) => n.ToString().Select(CharUnicodeInfo.GetDecimalDigitValue).ToArray();
+
+    private static int Pow(int n, int m) => (int) Math.Pow(n, m);
 }
