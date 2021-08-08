2940d63acd94b5d1728abcbd0c321b385fdaa4a0
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-08 15:52:55 +0200

    csharp/complex-numbers: add iteration

--- csharp/complex-numbers/ComplexNumbers.cs
+++ csharp/complex-numbers/ComplexNumbers.cs
@@ -5,36 +5,33 @@ public struct ComplexNumber
     private readonly double _real;
     private readonly double _imaginary;
 
-    public ComplexNumber(double real, double imaginary)
-        => (_real, _imaginary) = (real, imaginary);
+    public ComplexNumber(double real, double imaginary) => (_real, _imaginary) = (real, imaginary);
 
     public double Real() => _real;
 
     public double Imaginary() => _imaginary;
 
-    public ComplexNumber Mul(ComplexNumber other) 
-        => new ComplexNumber(_real * other._real - _imaginary * other._imaginary, _imaginary * other._real + _real * other._imaginary);
+    public ComplexNumber Mul(ComplexNumber other) =>
+        new(_real * other._real - _imaginary * other._imaginary, _imaginary * other._real + _real * other._imaginary);
 
-    public ComplexNumber Add(ComplexNumber other)
-        => new ComplexNumber(_real + other._real, _imaginary + other._imaginary);
+    public ComplexNumber Add(ComplexNumber other) =>
+        new(_real + other._real, _imaginary + other._imaginary);
 
-    public ComplexNumber Sub(ComplexNumber other)
-        => new ComplexNumber(_real - other._real, _imaginary - other._imaginary);
+    public ComplexNumber Sub(ComplexNumber other) =>
+        new(_real - other._real, _imaginary - other._imaginary);
 
     public ComplexNumber Div(ComplexNumber other)
     {
-        var real = (_real * other._real + _imaginary * other._imaginary) / (Math.Pow(other._real, 2) + Math.Pow(other._imaginary, 2));
-        var imaginary = (_imaginary * other._real - _real * other._imaginary) / (Math.Pow(other._real, 2) + Math.Pow(other._imaginary, 2));
-        
-        return new ComplexNumber(real, imaginary);
+        var numerator = Mul(other.Conjugate());
+        var denominator = other.Mul(other.Conjugate());
+
+        return new(numerator._real / denominator._real, numerator._imaginary / denominator._real);
     }
 
-    public double Abs()
-        => Math.Sqrt(Math.Pow(_real, 2) + Math.Pow(_imaginary, 2));
+    public ComplexNumber Exp()  =>
+        new(Math.Exp(_real) * Math.Cos(_imaginary), Math.Exp(_real) * Math.Sin(_imaginary));
 
-    public ComplexNumber Conjugate()
-        => new ComplexNumber(_real, -_imaginary);
+    public ComplexNumber Conjugate() => new(_real, -_imaginary);
 
-    public ComplexNumber Exp() 
-        => new ComplexNumber(Math.Exp(_real) * Math.Cos(_imaginary), Math.Exp(_real) * Math.Sin(_imaginary));
+    public double Abs() => Math.Sqrt(Math.Pow(_real, 2) + Math.Pow(_imaginary, 2));
 }
