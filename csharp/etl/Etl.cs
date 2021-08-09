ada4762663ffcf959dd315f85b770fc00c770e54
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-09 14:28:54 +0200

    csharp/etl: add iteration

--- csharp/etl/Etl.cs
+++ csharp/etl/Etl.cs
@@ -3,11 +3,8 @@ using System.Linq;
 
 public static class Etl
 {
-    public static Dictionary<string, int> Transform(Dictionary<int, string[]> input)
-    {
-        var keyValuePairs = input.SelectMany(i => i.Value.Select(v => new KeyValuePair<string, int>(v.ToLowerInvariant(), i.Key)))
-                                 .OrderBy(k => k.Key);
-
-        return keyValuePairs.ToDictionary(kv => kv.Key, kv => kv.Value);
-    }
+    public static Dictionary<string, int> Transform(Dictionary<int, string[]> input) =>
+        input.SelectMany(kv => kv.Value.Select(v => KeyValuePair.Create(v.ToLowerInvariant(), kv.Key)))
+            .OrderBy(kv => kv.Key)
+            .ToDictionary(kv => kv.Key, kv => kv.Value);
 }
