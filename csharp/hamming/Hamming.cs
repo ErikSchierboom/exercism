7ff4abd1ab7dbfe1d1256645a825889c8b0feed2
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-09 14:48:18 +0200

    csharp/hamming: add iteration

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -5,11 +5,8 @@ public static class Hamming
 {
     public static int Distance(string strand1, string strand2)
     {
-        if (strand1.Length != strand2.Length)
-        {
-            throw new ArgumentException("Strands have different length");
-        }
+        if (strand1.Length != strand2.Length) throw new ArgumentException("Strands have different length");
 
-        return strand1.Zip(strand2, (nucleotide1, nucleotide2) => nucleotide1 == nucleotide2 ? 0 : 1).Sum();
+        return strand1.Zip(strand2).Count(nucleotidePair => nucleotidePair.First != nucleotidePair.Second);
     }
 }
