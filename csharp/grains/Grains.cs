76ced68a122221375add945f777edf1dda6f7112
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-09 14:43:39 +0200

    csharp/grains: add iteration

--- csharp/grains/Grains.cs
+++ csharp/grains/Grains.cs
@@ -5,13 +5,10 @@ public static class Grains
 {
     public static double Square(int i)
     {
-        if (i <= 0 || i > 64)
-        {
-            throw new ArgumentOutOfRangeException(nameof(i));
-        }
+        if (i is <= 0 or > 64) throw new ArgumentOutOfRangeException(nameof(i));
         
         return Math.Pow(2, i - 1);
     }
 
-    public static double Total() => Enumerable.Range(1, 64).Select(Square).Sum();
+    public static double Total() => Enumerable.Range(1, 64).Sum(Square);
 }
