85a971d2929c744c13540cc87e484fa0b7e49935
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-11 16:42:34 +0200

    ruby/scrabble-score: add iteration

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,18 +1,4 @@
 class Scrabble
-  LETTER_SCORES = [
-    [%w(A E I O U L N R S T), 1],
-    [%w(D G), 2],
-    [%w(B C M P), 3],
-    [%w(F H V W Y), 4],
-    [%w(K), 5],
-    [%w(J X), 8],
-    [%w(Q Z), 10]
-  ].flat_map { |letters, score|
-    letters.map { |letter| [letter, score] }
-  }.to_h
-
-  private_constant :LETTER_SCORES
-
   def self.score(*args)
     new(*args).score
   end
@@ -26,7 +12,20 @@ class Scrabble
   end
 
   private
-
   attr_reader :letters
+
+  LETTER_SCORES = [
+    [%w(A E I O U L N R S T), 1],
+    [%w(D G), 2],
+    [%w(B C M P), 3],
+    [%w(F H V W Y), 4],
+    [%w(K), 5],
+    [%w(J X), 8],
+    [%w(Q Z), 10]
+  ].flat_map { |letters, score|
+    letters.map { |letter| [letter, score] }
+  }.to_h
+
+  private_constant :LETTER_SCORES
 end
 
