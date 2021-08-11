8c85d8b2fd0561d5c12fa7999ca9788e95b52df3
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-11 13:18:41 +0200

    ruby/atbash-cipher: add iteration

--- ruby/atbash-cipher/atbash_cipher.rb
+++ ruby/atbash-cipher/atbash_cipher.rb
@@ -1,21 +1,17 @@
 module Atbash
   def self.encode(plaintext)
     plaintext
+      .downcase
       .scan(/[[:alnum:]]/)
-      .map(&method(:encode_letter))
+      .map { |letter| ENCODING_MAP[letter] || letter }
       .each_slice(GROUP_SIZE)
       .map { |slice| slice.join('') }
       .join(' ')
   end
 
-  def self.encode_letter(letter)
-    ENCODED_LETTERS[PLAINTEXT_LETTERS.index(letter.downcase)]
-  end
-
   private
-
-  PLAINTEXT_LETTERS = 'abcdefghijklmnopqrstuvwxyz0123456789'.freeze
-  ENCODED_LETTERS = 'zyxwvutsrqponmlkjihgfedcba0123456789'.freeze
+  LETTERS = ('a' .. 'z').to_a.freeze
+  ENCODING_MAP = LETTERS.zip(LETTERS.reverse).to_h
   GROUP_SIZE = 5
-  private_constant :PLAINTEXT_LETTERS, :ENCODED_LETTERS, :GROUP_SIZE
+  private_constant :LETTERS, :ENCODING_MAP, :GROUP_SIZE
 end
