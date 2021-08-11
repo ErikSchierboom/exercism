cb801ee3ebb7f4b8a6cc0132fc4de81a7356a990
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-11 13:50:32 +0200

    ruby/hamming: add iteration

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,7 +1,7 @@
 module Hamming
   def self.compute(strand1, strand2)
-    raise ArgumentError, 'Strands length must be the same' unless strand1.length == strand2.length
+    raise ArgumentError unless strand1.length == strand2.length
 
-    strand1.chars.zip(strand2.chars).count { |char1, char2| char1 != char2 }
+    strand1.chars.zip(strand2.chars).count { |letter1, letter2| letter1 != letter2 }
   end
 end
