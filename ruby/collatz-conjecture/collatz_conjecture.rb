aa6924f0b9446cdb50ae6fceec6e383a917a28a9
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-11 13:27:23 +0200

    ruby/collatz-conjecture: add iteration

--- ruby/collatz-conjecture/collatz_conjecture.rb
+++ ruby/collatz-conjecture/collatz_conjecture.rb
@@ -1,9 +1,14 @@
 module CollatzConjecture
-  def self.steps(number, count = 0)
+  def self.steps(number)
     raise ArgumentError, "Number must be > 0" unless number > 0
 
-    return count if number == 1
+    count = 0
 
-    steps(number.even? ? number / 2 : number * 3 + 1, count + 1)
+    until number == 1 do
+      number = number.even? ? number / 2 : number * 3 + 1
+      count += 1
+    end
+
+    count
   end
 end
