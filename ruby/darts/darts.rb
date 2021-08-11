7a53bc43a9f46557a61d187c39064f19d20e4b76
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-11 13:34:34 +0200

    ruby/darts: add iteration

--- ruby/darts/darts.rb
+++ ruby/darts/darts.rb
@@ -1,29 +1,26 @@
 class Darts
   def initialize(x, y)
-    @distance_from_center = Math.hypot(x, y)
+    @x = x
+    @y = y
   end
 
   def score
-    return 10 if landed_in_inner_circle?
-    return 5 if landed_in_middle_circle?
-    return 1 if landed_in_outer_circle?
-
-    0
+    case distance_from_center
+    when 0..1
+      10
+    when 1..5
+      5
+    when 5..10
+      1
+    else
+      0
+    end
   end
 
   private
+  attr_reader :x, :y
 
-  attr_reader :distance_from_center
-
-  def landed_in_inner_circle?
-    distance_from_center <= 1
-  end
-
-  def landed_in_middle_circle?
-    distance_from_center > 1 && distance_from_center <= 5
-  end
-
-  def landed_in_outer_circle?
-    distance_from_center > 5 && distance_from_center <= 10
+  def distance_from_center
+    Math.hypot(x, y)
   end
 end
