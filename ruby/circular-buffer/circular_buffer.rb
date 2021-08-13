20d8b033501fe9f7c2dade89ddcf6bd983743436
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-13 18:51:06 +0200

    ruby/circular-buffer: add solution

---
+++ ruby/circular-buffer/circular_buffer.rb
@@ -0,0 +1,37 @@
+class CircularBuffer
+  class BufferEmptyException < StandardError
+  end
+
+  class BufferFullException < StandardError
+  end
+
+  def initialize(size)
+    @size = size
+    @elements = []
+  end
+
+  def read
+    raise BufferEmptyException if elements.empty?
+
+    elements.pop
+  end
+
+  def write(value)
+    raise BufferFullException if elements.size == size
+
+    elements.unshift(value)
+  end
+
+  def write!(value)
+    elements.pop if elements.size == size
+    
+    write(value)
+  end
+
+  def clear
+    elements.clear
+  end
+
+  private
+  attr_reader :size, :elements
+end
