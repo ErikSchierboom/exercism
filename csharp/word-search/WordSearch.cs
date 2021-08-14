a753f21db5af501990fcd9b8243fa3caab22c7d2
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-14 17:49:48 +0200

    csharp/word-search: add iteration

--- csharp/word-search/WordSearch.cs
+++ csharp/word-search/WordSearch.cs
@@ -3,9 +3,10 @@ using System.Linq;
 
 public class WordSearch
 {
-    private readonly string[] rows;
-    private readonly int width;
-    private readonly int height;
+    private readonly string[] _rows;
+    private readonly int _width;
+    private readonly int _height;
+    private readonly (int, int)[] _positions;
 
     private static readonly (int, int)[] Directions =
     {
@@ -21,24 +22,20 @@ public class WordSearch
 
     public WordSearch(string puzzle)
     {
-        rows = puzzle.Split('\n');
-        width = rows[0].Length;
-        height = rows.Length;
-        Positions();
+        _rows = puzzle.Split('\n');
+        _width = _rows[0].Length;
+        _height = _rows.Length;
+        _positions = Positions();
     }
 
-    public Dictionary<string, ((int, int), (int, int))?> Search(IEnumerable<string> words)
-    {
-        return words.ToDictionary(word => word, Search);
-    }
+    public Dictionary<string, ((int, int), (int, int))?> Search(IEnumerable<string> words) =>
+        words.ToDictionary(word => word, Search);
 
-    private ((int, int), (int, int))? Search(string word)
-    {
-        return
-            Positions()
-                .SelectMany(position => Directions.SelectMany(direction => Find(word, position, direction)))
-                .FirstOrDefault();
-    }
+    private ((int, int), (int, int))? Search(string word) =>
+        (from position in _positions
+         from direction in Directions
+         from location in Find(word, position, direction) 
+         select location).FirstOrDefault();
 
     private IEnumerable<((int, int), (int, int))> Find(string word, (int, int) position, (int, int) direction)
     {
@@ -59,18 +56,18 @@ public class WordSearch
 
     private char? FindChar((int, int) coordinate)
     {
-        if (coordinate.Item1 > 0 && coordinate.Item1 <= width && coordinate.Item2 > 0 && coordinate.Item2 <= height)
+        var (x, y) = coordinate;
+        
+        if (x > 0 && x <= _width && y > 0 && y <= _height)
         {
-            return rows[coordinate.Item2 - 1][coordinate.Item1 - 1];
+            return _rows[y - 1][x - 1];
         }
 
         return null;
     }
 
-    private (int, int)[] Positions()
-    {
-        return Enumerable.Range(1, width).SelectMany(x =>
-               Enumerable.Range(1, height).Select(y => (x, y)))
-                .ToArray();
-    }
+    private (int, int)[] Positions() =>
+        (from x in Enumerable.Range(1, _width)
+         from y in Enumerable.Range(1, _height)
+         select (x, y)).ToArray();
 }
