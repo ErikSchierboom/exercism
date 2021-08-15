edd4066088fa7585eacb51be4e87f708f66c4c33
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-15 12:53:47 +0200

    csharp/sum-of-multiples: add iteration

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -3,8 +3,8 @@ using System.Linq;
 
 public static class SumOfMultiples
 {
-    public static int Sum(IEnumerable<int> factors, int limit)
-        => Enumerable.Range(1, limit - 1)
+    public static int Sum(IEnumerable<int> factors, int limit) =>
+        Enumerable.Range(1, limit - 1)
             .Where(i => factors.Any(factor => i % factor == 0))
             .Sum();
 }
