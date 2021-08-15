f70329063f098ec477f742292ecf293c85fea1d9
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-15 12:52:10 +0200

    csharp/triangle: add iteration

--- csharp/triangle/Triangle.cs
+++ csharp/triangle/Triangle.cs
@@ -1,70 +1,15 @@
-﻿using System;
-using System.Linq;
-
-public enum TriangleKind
+﻿public static class Triangle
 {
-    Equilateral,
-    Isosceles,
-    Scalene,
-    Invalid
+    public static bool IsScalene(double x, double y, double z) =>
+        IsValid(x, y, z) && (x != y && y != z);
+
+    public static bool IsIsosceles(double x, double y, double z) =>
+        IsValid(x, y, z) && (x == y || y == z || x == z);
+
+    public static bool IsEquilateral(double x, double y, double z) =>
+        IsValid(x, y, z) && x == y && y == z;
+
+    private static bool IsValid(double x, double y, double z) =>
+        x != 0 && y != 0 && z != 0 &&
+        x + y >= z && x + z >= y && y + z >= x;
 }
-
-public static class Triangle
-{
-    
-    public static bool IsScalene(double side1, double side2, double side3)
-    {
-        return Kind(side1, side2, side3) == TriangleKind.Scalene;
-    }
-
-    public static bool IsIsosceles(double side1, double side2, double side3) 
-    {
-        var triangle = Kind(side1, side2, side3);
-        return triangle == TriangleKind.Isosceles || triangle == TriangleKind.Equilateral;
-    }
-
-    public static bool IsEquilateral(double side1, double side2, double side3) 
-    {
-        return Kind(side1, side2, side3) == TriangleKind.Equilateral;
-    }
-
-    private static TriangleKind Kind(double side1, double side2, double side3)
-    {
-        if (AllSidesAreZero(side1, side2, side3) ||
-            HasImpossibleSides(side1, side2, side3) ||
-            ViolatesTriangleInequality(side1, side2, side3))
-        {
-            return TriangleKind.Invalid;
-        }
-
-        int uniqueSides = UniqueSides(side1, side2, side3);
-        if (uniqueSides == 1)
-            return TriangleKind.Equilateral;
-        if (uniqueSides == 2)
-            return TriangleKind.Isosceles;
-        return TriangleKind.Scalene;
-    }
-
-    private static bool AllSidesAreZero(double side1, double side2, double side3)
-    {
-        return side1 == 0 && side2 == 0 && side3 == 0;
-    }
-
-    private static bool HasImpossibleSides(double side1, double side2, double side3)
-    {
-        return side1 < 0 || side2 < 0 || side3 < 0;
-    }
-
-    private static bool ViolatesTriangleInequality(double side1, double side2, double side3)
-    {
-        return side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1;
-    }
-
-    private static int UniqueSides(double side1, double side2, double side3)
-    {
-        double[] sides = { side1, side2, side3 };
-        return sides.Distinct().Count();
-    }
-}
-
-public class TriangleException : Exception { }
