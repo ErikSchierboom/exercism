079e83181df959a0a0587e6713dfd2d8c6261488
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-15 11:17:19 +0200

    exercism/csharp: add iteration

--- csharp/word-count/WordCount.cs
+++ csharp/word-count/WordCount.cs
@@ -2,16 +2,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-public class WordCount
+public static class WordCount
 {
-    public static Dictionary<string, int> CountWords(string phrase)
-    {
-        return GetWords(phrase).GroupBy(w => w.ToLowerInvariant())
+    public static Dictionary<string, int> CountWords(string phrase) =>
+        phrase.Words()
+            .GroupBy(w => w.ToLowerInvariant())
             .ToDictionary(g => g.Key, g => g.Count());
-    }
 
-    private static IEnumerable<string> GetWords(string phrase)
-    {
-        return Regex.Matches(phrase, @"\w+('\w+)*").Cast<Match>().Select(m => m.Value);
-    }
+    private static IEnumerable<string> Words(this string phrase) =>
+        Regex.Matches(phrase, @"\w+('\w+)*").Select(m => m.Value);
 }
