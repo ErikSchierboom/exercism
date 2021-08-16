ac48c0659d81c34b7a6bbe3359dd4bd57676b7b6
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-16 11:11:40 +0200

    csharp/collatz-conjecture: add iteration

--- csharp/collatz-conjecture/CollatzConjecture.cs
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -4,26 +4,17 @@ public static class CollatzConjecture
 {
     public static int Steps(int number)
     {
-        if (number <= 0)
+        if (number <= 0) throw new ArgumentException(nameof(number));
+
+        var remainder = number;
+        var stepCount = 0;
+
+        while (remainder != 1)
         {
-            throw new ArgumentException();
+            remainder = remainder % 2 == 0 ? remainder / 2 : remainder * 3 + 1;
+            stepCount++;
         }
 
-        return Steps(number, 0);
-
-        int Steps(int remainder, int stepCount)
-        {
-            if (remainder == 1)
-            {
-                return stepCount;
-            }
-
-            if (remainder % 2 == 0)
-            {
-                return Steps(remainder / 2, stepCount + 1);
-            }
-
-            return Steps(remainder * 3 + 1, stepCount + 1);
-        }
+        return stepCount;
     }
 }
