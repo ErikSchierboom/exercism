96d0da78f9789210551d26ca0541187915563a6f
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-16 16:54:35 +0200

    csharp/raindrops: add iteration

--- csharp/raindrops/Raindrops.cs
+++ csharp/raindrops/Raindrops.cs
@@ -5,22 +5,11 @@ public static class Raindrops
 {
     public static string Convert(int number)
     {
-        var drops = new StringBuilder(capacity: 15);
+        var drops = new StringBuilder(15);
 
-        if (number % 3 == 0)
-        {
-            drops.Append("Pling");
-        }
-
-        if (number % 5 == 0)
-        {
-            drops.Append("Plang");
-        }
-
-        if (number % 7 == 0)
-        {
-            drops.Append("Plong");
-        }
+        if (number % 3 == 0) drops.Append("Pling");
+        if (number % 5 == 0) drops.Append("Plang");
+        if (number % 7 == 0) drops.Append("Plong");
 
         return drops.Length > 0 ? drops.ToString() : number.ToString(CultureInfo.InvariantCulture);
     }
