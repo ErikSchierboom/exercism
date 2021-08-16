2dd80ddfa7fe1cdcd2adc71fdb1b0ce718a47a01
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-16 17:19:38 +0200

    csharp/robot-name: add iteration

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -4,60 +4,47 @@ using System.Linq;
 
 public class Robot
 {
-    private const string Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private const string Numbers = "0123456789";
-    private static readonly Random Random = new Random();
-    private static readonly HashSet<string> IssuedNames = new HashSet<string>();
-
-    public Robot()
-    {
-        Name = GenerateNewName();
-    }
+    public Robot() => Name = RobotName.Generate();
 
     public string Name { get; private set; }
 
-    public void Reset()
+    public void Reset() => Name = RobotName.Generate();
+}
+
+public static class RobotName
+{
+    private const string Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private const string Numbers = "0123456789";
+
+    private static readonly Random Random = new();
+    private static readonly Stack<string> RandomNames = Names().Shuffle().ToStack();
+
+    public static string Generate() => RandomNames.Pop();
+    
+    private static IEnumerable<string> Names() =>
+        from letter1 in Letters
+        from letter2 in Letters
+        from number1 in Numbers
+        from number2 in Numbers
+        from number3 in Numbers
+        select $"{letter1}{letter2}{number1}{number2}{number3}";
+
+    private static T[] Shuffle<T>(this IEnumerable<T> enumerable)
     {
-        Name = GenerateNewName();
-    }
-
-    private string GenerateNewName()
-    {
-        string name;
-
-        lock (this)
-        {
-            do
-            {
-                name = GenerateName();
-            } while (IssuedNames.Contains(name));
-
-            IssuedNames.Add(name);
+        var arr = enumerable.ToArray();
+        
+        var n = arr.Length;  
+        while (n > 1)
+        {  
+            n--;  
+            var k = Random.Next(n + 1);  
+            var value = arr[k];  
+            arr[k] = arr[n];  
+            arr[n] = value;  
         }
 
-        return name;
+        return arr;
     }
 
-    private static string GenerateName()
-    {
-        return new string(RandomLetters().Take(2).Concat(RandomNumbers().Take(3)).ToArray());
-    }
-
-    private static IEnumerable<char> RandomLetters()
-    {
-        return RandomCharsFromString(Letters);
-    }
-
-    private static IEnumerable<char> RandomNumbers()
-    {
-        return RandomCharsFromString(Numbers);
-    }
-
-    private static IEnumerable<char> RandomCharsFromString(string str)
-    {
-        while (true)
-        {
-            yield return str[Random.Next(str.Length)];
-        }
-    }
+    private static Stack<T> ToStack<T>(this IEnumerable<T> enumerable) => new(enumerable);
 }
