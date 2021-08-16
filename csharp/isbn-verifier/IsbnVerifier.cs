ce96832f60a62adbe710bfdc2d5115b553955aeb
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-16 15:18:11 +0200

    csharp/isbn-verifier: add iteration

--- csharp/isbn-verifier/IsbnVerifier.cs
+++ csharp/isbn-verifier/IsbnVerifier.cs
@@ -5,16 +5,16 @@ public static class IsbnVerifier
 {
     public static bool IsValid(string number)
     {
-        var normalizedNumber = NormalizeNumber(number);
+        var normalizedNumber = SanitizeNumber(number);
         return IsValidNumber(normalizedNumber) && Checksum(normalizedNumber) % 11 == 0;
     }
 
-    private static string NormalizeNumber(string number) => number.Replace("-", "");
+    private static string SanitizeNumber(string number) => number.Replace("-", "");
 
     private static bool IsValidNumber(string normalizedNumber) => Regex.IsMatch(normalizedNumber, @"^\d{9}[\dX]$");
 
-    private static int Checksum(string normalizedNumber) 
-        => normalizedNumber
+    private static int Checksum(string normalizedNumber) =>
+        normalizedNumber
             .Reverse()
             .Select((digit, i) => DigitToValue(digit) * (i + 1))
             .Sum();
