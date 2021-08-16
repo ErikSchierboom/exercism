c4d44f688cef95b2402b087e8c20ae78dcf9ad2c
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-16 16:26:11 +0200

    csharp/pig-latin: add iteration

--- csharp/pig-latin/PigLatin.cs
+++ csharp/pig-latin/PigLatin.cs
@@ -2,19 +2,14 @@ using System.Text.RegularExpressions;
 
 public static class PigLatin
 {
-    private static readonly Regex VowelRegex = new Regex(@"(?<begin>^|\s+)(?<vowel>a|e|i|o|u|yt|xr)(?<rest>\w+)", RegexOptions.Compiled);
-    private static readonly Regex ConsonantRegex = new Regex(@"(?<begin>^|\s+)(?<consonant>ch|qu|thr|th|sch|yt|\wqu|\w)(?<rest>\w+)", RegexOptions.Compiled);
+    private static readonly Regex VowelRegex = new(@"(?<begin>^|\s+)(?<vowel>a|e|i|o|u|yt|xr)(?<rest>\w+)", RegexOptions.Compiled);
+    private static readonly Regex ConsonantRegex = new(@"(?<begin>^|\s+)(?<consonant>ch|qu|thr|th|rh|sch|yt|\wqu|\w)(?<rest>\w+)", RegexOptions.Compiled);
 
     private const string VowelReplacement = "${begin}${vowel}${rest}ay";
     private const string ConsonantReplacement = "${begin}${rest}${consonant}ay";
 
-    public static string Translate(string sentence)
-    {
-        if (VowelRegex.IsMatch(sentence))
-        {
-            return VowelRegex.Replace(sentence, VowelReplacement);
-        }
-
-        return ConsonantRegex.Replace(sentence, ConsonantReplacement);
-    }
+    public static string Translate(string sentence) =>
+        VowelRegex.IsMatch(sentence)
+            ? VowelRegex.Replace(sentence, VowelReplacement)
+            : ConsonantRegex.Replace(sentence, ConsonantReplacement);
 }
