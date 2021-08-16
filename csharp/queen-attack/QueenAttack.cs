bc0ff6b06bee1692836dd969f806d4a368f655bb
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-16 16:53:28 +0200

    csharp/queen-attack: add iteration

--- csharp/queen-attack/QueenAttack.cs
+++ csharp/queen-attack/QueenAttack.cs
@@ -1,31 +1,17 @@
 ﻿using System;
 
-public class Queen
-{
-    public Queen(int row, int column)
-    {
-        Row = row;
-        Column = column;
-    }
-
-    public int Row { get; }
-    public int Column { get; }
-}
-
 public static class QueenAttack
 {
-    public static bool CanAttack(Queen white, Queen black)
-    {
-        return black.Row == white.Row ||
-               black.Column == white.Column ||
-               Math.Abs(black.Row - white.Row) == Math.Abs(black.Column - white.Column);
-    }
+    public static bool CanAttack((int Row, int Col) white, (int Row, int Col) black) =>
+        black.Row == white.Row ||
+        black.Col == white.Col ||
+        Math.Abs(black.Row - white.Row) == Math.Abs(black.Col - white.Col);
 
-    public static Queen Create(int row, int column)
+    public static (int Row, int Col) Create(int row, int column)
     {
-        if (row < 0 || row > 7) throw new ArgumentOutOfRangeException(nameof(row));
-        if (column < 0 || column > 7) throw new ArgumentOutOfRangeException(nameof(column));
+        if (row is < 0 or > 7) throw new ArgumentOutOfRangeException(nameof(row));
+        if (column is < 0 or > 7) throw new ArgumentOutOfRangeException(nameof(column));
         
-        return new Queen(row, column);
+        return (row, column);
     }
 }
