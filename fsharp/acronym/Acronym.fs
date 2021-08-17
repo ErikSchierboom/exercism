8d688e247e6c102a4c4b9eefe28f028c4a8aca3a
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-17 21:43:07 +0200

    fsharp/acronym: add iteration

--- fsharp/acronym/Acronym.fs
+++ fsharp/acronym/Acronym.fs
@@ -1,10 +1,8 @@
 ﻿module Acronym
     
-open System
 open System.Text.RegularExpressions
 
 let abbreviate (phrase: string) =      
-    let words = Regex.Matches(phrase, "[A-Z]+[a-z]*|[a-z]+")
-    let acronymChar = string << Char.ToUpperInvariant << Seq.head
-    let acronymChars = [for word in words do yield acronymChar word.Value]
-    String.concat "" acronymChars
+    Regex.Matches(phrase, "[A-Z]+[a-z]*|[a-z]+")
+    |> Seq.map (fun m -> m.Value.Substring(0, 1).ToUpper())
+    |> String.concat ""
