4510307839f86c68c64a3783e2baf3e5ed4244fc
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-18 20:34:46 +0200

    fsharp/beer-song: add iteration

--- fsharp/beer-song/BeerSong.fs
+++ fsharp/beer-song/BeerSong.fs
@@ -1,21 +1,28 @@
 ﻿module BeerSong
 
-let private verse n = 
+let private bottlesOfBeer n =
     match n with
-    | 0 -> 
-        [ "No more bottles of beer on the wall, no more bottles of beer.";
-          "Go to the store and buy some more, 99 bottles of beer on the wall." ]
-    | 1 -> 
-        [ "1 bottle of beer on the wall, 1 bottle of beer.";
-          "Take it down and pass it around, no more bottles of beer on the wall." ]
-    | 2 -> 
-        [ "2 bottles of beer on the wall, 2 bottles of beer.";
-          "Take one down and pass it around, 1 bottle of beer on the wall." ]
-    | _ -> 
-        [ sprintf "%d bottles of beer on the wall, %d bottles of beer." n n;
-          sprintf "Take one down and pass it around, %d bottles of beer on the wall."  (n-1) ]
+    | 0 -> "No more bottles of beer"
+    | 1 -> "1 bottle of beer"
+    | _ -> $"{n} bottles of beer"
+    
+let private instruction n =
+    match n with
+    | 0 -> "Go to the store and buy some more"
+    | 1 -> "Take it down and pass it around"
+    | _ -> "Take one down and pass it around"
+
+let private firstVerseLine n =
+    let bottles = bottlesOfBeer n 
+    $"{bottles} on the wall, {bottles.ToLower()}."
+
+let private secondVerseLine n =
+    let bottles = bottlesOfBeer (if n = 0 then 99 else n - 1)
+    $"{instruction n}, {bottles.ToLower()} on the wall."
+
+let private verse n = [firstVerseLine n; secondVerseLine n]
 
 let recite startBottles takeDown = 
-    [startBottles .. -1 .. (startBottles - takeDown + 1)] 
+    [startBottles .. -1 .. (startBottles - takeDown + 1)]
     |> List.map verse
     |> List.reduce (fun x y -> x @ "" :: y)
