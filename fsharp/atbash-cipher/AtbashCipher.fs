9e9bcb8661e5e421b292f15aa0a58aac3c718032
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-18 17:51:53 +0200

    fsharp/atbash-cipher: add iteration

--- fsharp/atbash-cipher/AtbashCipher.fs
+++ fsharp/atbash-cipher/AtbashCipher.fs
@@ -1,29 +1,16 @@
 ﻿module AtbashCipher
 
-open System
+let private letters = ['a' .. 'z']
 
-let rec mapMaybe f list = 
-    match list with
-    | [] -> []
-    | x::xs -> 
-        match f x with
-        | Some(a) -> a :: mapMaybe f xs
-        | None -> mapMaybe f xs
+let private encodingMap = List.zip letters (List.rev letters) |> Map.ofList
 
-let stringFromChars (chars:char seq) = String(Array.ofSeq chars)
-let chunksOfSize n xs = 
-    xs 
-    |> Seq.mapi(fun i x -> i / n, x)
-    |> Seq.groupBy fst
-    |> Seq.map (fun (_, g) -> Seq.map snd g)
+let private encodeChar (c: char) = Map.tryFind c encodingMap |> Option.defaultValue c 
 
-let mapLetter letter = if Char.IsDigit letter then (letter, letter) else (letter, (char)((int)'z' - (int)letter + (int)'a'))
-let lettersMap = ['a' .. 'z'] @ ['0'..'9'] |> List.map mapLetter |> Map.ofList
-
-let encodeLetter letter = Map.tryFind (Char.ToLower letter) lettersMap
-let encodeInChunks = String.concat " " << Seq.map stringFromChars << chunksOfSize 5
-let encodeStr = encodeInChunks << mapMaybe encodeLetter
-    
-let encode (str:string) = str |> List.ofSeq |> encodeStr
+let encode (str:string) =
+    str.ToLower()
+    |> Seq.filter System.Char.IsLetterOrDigit
+    |> Seq.chunkBySize 5
+    |> Seq.map (fun chunk -> Seq.map encodeChar chunk |> Seq.toArray |> System.String)
+    |> String.concat " "
 
 let decode (str:string) = (encode str).Replace(" ", "")
