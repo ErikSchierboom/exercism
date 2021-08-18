60e1971fb15c7d22ac5de556296782af72fd6b6f
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-18 21:24:28 +0200

    fsharp/collatz-conjecture: add iteration

--- fsharp/collatz-conjecture/CollatzConjecture.fs
+++ fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -3,13 +3,9 @@
 let steps number = 
     let rec helper count current =
         match current with
-        | _ when current < 1 -> 
-            None
-        | 1 -> 
-            Some count
-        | _ when current % 2 = 0 ->
-            helper (count + 1) (current / 2)
-        | _ ->
-            helper (count + 1) (current * 3  + 1)
+        | _ when current < 1 ->  None
+        | 1 -> Some count
+        | _ when current % 2 = 0 -> helper (count + 1) (current / 2)
+        | _ -> helper (count + 1) (current * 3  + 1)
 
     helper 0 number
