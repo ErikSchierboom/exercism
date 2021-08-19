5404e9a659ebf773d6d3a8fcec819d0f7564bc26
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-19 13:31:18 +0200

    fsharp/hamming: add iteration

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -1,9 +1,10 @@
 ﻿module Hamming
 
-let distance (strand1: string) (strand2: string): int option = 
+let distance (strand1: string) (strand2: string) = 
     if strand1.Length <> strand2.Length then
         None
     else    
         Seq.zip strand1 strand2
-        |> Seq.sumBy (fun (n1, n2) -> if n1 = n2 then 0 else 1)
+        |> Seq.filter (fun (n1, n2) -> n1 <> n2)
+        |> Seq.length
         |> Some
