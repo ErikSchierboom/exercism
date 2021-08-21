35bc26c0e21a64b9219715a681c294ee8e98142b
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-21 15:10:58 +0200

    fsharp/diamond: add iteration

--- fsharp/diamond/Diamond.fs
+++ fsharp/diamond/Diamond.fs
@@ -1,16 +1,15 @@
 module Diamond
 
-let make letter =     
-        
+let make letter =
     let makeLine letterCount (row, letter) = 
         let outerSpaces  = "".PadRight(letterCount - row - 1)
         let innerSpaces = "".PadRight(if row = 0 then 0 else row * 2 - 1)
 
-        if letter = 'A' then sprintf "%s%c%s" outerSpaces letter outerSpaces
-        else sprintf "%s%c%s%c%s" outerSpaces letter innerSpaces letter outerSpaces
+        if letter = 'A' then $"{outerSpaces}{letter}{outerSpaces}"
+        else $"{outerSpaces}{letter}{innerSpaces}{letter}{outerSpaces}"
     
     let letters = ['A'..letter] |> List.mapi (fun x y -> x, y)
 
-    letters @ (letters |> List.rev |> List.tail)
-    |> List.map (makeLine letters.Length)
-    |> List.reduce (fun x y -> sprintf "%s\n%s" x y)
+    letters @ (List.rev letters |> List.tail)
+    |> Seq.map (makeLine letters.Length)
+    |> String.concat "\n"
