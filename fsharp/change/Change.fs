851153f3578b9d4cb7189f1e6b9066db4aa4bf09
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-21 15:05:49 +0200

    fsharp/change: add iteration

--- fsharp/change/Change.fs
+++ fsharp/change/Change.fs
@@ -1,18 +1,18 @@
 ﻿module Change
 
-let calculateFewestCoins coins map target =
+let private calculateFewestCoins coins map target =
     coins
-    |> List.filter (fun coin -> coin <= target) 
-    |> List.choose (fun coin -> Map.tryFind (target - coin) map |> Option.map (fun change -> coin :: change))
-    |> List.sortBy List.length
-    |> List.tryHead 
+    |> Seq.filter (fun coin -> coin <= target) 
+    |> Seq.choose (fun coin -> Map.tryFind (target - coin) map |> Option.map (fun change -> coin :: change))
+    |> Seq.sortBy List.length
+    |> Seq.tryHead 
 
-let updateFewestCoinsMap coins fewestCoinsMap amount = 
+let private updateFewestCoinsMap coins fewestCoinsMap amount = 
     match calculateFewestCoins coins fewestCoinsMap amount with
     | Some fewestCoins -> Map.add amount fewestCoins fewestCoinsMap
     | None -> fewestCoinsMap
 
 let findFewestCoins coins amount = 
     [1..amount]
-    |> List.fold (updateFewestCoinsMap coins) (Map.ofList [(0, [])])
+    |> Seq.fold (updateFewestCoinsMap coins) (Map.ofList [(0, [])])
     |> Map.tryFind amount
