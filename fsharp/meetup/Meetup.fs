fb349c1b6c80e792500018faf2f252fb1d375769
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-21 20:38:07 +0200

    fsharp/meetup: add iteration

--- fsharp/meetup/Meetup.fs
+++ fsharp/meetup/Meetup.fs
@@ -1,19 +1,17 @@
 ﻿module Meetup
 
-open System
-
 type Week = First | Second | Third | Fourth | Last | Teenth
 
 let meetup year month week dayOfWeek = 
     let daysOfWeek = 
-        [1 .. DateTime.DaysInMonth(year, month)]
-        |> List.map (fun day -> DateTime(year, month, day))
-        |> List.filter (fun date -> date.DayOfWeek = dayOfWeek)
+        [1 .. System.DateTime.DaysInMonth(year, month)]
+        |> Seq.map (fun day -> System.DateTime(year, month, day))
+        |> Seq.filter (fun date -> date.DayOfWeek = dayOfWeek)
                
     match week with
-    | Week.First  -> daysOfWeek |> List.item 0
-    | Week.Second -> daysOfWeek |> List.item 1
-    | Week.Third  -> daysOfWeek |> List.item 2
-    | Week.Fourth -> daysOfWeek |> List.item 3
-    | Week.Last   -> daysOfWeek |> List.last 
-    | Week.Teenth -> daysOfWeek |> List.find (fun date -> date.Day >= 13)
+    | Week.First  -> daysOfWeek |> Seq.item 0
+    | Week.Second -> daysOfWeek |> Seq.item 1
+    | Week.Third  -> daysOfWeek |> Seq.item 2
+    | Week.Fourth -> daysOfWeek |> Seq.item 3
+    | Week.Last   -> daysOfWeek |> Seq.last 
+    | Week.Teenth -> daysOfWeek |> Seq.find (fun date -> date.Day >= 13)
