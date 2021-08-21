4308e24194ad0867566a183ffae4ca2678334c86
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-21 10:29:40 +0200

    fsharp/roman-numerals: add iteration

--- fsharp/roman-numerals/RomanNumerals.fs
+++ fsharp/roman-numerals/RomanNumerals.fs
@@ -1,24 +1,28 @@
 ﻿module RomanNumerals
 
-let numeralThresholds = [(1000, "M");
-                            (900,  "CM");
-                            (500,  "D");
-                            (400,  "CD");
-                            (100,  "C");
-                            (90,   "XC");
-                            (50,   "L");
-                            (40,   "XL");
-                            (10,   "X");
-                            (9,    "IX");
-                            (5,    "V");
-                            (4,    "IV");
-                            (1,    "I")]
+let private decimalToNumeral =
+    [(1000, "M")
+     ( 900, "CM")
+     ( 500, "D")
+     ( 400, "CD")
+     ( 100, "C")
+     (  90, "XC")
+     (  50, "L")
+     (  40, "XL")
+     (  10, "X")
+     (   9, "IX")
+     (   5, "V")
+     (   4, "IV")
+     (   1, "I")]
 
-let rec toRomanLoop remainder acc thresholds =         
-    match thresholds with
-        | [] -> acc
-        | (threshold, numeral)::xs ->
-            if threshold <= remainder then toRomanLoop (remainder - threshold) (acc + numeral) thresholds
-            else toRomanLoop remainder acc xs
-
-let roman (arabicNumeral: int) = toRomanLoop arabicNumeral "" numeralThresholds
+let roman n =
+    let rec inner acc remainder thresholds =         
+        match thresholds with
+        | [] ->
+            acc
+        | (threshold, numeral)::_ when threshold <= remainder ->
+            inner (acc + numeral) (remainder - threshold) thresholds
+        | _::xs->
+            inner acc remainder xs
+    
+    inner "" n decimalToNumeral
