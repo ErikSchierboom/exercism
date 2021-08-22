8f91943eee9f466c0c9a4adcbbbeab95b9c9b6c2
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-22 13:25:14 +0200

    fsharp/perfect-numbers: add iteration

--- fsharp/perfect-numbers/PerfectNumbers.fs
+++ fsharp/perfect-numbers/PerfectNumbers.fs
@@ -2,10 +2,11 @@ module PerfectNumbers
 
 type Classification = Perfect | Abundant | Deficient 
 
-let sumOfFactors n = [for x in 1..n / 2 do if n % x = 0 then yield x] |> List.sum
-
-let classify n = 
-    match sumOfFactors n with
-    | x when x = n -> Perfect
-    | x when x < n -> Deficient
-    | _ -> Abundant
+let classify n =
+    if n < 1 then
+        None
+    else
+        let sumOfFactors = [for x in 1..n / 2 do if n % x = 0 then x] |> List.sum
+        if sumOfFactors < n then Some Deficient
+        elif sumOfFactors > n then Some Abundant
+        else Some Perfect
