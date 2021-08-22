0e6a268e5fa5486bf7f2af2b5aead0121919603b
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-22 11:17:21 +0200

    fsharp/parallel-letter-frequency: add iteration

--- fsharp/parallel-letter-frequency/ParallelLetterFrequency.fs
+++ fsharp/parallel-letter-frequency/ParallelLetterFrequency.fs
@@ -2,24 +2,25 @@ module ParallelLetterFrequency
 
 open System
 
-let merge x y = 
-    let folder acc key value =
-        match Map.tryFind key acc with
-        | Some z -> Map.add key (z + value) acc
-        | None -> Map.add key value acc
-    Map.fold folder x y
+module Map =
+    let merge x y = 
+        let folder acc key value =
+            match Map.tryFind key acc with
+            | Some z -> Map.add key (z + value) acc
+            | None -> Map.add key value acc
+        Map.fold folder x y
 
-let calculateFrequency text = 
-    text
-    |> Seq.filter (Char.IsLetter)
-    |> Seq.countBy (fun c -> Char.ToLower c)
-    |> Map.ofSeq
-
-let asyncCalculateFrequency text = async { return calculateFrequency(text) }
+let private asyncCalculateFrequency text = async {
+    return
+        text
+        |> Seq.filter Char.IsLetter
+        |> Seq.countBy Char.ToLower
+        |> Map.ofSeq
+}
 
 let frequency texts = 
     texts
     |> Seq.map asyncCalculateFrequency   
     |> Async.Parallel
     |> Async.RunSynchronously 
-    |> Seq.fold merge Map.empty
+    |> Seq.fold Map.merge Map.empty
