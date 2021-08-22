23e4bb222dce5b29b939fff2bf721e125eefae55
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-22 14:19:51 +0200

    fsharp/luhn: add iteration

--- fsharp/luhn/Luhn.fs
+++ fsharp/luhn/Luhn.fs
@@ -1,29 +1,16 @@
 module Luhn
-        
-let digit c = (int)c - (int)'0'
-let digits number = number.ToString() |> Seq.map digit
 
-let addend index digit = 
-    if (index % 2 = 0) then digit else 
-    if (digit >= 5) then digit * 2 - 9 
-    else digit * 2
+let private hasValidFormat (input: string) =
+    let normalized = String.filter (System.Char.IsWhiteSpace >> not) input
+    Seq.forall System.Char.IsDigit normalized && normalized.Length > 1
 
-let checkDigit (number:int64) = number % 10L
+let private checksum (input: string) =
+    let multipliers = Seq.init input.Length (fun i -> if (i + input.Length) % 2 = 0 then 2 else 1)
+    
+    String.filter System.Char.IsDigit input
+    |> Seq.map System.Globalization.CharUnicodeInfo.GetDigitValue
+    |> Seq.zip multipliers
+    |> Seq.map (fun (n, m) -> n * m)
+    |> Seq.sumBy (fun n -> if n > 9 then n - 9 else n)
 
-let addends (number:int64) = 
-    number 
-    |> digits 
-    |> List.ofSeq 
-    |> List.rev 
-    |> List.mapi addend 
-    |> List.rev
-
-let checksum (number:int64) = (number |> addends |> List.sum) % 10
-
-let valid (number:int64) = checksum number = 0
-
-let create (target:int64) = 
-    let targetNumber = target * 10L
-
-    if valid targetNumber then targetNumber
-    else targetNumber + 10L - (int64)(checksum targetNumber)
+let valid (input: string) = hasValidFormat input && checksum input % 10 = 0
