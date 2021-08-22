7c37808a95e38e4f11a10d8e752115fdfa965914
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-22 11:20:11 +0200

    fsharp/pascals-triangle: add iteration

--- fsharp/pascals-triangle/PascalsTriangle.fs
+++ fsharp/pascals-triangle/PascalsTriangle.fs
@@ -1,9 +1,5 @@
 ﻿module PascalsTriangle
     
-let rows count = 
-    let row i = 
-        [1 .. i - 1] 
-        |> List.scan (fun acc j -> acc * (i - j) / j) 1 
-   
-    [1..count] 
-    |> List.map row
+let rows size =
+    let row i = List.scan (fun acc j -> acc * (i - j) / j) 1 [1 .. i - 1]
+    if size < 0 then None else Some (List.map row [1..size])
