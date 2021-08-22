7e6e78a26159ac3dc279623f3c451a7ce42df687
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-22 14:50:27 +0200

    fsharp/robot-name: add iteration

--- fsharp/robot-name/RobotName.fs
+++ fsharp/robot-name/RobotName.fs
@@ -1,20 +1,16 @@
 module RobotName
 
-let random = System.Random()
-
 type Robot = { name: string }
 
-let letters = ['A'..'Z']
-let digits = ['0'..'9']
+module Array =
+    let private r = System.Random()
+    
+    let random (arr: 'T[]) = arr.[r.Next(arr.Length)]
+    
+    let sample n (arr: 'T[]) = Array.init n (fun _ -> random arr)
 
-let NumberOfLetters = 2;
-let NumberOfDigits = 3    
-
-let takeRandomElements xs length = List.init length (fun _ -> List.item (random.Next(List.length xs)) xs)
-let generateRandomString chars length = new System.String(takeRandomElements chars length |> List.toArray)
-let generateLetters() = generateRandomString letters NumberOfLetters
-let generateDigits() = generateRandomString digits NumberOfDigits
-let generateName() = generateLetters() + generateDigits()
+let private generateName() =
+    System.String(Array.append (Array.sample 2 [|'A'..'Z'|]) (Array.sample 3 [|'0'..'9'|]))
 
 let mkRobot() = { name = generateName() }
 
