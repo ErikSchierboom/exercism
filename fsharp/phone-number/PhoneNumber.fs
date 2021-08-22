c01949b5a7973df967506b85c0dfe44d65b5839e
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-22 14:27:23 +0200

    fsharp/phone-number: add iteration

--- fsharp/phone-number/PhoneNumber.fs
+++ fsharp/phone-number/PhoneNumber.fs
@@ -1,11 +1,12 @@
 ﻿module PhoneNumber
 
-open System
+let clean input = 
+    let digits = String.filter System.Char.IsDigit input
+    let digitsMinusCountryCode = if digits.[0] = '1' then digits.[1..] else digits
 
-let clean (input: string) = 
-    let digits = input.ToCharArray() |> Array.filter (Char.IsDigit) |> String
-
-    match digits.Length with
-    | 10 -> Some digits
-    | 11 when digits.Chars 0 = '1' ->  Some (digits.Substring(1))
-    | _ -> None
+    if digitsMinusCountryCode.Length = 10 &&
+        digitsMinusCountryCode.[0] <> '0' && digitsMinusCountryCode.[0] <> '1' &&
+        digitsMinusCountryCode.[3] <> '0' && digitsMinusCountryCode.[3] <> '1' then
+        Some digitsMinusCountryCode
+    else
+        None
