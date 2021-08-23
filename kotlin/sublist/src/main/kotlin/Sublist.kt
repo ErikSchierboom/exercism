1441524f6533359eb60aec95f898920ada9eabf0
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-23 19:52:44 +0200

    kotlin/sublist: add iteration

--- kotlin/sublist/src/main/kotlin/Sublist.kt
+++ kotlin/sublist/src/main/kotlin/Sublist.kt
@@ -1,11 +1,12 @@
 fun <T> List<T>.relationshipTo(other: List<T>) =
     when {
-        size > other.size -> if (other sublistOf this) Relationship.SUPERLIST else Relationship.UNEQUAL
-        size < other.size -> if (this sublistOf other) Relationship.SUBLIST else Relationship.UNEQUAL
-        else -> if (this == other) Relationship.EQUAL else Relationship.UNEQUAL
+        this == other -> Relationship.EQUAL
+        this.isSublistOf(other) -> Relationship.SUBLIST
+        other.isSublistOf(this) -> Relationship.SUPERLIST
+        else -> Relationship.UNEQUAL
     }
 
-private infix fun <T> List<T>.sublistOf(other: List<T>) =
+private fun <T> List<T>.isSublistOf(other: List<T>) =
     when {
         isEmpty() -> true
         size > other.size -> false
