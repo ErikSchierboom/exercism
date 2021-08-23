a1a7de664b9e9c8d16df8ddb6fda7b39745d6c7d
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-23 20:08:43 +0200

    kotlin/kindergarten-garden: add iteration

--- kotlin/kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
+++ kotlin/kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
@@ -3,7 +3,7 @@ val STUDENTS = arrayOf("Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny
 
 class KindergartenGarden(diagram: String) {
     val plantsForStudent = diagram.lines()
-        .map { line -> line.map { code -> PLANTS.first { it[0].uppercase() == code.toString() } } }
+        .map { line -> line.map { code -> PLANTS.first { it[0].uppercaseChar() == code } } }
         .let { it[0].chunked(2).zip(it[1].chunked(2), { top, bottom -> top + bottom }) }
         .mapIndexed { i, plants -> STUDENTS[i] to plants}
         .toMap()
