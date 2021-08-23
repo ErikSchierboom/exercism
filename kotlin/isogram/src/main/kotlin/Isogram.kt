7d6d87e0b07f2b44c2f24cde70d5cabdef812110
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-23 17:53:12 +0200

    kotlin/isogram: add iteration

--- kotlin/isogram/src/main/kotlin/Isogram.kt
+++ kotlin/isogram/src/main/kotlin/Isogram.kt
@@ -1,10 +1,6 @@
 object Isogram {
     fun isIsogram(input: String): Boolean {
-        val letters = input
-                .toLowerCase()
-                .toCharArray()
-                .filter(Character::isLetter)
-
-        return letters.distinct().count() == letters.count()
+        val letters = input.lowercase().filter(Character::isLetter)
+        return letters.toSet().count() == letters.count()
     }
 }
