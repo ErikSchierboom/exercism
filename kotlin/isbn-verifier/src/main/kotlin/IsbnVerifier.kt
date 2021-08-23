68b3da92b45cc9b2d600244f8694023b05d06c64
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-23 18:09:15 +0200

    kotlin/isbn-verifier: add iteration

--- kotlin/isbn-verifier/src/main/kotlin/IsbnVerifier.kt
+++ kotlin/isbn-verifier/src/main/kotlin/IsbnVerifier.kt
@@ -5,6 +5,5 @@ class IsbnVerifier {
     private fun String.hasValidFormat() = matches("""^\d{9}(\d|X)$""".toRegex())
     private fun String.hasValidChecksum() = checksum().rem(11) == 0
     private fun String.checksum() = mapIndexed { i, c -> c.toDigit().times(10 - i) }.sum()
-
-    private fun Char.toDigit() = if (this == 'X') 10 else Character.getNumericValue(this)
+    private fun Char.toDigit() = if (this == 'X') 10 else this.digitToInt()
 }
