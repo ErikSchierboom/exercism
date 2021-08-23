d9a8703399035a6153edd131ea212498db167c84
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-23 18:18:35 +0200

    kotlin/armstrong-numbers: add iteration

--- kotlin/armstrong-numbers/src/main/kotlin/ArmstrongNumber.kt
+++ kotlin/armstrong-numbers/src/main/kotlin/ArmstrongNumber.kt
@@ -3,7 +3,7 @@ import kotlin.math.pow
 object ArmstrongNumber {
     fun check(input: Int) = input.armstrongSum() == input
 
-    private fun Int.armstrongSum() = digits().run { sumBy { it.pow(size) } }
-    private fun Int.digits() = toString().map(Character::getNumericValue)
+    private fun Int.armstrongSum() = digits().run { sumOf { it.pow(size) } }
+    private fun Int.digits() = toString().map(Char::digitToInt)
     private fun Int.pow(n: Int) = toDouble().pow(n).toInt()
 }
