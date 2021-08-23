218ad8bc6fea8d527e7f88395c8652fcda71c9b2
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-23 16:41:31 +0200

    kotlin/anagram: add iteration

--- kotlin/anagram/src/main/kotlin/Anagram.kt
+++ kotlin/anagram/src/main/kotlin/Anagram.kt
@@ -2,6 +2,6 @@ class Anagram(private val target: String) {
     fun match(candidates: Set<String>) = candidates.filter { it.anagramOf(target) }.toSet()
 
     private fun String.anagramOf(other: String) = sortedLowerCase() == other.sortedLowerCase() && notEqualTo(other)
-    private fun String.sortedLowerCase() = toLowerCase().asIterable().sorted()
+    private fun String.sortedLowerCase() = lowercase().asIterable().sorted()
     private fun String.notEqualTo(other: String) = !equals(other, ignoreCase = true)
 }
