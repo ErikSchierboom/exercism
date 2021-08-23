9164c2f95059cd5b38b76c6dc367da21c38db1d9
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-23 11:04:01 +0200

    kotlin/acronym: add iteration

--- kotlin/acronym/src/main/kotlin/Acronym.kt
+++ kotlin/acronym/src/main/kotlin/Acronym.kt
@@ -2,5 +2,5 @@ object Acronym {
     fun generate(phrase: String) = phrase.parts().map { it.acronymLetter() }.joinToString("")
 
     private fun String.parts() = "[A-Z]+[a-z']*|[a-z]+".toRegex().findAll(this).map { it.value }
-    private fun String.acronymLetter() = this[0].toUpperCase()
+    private fun String.acronymLetter() = this[0].uppercase()
 }
