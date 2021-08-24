e4f348aa291fae05d060987260c77cf38a8d337b
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-24 17:43:02 +0200

    kotlin/saddle-points: add iteration

--- kotlin/saddle-points/src/main/kotlin/Matrix.kt
+++ kotlin/saddle-points/src/main/kotlin/Matrix.kt
@@ -2,15 +2,14 @@ data class MatrixCoordinate(val row: Int, val col: Int)
 
 class Matrix(input: List<List<Int>>) {
     val saddlePoints: Set<MatrixCoordinate> by lazy {
-        val rows = input.size
-        val columns = input.getOrElse(0) { emptyList() }.size
+        val rowMax = input.map { it.maxOrNull() ?: 0 }
+        val colMin = input.transpose().map { it.minOrNull() ?: 0 }
 
-        val rowMax = input.map { it.max() }
-        val colMin = 0.until(columns).map { col -> 0.until(rows).map { row -> input[row][col] }.min() }
-
-        1.rangeTo(rows)
-            .flatMap { row -> 1.rangeTo(columns).map { col -> MatrixCoordinate(row, col) } }
+        rowMax.indices
+            .flatMap { row -> colMin.indices.map { col -> MatrixCoordinate(row + 1, col + 1) } }
             .filter { rowMax[it.row - 1] == colMin[it.col - 1] }
             .toSet()
     }
+
+    private fun List<List<Int>>.transpose() = first().indices.map { col -> map { it[col] } }
 }
