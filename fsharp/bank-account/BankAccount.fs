2e1b349539cf919af7d645724fe8dd1776669fe2
Erik Schierboom <erik_schierboom@hotmail.com>
2021-09-02 09:15:45 +0200

    fsharp/bank-account: add iteration

--- fsharp/bank-account/BankAccount.fs
+++ fsharp/bank-account/BankAccount.fs
@@ -3,27 +3,24 @@
 open System
 
 type BankAccount() = 
-    member val Lock = new Object()
+    member val Lock = Object()
     member val Balance: decimal option = None with get,set
 
 let mkBankAccount() = BankAccount()
 
+let getBalance (account: BankAccount) = account.Balance
+
 let openAccount (account: BankAccount) = 
     lock account.Lock (fun () ->
         account.Balance <- Some 0.0m
-        account
-    )
+        account)
 
 let closeAccount (account: BankAccount) = 
     lock account.Lock (fun () ->
         account.Balance <- None    
-        account
-    )
-
-let getBalance (account: BankAccount) = account.Balance
+        account)
 
 let updateBalance change (account: BankAccount) = 
     lock account.Lock (fun () ->
         account.Balance <- Option.map ((+) change) account.Balance    
-        account
-    )
+        account)
