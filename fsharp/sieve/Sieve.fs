bc24673c7f9d44f8dfacbec34ee3af354d27534e
Erik Schierboom <erik_schierboom@hotmail.com>
2021-09-02 09:16:44 +0200

    fsharp/sieve: add iteration

--- fsharp/sieve/Sieve.fs
+++ fsharp/sieve/Sieve.fs
@@ -5,6 +5,4 @@ let rec private sieve remainder primes =
     | [] -> primes |> List.rev
     | p::xs -> sieve (xs |> List.filter (fun x -> x % p > 0)) (p :: primes)
     
-let primes limit = 
-    let possiblePrimes = [2 .. limit]    
-    sieve possiblePrimes []
+let primes limit = sieve [2 .. limit] []
