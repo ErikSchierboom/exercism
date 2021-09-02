b5cd5da9e75f5fb7be11ad9d6d9e887b85421be8
Erik Schierboom <erik_schierboom@hotmail.com>
2021-09-02 09:20:52 +0200

    fsharp/queen-attack: add iteration

--- fsharp/queen-attack/QueenAttack.fs
+++ fsharp/queen-attack/QueenAttack.fs
@@ -1,11 +1,8 @@
 ﻿module QueenAttack
 
-open System
-
-let abs (x:int) = Math.Abs x
-
 let create (row, col) = row >= 0 && row < 8 && col >= 0 && col < 8
 
 let canAttack (row1, col1) (row2, col2) =
+    let abs (x:int) = System.Math.Abs x
     if row1 = row2 && col1 = col2 then false
     else row1 = row2 || col1 = col2 || (row1 - row2 |> abs) = (col1 - col2 |> abs)
