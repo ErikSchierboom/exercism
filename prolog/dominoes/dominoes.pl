f656a98e77dc81ef868f04cc533530d72979eb16
Erik Schierboom <erik_schierboom@hotmail.com>
2021-09-13 10:20:26 +0200

    prolog/dominoes: add iteration

--- prolog/dominoes/dominoes.pl
+++ prolog/dominoes/dominoes.pl
@@ -1,13 +1,9 @@
-rotate(L,N,A) :- length(P,N), append(P,S,L), append(S,P,A).
-
-rotations(L, C) :- length(L,N), M is N-1, between(0,M,B), rotate(L,B,C).
-
-chain([(L1,X),(X,R1)|T], [(L1,R1)|T]).
-chain([(L1,X),(L2,X)|T], [(L1,L2)|T]).
+chain((L1,X),(X,R1), (L1,R1)).
+chain((L1,X),(L2,X), (L1,L2)).
 
 can_chain([]).
 can_chain([(X,X)]).
 can_chain([H|T]) :-
-    rotations(T, R),
-    chain([H|R], C),
-    can_chain(C), !.
+    select(X,T,R),
+    chain(H,X,C),
+    can_chain([C|R]), !.
