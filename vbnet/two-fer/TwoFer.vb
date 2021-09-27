cbce1700a6863bd6d0b35ee9a88f63013c957995
Erik Schierboom <erik_schierboom@hotmail.com>
2021-09-27 15:48:16 +0200

    vbnet/two-fer: add iteration

--- vbnet/two-fer/TwoFer.vb
+++ vbnet/two-fer/TwoFer.vb
@@ -1,5 +1,5 @@
 Public Module TwoFer
-    Public Function Speak(Optional ByVal name As String = "you") As String
+    Public Function Speak(Optional name As String = "you") As String
         return $"One for {name}, one for me."
     End Function
 End Module
