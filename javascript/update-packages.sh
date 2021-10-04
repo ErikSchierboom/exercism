382118b3c8e5b9daf870f3cff0c41c97794924b4
Erik Schierboom <erik_schierboom@hotmail.com>
2021-10-04 20:58:21 +0200

    javascript: commit from update-packages.sh

--- javascript/update-packages.sh
+++ javascript/update-packages.sh
@@ -1,1 +1,5 @@
 find . -mindepth 1 -maxdepth 1 -type d -execdir sh -c 'pushd {} && npx npm-check-updates -u && npm install && popd' \;
+git add .
+git commit -m "javascript: update packages"
+git push
+
