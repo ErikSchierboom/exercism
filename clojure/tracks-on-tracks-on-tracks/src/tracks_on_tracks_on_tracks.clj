0b9aa042dade999be7b028b4b32db7ef241ba445
Erik Schierboom <erik_schierboom@hotmail.com>
2021-10-04 20:34:51 +0200

    clojure/tracks-on-tracks-on-tracks: add solution

---
+++ clojure/tracks-on-tracks-on-tracks/src/tracks_on_tracks_on_tracks.clj
@@ -0,0 +1,26 @@
+(ns tracks-on-tracks-on-tracks)
+
+(defn new-list []
+  '())
+
+(defn add-language [lang lang-list]
+  (cons lang lang-list))
+
+(defn first-language [lang-list]
+  (first lang-list))
+
+(defn remove-language [lang-list]
+  (rest lang-list))
+
+(defn count-languages [lang-list]
+  (count lang-list)) 
+
+(defn learning-list []
+  (->>
+    (new-list)
+    (add-language "Clojure")
+    (add-language "Lisp")
+    (remove-language)
+    (add-language "Java")
+    (add-language "JavaScript")
+    (count-languages)))
