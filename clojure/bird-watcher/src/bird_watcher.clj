4a7769ae147c69e4bc7c621815feff6cf92ed4ca
Erik Schierboom <erik_schierboom@hotmail.com>
2021-10-04 21:06:02 +0200

    clojure/bird-watcher: add solution

---
+++ clojure/bird-watcher/src/bird_watcher.clj
@@ -0,0 +1,21 @@
+(ns bird-watcher)
+
+(def last-week [0 2 5 3 7 8 4])
+
+(defn today [birds]
+  (last birds))
+
+(defn inc-bird [birds]
+  (conj (pop birds) (inc (peek birds))))
+
+(defn day-without-birds? [birds]
+  (boolean (some zero? birds)))
+
+(defn n-days-count [birds n]
+  (apply + (take n birds)))
+
+(defn busy-days [birds]
+  (count (filter #(>= % 5) birds)))
+
+(defn odd-week? [birds]
+  (= birds [1 0 1 0 1 0 1]))
