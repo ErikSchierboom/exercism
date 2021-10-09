08af870355293b682cc5f7318bb57182cc7aad9c
Erik Schierboom <erik_schierboom@hotmail.com>
2021-10-09 09:20:48 +0200

    clojurescript/anagram: add solution

---
+++ clojurescript/anagram/src/anagram.cljs
@@ -0,0 +1,12 @@
+(ns anagram
+  (:require [clojure.string :as str]))
+
+(defn anagram-of? [word candidate]
+  (let [lower-word (str/lower-case word)
+        lower-candidate (str/lower-case candidate)]
+    (and
+      (not= lower-word lower-candidate)
+      (= (sort lower-word) (sort lower-candidate)))))
+
+(defn anagrams-for [word prospect-list]
+  (filter (partial anagram-of? word) prospect-list))
