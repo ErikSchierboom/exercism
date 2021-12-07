d66f58996e0057179425ee1f0a387a685e16a28b
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-07 16:23:45 +0100

    csharp/variable-length-quantity: fix filename

--- csharp/variable-length-quantity/VariableLengthQuantity.cs
+++ csharp/variable-length-quantity/VariableLengthQuantity.cs
@@ -1,14 +1,66 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class VariableLengthQuantity
 {
-    public static uint[] Encode(uint[] numbers)
+    private const uint SevenBitsMask = 0x7fu;
+    private const uint EightBitMask = 0x80u;
+
+    public static uint[] ToBytes(uint[] numbers) => numbers.SelectMany(ToBytesSingle).ToArray();
+
+    private static IEnumerable<uint> ToBytesSingle(uint number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (number == 0)
+        {
+            return new[] { 0u };
+        }
+
+        var bytes = new List<uint>(4);
+
+        while (number > 0)
+        {
+            var tmp = number & SevenBitsMask;
+            number >>= 7;
+
+            if (bytes.Any())
+            {
+                tmp |= EightBitMask;
+            }
+
+            bytes.Add(tmp);
+        }
+
+        bytes.Reverse();
+        return bytes;
     }
 
-    public static uint[] Decode(uint[] bytes)
+    public static uint[] FromBytes(uint[] bytes)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var numbers = new List<uint>();
+        var tmp = 0u;
+
+        for (var index = 0; index < bytes.Length; index++)
+        {
+            if ((tmp & 0xfe000000u) > 0)
+            {
+                throw new InvalidOperationException("Overflow exception.");
+            }
+
+            var b = bytes[index];
+            tmp = (tmp << 7) | (b & 0x7fu);
+
+            if ((b & 0x80) == 0)
+            {
+                numbers.Add(tmp);
+                tmp = 0;
+            }
+            else if (index == bytes.Length - 1)
+            {
+                throw new InvalidOperationException("Incomplete byte sequence.");
+            }
+        }
+        
+        return numbers.ToArray();
     }
 }
