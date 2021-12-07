68d937d4b176280a0ea33a46901826b6595d3431
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-07 16:05:54 +0100

    fsharp/tree-building: fix filename

--- fsharp/tree-building/TreeBuilding.fs
+++ fsharp/tree-building/TreeBuilding.fs
@@ -1,72 +1,39 @@
-﻿// This is the file you need to modify for your own solution.
-// The unit tests will use this code, and it will be used by the benchmark tests
-// for the "Mine" row of the summary table.
+﻿module TreeBuilding
 
-// Remember to not only run the unit tests for this exercise, but also the
-// benchmark tests using `dotnet run -c Release`.
-// Please refer to the instructions for more information about the benchmark tests.
-
-module TreeBuilding
-
-open TreeBuildingTypes
-
-type Tree =
+type Record = { RecordId: int; ParentId: int }
+type Tree = 
     | Branch of int * Tree list
     | Leaf of int
 
-let recordId t =
-    match t with
-    | Branch (id, c) -> id
-    | Leaf id -> id
+let addOrAppend key value map =
+    let list = defaultArg (Map.tryFind key map) []
+    Map.add key (list @ [value]) map
 
-let isBranch t =
-    match t with
-    | Branch (id, c) -> true
-    | Leaf id -> false
+let invalidNode previous x = 
+    x.ParentId >= x.RecordId || 
+    x.RecordId <> previous + 1
 
-let children t =
-    match t with
-    | Branch (id, c) -> c
-    | Leaf id -> []
+let rec recordsToMap previous map remainder =
+    match remainder with
+    | [] -> 
+        map
+    | x::_ when invalidNode previous x ->
+        failwith "Invalid record"
+    | x::xs ->
+        let updatedMap = addOrAppend x.ParentId x.RecordId map
+        recordsToMap x.RecordId updatedMap xs
 
-let buildTree records =
-    let records' = List.sortBy (fun x -> x.RecordId) records
+let rec mapToTree map recordId =
+    match Map.tryFind recordId map with
+    | Some x -> Branch (recordId, x |> List.map (mapToTree map))
+    | None   -> Leaf recordId        
 
-    if List.isEmpty records' then failwith "Empty input"
-    else
-        let root = records'.[0]
-        if (root.ParentId = 0 |> not) then
-            failwith "Root node is invalid"
-        else
-            if (root.RecordId = 0 |> not) then failwith "Root node is invalid"
-            else
-                let mutable prev = -1
-                let mutable leafs = []
+let sortRecords records = List.sortBy (fun x -> x.RecordId) records
 
-                for r in records' do
-                    if (r.RecordId <> 0 && (r.ParentId > r.RecordId || r.ParentId = r.RecordId)) then
-                        failwith "Nodes with invalid parents"
-                    else
-                        if r.RecordId <> prev + 1 then
-                            failwith "Non-continuous list"
-                        else
-                            prev <- r.RecordId
-                            if (r.RecordId = 0) then
-                                leafs <- leafs @ [(-1, r.RecordId)]
-                            else
-                                leafs <- leafs @ [(r.ParentId, r.RecordId)]
-
-                let root = leafs.[0]
-
-                let grouped = leafs |> List.groupBy fst |> List.map (fun (x, y) -> (x, List.map snd y))
-                let parents = List.map fst grouped
-                let map = grouped |> Map.ofSeq
-
-                let rec helper key =
-                    if Map.containsKey key map then
-                        Branch (key, List.map (fun i -> helper i) (Map.find key map))
-                    else
-                        Leaf key
-
-                let root = helper 0
-                root
+let buildTree records = 
+    match records with
+    | [] -> 
+        failwith "Empty input"
+    | _ ->
+        let parentChildrenMap = recordsToMap -1 Map.empty (sortRecords records)
+        mapToTree parentChildrenMap 0
