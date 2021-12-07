ecd3279480c46a1965dc11bfb9313de3a40643e8
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-07 16:02:58 +0100

    fsharp/difference-of-squares: fix filename

--- fsharp/difference-of-squares/Example.fs
+++
@@ -1,7 +0,0 @@
-module DifferenceOfSquares
-
-let square x = x * x
-
-let squareOfSums number = [1..number] |> List.sum |> square
-let sumOfSquares number = [1..number] |> List.sumBy square
-let difference number = squareOfSums number - sumOfSquares number
