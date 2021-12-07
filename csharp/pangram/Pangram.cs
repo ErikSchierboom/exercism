3e488159db308a0002bfb301d8cf9ca522208782
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-07 16:24:02 +0100

    csharp/pangram: fix filename

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,9 +1,12 @@
-﻿using System;
+using System.Linq;
 
 public static class Pangram
 {
+    private const string Letters = "abcdefghijklmnopqrstuvwxyz";
+
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+        var lowerCaseInput = input.ToLower();
+        return Letters.All(letter => lowerCaseInput.Contains(letter));
     }
 }
