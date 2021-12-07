d4627487458920e607bad06552be38633787b7fa
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-07 16:23:41 +0100

    csharp/proverb: fix filename

--- csharp/proverb/Proverb.cs
+++ csharp/proverb/Proverb.cs
@@ -1,9 +1,18 @@
-﻿using System;
+using System.Linq;
 
 public static class Proverb
 {
-    public static string[] Recite(string[] subjects)
+    private static readonly string[] Subjects = { "nail", "shoe", "horse", "rider", "message", "battle", "kingdom" };
+
+    public static string Line(int line)
     {
-        throw new NotImplementedException();
+        if (line == 7)
+        { 
+            return "And all for the want of a horseshoe nail.";
+        }
+
+        return $"For want of a {Subjects[line - 1]} the {Subjects[line]} was lost.";
     }
+
+    public static string AllLines() => string.Join("\n", Enumerable.Range(1, 7).Select(Line));
 }
