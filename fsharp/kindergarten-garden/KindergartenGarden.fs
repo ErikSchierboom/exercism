8009699dc9b30493fd26fc5d7e6fa276fb06a5ba
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-07 16:02:55 +0100

    fsharp/kindergarten-garden: fix filename

--- fsharp/kindergarten-garden/KindergartenGarden.fs
+++ fsharp/kindergarten-garden/KindergartenGarden.fs
@@ -1,8 +1,13 @@
-﻿module KindergartenGarden
+module KinderGartenGarden
 
 type Plant = Violets | Radishes | Clover | Grass
 
-let private students = ["Alice"; "Bob"; "Charlie"; "David"; "Eve"; "Fred"; "Ginny"; "Harriet"; "Ileana"; "Joseph"; "Kincaid"; "Larry"]
+let plantsPerChildPerRow = 2
+let rowSeparator = '\n'
+
+let private defaultChildren = ["Alice"; "Bob"; "Charlie"; "David";
+                               "Eve"; "Fred"; "Ginny"; "Harriet";
+                               "Ileana"; "Joseph"; "Kincaid"; "Larry"]
 
 let private plantFromCode code =
     match code with
@@ -10,13 +15,21 @@ let private plantFromCode code =
     | 'R' -> Plant.Radishes
     | 'C' -> Plant.Clover
     | 'G' -> Plant.Grass
-    | x -> failwithf $"{x} is an invalid plant code"
+    | x -> failwithf "%c is an invalid plant code" x
 
-let plants (windowSills: string) student =
-    windowSills.Split()
-    |> Array.map (Seq.map plantFromCode)
-    |> fun rows -> Seq.zip (Seq.chunkBySize 2 rows.[0]) (Seq.chunkBySize 2 rows.[1])
-    |> Seq.map (fun (left, right) -> Seq.append left right |> Seq.toList)
-    |> Seq.zip students
+let private plantsInRow (row: string) = 
+    Seq.map plantFromCode row 
+    |> Seq.chunkBySize plantsPerChildPerRow
+    |> Seq.map List.ofArray
+
+let garden children (windowSills: string) =     
+    let rows = windowSills.Split rowSeparator
+    let row1 = plantsInRow rows.[0]
+    let row2 = plantsInRow rows.[1]
+    Seq.zip3 (children |> List.sort) row1 row2 
+    |> Seq.map (fun (child, plants1, plants2) -> (child, plants1 @ plants2))
     |> Map.ofSeq
-    |> Map.find student
+
+let defaultGarden windowSills = garden defaultChildren windowSills
+
+let lookupPlants child childPlantsMap = defaultArg (Map.tryFind child childPlantsMap) []
