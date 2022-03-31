37704edcf090d36a464ede3c027e9aca4dc30ce5
Erik Schierboom <erik_schierboom@hotmail.com>
2022-03-31 12:00:17 +0200

    Lots of refactoring

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -1,7 +1,5 @@
 module FoodChain
-  def self.song
-    NUMBER_OF_VERSES.times.map { |n| Verse.new(n + 1) }.join("#{$/}")
-  end
+  def self.song = NUMBER_OF_VERSES.times.map { |n| Verse.new(n + 1) }.join($/)
 
   class Verse
     def initialize(number)
@@ -9,28 +7,19 @@ module FoodChain
     end
 
     def to_s
-      "#{fact}#{$/}#{embellishment}#{conclusion}#{$/}"
+      <<~VERSE
+        #{fact}
+        #{conclusion}
+      VERSE
     end
 
     private
+
     attr_reader :number
 
-    def fact
-      case number
-      when 1
-        "I know an old lady who swallowed a fly."
-      when 8
-        "I know an old lady who swallowed a horse."
-      else
-        (subject, followup) = SUBJECTS[number - 2]
-        "I know an old lady who swallowed a #{subject}."
-      end
-    end
-
-    def embellishment
-      (subject, followup) = SUBJECTS[number - 2]
-      "#{followup}#{$/}" if number > 1 && number < 8
-    end
+    def fact = "I know an old lady who swallowed a #{subject}.#{$/}#{comment}".strip
+    def subject = SUBJECTS[number - 1][0]
+    def comment = SUBJECTS[number - 1][1]
 
     def conclusion
       if number == 8
@@ -41,22 +30,25 @@ module FoodChain
     end
 
     SUBJECTS = [
+      ['fly', nil],
       ['spider', 'It wriggled and jiggled and tickled inside her.'],
-      ['bird',   'How absurd to swallow a bird!'],
-      ['cat',    'Imagine that, to swallow a cat!'],
-      ['dog',    'What a hog, to swallow a dog!'],
-      ['goat',   'Just opened her throat and swallowed a goat!'],
-      ['cow',    "I don't know how she swallowed a cow!"]
+      ['bird', 'How absurd to swallow a bird!'],
+      ['cat', 'Imagine that, to swallow a cat!'],
+      ['dog', 'What a hog, to swallow a dog!'],
+      ['goat', 'Just opened her throat and swallowed a goat!'],
+      ['cow', 'I don\'t know how she swallowed a cow!'],
+      ['horse', nil]
     ].freeze
-    HISTORY =
-      ["I don't know how she swallowed a cow!",
-       'She swallowed the cow to catch the goat.',
-       'She swallowed the goat to catch the dog.',
-       'She swallowed the dog to catch the cat.',
-       'She swallowed the cat to catch the bird.',
-       'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.',
-       'She swallowed the spider to catch the fly.',
-       "I don't know why she swallowed the fly. Perhaps she'll die."].freeze
+    HISTORY = [
+      "I don't know how she swallowed a cow!",
+      'She swallowed the cow to catch the goat.',
+      'She swallowed the goat to catch the dog.',
+      'She swallowed the dog to catch the cat.',
+      'She swallowed the cat to catch the bird.',
+      'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.',
+      'She swallowed the spider to catch the fly.',
+      "I don't know why she swallowed the fly. Perhaps she'll die."
+    ].freeze
 
     private_constant :SUBJECTS, :HISTORY
   end
