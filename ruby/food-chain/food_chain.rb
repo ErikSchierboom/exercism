49f631e49e76b3edd14f95ee572e16bd3f926e44
Erik Schierboom <erik_schierboom@hotmail.com>
2022-04-04 11:07:12 +0200

    Morte work

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -1,58 +1,37 @@
 module FoodChain
-  def self.song = NUMBER_OF_VERSES.times.map { |n| Verse.new(n + 1) }.join($/)
+  def self.song = 1.upto(NUMBER_OF_VERSES).map(&Verse.method(:new)).join($/)
 
   class Verse
-    def initialize(number)
-      @number = number
-    end
+    def initialize(number) = @number = number
 
-    def to_s
-      <<~VERSE
-        #{fact}
-        #{conclusion}
-      VERSE
-    end
+    def to_s = [fact, embellishment, swallows, conclusion].compact.join($/) + $/
 
     private
 
     attr_reader :number
 
-    def fact = "I know an old lady who swallowed a #{subject}.#{$/}#{comment}".strip
-
+    def fact = "I know an old lady who swallowed a #{subject}."
     def subject = SUBJECTS[number - 1][0]
+    def embellishment = SUBJECTS[number - 1][1]
+    def conclusion = SUBJECTS[number - 1][2] || "I don't know why she swallowed the fly. Perhaps she'll die."
+    def swallows
+      return if number == 8 || number == 1
 
-    def comment = SUBJECTS[number - 1][1]
-
-    def conclusion
-      if number == 8
-        "She's dead, of course!"
-      else
-        HISTORY[-number..].join($/)
-      end
+      (number - 1).downto(1).map {|n| "She swallowed the #{SUBJECTS[n][0]} to catch the #{SUBJECTS[n - 1][0]}#{SUBJECTS[n - 1][3]}."}.join($/)
     end
 
     SUBJECTS = [
-      ['fly', nil],
-      ['spider', 'It wriggled and jiggled and tickled inside her.'],
-      ['bird', 'How absurd to swallow a bird!'],
-      ['cat', 'Imagine that, to swallow a cat!'],
-      ['dog', 'What a hog, to swallow a dog!'],
-      ['goat', 'Just opened her throat and swallowed a goat!'],
-      ['cow', 'I don\'t know how she swallowed a cow!'],
-      ['horse', nil]
-    ].freeze
-    HISTORY = [
-      "I don't know how she swallowed a cow!",
-      'She swallowed the cow to catch the goat.',
-      'She swallowed the goat to catch the dog.',
-      'She swallowed the dog to catch the cat.',
-      'She swallowed the cat to catch the bird.',
-      'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.',
-      'She swallowed the spider to catch the fly.',
-      "I don't know why she swallowed the fly. Perhaps she'll die."
+      ['fly', nil, nil, nil],
+      ['spider', 'It wriggled and jiggled and tickled inside her.', nil, ' that wriggled and jiggled and tickled inside her'],
+      ['bird', 'How absurd to swallow a bird!', nil, nil],
+      ['cat', 'Imagine that, to swallow a cat!', nil, nil],
+      ['dog', 'What a hog, to swallow a dog!', nil, nil],
+      ['goat', 'Just opened her throat and swallowed a goat!', nil, nil],
+      ['cow', 'I don\'t know how she swallowed a cow!', nil, nil],
+      ['horse', nil, 'She\'s dead, of course!', nil]
     ].freeze
 
-    private_constant :SUBJECTS, :HISTORY
+    private_constant :SUBJECTS
   end
 
   NUMBER_OF_VERSES = 8
