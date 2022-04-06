68bfaa0ed2d1518dd5229bd71f7c54c5cd2f7da2
Erik Schierboom <erik_schierboom@hotmail.com>
2022-04-06 13:33:14 +0200

    Prepare for class hierarchy

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -10,26 +10,28 @@ end
 class Verse
   def self.lyrics(number) = new(number).lyrics
 
-  attr_reader :number, :subject
+  attr_reader :number, :animal
 
   def initialize(number)
     @number = number
-    @subject = SUBJECTS[number - 1]
+    @animal = ANIMALS[number - 1]
   end
 
-  def lyrics = [fact, subject.embellishment, swallows, subject.conclusion].compact.join($/) + $/
+  def lyrics = [fact, embellishment, swallows, conclusion].compact.join($/) + $/
 
   private
 
-  def fact = "I know an old lady who swallowed a #{subject.subject}."
+  def fact = "I know an old lady who swallowed a #{animal.subject}."
+  def embellishment = animal.embellishment
+  def conclusion = animal.conclusion
 
   def swallows
-    return if subject.embellishment.nil?
+    return if animal.embellishment.nil?
 
-    number.downto(2).map {|n| "She swallowed the #{SUBJECTS[n - 1].subject} to catch the #{SUBJECTS[n - 2].subject}#{SUBJECTS[n - 2].addition}."}.join($/)
+    number.downto(2).map {|n| "She swallowed the #{ANIMALS[n - 1].subject} to catch the #{ANIMALS[n - 2].subject}#{ANIMALS[n - 2].addition}."}.join($/)
   end
 
-  SUBJECTS = [
+  ANIMALS = [
     ['fly', nil, nil, nil],
     ['spider', 'It wriggled and jiggled and tickled inside her.', nil, ' that wriggled and jiggled and tickled inside her'],
     ['bird', 'How absurd to swallow a bird!', nil, nil],
@@ -42,5 +44,5 @@ class Verse
     OpenStruct.new(subject:, embellishment:, conclusion: conclusion || "I don't know why she swallowed the fly. Perhaps she'll die.", addition:)
   end.freeze
 
-  private_constant :SUBJECTS
+  private_constant :ANIMALS
 end
