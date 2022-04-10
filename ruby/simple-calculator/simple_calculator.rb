16d0d050af8213653fb7b368c47495d5758cfb2f
Erik Schierboom <erik_schierboom@hotmail.com>
2022-04-10 11:31:57 +0200

    ruby/simple_calculator: add iteration

--- ruby/simple-calculator/simple_calculator.rb
+++ ruby/simple-calculator/simple_calculator.rb
@@ -1,14 +1,14 @@
 class SimpleCalculator
-  OPERATIONS = { '+' => :+, '/' => :/, '*' => :* }.freeze
+  ALLOWED_OPERATIONS = %w[+ / *].freeze
 
   class UnsupportedOperation < StandardError
   end
 
   def self.calculate(first_operand, second_operand, operation)
-    raise UnsupportedOperation unless OPERATIONS.key?(operation)
+    raise UnsupportedOperation unless ALLOWED_OPERATIONS.include?(operation)
     raise ArgumentError unless first_operand.is_a?(Integer) && second_operand.is_a?(Integer)
 
-    result = first_operand.send(OPERATIONS[operation], second_operand)
+    result = first_operand.send(operation, second_operand)
     "#{first_operand} #{operation} #{second_operand} = #{result}"
   rescue ZeroDivisionError => _
     'Division by zero is not allowed.'
