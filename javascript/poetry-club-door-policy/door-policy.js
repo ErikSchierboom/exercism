195008bea810e6c46ba7ff20d4defa2c12830706
Erik Schierboom <erik_schierboom@hotmail.com>
2022-04-11 13:46:19 +0200

    javascript/poetry-club-door-policy: add solution

---
+++ javascript/poetry-club-door-policy/door-policy.js
@@ -0,0 +1,56 @@
+// @ts-check
+
+/**
+ * Capitalize A word
+ *
+ * @param {string} word
+ * @returns {string}
+ */
+function capitalize(word) {
+  return word[0].toUpperCase() + word.substring(1).toLowerCase()
+}
+
+/**
+ * Respond with the correct character, given the line of the
+ * poem, if this were said at the front door.
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+export function frontDoorResponse(line) {
+  return line[0]
+}
+
+/**
+ * Format the password for the front-door, given the response
+ * letters.
+ *
+ * @param {string} word the letters you responded with before
+ * @returns {string} the front door password
+ */
+export function frontDoorPassword(word) {
+  return capitalize(word)
+}
+
+/**
+ * Respond with the correct character, given the line of the
+ * poem, if this were said at the back door.
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+export function backDoorResponse(line) {
+  const trimmedLine = line.trimEnd()
+  return trimmedLine[trimmedLine.length - 1]
+}
+
+/**
+ * Format the password for the back door, given the response
+ * letters.
+ *
+ * @param {string} word the letters you responded with before
+ * @returns {string} the back door password
+ */
+export function backDoorPassword(word) {
+  return `${capitalize(word)}, please`
+}
