e74ca9b203c9b21f01f4485b858e5229aefcbace
Erik Schierboom <erik_schierboom@hotmail.com>
2022-04-25 09:08:07 +0200

    ruby/resistor-color-trio: add solution

---
+++ ruby/resistor-color-trio/resistor_color_trio_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require_relative 'resistor_color_trio'
+
+# Common test data version: 1.1.0 2c41a51
+class ResistorColorTrioTest < Minitest::Test
+  def test_orange_and_orange_and_black
+    assert_equal "Resistor value: 33 ohms", ResistorColorTrio.new(%w[orange orange black]).label
+  end
+
+  def test_blue_and_grey_and_brown
+    assert_equal "Resistor value: 680 ohms", ResistorColorTrio.new(%w[blue grey brown]).label
+  end
+
+  def test_red_and_black_and_red
+    assert_equal "Resistor value: 2 kiloohms", ResistorColorTrio.new(%w[red black red]).label
+  end
+
+  def test_green_and_brown_and_orange
+    assert_equal "Resistor value: 51 kiloohms", ResistorColorTrio.new(%w[green brown orange]).label
+  end
+
+  def test_yellow_and_violet_and_yellow
+    assert_equal "Resistor value: 470 kiloohms", ResistorColorTrio.new(%w[yellow violet yellow]).label
+  end
+
+  def test_invalid_color
+    assert_raises(ArgumentError) do
+      ResistorColorTrio.new(%w[yellow purple black]).label
+    end
+  end
+end
