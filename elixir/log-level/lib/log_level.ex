9e5022e3ac8647f9b8848155a828e00875e92fdd
Erik Schierboom <erik_schierboom@hotmail.com>
2022-04-28 16:30:36 +0200

    elixir/log-level: add iteration

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,11 +1,15 @@
 defmodule LogLevel do
-  def to_label(0, false), do: :trace
-  def to_label(1, _), do: :debug
-  def to_label(2, _), do: :info
-  def to_label(3, _), do: :warning
-  def to_label(4, _), do: :error
-  def to_label(5, false), do: :fatal
-  def to_label(_, _), do: :unknown
+  def to_label(level, legacy?) do
+    cond do
+      level == 0 and not legacy? -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 and not legacy? -> :fatal
+      true -> :unknown
+    end
+  end
 
   @spec alert_recipient(any, any) :: :dev1 | :dev2 | false | :ops
   def alert_recipient(level, legacy?) do
