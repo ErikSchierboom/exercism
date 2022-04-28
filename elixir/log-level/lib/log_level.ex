4cebaf40ff660ea5e8557833232ecfcb323931ea
Erik Schierboom <erik_schierboom@hotmail.com>
2022-04-28 16:28:37 +0200

    elixir/log-level: add solution

---
+++ elixir/log-level/lib/log_level.ex
@@ -0,0 +1,21 @@
+defmodule LogLevel do
+  def to_label(0, false), do: :trace
+  def to_label(1, _), do: :debug
+  def to_label(2, _), do: :info
+  def to_label(3, _), do: :warning
+  def to_label(4, _), do: :error
+  def to_label(5, false), do: :fatal
+  def to_label(_, _), do: :unknown
+
+  @spec alert_recipient(any, any) :: :dev1 | :dev2 | false | :ops
+  def alert_recipient(level, legacy?) do
+    label = to_label(level, legacy?)
+
+    cond do
+      label in [:error, :fatal] -> :ops
+      label == :unknown and legacy? -> :dev1
+      label == :unknown -> :dev2
+      true -> false
+    end
+  end
+end
