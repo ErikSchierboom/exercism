e7a820c520b6dc2101652b663faee8145242a943
Erik Schierboom <erik_schierboom@hotmail.com>
2022-04-29 16:55:11 +0200

    elixir/strain: add iteration

--- elixir/strain/lib/strain.ex
+++ elixir/strain/lib/strain.ex
@@ -1,8 +1,15 @@
 defmodule Strain do
   @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
-  def keep(list, fun) do
-    for element <- list, fun.(element), do: element
+  def keep(list, fun), do: keep(list, fun, [])
+
+  defp keep([head | tail], fun, accumulator) do
+    cond do
+      fun.(head) -> keep(tail, fun, accumulator ++ [head])
+      true -> keep(tail, fun, accumulator)
+    end
   end
+
+  defp keep([], _, accumulator), do: accumulator
 
   @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
   def discard(list, fun) do
