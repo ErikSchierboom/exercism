a02c73629880ffba01c6b85276eac59ba71d05a0
Erik Schierboom <erik_schierboom@hotmail.com>
2022-04-29 14:46:03 +0200

    elixir/strain: add solution

--- elixir/strain/lib/strain.ex
+++ elixir/strain/lib/strain.ex
@@ -1,21 +1,11 @@
 defmodule Strain do
-  @doc """
-  Given a `list` of items and a function `fun`, return the list of items where
-  `fun` returns true.
-
-  Do not use `Enum.filter`.
-  """
   @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
   def keep(list, fun) do
+    for element <- list, fun.(element), do: element
   end
 
-  @doc """
-  Given a `list` of items and a function `fun`, return the list of items where
-  `fun` returns false.
-
-  Do not use `Enum.reject`.
-  """
   @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
   def discard(list, fun) do
+    keep(list, &(not fun.(&1)))
   end
 end
