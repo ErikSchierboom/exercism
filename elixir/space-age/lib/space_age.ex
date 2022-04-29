f25c7b18e01c8eae685a28cc5bdb66d399dfc16f
Erik Schierboom <erik_schierboom@hotmail.com>
2022-04-29 20:21:39 +0200

    elixir/space-age: add iteration

--- elixir/space-age/lib/space_age.ex
+++ elixir/space-age/lib/space_age.ex
@@ -8,21 +8,28 @@ defmodule SpaceAge do
           | :saturn
           | :uranus
           | :neptune
+
+  @planet_periods %{
+    :mercury => 0.2408467,
+    :venus => 0.61519726,
+    :earth => 1.0,
+    :mars => 1.8808158,
+    :jupiter => 11.862615,
+    :saturn => 29.447498,
+    :uranus => 84.016846,
+    :neptune => 164.79132
+  }
+
   @seconds_on_earth 31_557_600.0
 
-  defp planet_period(:mercury), do: 0.2408467
-  defp planet_period(:venus), do: 0.61519726
-  defp planet_period(:earth), do: 1.0
-  defp planet_period(:mars), do: 1.8808158
-  defp planet_period(:jupiter), do: 11.862615
-  defp planet_period(:saturn), do: 29.447498
-  defp planet_period(:uranus), do: 84.016846
-  defp planet_period(:neptune), do: 164.79132
-  defp round_to_two_decimals(value), do: round(value * 100.0) / 100.0
-
-  @spec age_on(planet, pos_integer) :: float
+  @spec age_on(planet, pos_integer) :: {atom, float}
   def age_on(planet, seconds) do
-    age_on_planet = seconds / planet_period(planet) / @seconds_on_earth
-    round_to_two_decimals(age_on_planet)
+    case @planet_periods[planet] do
+      nil ->
+        {:error, "not a planet"}
+
+      planet_period ->
+        {:ok, Float.round(seconds / planet_period / @seconds_on_earth, 2)}
+    end
   end
 end
