02d0262868ad8fbec90ea3dcf3fe6168f0e690e5
Erik Schierboom <erik_schierboom@hotmail.com>
2022-05-16 20:33:18 +0200

    elixir/protein-translation: add iteration

--- elixir/protein-translation/lib/protein_translation.ex
+++ elixir/protein-translation/lib/protein_translation.ex
@@ -1,33 +1,36 @@
 defmodule ProteinTranslation do
-  @doc """
-  Given an RNA string, return a list of proteins specified by codons, in order.
-  """
-  @spec of_rna(String.t()) :: {:ok, list(String.t())} | {:error, String.t()}
   def of_rna(rna) do
+    Regex.scan(~r(\w{3}), rna)
+    |> Stream.map(&Enum.join/1)
+    |> Stream.map(&of_codon/1)
+    |> Enum.reduce_while({:ok, []}, fn
+      {:ok, "STOP"}, {:ok, codons} ->
+        {:halt, {:ok, codons}}
+
+      {:ok, codon}, {:ok, codons} ->
+        {:cont, {:ok, codons ++ [codon]}}
+
+      {:error, "invalid codon"}, _ ->
+        {:halt, {:error, "invalid RNA"}}
+    end)
   end
 
-  @doc """
-  Given a codon, return the corresponding protein
-
-  UGU -> Cysteine
-  UGC -> Cysteine
-  UUA -> Leucine
-  UUG -> Leucine
-  AUG -> Methionine
-  UUU -> Phenylalanine
-  UUC -> Phenylalanine
-  UCU -> Serine
-  UCC -> Serine
-  UCA -> Serine
-  UCG -> Serine
-  UGG -> Tryptophan
-  UAU -> Tyrosine
-  UAC -> Tyrosine
-  UAA -> STOP
-  UAG -> STOP
-  UGA -> STOP
-  """
-  @spec of_codon(String.t()) :: {:ok, String.t()} | {:error, String.t()}
-  def of_codon(codon) do
-  end
+  def of_codon("UGU"), do: {:ok, "Cysteine"}
+  def of_codon("UGC"), do: {:ok, "Cysteine"}
+  def of_codon("UUA"), do: {:ok, "Leucine"}
+  def of_codon("UUG"), do: {:ok, "Leucine"}
+  def of_codon("AUG"), do: {:ok, "Methionine"}
+  def of_codon("UUU"), do: {:ok, "Phenylalanine"}
+  def of_codon("UUC"), do: {:ok, "Phenylalanine"}
+  def of_codon("UCU"), do: {:ok, "Serine"}
+  def of_codon("UCC"), do: {:ok, "Serine"}
+  def of_codon("UCA"), do: {:ok, "Serine"}
+  def of_codon("UCG"), do: {:ok, "Serine"}
+  def of_codon("UGG"), do: {:ok, "Tryptophan"}
+  def of_codon("UAU"), do: {:ok, "Tyrosine"}
+  def of_codon("UAC"), do: {:ok, "Tyrosine"}
+  def of_codon("UAA"), do: {:ok, "STOP"}
+  def of_codon("UAG"), do: {:ok, "STOP"}
+  def of_codon("UGA"), do: {:ok, "STOP"}
+  def of_codon(_), do: {:error, "invalid codon"}
 end
