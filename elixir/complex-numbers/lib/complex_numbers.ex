752d92ffc27a2bb8a2b7a99c49f1b207a3287d63
Erik Schierboom <erik_schierboom@hotmail.com>
2022-05-19 08:54:53 +0200

    elixir/complex-numbers: add iteration

--- elixir/complex-numbers/lib/complex_numbers.ex
+++ elixir/complex-numbers/lib/complex_numbers.ex
@@ -8,16 +8,13 @@ defmodule ComplexNumbers do
   def imaginary({_, i}), do: i
 
   def mul({r1, i1}, {r2, i2}), do: {r1 * r2 - i1 * i2, i1 * r2 + r1 * i2}
-  def mul(r, {r2, i2}), do: mul({r, 0}, {r2, i2})
-  def mul({r1, i1}, r), do: mul({r1, i1}, {r, 0})
+  def mul(r1, r2), do: mul(to_complex(r1), to_complex(r2))
 
   def add({r1, i1}, {r2, i2}), do: {r1 + r2, i1 + i2}
-  def add(r, {r2, i2}), do: add({r, 0}, {r2, i2})
-  def add({r1, i1}, r), do: add({r1, i1}, {r, 0})
+  def add(r1, r2), do: add(to_complex(r1), to_complex(r2))
 
   def sub({r1, i1}, {r2, i2}), do: {r1 - r2, i1 - i2}
-  def sub(r, {r2, i2}), do: sub({r, 0}, {r2, i2})
-  def sub({r1, i1}, r), do: sub({r1, i1}, {r, 0})
+  def sub(r1, r2), do: sub(to_complex(r1), to_complex(r2))
 
   def div({r1, i1}, {r2, i2}) do
     {r3, i3} = mul({r1, i1}, conjugate({r2, i2}))
@@ -26,14 +23,14 @@ defmodule ComplexNumbers do
     {r3 / r4, i3 / r4}
   end
 
-  def div(r, {r2, i2}), do: div({r, 0}, {r2, i2})
-  def div({r1, i1}, r), do: div({r1, i1}, {r, 0})
+  def div(r1, r2), do: div(to_complex(r1), to_complex(r2))
 
   def abs({r, i}), do: :math.sqrt(r ** 2 + i ** 2)
 
   def conjugate({r, i}), do: {r, -i}
 
-  def exp({r, i}) do
-    {:math.exp(r) * :math.cos(i), :math.exp(r) * :math.sin(i)}
-  end
+  def exp({r, i}), do: {:math.exp(r) * :math.cos(i), :math.exp(r) * :math.sin(i)}
+
+  defp to_complex({r, i}), do: {r, i}
+  defp to_complex(r), do: {r, 0}
 end
