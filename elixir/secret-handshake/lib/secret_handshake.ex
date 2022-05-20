a0e4416e90be7881ee6e010ed00ba5a66af01c75
Erik Schierboom <erik_schierboom@hotmail.com>
2022-05-20 16:08:37 +0200

    elixir/secret-handshake: add iteration

--- elixir/secret-handshake/lib/secret_handshake.ex
+++ elixir/secret-handshake/lib/secret_handshake.ex
@@ -1,25 +1,18 @@
 defmodule SecretHandshake do
   @commands %{
-    0b00001 => &SecretHandshake.wink/1,
-    0b00010 => &SecretHandshake.double_blink/1,
-    0b00100 => &SecretHandshake.close_your_eyes/1,
-    0b01000 => &SecretHandshake.jump/1,
-    0b10000 => &Enum.reverse/1
+    0b00001 => "wink",
+    0b00010 => "double blink",
+    0b00100 => "close your eyes",
+    0b01000 => "jump",
+    0b10000 => :reverse
   }
 
   def commands(code) do
     @commands
-    |> Enum.reduce([], fn {command_code, command_fn}, handshake ->
-      if Bitwise.&&&(code, command_code) == 0 do
-        handshake
-      else
-        command_fn.(handshake)
-      end
-    end)
+    |> Enum.filter(fn {action_code, _} -> Bitwise.&&&(code, action_code) == action_code end)
+    |> Enum.reduce([], fn {_, action}, handshake -> command(action, handshake) end)
   end
 
-  def wink(handshake), do: handshake ++ ["wink"]
-  def double_blink(handshake), do: handshake ++ ["double blink"]
-  def close_your_eyes(handshake), do: handshake ++ ["close your eyes"]
-  def jump(handshake), do: handshake ++ ["jump"]
+  defp command(:reverse, handshake), do: Enum.reverse(handshake)
+  defp command(action, handshake), do: handshake ++ [action]
 end
