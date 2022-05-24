1f6ba17645c1de3440a0a279d879289f49b75038
Erik Schierboom <erik_schierboom@hotmail.com>
2022-05-24 16:39:17 +0200

    elixir/gigasecond: add iteration

--- elixir/gigasecond/lib/gigasecond.ex
+++ elixir/gigasecond/lib/gigasecond.ex
@@ -1,7 +1,7 @@
 defmodule Gigasecond do
-  def from({{year, month, day}, {hours, minutes, seconds}}) do
-    NaiveDateTime.new!(year, month, day, hours, minutes, seconds)
+  def from(date) do
+    NaiveDateTime.from_erl!(date)
     |> NaiveDateTime.add(1_000_000_000)
-    |> then(&{{&1.year, &1.month, &1.day}, {&1.hour, &1.minute, &1.second}})
+    |> NaiveDateTime.to_erl()
   end
 end
