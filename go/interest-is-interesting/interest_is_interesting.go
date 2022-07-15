7b8b91eb21bf4bd15e93b4f1c768cf7a6894b74d
Erik Schierboom <erik_schierboom@hotmail.com>
2022-07-15 19:06:33 +0200

    go/interest-is-interesting: add solution

---
+++ go/interest-is-interesting/interest_is_interesting.go
@@ -0,0 +1,37 @@
+package interest
+
+// InterestRate returns the interest rate for the provided balance.
+func InterestRate(balance float64) float32 {
+	switch {
+	case balance < 0:
+		return 3.213
+	case balance < 1000:
+		return 0.5
+	case balance < 5000:
+		return 1.621
+	default:
+		return 2.475
+	}
+}
+
+// Interest calculates the interest for the provided balance.
+func Interest(balance float64) float64 {
+	return balance * float64(InterestRate(balance)/100)
+}
+
+// AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
+func AnnualBalanceUpdate(balance float64) float64 {
+	return balance + Interest(balance)
+}
+
+// YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance:
+func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
+	var years int
+
+	for balance < targetBalance {
+		balance = AnnualBalanceUpdate(balance)
+		years++
+	}
+
+	return years
+}
