1bf91d0e23cb9b4764bcd43a9f5584ec7985d61e
Erik Schierboom <erik_schierboom@hotmail.com>
2022-07-21 09:32:45 +0200

    go/census: add solution

---
+++ go/census/census.go
@@ -0,0 +1,43 @@
+// Package census simulates a system used to collect census data.
+package census
+
+// Resident represents a resident in this city.
+type Resident struct {
+	Name    string
+	Age     int
+	Address map[string]string
+}
+
+// NewResident registers a new resident in this city.
+func NewResident(name string, age int, address map[string]string) *Resident {
+	return &Resident{
+		Name:    name,
+		Age:     age,
+		Address: address,
+	}
+}
+
+// HasRequiredInfo determines if a given resident has all of the required information.
+func (r *Resident) HasRequiredInfo() bool {
+	return r.Name != "" && r.Address != nil && r.Address["street"] != ""
+}
+
+// Delete deletes a resident's information.
+func (r *Resident) Delete() {
+	r.Name = ""
+	r.Age = 0
+	r.Address = nil
+}
+
+// Count counts all residents that have provided the required information.
+func Count(residents []*Resident) int {
+	var count int
+
+	for _, resident := range residents {
+		if resident.HasRequiredInfo() {
+			count++
+		}
+	}
+
+	return count
+}
