aff182d65334481e845dd042cc6c1f05a8f54fc2
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-06 11:05:40 +0100

    csharp/series: add iteration

--- csharp/series/Series.cs
+++ csharp/series/Series.cs
@@ -5,10 +5,9 @@ public static class Series
 {
     public static IEnumerable<string> Slices(string input, int length)
     {
-        if (length < 1 || length > input.Length)
-            throw new ArgumentException("Invalid slice length", nameof(length));
+        if (length < 1 || length > input.Length) throw new ArgumentException("Invalid length");
 
-        for (var i = 0; i < input.Length - length + 1; i++)
-            yield return input[i..(i + length)];
+        for (var i = 0; i <= input.Length - length; i++)
+            yield return input[i..(i+length)];
     }
 }
