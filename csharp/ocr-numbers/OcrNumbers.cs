e63ed830f0c792b98b36b4c57d0abdc935a5f44f
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-07 13:52:23 +0100

    csharp/ocr-numbers: add iteration

--- csharp/ocr-numbers/OcrNumbers.cs
+++ csharp/ocr-numbers/OcrNumbers.cs
@@ -8,20 +8,22 @@ public static class OcrNumbers
     private const int CharacterHeight = 4;
 
     public static string Convert(string input)
-    {
+    {   
         var lines = input.Split('\n');
         if (lines.Length % 4 != 0 || lines[0].Length % 3 != 0)
             throw new ArgumentException();
-        
-        return Positions(lines).Aggregate("", (str, pos) => str + ConvertCharacter(lines, pos.Item1, pos.Item2));
-    }
 
-    private static IEnumerable<Tuple<int, int>> Positions(string[] lines)
-    {
-        return from x in Enumerable.Range(0, Rows(lines))
-               from y in Enumerable.Range(0, Cols(lines))
-               select Tuple.Create(x, y);
+        var lineBlocks = lines.Chunk(4);
+        return string.Join(",", lineBlocks.Select(Convert));
     }
+    
+    public static string Convert(string[] lines) =>
+        Positions(lines).Aggregate("", (str, pos) => str + ConvertCharacter(lines, pos.Item1, pos.Item2));
+
+    private static IEnumerable<Tuple<int, int>> Positions(string[] lines) =>
+        from x in Enumerable.Range(0, Rows(lines))
+        from y in Enumerable.Range(0, Cols(lines))
+        select Tuple.Create(x, y);
 
     private static int Cols(string[] lines) => lines[0].Length / CharacterWidth;
 
@@ -29,16 +31,11 @@ public static class OcrNumbers
     
     private static char ConvertCharacter(string[] input, int row, int col) => MatchCharacter(Character(input, row, col));
 
-    private static string Character(string[] input, int row, int col)
-    {
-        return Enumerable.Range(row, CharacterHeight)
-                         .Aggregate("", (str, offset) => str + input[row * CharacterHeight + offset].Substring(col * CharacterWidth, CharacterWidth));
-    }
+    private static string Character(string[] input, int row, int col) =>
+        Enumerable.Range(row, CharacterHeight)
+            .Aggregate("", (str, offset) => str + input[row * CharacterHeight + offset].Substring(col * CharacterWidth, CharacterWidth));
 
-    private static char MatchCharacter(string character)
-    {
-        return CharactersMap.ContainsKey(character) ? CharactersMap[character] : '?';
-    }
+    private static char MatchCharacter(string character) => CharactersMap.ContainsKey(character) ? CharactersMap[character] : '?';
 
     private static readonly IReadOnlyDictionary<string, char> CharactersMap = new Dictionary<string, char>
     {
