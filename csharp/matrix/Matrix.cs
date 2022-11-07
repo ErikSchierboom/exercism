da1b9ffa1c6b33f150a2bddc6d6829f16e8631e4
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-07 13:49:10 +0100

    csharp/matrix: add iteration

--- csharp/matrix/Matrix.cs
+++ csharp/matrix/Matrix.cs
@@ -11,37 +11,26 @@ public class Matrix
         _cols = ParseCols(_rows);
     }
 
-    public int Rows => _rows.Length;
-    public int Cols => _cols.Length;
+    public int[] Row(int row) => _rows[row - 1];
+    public int[] Column(int col) => _cols[col - 1];
 
-    public int[] Row(int row) => _rows[row];
-    public int[] Column(int col) => _cols[col];
-
-    private static int[][] ParseRows(string input)
-    {
-        return input.Split('\n')
+    private static int[][] ParseRows(string input) =>
+        input.Split('\n')
             .Select(ParseRow)
             .ToArray();
-    }
 
-    private static int[] ParseRow(string row)
-    {
-        return row.Split(' ')
+    private static int[] ParseRow(string row) =>
+        row.Split(' ')
             .Select(int.Parse)
             .ToArray();
-    }
 
-    private static int[][] ParseCols(int[][] rows)
-    {
-        return Enumerable.Range(0, rows[0].Length)
+    private static int[][] ParseCols(int[][] rows) =>
+        Enumerable.Range(0, rows[0].Length)
             .Select(y => ParseCol(rows, y))
             .ToArray();
-    }
 
-    private static int[] ParseCol(int[][] rows, int y)
-    {
-        return Enumerable.Range(0, rows.Length)
+    private static int[] ParseCol(int[][] rows, int y) =>
+        Enumerable.Range(0, rows.Length)
             .Select(x => rows[x][y])
             .ToArray();
-    }
 }
