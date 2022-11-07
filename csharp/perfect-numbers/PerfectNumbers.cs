9eb22a8a728ef4299011368626a28212b48d1f68
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-07 08:58:50 +0100

    csharp/perfect-numbers: add iteration

--- csharp/perfect-numbers/PerfectNumbers.cs
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -13,15 +13,12 @@ public class PerfectNumbers
     public static Classification Classify(int number)
     {
         if (number < 1)
-            throw new ArgumentOutOfRangeException();
+            throw new ArgumentOutOfRangeException(nameof(number));
         
-        var sumOfFactors = Enumerable.Range(1, number / 2).Where(x => number%x == 0).Sum();
-        if (sumOfFactors < number)
-            return Classification.Deficient;
+        var sumOfFactors = Enumerable.Range(1, number / 2).Where(x => number % x == 0).Sum();
         
-        if (sumOfFactors > number)
-            return Classification.Abundant;
-        
-        return Classification.Perfect;
+        return sumOfFactors < number ? Classification.Deficient :
+               sumOfFactors > number ? Classification.Abundant :
+               Classification.Perfect;
     }
 }
