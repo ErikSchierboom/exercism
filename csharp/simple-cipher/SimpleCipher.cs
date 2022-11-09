2c15b4722bb8108562f084bc61b653ba7155d4cc
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-09 07:33:03 +0100

    csharp/simple-cipher: add iteration

--- csharp/simple-cipher/SimpleCipher.cs
+++ csharp/simple-cipher/SimpleCipher.cs
@@ -1,32 +1,43 @@
 using System;
+using System.Linq;
 
 public class SimpleCipher
 {
-    public SimpleCipher()
+    private const string Alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private const int KeyLength = 100;
+
+    public SimpleCipher(): this(GenerateKey())
     {
-        throw new NotImplementedException("You need to implement this function.");
     }
 
     public SimpleCipher(string key)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!IsValidKey(key))
+            throw new ArgumentException("Invalid key specified.", nameof(key));
+
+        Key = key;
     }
     
-    public string Key 
-    {
-        get
+    public string Key { get; }
+
+    public string Encode(string plainText) => Shift(plainText, (x, y) => x + y);
+
+    public string Decode(string encryptedText) => Shift(encryptedText, (x, y) => x - y);
+
+    private string Shift(string text, Func<int, int, int> op) =>
+        string.Join("", text.Select((c, i) =>
         {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
+            var alphabetIndex = Alphabet.IndexOf(c);
+            var keyIndex = Alphabet.IndexOf(Key[Mod(i, Key.Length)]);
+            return Alphabet[Mod(op(alphabetIndex, keyIndex), Alphabet.Length)];
+        }));
 
-    public string Encode(string plaintext)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    private static string GenerateKey() =>
+        new(Enumerable.Repeat(Alphabet, KeyLength)
+            .Select(s => s[Random.Shared.Next(s.Length)])
+            .ToArray());
 
-    public string Decode(string ciphertext)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    private static bool IsValidKey(string key) => !string.IsNullOrEmpty(key) && key.All(c => Alphabet.Contains(c));
+    
+    private static int Mod(int x, int m) => (x % m + m) % m;
 }
