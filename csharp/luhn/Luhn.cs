574ae84d4e031ffb1c6df9cd484e423fc50ff3ae
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-09 08:03:15 +0100

    csharp/luhn: add iteration

--- csharp/luhn/Luhn.cs
+++ csharp/luhn/Luhn.cs
@@ -1,25 +1,25 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 public static class Luhn
 {
     public static bool IsValid(string number)
     {
-        var addends = GetDigits(number).Reverse().Select(GetDigitValue).Reverse();
-        var checksum = addends.Sum();
-        return checksum % 10 == 0;
+        var sanitizedNumber = number.Sanitize(); 
+        return sanitizedNumber.Valid() && sanitizedNumber.Checksum() % 10 == 0;
     }
 
-    private static IEnumerable<int> GetDigits(string number) => number.Select(c => c - '0');
-
-    private static int GetDigitValue(int digit, int index)
+    private static string Sanitize(this string number) => number.Replace(" ", "");
+    private static bool Valid(this string number) => number.All(char.IsDigit) && number.Length > 1;
+    private static int Checksum(this string number) => number.Addends().Sum();
+    private static IEnumerable<int> Addends(this string number) => number.Digits().Select((digit, i) =>
     {
-        if (index % 2 == 0)
-        {
+        if ((number.Length - i + 1) % 2 == 0)
             return digit;
-        }
 
-        var doubledValue = digit * 2;
-        return doubledValue > 9 ? doubledValue - 9 : doubledValue;
-    }
+        return digit >= 5 ? digit * 2 - 9 : digit * 2;
+    });
+    
+    private static IEnumerable<int> Digits(this string number) => number.Select(CharUnicodeInfo.GetDigitValue);
 }
