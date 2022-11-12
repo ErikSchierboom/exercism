95d1c8f2da36ce6bf2a3c9993e4a9135bff148da
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-12 10:56:56 +0100

    csharp/phone-number: add iteration

--- csharp/phone-number/PhoneNumber.cs
+++ csharp/phone-number/PhoneNumber.cs
@@ -1,38 +1,14 @@
-﻿using System.Linq;
+﻿using System;
+using System.Text.RegularExpressions;
 
-public class PhoneNumber
+public static class PhoneNumber
 {
-    private const string InvalidPhoneNumber = "0000000000";
-
-    public PhoneNumber(string phoneNumber)
-    {
-        this.Number = Clean(phoneNumber);
-        this.AreaCode = this.Number.Substring(0, 3);
-    }
-
-    public string Number { get; private set; }
-
-    public string AreaCode { get; private set; }
-
     public static string Clean(string phoneNumber)
     {
-        var digits = phoneNumber.Where(char.IsDigit).ToArray();
+        var match = Regex.Match(phoneNumber, @"^(\+?1)?[^\d]*?([2-9]\d{2})[^\d]*?([2-9]\d{2})[^\d]*?(\d{4})[^\d]*$");
+        if (!match.Success)
+            throw new ArgumentException("Invalid phone number", nameof(phoneNumber));
 
-        if (digits.Length == 10)
-        {
-            return new string(digits);
-        }
-
-        if (digits.Length == 11 && digits.First() == '1')
-        {
-            return new string(digits.Skip(1).ToArray());
-        }
-
-        return InvalidPhoneNumber;
-    }
-
-    public override string ToString()
-    {
-        return string.Format("({0}) {1}-{2}", this.AreaCode, this.Number.Substring(3, 3), this.Number.Substring(6, 4));
+        return $"{match.Groups[2].Value}{match.Groups[3].Value}{match.Groups[4].Value}";
     }
 }
