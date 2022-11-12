fe61cc0752ebefdadf5411a391e535127eefcbf6
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-12 12:14:37 +0100

    csharp/phone-number: add iteration

--- csharp/phone-number/PhoneNumber.cs
+++ csharp/phone-number/PhoneNumber.cs
@@ -5,10 +5,10 @@ public static class PhoneNumber
 {
     public static string Clean(string phoneNumber)
     {
-        var match = Regex.Match(phoneNumber, @"^(\+?1)?[^\d]*?([2-9]\d{2})[^\d]*?([2-9]\d{2})[^\d]*?(\d{4})[^\d]*$");
+        var match = Regex.Match(phoneNumber, @"^[^\d]*?1?[^\d]*?([2-9]\d{2})[^\d]*?([2-9]\d{2})[^\d]*?(\d{4})[^\d]*$");
         if (!match.Success)
             throw new ArgumentException("Invalid phone number", nameof(phoneNumber));
 
-        return $"{match.Groups[2].Value}{match.Groups[3].Value}{match.Groups[4].Value}";
+        return $"{match.Groups[1].Value}{match.Groups[2].Value}{match.Groups[3].Value}";
     }
 }
