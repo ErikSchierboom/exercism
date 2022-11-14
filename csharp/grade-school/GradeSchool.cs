a707ce4f8c42b2bc369bd6a1350cd319253b1803
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-14 13:16:23 +0100

    csharp/grade-school: add iteration

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -1,26 +1,23 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 public class GradeSchool
 {
-    private readonly SortedDictionary<int, List<string>> _roster = new();
+    private readonly SortedDictionary<int, SortedSet<string>> _roster = new();
     
     public bool Add(string student, int grade)
     {
         if (_roster.Values.Any(students => students.Contains(student)))
             return false;
 
-        if (!_roster.TryAdd(grade, new List<string> { student }))
-        {
+        if (!_roster.TryAdd(grade, new SortedSet<string> { student }))
             _roster[grade].Add(student);
-            _roster[grade].Sort();
-        }
 
         return true;
     }
 
-    public IEnumerable<string> Roster() => _roster.Values.SelectMany(grade => grade).ToList();
+    public IEnumerable<string> Roster() => _roster.Values.SelectMany(students => students);
 
-    public IEnumerable<string> Grade(int grade) => _roster.TryGetValue(grade, out var students) ? students : new List<string>();
+    public IEnumerable<string> Grade(int grade) =>
+        _roster.TryGetValue(grade, out var students) ? students : Enumerable.Empty<string>();
 }
