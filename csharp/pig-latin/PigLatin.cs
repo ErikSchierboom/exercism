fc2cfca4066cf02169ae5e8db4c5b4dda4c0981f
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-17 19:07:34 +0100

    csharp/pig-latin: add iteration

--- csharp/pig-latin/PigLatin.cs
+++ csharp/pig-latin/PigLatin.cs
@@ -3,7 +3,7 @@ using System.Text.RegularExpressions;
 public static class PigLatin
 {
     private const string VowelPattern = @"(?<begin>^|\s+)(?<vowel>[aeiou]|xr|yt)(?<rest>\w+)";
-    private const string ConsonantPattern = @"(?<begin>^|\s+)(?<consonant>(.?qu|[^aeiou]+))(?<rest>[aeiouy]\w*)";
+    private const string ConsonantPattern = @"(?<begin>^|\s+)(?<consonant>([^aeiou]?qu|[^aeiou]+))(?<rest>[aeiouy]\w*)";
     
     private const string VowelReplacement = "${begin}${vowel}${rest}ay";
     private const string ConsonantReplacement = "${begin}${rest}${consonant}ay";
