d8b15349420407dc0d939540f74f55a630547950
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-17 07:13:29 +0100

    csharp/pig-latin: add iteration

--- csharp/pig-latin/PigLatin.cs
+++ csharp/pig-latin/PigLatin.cs
@@ -2,14 +2,14 @@ using System.Text.RegularExpressions;
 
 public static class PigLatin
 {
-    private const string VowelRegex = @"(?<begin>^|\s+)(?<vowel>a|e|i|o|u|xr|yt)(?<rest>\w+)";
-    private const string ConsonantRegex = @"(?<begin>^|\s+)(?<consonant>ch|qu|thr|th|rh|sch|yt|\wqu|\w)(?<rest>\w+)";
+    private const string VowelPattern = @"(?<begin>^|\s+)(?<vowel>[aeiou]|xr|yt)(?<rest>\w+)";
+    private const string ConsonantPattern = @"(?<begin>^|\s+)(?<consonant>(.?qu|[^aeiou]+))(?<rest>[aeiouy]\w*)";
     
     private const string VowelReplacement = "${begin}${vowel}${rest}ay";
     private const string ConsonantReplacement = "${begin}${rest}${consonant}ay";
 
     public static string Translate(string sentence) =>
-        Regex.IsMatch(sentence, VowelRegex)
-            ? Regex.Replace(sentence, VowelRegex, VowelReplacement)
-            : Regex.Replace(sentence, ConsonantRegex, ConsonantReplacement);
+        Regex.IsMatch(sentence, VowelPattern)
+            ? Regex.Replace(sentence, VowelPattern, VowelReplacement)
+            : Regex.Replace(sentence, ConsonantPattern, ConsonantReplacement);
 }
