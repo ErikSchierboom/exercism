ce1c418c0e61b1d843135976bcb59de8187952e6
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-20 12:35:03 +0100

    csharp/poker: add iteration

--- csharp/poker/Poker.cs
+++ csharp/poker/Poker.cs
@@ -4,93 +4,128 @@ using System.Linq;
 
 public static class Poker
 {
-    private struct Card
+    private class StraightFlushComparer : IComparer<Hand>
     {
-        public int Rank;
-        public int Suit;
+        public int Compare(Hand x, Hand y) => Score(x).CompareTo(Score(y));
+        private static int Score(Hand hand) =>
+            hand.IsFlush && hand.IsSmallStraight ? hand.Ranks[3] :
+            hand.IsFlush && hand.IsLargeStraight ? hand.Ranks[4] :
+            0;
     }
 
-    private struct Hand
+    private class FourOfAKindComparer : IComparer<Hand>
     {
-        public string Input;
-        public int Score;
+        public int Compare(Hand x, Hand y) => Score(x).CompareTo(Score(y));
+        private static (int, int) Score(Hand hand) =>
+            hand.IsFourOfAKind ? (hand.Ranks[1], Math.Min(hand.Ranks[0], hand.Ranks[4])) : (0, 0);
     }
 
-    public static IEnumerable<string> BestHands(IEnumerable<string> hands)
+    private class FullHouseComparer : IComparer<Hand>
     {
-        var scoredHands = hands.Select(ParseHand).ToArray();
-        var maxScore = scoredHands.Max(h => h.Score);
-        return scoredHands.Where(h => h.Score == maxScore).Select(h => h.Input).ToList();
+        public int Compare(Hand x, Hand y) => Score(x).CompareTo(Score(y));
+        private static (int, int) Score(Hand hand) =>
+            hand.IsFullHouse
+                ? hand.Ranks[2] == hand.Ranks[4] 
+                ? (hand.Ranks[2], hand.Ranks[0])
+                : (hand.Ranks[0], hand.Ranks[4])
+                : (0, 0);
     }
 
-    private static Hand ParseHand(string hand) => new Hand { Input = hand, Score = ScoreHand(ParseCards(hand)) };
-
-    private static Card[] ParseCards(string hand) => hand.Split(' ').Select(ParseCard).OrderByDescending(c => c.Rank).ToArray();
-
-    private static Card ParseCard(string card) => new Card { Rank = ParseRank(card), Suit = ParseSuit(card) };
-    
-    private static int ParseRank(string card) => "..23456789TJQKA".IndexOf(card[0]);
-
-    private static int ParseSuit(string card) => ".HSDC".IndexOf(card[1]);
-    
-    private static int ScoreHand(Card[] cards)
+    private class FlushComparer : IComparer<Hand>
     {
-        var cardsByRank = cards
-            .GroupBy(c => c.Rank)
-            .OrderByDescending(c => c.Count())
-            .Select(g => g.Max(c => c.Rank))
-            .ToArray();
+        public int Compare(Hand x, Hand y) => Score(x).CompareTo(Score(y));
+        private static (int, int, int, int, int) Score(Hand hand) =>
+            hand.IsFlush ? (hand.Ranks[4], hand.Ranks[3], hand.Ranks[2], hand.Ranks[1], hand.Ranks[0]) : (0, 0, 0, 0, 0);
+    }
 
-        var rankCounts = cards
-            .GroupBy(c => c.Rank)
-            .Select(g => g.Count())
-            .OrderByDescending(c => c)
-            .ToArray();
+    private class StraightComparer : IComparer<Hand>
+    {
+        public int Compare(Hand x, Hand y) => Score(x).CompareTo(Score(y));
+
+        private static int Score(Hand hand) =>
+            hand.IsSmallStraight ? hand.Ranks[3] :
+            hand.IsLargeStraight ? hand.Ranks[4] :
+            0;
+    }
+
+    private class ThreeOfAKindComparer : IComparer<Hand>
+    {
+        public int Compare(Hand x, Hand y) => Score(x).CompareTo(Score(y));
+        private static (int, int, int) Score(Hand hand) => hand.IsThreeOfAKind
+            ? hand.Ranks[4] == hand.Ranks[2] ? (hand.Ranks[4], hand.Ranks[1], hand.Ranks[0])
+            : hand.Ranks[3] == hand.Ranks[1] ? (hand.Ranks[3], hand.Ranks[4], hand.Ranks[0])
+            : (hand.Ranks[0], hand.Ranks[4], hand.Ranks[3])
+            : (0, 0, 0);
+    }
+
+    private class TwoPairComparer : IComparer<Hand>
+    {
+        public int Compare(Hand x, Hand y) => Score(x).CompareTo(Score(y));
+        private static (int, int) Score(Hand hand) => hand.IsTwoPair
+            ? hand.Ranks[0] != hand.Ranks[1] ? (hand.Ranks[4], hand.Ranks[2]) 
+            : hand.Ranks[1] != hand.Ranks[2] ? (hand.Ranks[3], hand.Ranks[0])
+            : (hand.Ranks[2], hand.Ranks[0])
+            : (0, 0);
+    }
+
+    private class OnePairComparer : IComparer<Hand>
+    {
+        public int Compare(Hand x, Hand y) => Score(x).CompareTo(Score(y));
+        private static (int, int, int, int) Score(Hand hand) => hand.IsOnePair
+            ? hand.Ranks[0] == hand.Ranks[1] ? (hand.Ranks[0], hand.Ranks[4], hand.Ranks[3], hand.Ranks[2]) 
+            : hand.Ranks[1] == hand.Ranks[2] ? (hand.Ranks[1], hand.Ranks[4], hand.Ranks[3], hand.Ranks[0])
+            : hand.Ranks[2] == hand.Ranks[3] ? (hand.Ranks[2], hand.Ranks[4], hand.Ranks[1], hand.Ranks[0])
+            : (hand.Ranks[3], hand.Ranks[2], hand.Ranks[1], hand.Ranks[0])
+            : (0, 0, 0, 0);
+    }
+
+    private class HighCardComparer : IComparer<Hand>
+    {
+        public int Compare(Hand x, Hand y) => Score(x).CompareTo(Score(y));
+        private static (int, int, int, int, int) Score(Hand hand) => (hand.Ranks[4], hand.Ranks[3], hand.Ranks[2], hand.Ranks[1], hand.Ranks[0]);
+    }
+
+    private class HandComparer : IComparer<Hand>
+    {
+        private static readonly IComparer<Hand>[] Comparers =
+        {
+            new StraightFlushComparer(), new FourOfAKindComparer(), new FullHouseComparer(), new FlushComparer(),
+            new StraightComparer(), new ThreeOfAKindComparer(), new TwoPairComparer(), new OnePairComparer(), new HighCardComparer()
+        };
+
+        public int Compare(Hand x, Hand y) =>
+            Comparers.Aggregate(0, (comparison, comparer) => comparison == 0 ? comparer.Compare(x, y) : comparison);
+    }
+
+    private record Card(int Rank, int Suit);
+
+    private record Hand(string Text, Card[] Cards)
+    {
+        public int[] Ranks => Cards.Select(card => card.Rank).ToArray();
+        public int[] Suits => Cards.Select(card => card.Suit).ToArray();
+
+        public bool IsFlush => Suits.Distinct().Count() == 1;
+        public bool IsSmallStraight => Ranks[4] - Ranks[0] == 12 && Ranks.Distinct().Count() == 5;
+        public bool IsLargeStraight => Ranks[4] - Ranks[0] == 4 && Ranks.Distinct().Count() == 5;
+        public bool IsFourOfAKind => HasRankOfCount(4);
+        public bool IsFullHouse => HasRankOfCount(3) && HasRankOfCount(2);
+        public bool IsThreeOfAKind => HasRankOfCount(3);
+        public bool IsTwoPair => HasRankOfCount(2, numberOfTimes: 2);
+        public bool IsOnePair => HasRankOfCount(2);
         
-        var ranks = cards.Select(c => c.Rank).ToArray();
-        var suits = cards.Select(c => c.Suit).ToArray();
-
-        if (ranks.SequenceEqual(new[] { 14, 5, 4, 3, 2 }))
-        {
-            ranks = new[] { 1, 2, 3, 4, 5 };
-        }
-        
-        var flush = suits.Distinct().Count() == 1;
-        var straight = ranks.Distinct().Count() == 5 && ranks[0] - ranks[4] == 4;
-        
-        if (straight && flush)
-        {
-            return 800 + ranks.First();
-        }
-        if (rankCounts.SequenceEqual(new [] { 4, 1 }))
-        {
-            return 700 + cardsByRank[0];
-        }
-        if (rankCounts.SequenceEqual(new[] { 3, 2 }))
-        {
-            return 600 + cardsByRank[0];
-        }
-        if (flush)
-        {
-            return 500 + ranks.First();
-        }
-        if (straight)
-        {
-            return 400 + ranks.First();
-        }
-        if (rankCounts.SequenceEqual(new[] { 3, 1, 1 }))
-        {
-            return 300 + cardsByRank[0];
-        }
-        if (rankCounts.SequenceEqual(new[] { 2, 2, 1 }))
-        {
-            return 200 + Math.Max(cardsByRank[0], cardsByRank[1]);
-        }
-        if (rankCounts.SequenceEqual(new[] { 2, 1, 1, 1 }))
-        {
-            return 100 + cardsByRank[0];
-        }
-
-        return ranks.Max();
+        private bool HasRankOfCount(int count, int numberOfTimes = 1) =>
+            Ranks.GroupBy(rank => rank).Count(g => g.Count() == count) == numberOfTimes;
     }
+
+    public static IEnumerable<string> BestHands(IEnumerable<string> handDescriptions)
+    {
+        var hands = handDescriptions.Select(ParseHand).OrderDescending(new HandComparer()).ToArray();
+        return hands.TakeWhile(h => new HandComparer().Compare(h, hands[0]) == 0).Select(h => h.Text).ToArray();
+    }
+
+    private static Hand ParseHand(string hand) => new(hand, ParseCards(hand));
+    private static Card[] ParseCards(string hand) => hand.Split(' ').Select(ParseCard).OrderBy(card => card.Rank).ToArray();
+    private static Card ParseCard(string card) => new(card.Rank(), card.Suit());
+    private static int Rank(this string card) => "..234567890JQKA".IndexOf(card[^2]);
+    private static int Suit(this string card) => ".HSDC".IndexOf(card[^1]);
 }
