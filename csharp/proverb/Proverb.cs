8753d98ea9a9c91884896c2333b71f555e2078db
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-21 13:31:31 +0100

    csharp/proverb: add iteration

--- csharp/proverb/Proverb.cs
+++ csharp/proverb/Proverb.cs
@@ -3,9 +3,14 @@ using System.Linq;
 
 public static class Proverb
 {
-    public static string[] Recite(string[] subjects) =>
-        subjects.Zip(subjects.Skip(1))
-            .Select((subject => $"For want of a {subject.First} the {subject.Second} was lost."))
-            .Concat(subjects.Any() ? new[] { $"And all for the want of a {subjects[0]}." } : Array.Empty<string>())
+    public static string[] Recite(string[] subjects)
+    {
+        if (subjects.Length == 0)
+            return Array.Empty<string>();
+
+        return subjects.Zip(subjects.Skip(1))
+            .Select((pair => $"For want of a {pair.First} the {pair.Second} was lost."))
+            .Append($"And all for the want of a {subjects.First()}.")
             .ToArray();
+    }
 }
