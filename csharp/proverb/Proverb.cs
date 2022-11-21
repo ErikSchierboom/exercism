2938e573f47d6d3b5148b87d0477abe310dc6475
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-21 10:40:54 +0100

    csharp/proverb: add iteration

--- csharp/proverb/Proverb.cs
+++ csharp/proverb/Proverb.cs
@@ -1,16 +1,11 @@
+using System;
 using System.Linq;
 
 public static class Proverb
 {
-    public static string Line(string[] subjects, int line)
-    {
-        if (line == 7)
-        { 
-            return "And all for the want of a horseshoe nail.";
-        }
-
-        return $"For want of a {subjects[line - 1]} the {subjects[line]} was lost.";
-    }
-
-    public static string[] Recite(string[] subjects) => Enumerable.Range(1, 7).Select(i => Line(subjects, i)).ToArray();
+    public static string[] Recite(string[] subjects) =>
+        subjects.Zip(subjects.Skip(1))
+            .Select((subject => $"For want of a {subject.First} the {subject.Second} was lost."))
+            .Concat(subjects.Any() ? new[] { $"And all for the want of a {subjects[0]}." } : Array.Empty<string>())
+            .ToArray();
 }
