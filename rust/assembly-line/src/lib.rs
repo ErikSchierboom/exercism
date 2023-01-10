9862e2055e01281e697adf095a2a58ce15aaf229
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-10 10:25:22 +0100

    rust/assembly-line: add solution

---
+++ rust/assembly-line/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    let success_rate = match speed {
+        1..=4 => 1.0,
+        5..=8 => 0.9,
+        9..=10 => 0.77,
+        _ => 0.0
+    };
+
+    221.0 * speed as f64 * success_rate
+}
+
+pub fn working_items_per_minute(speed: u8) -> u32 {
+    (production_rate_per_hour(speed) / 60.0) as u32
+}
