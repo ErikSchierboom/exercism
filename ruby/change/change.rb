d05ce73e01e95716bb6b31fa3c1aedc8c00251dd
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-17 09:27:49 +0100

    ruby/change: add iteration

--- ruby/change/change.rb
+++ ruby/change/change.rb
@@ -1,31 +1,24 @@
 module Change
   def self.generate(coins, amount)
     raise Change::NegativeTargetError if amount.negative?
-
     coins_per_amount = fewest_coins_per_amount(coins, amount)
 
-    raise Change::ImpossibleCombinationError if coins_per_amount[amount].nil?
-    
+    raise Change::ImpossibleCombinationError if coins_per_amount[amount].nil?    
     coins_per_amount[amount]
   end
 
   def self.fewest_coins_per_amount(coins, amount)
-    (1..amount).inject([[]]) do |fewest_coins, amount|
+    (1..amount).each_with_object([[]]) do |amount, fewest_coins|
       fewest_coins[amount] = most_efficient_change(coins, fewest_coins, amount)
-      fewest_coins
     end
   end
 
   def self.most_efficient_change(coins, fewest_coins, amount)
     coins.select {|coin| coin <= amount}
-        .map {|coin| fewest_coins[amount - coin]&.then {|change| [coin] + change } }
-        .compact
+        .filter_map {|coin| fewest_coins[amount - coin]&.then {|change| [coin] + change } }
         .min {|a, b| a.size <=> b.size}
   end
 
-  class ImpossibleCombinationError < StandardError
-  end
-  
-  class NegativeTargetError < StandardError
-  end
+  class ImpossibleCombinationError < StandardError; end  
+  class NegativeTargetError < StandardError; end
 end
