c348f183dea9738b4fc3e2128aad49e1032fa09b
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-17 09:21:21 +0100

    ruby/raindrops: add iteration

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -2,7 +2,7 @@ module Raindrops
   FACTORS_TO_SOUNDS = { 3 => 'Pling', 5 => 'Plang', 7 => 'Plong' }.freeze
 
   def self.convert(number)
-    sounds = FACTORS_TO_SOUNDS.select { |factor, _| (number % factor).zero? }.values
+    sounds = FACTORS_TO_SOUNDS.filter_map { |factor, sound| sound if (number % factor).zero? }
     sounds.empty? ? number.to_s : sounds.join
   end
 end
