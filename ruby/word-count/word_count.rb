0de8c5af303927e90d642fd08af48d879de91bb9
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-18 17:40:28 +0100

    ruby/word-count: add iteration

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,12 +1,5 @@
 class Phrase
-  def initialize(phrase)
-    @phrase = phrase
-  end
+  def initialize(phrase) = @phrase = phrase
 
-  def word_count
-    @phrase
-      .downcase
-      .scan(/\w+(?:'\w+)?/)
-      .each_with_object(Hash.new(0)) { |word, hash| hash[word] += 1 }
-  end
+  def word_count = @phrase.downcase.scan(/\w+(?:'\w+)?/).tally
 end
