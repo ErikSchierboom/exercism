353853a4c174d212243b2d94b15a66493fc4287d
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-20 20:37:04 +0100

    crystal/anagram: add iteration

--- crystal/anagram/src/anagram.cr
+++ crystal/anagram/src/anagram.cr
@@ -3,11 +3,11 @@ module Anagram
     candidates.select { |candidate| anagram?(candidate, word) }
   end
 
-  def self.anagram?(candidate, word)
+  private def self.anagram?(candidate, word)
     normalize(candidate) == normalize(word) unless candidate.compare(word, case_insensitive: true).zero?
   end
 
-  def self.normalize(string)
+  private def self.normalize(string)
     string.downcase.chars.sort
   end
 end
