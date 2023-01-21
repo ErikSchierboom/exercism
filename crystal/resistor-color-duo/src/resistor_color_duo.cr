8d9ef19019e6f9958e8fbb2fb503e13fe41b5ccd
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-21 21:30:27 +0100

    crystal/resistor-color-duo: add iteration

--- crystal/resistor-color-duo/src/resistor_color_duo.cr
+++ crystal/resistor-color-duo/src/resistor_color_duo.cr
@@ -1,6 +1,6 @@
 module ResistorColorDuo
   def self.value(values : Array(String)) : Number
-    values[0..1].reduce(0) { |acc, color| acc * 10 + COLORS.index(color).not_nil! }
+    values.first(2).reduce(0) { |acc, color| acc * 10 + COLORS.index(color).not_nil! }
   end
 
   COLORS = %w[black brown red orange yellow green blue violet grey white]
