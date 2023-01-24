b418da32e409ec3acdaee121e067f2a6777bb7e7
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-24 15:47:24 +0100

    fsharp/rotational-cipher: add iteration

--- fsharp/rotational-cipher/RotationalCipher.fs
+++ fsharp/rotational-cipher/RotationalCipher.fs
@@ -2,7 +2,5 @@
 
 let rotate n plaintext =
     let rotateMap n list = Seq.zip list (List.skip n list @ List.take n list)
-    let letterMap = Seq.append (rotateMap n ['a'..'z']) (rotateMap n ['A'..'Z']) |> Map.ofSeq 
-
-    plaintext
-    |> String.map (fun letter -> Map.tryFind letter letterMap |> Option.defaultValue letter)
+    let letterMap = Seq.append (rotateMap n ['a'..'z']) (rotateMap n ['A'..'Z']) |> Map.ofSeq
+    String.map (fun letter -> Map.tryFind letter letterMap |> Option.defaultValue letter) plaintext
