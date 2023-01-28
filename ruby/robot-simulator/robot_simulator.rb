fc11c26de5f27eb96e23dddc3dd3f094e13e643f
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 15:25:34 +0100

    ruby/robot-simulator: add iteration

--- ruby/robot-simulator/robot_simulator.rb
+++ ruby/robot-simulator/robot_simulator.rb
@@ -1,7 +1,4 @@
 class Robot
-  BEARINGS = %i[east west north south].freeze
-  private_constant :BEARINGS
-
   attr_accessor :coordinates, :bearing
 
   def initialize
@@ -15,66 +12,53 @@ class Robot
     @bearing = direction
   end
 
-  def at(x, y)
-    @coordinates = [x, y]
-  end
+  def at(x, y) = @coordinates = [x, y]
 
   def turn_right
     case @bearing
-    when :north
-      @bearing = :east
-    when :east
-      @bearing = :south
-    when :south
-      @bearing = :west
-    when :west
-      @bearing = :north
+    when :north then @bearing = :east
+    when :east  then @bearing = :south
+    when :south then @bearing = :west
+    when :west  then @bearing = :north
     end
   end
 
   def turn_left
     case @bearing
-    when :north
-      @bearing = :west
-    when :east
-      @bearing = :north
-    when :south
-      @bearing = :east
-    when :west
-      @bearing = :south
+    when :north then @bearing = :west
+    when :east  then @bearing = :north
+    when :south then @bearing = :east
+    when :west  then @bearing = :south
     end
   end
 
   def advance
     case @bearing
-    when :north
-      @coordinates[1] += 1
-    when :east
-      @coordinates[0] += 1
-    when :south
-      @coordinates[1] -= 1
-    when :west
-      @coordinates[0] -= 1
+    when :north then @coordinates[1] += 1
+    when :east  then @coordinates[0] += 1
+    when :south then @coordinates[1] -= 1
+    when :west  then @coordinates[0] -= 1
     end
   end
+
+  BEARINGS = %i[east west north south].freeze
+  private_constant :BEARINGS
 end
 
 class Simulator
-  LETTER_TO_INSTRUCTION = { 'A' => :advance, 'L' => :turn_left, 'R' => :turn_right }.freeze
-  private_constant :LETTER_TO_INSTRUCTION
-
   def place(robot, x:, y:, direction:)
     robot.at(x, y)
     robot.orient(direction)
   end
 
   def evaluate(robot, word)
-    instructions(word).each_with_object(robot) do |instruction, evaluatingRobot|
-      evaluatingRobot.send(instruction)
+    instructions(word).each_with_object(robot) do |instruction, evaluating_robot|
+      evaluating_robot.send(instruction)
     end
   end
 
-  def instructions(word)
-    word.each_char.map { |letter| LETTER_TO_INSTRUCTION[letter] }
-  end
+  def instructions(word) = word.each_char.map { |letter| LETTER_TO_INSTRUCTION[letter] }
+
+  LETTER_TO_INSTRUCTION = { 'A' => :advance, 'L' => :turn_left, 'R' => :turn_right }.freeze
+  private_constant :LETTER_TO_INSTRUCTION
 end
