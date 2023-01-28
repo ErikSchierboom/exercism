f57cc7fc1f7e55d2c00081d990ed216c19f90dbe
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 15:21:17 +0100

    ruby/scrabble-score: add iteration

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,31 +1,20 @@
 class Scrabble
-  def self.score(*args)
-    new(*args).score
-  end
+  def self.score(*args) = new(*args).score
 
-  def initialize(word)
-    @letters = word.to_s.strip.upcase.chars
-  end
-
-  def score
-    letters.sum { |letter| LETTER_SCORES[letter] }
-  end
+  def initialize(word) = @letters = word.to_s.strip.upcase.chars
+  def score = @letters.sum { |letter| letter_score(letter) }
 
   private
-  attr_reader :letters
-
-  LETTER_SCORES = [
-    [%w(A E I O U L N R S T), 1],
-    [%w(D G), 2],
-    [%w(B C M P), 3],
-    [%w(F H V W Y), 4],
-    [%w(K), 5],
-    [%w(J X), 8],
-    [%w(Q Z), 10]
-  ].flat_map { |letters, score|
-    letters.map { |letter| [letter, score] }
-  }.to_h
-
-  private_constant :LETTER_SCORES
+  def letter_score(letter)
+    case letter
+    when *'AEIOULNRST'.chars then 1
+    when *'DG'.chars         then 2
+    when *'BCMP'.chars       then 3
+    when *'FHVWY'.chars      then 4
+    when *'K'.chars          then 5
+    when *'JX'.chars         then 8
+    when *'QZ'.chars         then 10
+    end
+  end
 end
 
