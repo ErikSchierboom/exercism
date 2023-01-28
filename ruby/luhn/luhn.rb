80d381968db989916f8a805e6361f4c4e92ba403
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 15:45:56 +0100

    ruby/luhn: add iteration

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -6,16 +6,8 @@ module Luhn
   end
 
   private
-  def self.checksum(number)
-    clean(number)
-      .to_i
-      .then(&method(:luhn_sum))
-      .modulo(10)
-  end
-
-  def self.clean(number)
-    number.delete(' ')
-  end
+  def self.checksum(number) = clean(number).to_i.then(&method(:luhn_sum)).modulo(10)
+  def self.clean(number) = number.delete(' ')
 
   def self.luhn_sum(number)
     number
@@ -24,9 +16,7 @@ module Luhn
       .sum { |even, odd| even + double(odd.to_i) }
   end
 
-  def self.double(digit)
-    (digit * 2).digits.sum
-  end
+  def self.double(digit) = (digit * 2).digits.sum
 
   VALID_NUMBER_REGEX = /^\d{2,}$/.freeze
   private_constant :VALID_NUMBER_REGEX
