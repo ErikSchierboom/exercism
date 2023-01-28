c471dac122629dfd24422b7f7c675f1bec891199
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 15:23:38 +0100

    ruby/roman-numerals: add iteration

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -7,25 +7,24 @@ class Numeric
         remainder -= threshold
         numerals << numeral
       end
-    end.join('')
+    end.join
   end
 
   private
-
   NUMERAL_THRESHOLDS = {
-    1000 => 'M',
-    900 => 'CM',
-    500 => 'D',
-    400 => 'CD',
-    100 => 'C',
-    90 => 'XC',
-    50 => 'L',
-    40 => 'XL',
-    10 => 'X',
-    9 => 'IX',
-    5 => 'V',
-    4 => 'IV',
-    1 => 'I'
+    1000 =>  'M',
+     900 => 'CM',
+     500 =>  'D',
+     400 => 'CD',
+     100 =>  'C',
+      90 => 'XC',
+      50 =>  'L',
+      40 => 'XL',
+      10 =>  'X',
+       9 => 'IX',
+       5 =>  'V',
+       4 => 'IV',
+       1 =>  'I'
   }.freeze
   private_constant :NUMERAL_THRESHOLDS
 end
