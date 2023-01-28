40caa7adc6f1e7d24f5dfc9547a82489d7292d77
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 15:13:55 +0100

    ruby/series: add iteration

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -2,11 +2,8 @@ class Series
   def initialize(numerals) = @numerals = numerals
 
   def slices(length)
-    raise ArgumentError if length > numerals.length
+    raise ArgumentError if length > @numerals.length
 
-    numerals.chars.each_cons(length).map(&:join)
+    @numerals.chars.each_cons(length).map(&:join)
   end
-
-  private
-  attr_reader :numerals
 end
