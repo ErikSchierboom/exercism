47f8c3db48aa1efcdf824d7e4caf254ee83001d8
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 20:46:40 +0100

    ruby/clock: add iteration

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,31 +1,17 @@
 class Clock
-  HOURS_PER_DAY = 24
-  MINUTES_PER_HOUR = 60
-  MINUTES_PER_DAY = MINUTES_PER_HOUR * HOURS_PER_DAY
-  private_constant :HOURS_PER_DAY, :MINUTES_PER_HOUR, :MINUTES_PER_DAY
+  attr_reader :minutes
 
   def initialize(hour: 0, minute: 0)
     @minutes = (hour * MINUTES_PER_HOUR + minute).modulo(MINUTES_PER_DAY)
   end 
 
-  def +(other)
-    Clock.new(minute: minutes + other.minutes)
-  end
+  def +(other) = Clock.new(minute: minutes + other.minutes)
+  def -(other) = Clock.new(minute: minutes - other.minutes)
+  def ==(other) = other.class == self.class && other.minutes == minutes
+  def to_s = format('%02d:%02d', *minutes.divmod(MINUTES_PER_HOUR))
 
-  def -(other)
-    Clock.new(minute: minutes - other.minutes)
-  end
-
-  def ==(other)
-    other.class == self.class && other.minutes == minutes
-  end
-
-  def to_s
-    hour, minute = minutes.divmod(MINUTES_PER_HOUR)
-    format('%02d:%02d', hour, minute)
-  end
-
-  protected
-
-  attr_reader :minutes
+  HOURS_PER_DAY = 24
+  MINUTES_PER_HOUR = 60
+  MINUTES_PER_DAY = MINUTES_PER_HOUR * HOURS_PER_DAY
+  private_constant :HOURS_PER_DAY, :MINUTES_PER_HOUR, :MINUTES_PER_DAY
 end
