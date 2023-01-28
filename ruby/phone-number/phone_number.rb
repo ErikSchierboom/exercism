a0bd843f40ef5a21ea6450c08b66ce7f3d7a1364
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 15:31:28 +0100

    ruby/phone-number: add iteration

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,8 +1,8 @@
 module PhoneNumber
   def self.clean(phone_number)
     @digits = phone_number.scan(/\d/)
-    @digits.unshift('1') unless @digits.size == 11
-    @digits[1..].join('') if valid?
+    @digits.unshift('1') unless correct_number_of_digits?
+    @digits[1..].join if valid?
   end
 
   private
@@ -11,19 +11,8 @@ module PhoneNumber
     correct_number_of_digits? && country_code_valid? && area_code_valid? && exchange_code_valid?
   end
 
-  def self.correct_number_of_digits?
-    @digits.size == 11
-  end
-
-  def self.country_code_valid?
-    @digits[0] == '1'
-  end
-
-  def self.area_code_valid?
-    @digits[1] != '0' && @digits[1] != '1'
-  end
-
-  def self.exchange_code_valid?
-    @digits[4] != '0' && @digits[4] != '1'
-  end
+  def self.correct_number_of_digits? = @digits.size == 11
+  def self.country_code_valid? = @digits[0] == '1'
+  def self.area_code_valid? = @digits[1] != '0' && @digits[1] != '1'
+  def self.exchange_code_valid? = @digits[4] != '0' && @digits[4] != '1'
 end
