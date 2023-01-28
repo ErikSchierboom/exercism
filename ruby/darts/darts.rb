22bd8f5b8009d4ff767cb9ddb01ccd29da05ae0b
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 20:44:39 +0100

    ruby/darts: add iteration

--- ruby/darts/darts.rb
+++ ruby/darts/darts.rb
@@ -6,21 +6,13 @@ class Darts
 
   def score
     case distance_from_center
-    when 0..1
-      10
-    when 1..5
-      5
-    when 5..10
-      1
-    else
-      0
+    when 0..1  then 10
+    when 1..5  then 5
+    when 5..10 then 1
+    else            0
     end
   end
 
   private
-  attr_reader :x, :y
-
-  def distance_from_center
-    Math.hypot(x, y)
-  end
+  def distance_from_center = Math.hypot(@x, @y)
 end
