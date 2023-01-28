19f4a796bb3e3cb192c54a9c10bbc574b3778cc9
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 15:49:52 +0100

    ruby/list-ops: add iteration

--- ruby/list-ops/list_ops.rb
+++ ruby/list-ops/list_ops.rb
@@ -1,75 +1,44 @@
 module ListOps
   def self.arrays(input)
     count = 0
-
-    input.each do
-      count += 1
-    end
-
+    input.each { count += 1 }
     count
   end
 
   def self.reverser(input)
     reversed = []
-
-    input.each do |element|
-      reversed.unshift(element)
-    end
-
+    input.each { |element| reversed.unshift(element) }
     reversed
   end
 
   def self.concatter(input1, input2)
     concatted = []
-
-    input1.each do |element|
-      concatted << element
-    end
-
-    input2.each do |element|
-      concatted << element
-    end
-
+    input1.each { |element| concatted << element }
+    input2.each { |element| concatted << element }
     concatted
   end
 
   def self.mapper(input)
     mapped = []
-
-    input.each do |element|
-      mapped << yield(element)
-    end
-
+    input.each { |element| mapped << yield(element) }
     mapped
   end
 
   def self.filterer(input)
     filtered = []
-
-    input.each do |element|
-      filtered << element if yield(element)
-    end
-
+    input.each { |element| filtered << element if yield(element) }
     filtered
   end
 
   def self.sum_reducer(input)
     sum = 0
-
-    input.each do |element|
-      sum += element
-    end
-
+    input.each { |element| sum += element }
     sum
   end
 
   def self.factorial_reducer(input)
     factorial = 1
-
-    input.each do |element|
-      factorial *= element
-    end
-
+    input.each { |element| factorial *= element }
     factorial
   end
 end
