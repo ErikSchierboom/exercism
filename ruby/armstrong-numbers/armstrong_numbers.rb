cd41fcaf4ff15e00da509dfe63e593a18941616b
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 20:58:08 +0100

    ruby/armstrong-numbers: add iteration

--- ruby/armstrong-numbers/armstrong_numbers.rb
+++ ruby/armstrong-numbers/armstrong_numbers.rb
@@ -1,5 +1,5 @@
 module ArmstrongNumbers
   def self.include?(number)
-    number.digits.sum { |digit| digit**number.digits.length } == number
+    number.digits.sum { |digit| digit ** number.digits.size } == number
   end
 end
