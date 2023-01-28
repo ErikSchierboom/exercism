9674e01182ef40124d21cee87e955fc9570dbe97
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 15:08:20 +0100

    ruby/strain: add iteration

--- ruby/strain/strain.rb
+++ ruby/strain/strain.rb
@@ -1,11 +1,4 @@
 module Enumerable
-  def keep
-    each_with_object([]) do |elem, acc|
-      acc << elem if yield(elem)
-    end
-  end
-
-  def discard
-    keep { |elem| !yield(elem) }
-  end
+  def keep = each_with_object([]) { |elem, acc| acc << elem if yield(elem) }
+  def discard = keep { |elem| !yield(elem) }
 end
