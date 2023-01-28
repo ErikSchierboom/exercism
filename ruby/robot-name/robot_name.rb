2d6ae7581f735b9ff80d3611fb3362e584698d0d
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 15:26:22 +0100

    ruby/robot-name: add iteration

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,19 +1,9 @@
 class Robot
   attr_reader :name
 
-  def initialize
-    reset
-  end
-
-  def reset
-    @name = Robot.random_names.next
-  end
-
-  def self.forget
-    random_names.rewind
-  end
-
-  def self.random_names
-    @@robot_names ||= ('AA000'..'ZZ999').to_a.shuffle.to_enum
-  end
+  def initialize = reset
+  def reset = @name = Robot.random_names.next
+  
+  def self.forget = random_names.rewind
+  def self.random_names = @@robot_names ||= ('AA000'..'ZZ999').to_a.shuffle.to_enum
 end
