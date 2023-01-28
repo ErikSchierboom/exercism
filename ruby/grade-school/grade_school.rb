4b4a316a53d8795628a94600efaff650b9f17cbd
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 20:41:52 +0100

    ruby/grade-school: add iteration

--- ruby/grade-school/grade_school.rb
+++ ruby/grade-school/grade_school.rb
@@ -1,21 +1,11 @@
 class School
-  def initialize
-    @grade_to_students = Hash.new([])
-  end
+  def initialize = @grade_to_students = Hash.new([])
 
-  def students(grade)
-    grade_to_students[grade]
-  end
-
-  def students_by_grade
-    grade_to_students.sort.map {|grade, students| {grade: grade, students: students}}
-  end
+  def students(grade) = @grade_to_students[grade]
+  def students_by_grade = @grade_to_students.sort.map {|grade, students| { grade:, students: }}
 
   def add(student, grade)
-    grade_to_students[grade] += [student]
-    grade_to_students[grade].sort!
+    @grade_to_students[grade] += [student]
+    @grade_to_students[grade].sort!
   end
-
-  private
-  attr_accessor :grade_to_students
 end
