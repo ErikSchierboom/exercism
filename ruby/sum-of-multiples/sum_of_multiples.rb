509990ce314ab9ed152fea9a851f7b55902e0059
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 15:34:21 +0100

    ruby/sum-of-multiples: add iteration

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -1,5 +1,7 @@
 class SumOfMultiples
   def initialize(*factors) = @factors = factors
 
-  def to(max) = (1...max).select { |n| @factors.any? { |factor| (n % factor).zero? } }.sum
+  def to(max)
+    (1...max).select { |n| @factors.any? { |factor| n.remainder(factor).zero? } }.sum
+  end
 end
