324d57d44d38d78fff8331edc5d85128ca15edcc
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 15:33:41 +0100

    ruby/perfect-numbers: add iteration

--- ruby/perfect-numbers/perfect_numbers.rb
+++ ruby/perfect-numbers/perfect_numbers.rb
@@ -1,30 +1,19 @@
 class PerfectNumber
-  def self.classify(number)
-    new(number).classify
-  end
+  def self.classify(number) = new(number).classify
 
-  def initialize(number)
-    @number = number
-  end
+  def initialize(number) = @number = number
 
   def classify
-    raise 'Number must be positive' unless number >= 0
+    raise 'Number must be positive' if @number.negative?
 
-    return 'deficient' if aliquot_sum < number
-    return 'abundant' if aliquot_sum > number
-
-    'perfect'
+    case aliquot_sum <=> @number
+    when -1 then 'deficient'
+    when  0 then 'perfect'
+    when  1 then 'abundant'
+    end
   end
 
   private
-
-  attr_reader :number
-
-  def aliquot_sum
-    factors.sum
-  end
-
-  def factors
-    1.upto(number / 2).select { |n| number.remainder(n).zero? }
-  end
+  def aliquot_sum = factors.sum
+  def factors = 1.upto(@number / 2).select { |n| @number.remainder(n).zero? }
 end
