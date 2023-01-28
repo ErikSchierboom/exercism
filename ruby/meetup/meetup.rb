02275256b869edb777cedcd16f7782127d6c760d
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 15:44:52 +0100

    ruby/meetup: add iteration

--- ruby/meetup/meetup.rb
+++ ruby/meetup/meetup.rb
@@ -10,32 +10,25 @@ class Meetup
     days = weekdays(weekday)
 
     case schedule
-    when :first then days.first
+    when :first  then days.first
     when :second then days[1]
-    when :third then days[2]
+    when :third  then days[2]
     when :fourth then days[3]
-    when :last then days.last
+    when :last   then days.last
     when :teenth then days.find { |date| date.mday >= FIRST_TEENTH_DAY_INDEX }
     end
   end
 
   private
-
   attr_reader :month, :year
 
   def weekdays(weekday)
     1.upto(days_in_month)
      .map { |day| Date.new(year, month, day) }
-     .select { |date| weekday(date) == weekday }
+     .select { |date| date.send("#{weekday}?") }
   end
 
-  def days_in_month
-    Date.new(year, month, -1).day
-  end
-
-  def weekday(date)
-    Date::DAYNAMES[date.wday].downcase.to_sym
-  end
+  def days_in_month = Date.new(year, month, -1).day
 
   FIRST_TEENTH_DAY_INDEX = 13
   private_constant :FIRST_TEENTH_DAY_INDEX
