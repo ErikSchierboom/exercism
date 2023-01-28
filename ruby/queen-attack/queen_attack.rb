975be4df2979cdfaab9adf3e2eb50f1b3a9ed24a
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 15:27:46 +0100

    ruby/queen-attack: add iteration

--- ruby/queen-attack/queen_attack.rb
+++ ruby/queen-attack/queen_attack.rb
@@ -2,11 +2,11 @@ class Queen
   attr_reader :row, :col
 
   def initialize(row, col)
-      raise ArgumentError, 'Invalid row' unless row.between?(MIN_ROW, MAX_ROW)
-      raise ArgumentError, 'Invalid col' unless col.between?(MIN_COL, MAX_COL)
+    raise ArgumentError, 'Invalid row' unless row.between?(MIN_ROW, MAX_ROW)
+    raise ArgumentError, 'Invalid col' unless col.between?(MIN_COL, MAX_COL)
 
-      @row = row
-      @col = col
+    @row = row
+    @col = col
   end
 
   MIN_ROW = 0
@@ -22,23 +22,12 @@ class Queens
     @black = Queen.new(black[0], black[1])
   end
 
-  def attack?
-    same_row? || same_col? || same_diagonal?
-  end
+  def attack? = same_row? || same_col? || same_diagonal?
 
   private
-
   attr_reader :white, :black
 
-  def same_row?
-    white.row == black.row
-  end
-
-  def same_col?
-    white.col == black.col
-  end
-
-  def same_diagonal?
-    (white.row - black.row).abs == (white.col - black.col).abs
-  end
+  def same_row? = white.row == black.row
+  def same_col? = white.col == black.col
+  def same_diagonal? = (white.row - black.row).abs == (white.col - black.col).abs
 end
