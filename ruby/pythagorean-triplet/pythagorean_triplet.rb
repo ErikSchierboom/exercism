b2bfeb9f4d31ea32bc5d9fb41adc83ca70282389
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 15:28:59 +0100

    ruby/pythagorean-triplet: add iteration

--- ruby/pythagorean-triplet/pythagorean_triplet.rb
+++ ruby/pythagorean-triplet/pythagorean_triplet.rb
@@ -3,25 +3,17 @@ class Triplet
     @x, @y, @z = x, y, z
   end
 
-  def sum
-    @x + @y + @z
-  end
-
-  def product
-    @x * @y * @z
-  end
-
-  def pythagorean?
-    @x**2 + @y**2 == @z**2
-  end
+  def sum = @x + @y + @z
+  def product = @x * @y * @z
+  def pythagorean? = @x**2 + @y**2 == @z**2
 
   def self.where(max_factor: 1, min_factor: 1, sum: nil)
     (min_factor...max_factor).flat_map do |x|
       (x.succ...max_factor).flat_map do |y|
-        (y.succ..max_factor).map do |z|
+        (y.succ..max_factor).filter_map do |z|
           triplet = new(x, y, z)
           triplet if triplet.pythagorean? && (sum.nil? || sum == triplet.sum)
-        end.compact
+        end
       end
     end
   end
