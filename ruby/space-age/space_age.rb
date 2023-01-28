96d6c27c32edc8db99776412a46bd0a72fa4079e
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-28 15:09:13 +0100

    ruby/space-age: add iteration

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,7 +1,7 @@
 class SpaceAge
   def initialize(age_in_seconds)
     ORBITAL_PERIODS_IN_EARTH_YEARS.each do |planet, orbital_period_in_earth_years|
-      define_singleton_method "on_#{planet.downcase}" do
+      define_singleton_method "on_#{planet}" do
         earth_years = age_in_seconds / (SECONDS_PER_EARTH_YEAR * orbital_period_in_earth_years)
         earth_years.round(2)
       end
@@ -11,14 +11,14 @@ class SpaceAge
   private
   SECONDS_PER_EARTH_YEAR = 31_557_600.00
   ORBITAL_PERIODS_IN_EARTH_YEARS = {
-    Mercury: 0.2408467,
-    Venus: 0.61519726,
-    Earth: 1.0,
-    Mars: 1.8808158,
-    Jupiter: 11.862615,
-    Saturn: 29.447498,
-    Uranus: 84.016846,
-    Neptune: 164.79132
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1.0,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
   }.freeze
   private_constant :SECONDS_PER_EARTH_YEAR
 end
