1c4e4397fb314261c639e66600d7f4f68a5b3cdd
Erik Schierboom <erik_schierboom@hotmail.com>
2023-02-24 14:27:17 +0100

    fsharp/bob: add iteration

--- fsharp/bob/Bob.fs
+++ fsharp/bob/Bob.fs
@@ -1,19 +1,19 @@
 ﻿module Bob
 
-let private (|Empty|_|) (phrase: string) =
+let private (|Silence|_|) (phrase: string): unit option =
     if System.String.IsNullOrWhiteSpace phrase then Some () else None
 
-let private (|Yell|_|) (phrase: string) =
+let private (|Yell|_|) (phrase: string): unit option =
     if phrase <> phrase.ToLower() && phrase = phrase.ToUpper() then Some () else None
     
-let private (|Question|_|) (phrase: string) =
+let private (|Question|_|) (phrase: string): unit option =
     if phrase.Trim().EndsWith "?" then Some () else None
 
 let response (phrase: string): string =
     match phrase with
-    | Empty -> "Fine. Be that way!"
+    | Silence         -> "Fine. Be that way!"
     | Yell & Question -> "Calm down, I know what I'm doing!"
-    | Yell -> "Whoa, chill out!"
-    | Question -> "Sure."
-    | _ -> "Whatever."
+    | Yell            -> "Whoa, chill out!"
+    | Question        -> "Sure."
+    | _               -> "Whatever."
     
