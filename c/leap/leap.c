5872364617323efd08422b6fcf3cbbc7e6d7b8d6
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-01 11:07:33 +0100

    c/leap: add solution

---
+++ c/leap/leap.c
@@ -0,0 +1,6 @@
+#include "leap.h"
+
+bool leap_year(int year)
+{
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+}
---
+++ c/leap/leap.h
@@ -0,0 +1,8 @@
+#ifndef LEAP_H
+#define LEAP_H
+
+#include <stdbool.h>
+
+bool leap_year(int year);
+
+#endif
