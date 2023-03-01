9c8da3124b08a42903cf03d3895f0bc678b4fb95
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-01 20:37:46 +0100

    cpp/hello-world: add iteration

---
+++ cpp/hello-world/hello_world_test.cpp
@@ -0,0 +1,16 @@
+// Include the header file with the definitions of the functions you create.
+#include "hello_world.h"
+
+// Include the test framework.
+#ifdef EXERCISM_TEST_SUITE
+#include <catch2/catch.hpp>
+#else
+#include "test/catch.hpp"
+#endif
+
+// Declares a single test.
+TEST_CASE("test_hello")
+{
+    // Check if your function returns "Hello, World!".
+    REQUIRE(hello_world::hello() == "Hello, World!");
+}
