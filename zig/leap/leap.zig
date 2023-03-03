1a808b0d9756b2a3769dbe9fc53578790ceabaf2
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-03 12:23:08 +0100

    zig/leap: add iteration

--- zig/leap/leap.zig
+++ zig/leap/leap.zig
@@ -1,3 +1,3 @@
 pub fn isLeapYear(year: u32) bool {
-    return @mod(year, 4) == 0 and (@mod(year, 100) != 0 or @mod(year, 400) == 0);
+    return year % 4 == 0 and year % 100 != 0 or year % 400 == 0;
 }
