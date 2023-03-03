4ba8f4acc06daab3a88dd60ff7280c3d2bb02c19
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-03 21:13:59 +0100

    zig/difference-of-squares: add iteration

--- zig/difference-of-squares/difference_of_squares.zig
+++ zig/difference-of-squares/difference_of_squares.zig
@@ -1,12 +1,12 @@
-pub fn squareOfSum(number: isize) isize {
-    const result = @divExact(number * (number + 1), 2);
+pub fn squareOfSum(number: usize) usize {
+    const result = number * (number + 1) / 2;
     return result * result;
 }
 
-pub fn sumOfSquares(number: isize) isize {
-    return @divExact(number * (number + 1) * (2 * number + 1), 6);
+pub fn sumOfSquares(number: usize) usize {
+    return (number * (number + 1) * (2 * number + 1)) / 6;
 }
 
-pub fn differenceOfSquares(number: isize) isize {
+pub fn differenceOfSquares(number: usize) usize {
     return squareOfSum(number) - sumOfSquares(number);
 }
