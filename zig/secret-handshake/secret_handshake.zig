1a62c8d12fa19de736fdbd045ec12cc65cd8a99e
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-03 12:52:45 +0100

    zig/secret-handshake: add iteration

--- zig/secret-handshake/secret_handshake.zig
+++ zig/secret-handshake/secret_handshake.zig
@@ -7,16 +7,11 @@ pub fn calculateHandshake(allocator: mem.Allocator, number: isize) mem.Allocator
     var actions = std.ArrayList(Signal).init(allocator);
     errdefer actions.deinit();
 
-    if (number & 0b00001 != 0)
-        try actions.append(Signal.wink);
-    if (number & 0b00010 != 0)
-        try actions.append(Signal.double_blink);
-    if (number & 0b00100 != 0)
-        try actions.append(Signal.close_your_eyes);
-    if (number & 0b01000 != 0)
-        try actions.append(Signal.jump);
-    if (number & 0b10000 != 0)
-        mem.reverse(Signal, actions.items);
+    if (number & 0b00001 != 0) try actions.append(Signal.wink);
+    if (number & 0b00010 != 0) try actions.append(Signal.double_blink);
+    if (number & 0b00100 != 0) try actions.append(Signal.close_your_eyes);
+    if (number & 0b01000 != 0) try actions.append(Signal.jump);
+    if (number & 0b10000 != 0) mem.reverse(Signal, actions.items);
 
     return actions.items;
 }
