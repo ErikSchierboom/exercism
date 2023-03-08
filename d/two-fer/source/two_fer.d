7f398e4c94f3275fac28d7d6c402f96e47fe285a
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-08 16:25:34 +0100

    d/two-fer: add solution

---
+++ d/two-fer/source/two_fer.d
@@ -0,0 +1,20 @@
+module two_fer;
+
+import std.format : format;
+
+@safe pure string twoFer(string name = "you")
+{
+    return format("One for %s, one for me.", name);
+}
+
+unittest
+{
+    // No name given
+    assert(twoFer() == "One for you, one for me.");
+
+    // A name given
+    assert(twoFer("Alice") == "One for Alice, one for me.");
+
+    // Another name given
+    assert(twoFer("Bob") == "One for Bob, one for me.");
+}
