87b1f962c70961b03da2c44f1173b16c91fe465e
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-10 13:02:32 +0100

    julia/leap: add iteration

--- julia/leap/leap.jl
+++ julia/leap/leap.jl
@@ -1,10 +1,4 @@
-"""
-    is_leap_year(year)
-
-Return `true` if `year` is a leap year in the gregorian calendar.
-
-"""
 function is_leap_year(year)
-    rem(year, 4) == 0 && rem(year, 100) != 0 || rem(year, 400) == 0
+    divisible_by(n) = year % n == 0
+    divisible_by(4) && !divisible_by(100) || divisible_by(400)
 end
-
