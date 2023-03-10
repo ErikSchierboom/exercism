59c803a8c80c918c4ac284767604e171419b7cc7
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-10 14:04:09 +0100

    julia/darts: add iteration

--- julia/darts/darts.jl
+++ julia/darts/darts.jl
@@ -1,7 +1,7 @@
 function score(x, y)
     distance = hypot(x, y)
-    distance <= 1.0 && return 10
-    distance <= 5.0 && return 5
-    distance <= 10.0 && return 1
+    distance ≤ 1.0 && return 10
+    distance ≤ 5.0 && return 5
+    distance ≤ 10.0 && return 1
     0
 end
