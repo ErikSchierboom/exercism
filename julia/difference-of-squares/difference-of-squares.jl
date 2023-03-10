013a657dadecd50137e26916fa3d9f8764a2617e
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-10 14:40:21 +0100

    julia/difference-of-squares: add iteration

--- julia/difference-of-squares/difference-of-squares.jl
+++ julia/difference-of-squares/difference-of-squares.jl
@@ -1,3 +1,3 @@
-square_of_sum(n) = (n * (n + 1) ÷ 2)^2
-sum_of_squares(n) = n * (n + 1) * (2 * n + 1) ÷ 6
+square_of_sum(n) = sum(1:n)^2
+sum_of_squares(n) = sum((1:n) .^ 2)
 difference(n) = square_of_sum(n) - sum_of_squares(n)
