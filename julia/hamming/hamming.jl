7a85bad48608172e64016e2560ee50b5ce1a789d
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-15 18:25:48 +0100

    julia/hamming: add iteration

--- julia/hamming/hamming.jl
+++ julia/hamming/hamming.jl
@@ -1,5 +1,5 @@
 function distance(a, b)
     length(a) == length(b) || throw(ArgumentError("Unequal lengths"))
 
-    length([1 for (x, y) = zip(a, b) if x ≠ y])
+    count(x ≠ y for (x, y) in zip(a, b))
 end
