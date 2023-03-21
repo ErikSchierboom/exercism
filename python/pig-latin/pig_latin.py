0db833655e9cf56aa57a0b605c6246166a11e825
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-21 17:16:26 +0100

    python/pig-latin: add solution

---
+++ python/pig-latin/pig_latin.py
@@ -0,0 +1,12 @@
+import re
+
+VOWEL_PATTERN = r'(?P<begin>^|\s+)(?P<vowel>[aeiou]|xr|yt)(?P<rest>\w+)'
+CONSONANT_PATTERN = r'(?P<begin>^|\s+)(?P<consonant>([^aeiou]?qu|[^aeiou]+))(?P<rest>[aeiouy]\w*)'
+
+VOWEL_REPLACEMENT = '\g<begin>\g<vowel>\g<rest>ay'
+CONSONANT_REPLACEMENT = '\g<begin>\g<rest>\g<consonant>ay'
+
+
+def translate(text):
+    return re.sub(VOWEL_PATTERN, VOWEL_REPLACEMENT, text) if re.match(VOWEL_PATTERN, text) else \
+        re.sub(CONSONANT_PATTERN, CONSONANT_REPLACEMENT, text)
