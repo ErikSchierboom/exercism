a38481384fb19a0dbcccfec4d4d4794d5f3b1721
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-22 17:47:17 +0100

    python/isogram: add iteration

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,3 +1,3 @@
 def is_isogram(string):
-    letters = [char for char in string.lower() if char.isalnum()]
+    letters = [char for char in string.lower() if char.isalpha()]
     return len(set(letters)) == len(letters)
