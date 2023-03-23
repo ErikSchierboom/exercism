6d0d97bac61202f917a23b5d8bf312acc88a0ccf
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-23 22:21:53 +0100

    r/collatz-conjecture: add iteration

--- r/collatz-conjecture/collatz-conjecture.R
+++ r/collatz-conjecture/collatz-conjecture.R
@@ -1,11 +1,17 @@
-collatz_step_counter <- function(num) {
-  count <- 0
-  
-  while (num != 1) {
-    num <- ifelse(num %% 2 == 0, yes = num / 2, no = num * 3 + 1)
-    count <- count + 1
+collatz_steps_count <- function(num, steps = 0) {
+  if(num < 1) {
+    stop("Input must be positive")
+  } else if (num == 1) {
+    steps
+  } else if (num %% 2 == 0) {
+    collatz_steps_count(num / 2, steps + 1)
+  } else {
+    collatz_steps_count(num * 3 + 1, steps + 1)
   }
-  
-  count
+}
+
+
+collatz_step_counter <- function(num) {
+  sapply(num, collatz_steps_count)
 }
 
