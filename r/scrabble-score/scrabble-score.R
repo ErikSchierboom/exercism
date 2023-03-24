a95039659c1dde49a9fd3cc241c379660eb94a1d
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-24 15:05:03 +0100

    r/scrabble-score: add iteration

--- r/scrabble-score/scrabble-score.R
+++ r/scrabble-score/scrabble-score.R
@@ -1,7 +1,9 @@
 scrabble_score <- function(input){
-  letter_scores = c(1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10)
-  names(letter_scores) = c('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+  letter_scores = c(
+    A = 1, B = 3, C = 3, D = 2, E = 1, F = 4, G = 2, H = 4, I = 1, J = 8,
+    K = 5, L = 1, M = 3, N = 1, O = 1, P = 3, Q = 10, R = 1, S = 1, T = 1,
+    U= 1, V = 4, W = 4, X = 8, Y = 4, Z = 10)
   
-  letters <- unlist(strsplit(toupper(input), split = ""))
-  sum(letter_scores[letters])
+  letters <- strsplit(toupper(input), split = "")
+  sum(letter_scores[unlist(letters)])
 }
