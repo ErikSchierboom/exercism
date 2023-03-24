4aa4deae26e2ab011274dbb44310fd34911d299a
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-24 15:58:00 +0100

    r/rna-transcription: add iteration

--- r/rna-transcription/rna-transcription.R
+++ r/rna-transcription/rna-transcription.R
@@ -1,8 +1,5 @@
 to_rna <- function(dna) {
-  mapping <- c('G' = 'C', 'C' = 'G', 'T' = 'A', 'A' = 'U')
-  nucleotides <- unlist(strsplit(dna, split = ""))
+  stopifnot(grepl("^[GCTA]*$", dna))
   
-  stopifnot(all(nucleotides %in% names(mapping)))
-  
-  paste(mapping[nucleotides], collapse="")
+  chartr("GCTA", "CGAU", dna)
 }
