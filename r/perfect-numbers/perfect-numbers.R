106515b705d2be25a4973ec8b29a6f39dd214295
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-26 20:53:33 +0200

    r/perfect-numbers: add iteration

--- r/perfect-numbers/perfect-numbers.R
+++ r/perfect-numbers/perfect-numbers.R
@@ -1,9 +1,12 @@
 number_type <- function(n) {
   stopifnot(n > 0)
+
+  if (n == 1) return("deficient")
   
-  possible_factors <- if(n > 1) { 1:(n/2) } else { c() }
+  possible_factors <- 1:(n/2)
   factors <- possible_factors[n %% possible_factors == 0]
   aliquot_sum <- sum(factors)
+
   if (aliquot_sum < n) {
     "deficient"
   } else if (aliquot_sum > n) {
