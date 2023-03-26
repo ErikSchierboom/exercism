18665cc06b30dc1dca5efbf6babfbc8b749d8ca1
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-26 21:32:58 +0200

    r/nucleotide-count: add iteration

--- r/nucleotide-count/nucleotide-count.R
+++ r/nucleotide-count/nucleotide-count.R
@@ -1,6 +1,6 @@
 nucleotide_count <- function(input) {
   stopifnot(grepl("^[ACGT]*$", input))
   
-  nucleotides <- unlist(strsplit(input, split=""))
-  sapply(c('A', 'C', 'G', 'T'), function(nucleotide) { sum(nucleotide == nucleotides)} ) |> as.list()
+  nucleotides <- strsplit(input, split="")[[1]]
+  sapply(c('A', 'C', 'G', 'T'), \(nucleotide) sum(nucleotide == nucleotides)) |> as.list()
 }
