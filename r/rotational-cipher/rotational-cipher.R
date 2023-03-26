13f261e02a57860fa2471c9afceac8c93be0928d
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-26 21:34:08 +0200

    r/rotational-cipher: add iteration

--- r/rotational-cipher/rotational-cipher.R
+++ r/rotational-cipher/rotational-cipher.R
@@ -1,5 +1,5 @@
 rotate <- function(text, key) {
-  rotated <- function(input) { c(tail(input, 26 - key), head(input, key)) }
+  rotated <- \(input) c(tail(input, 26 - key), head(input, key))
   unchanged = c(0:9, " ", "'", "!", ",", ".")
   from = paste(c(LETTERS, letters, unchanged), collapse="")
   to = paste(c(rotated(LETTERS), rotated(letters), unchanged), collapse="")
