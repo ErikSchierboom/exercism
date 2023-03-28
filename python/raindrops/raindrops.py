be23dedda1d8051bc84aed64c7db208584bcfd5e
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-28 13:56:31 +0200

    python/raindrops: add iteration

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,5 +1,5 @@
-DROPS = {3: 'Pling', 5: 'Plang', 7: 'Plong'}
+DROPS = (3, 'Pling'), (5, 'Plang'), (7, 'Plong')
 
 
 def convert(number):
-    return ''.join(sound for factor, sound in DROPS.items() if number % factor == 0) or str(number)
+    return ''.join(sound for factor, sound in DROPS if number % factor == 0) or str(number)
