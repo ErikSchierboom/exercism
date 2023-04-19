c1f54651e4e4e979c0a62992e1acb0da9d14b177
Erik Schierboom <erik_schierboom@hotmail.com>
2023-04-19 09:29:35 +0200

    python/etl: add iteration

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -1,3 +1,4 @@
 def transform(legacy_data):
-    return { letter.lower(): score for score, letters in legacy_data.items()
-                                   for letter in letters } 
+    return { letter.lower(): score
+             for score, letters in legacy_data.items()
+             for letter in letters } 
