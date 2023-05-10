e67022cee2ca1cf8ced0d8722d1e9071f48184e3
Erik Schierboom <erik_schierboom@hotmail.com>
2023-05-10 16:34:43 +0200

    scheme/grains: add iteration

--- scheme/grains/grains.scm
+++ scheme/grains/grains.scm
@@ -1,9 +1,9 @@
 (import (rnrs))
 
 (define (square n)
-  (if
-    (and (>= n 1) (<= n 64)) (expt 2 (- n 1))
-    (error 'test "invalid square")))
+  (if (<= 1 n 64)
+    (expt 2 (- n 1))
+    (error "n must be >= 1 and <= 64")))
 
 (define total
   (- (expt 2 64) 1))
