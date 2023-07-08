c018a33e6683b42b8c8f177eb8e291a009675d7d
Erik Schierboom <erik_schierboom@hotmail.com>
2023-07-08 14:17:25 +0200

    cpp/lasagna: add solution

---
+++ cpp/lasagna/lasagna.cpp
@@ -0,0 +1,29 @@
+// ovenTime returns the amount in minutes that the lasagna should stay in the
+// oven.
+int ovenTime()
+{
+    return 40;
+}
+
+/* remainingOvenTime returns the remaining
+   minutes based on the actual minutes already in the oven.
+*/
+int remainingOvenTime(int actualMinutesInOven)
+{
+    return ovenTime() - actualMinutesInOven;
+}
+
+/* preparationTime returns an estimate of the preparation time based on the
+   number of layers and the necessary time per layer.
+*/
+int preparationTime(int numberOfLayers)
+{
+    return numberOfLayers * 2;
+}
+
+// elapsedTime calculates the total time spent to create and bake the lasagna so
+// far.
+int elapsedTime(int numberOfLayers, int actualMinutesInOven)
+{
+    return actualMinutesInOven + preparationTime(numberOfLayers);
+}
