7f45ae2a521aad6e0e89a3f4004225eeefafb02d
Erik Schierboom <erik_schierboom@hotmail.com>
2023-07-11 08:19:54 +0200

    swift/series: add iteration

--- swift/series/Sources/Series/Series.swift
+++ swift/series/Sources/Series/Series.swift
@@ -6,7 +6,7 @@ struct Series {
     }
     
     func slices(_ size: Int) -> [[Int]] {
-        if size > digits.count { return [] }
+        guard size <= digits.count else { return [] }
 
         return (0...digits.count - size).map { idx in
             Array(digits[idx..<idx + size])
