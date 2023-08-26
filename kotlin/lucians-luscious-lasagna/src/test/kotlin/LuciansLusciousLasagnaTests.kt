e16f34f00e101f2865c333718b1de7f0e0cb0df7
Erik Schierboom <erik_schierboom@hotmail.com>
2023-08-26 16:37:46 +0200

    kotlin/lucians-luscious-lasagna: add solution

---
+++ kotlin/lucians-luscious-lasagna/src/test/kotlin/LuciansLusciousLasagnaTests.kt
@@ -0,0 +1,12 @@
+import org.junit.Test
+import org.junit.Ignore
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class LuciansLusciousLasagnaTests {
+    @Test
+    fun `test`()
+    {
+
+    }
+}
