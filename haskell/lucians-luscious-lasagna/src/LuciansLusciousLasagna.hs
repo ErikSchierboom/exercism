7e64e6d3d5fe949255d189777058bb463c0b8f12
Erik Schierboom <erik_schierboom@hotmail.com>
2023-08-30 17:55:56 +0200

    haskell/lucians-luscious-lasagna: add iteration

--- haskell/lucians-luscious-lasagna/src/LuciansLusciousLasagna.hs
+++ haskell/lucians-luscious-lasagna/src/LuciansLusciousLasagna.hs
@@ -1,8 +1,11 @@
 module LuciansLusciousLasagna (elapsedTimeInMinutes, expectedMinutesInOven, preparationTimeInMinutes) where
 
+expectedMinutesInOven :: Int
 expectedMinutesInOven = 40
 
+preparationTimeInMinutes :: Int -> Int
 preparationTimeInMinutes numberOfLayers = numberOfLayers * 2
 
+elapsedTimeInMinutes :: Int -> Int -> Int
 elapsedTimeInMinutes numberOfLayers actualTimeInOven = 
     actualTimeInOven + preparationTimeInMinutes numberOfLayers
