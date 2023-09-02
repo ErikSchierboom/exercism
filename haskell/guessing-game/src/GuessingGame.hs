a163279f74e143121c3eed572a36e694dce5c2be
Erik Schierboom <erik_schierboom@hotmail.com>
2023-09-02 13:38:17 +0200

    haskell/guessing-game: add iteration

--- haskell/guessing-game/src/GuessingGame.hs
+++ haskell/guessing-game/src/GuessingGame.hs
@@ -1,8 +1,8 @@
 module GuessingGame (reply) where
 
 reply :: Int -> String
+reply 42 = "Correct"
 reply guess
-  | guess == 41 || guess == 43 = "So close"
   | guess < 41 = "Too low"
   | guess > 43 = "Too high"
-  | otherwise = "Correct"
+  | otherwise = "So close"
