15b3773108c66a21cbb0d4dbeca6b6192a677877
Erik Schierboom <erik_schierboom@hotmail.com>
2023-09-08 16:17:21 +0200

    csharp/zipper: add iteration

--- csharp/zipper/Zipper.cs
+++ csharp/zipper/Zipper.cs
@@ -2,63 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class BinTree : IEquatable<BinTree>
-{
-    public BinTree(int value, BinTree left, BinTree right)
-    {
-        Value = value;
-        Left = left;
-        Right = right;
-    }
-
-    public BinTree(BinTree tree) : this(tree.Value, tree.Left, tree.Right)
-    {
-    }
-
-    public int Value { get; }
-    public BinTree Left { get; }
-    public BinTree Right { get; }
-
-    public bool Equals(BinTree other)
-    {
-        if (other == null || !Equals(Value, other.Value))
-            return false;
-
-        if (!ReferenceEquals(Left, other.Left) && (!Left?.Equals(other.Left) ?? false))
-            return false;
-
-        if (!ReferenceEquals(Right, other.Right) && (!Right?.Equals(other.Right) ?? false))
-            return false;
-
-        return true;
-    }
-}
-
-public abstract class BinTreeCrumb
-{
-    public BinTreeCrumb(int value, BinTree tree)
-    {
-        Value = value;
-        Tree = tree;
-    }
-
-    public int Value { get; }
-    public BinTree Tree { get; }
-}
-
-public class BinTreeLeftCrumb : BinTreeCrumb
-{
-    public BinTreeLeftCrumb(int value, BinTree tree) : base(value, tree)
-    {
-    }
-}
-
-public class BinTreeRightCrumb : BinTreeCrumb
-{
-    public BinTreeRightCrumb(int value, BinTree tree) : base(value, tree)
-    {
-    }
-}
+public record BinTree(int Value, BinTree Left, BinTree Right);
+public abstract record BinTreeCrumb(int Value, BinTree Tree);
+public record BinTreeLeftCrumb(int Value, BinTree Tree) : BinTreeCrumb(Value, Tree);
+public record BinTreeRightCrumb(int Value, BinTree Tree) : BinTreeCrumb(Value, Tree);
 
 public class Zipper : IEquatable<Zipper>
 {
@@ -67,21 +14,16 @@ public class Zipper : IEquatable<Zipper>
     private readonly BinTree right;
     private readonly List<BinTreeCrumb> crumbs;
 
-    public Zipper(int value, BinTree left, BinTree right, List<BinTreeCrumb> crumbs)
-    {
-        this.value = value;
-        this.left = left;
-        this.right = right;
-        this.crumbs = crumbs;
-    }
+    private Zipper(int value, BinTree left, BinTree right, List<BinTreeCrumb> crumbs) =>
+        (this.value, this.left, this.right, this.crumbs) = (value, left, right, crumbs);
     
     public int Value() => value;
 
-    public Zipper SetValue(int newValue) => new Zipper(newValue, left, right, crumbs);
+    public Zipper SetValue(int newValue) => new(newValue, left, right, crumbs);
 
-    public Zipper SetLeft(BinTree binTree) => new Zipper(value, binTree, right, crumbs);
+    public Zipper SetLeft(BinTree binTree) => new(value, binTree, right, crumbs);
 
-    public Zipper SetRight(BinTree binTree) => new Zipper(value, left, binTree, crumbs);
+    public Zipper SetRight(BinTree binTree) => new(value, left, binTree, crumbs);
 
     public Zipper Left()
     {
@@ -108,38 +50,26 @@ public class Zipper : IEquatable<Zipper>
 
         var firstCrumb = crumbs[0];
         var remainingCrumbs = crumbs.Skip(1).ToList();
-        
-        if (firstCrumb is BinTreeLeftCrumb)
-            return new Zipper(firstCrumb.Value, new BinTree(value, left, right), firstCrumb.Tree, remainingCrumbs);
-        
-        if (firstCrumb is BinTreeRightCrumb)
-            return new Zipper(firstCrumb.Value, firstCrumb.Tree, new BinTree(value, left, right), remainingCrumbs);
-        
-        return null;
-    }
 
-    public BinTree ToTree()
-    {
-        var tree = new BinTree(value, left, right);
-
-        foreach (var crumb in crumbs)
+        return firstCrumb switch
         {
-            if (crumb is BinTreeLeftCrumb)
-                tree = new BinTree(crumb.Value, new BinTree(tree), crumb.Tree);
-            if (crumb is BinTreeRightCrumb)
-                tree = new BinTree(crumb.Value, crumb.Tree, new BinTree(tree));
-        }
-
-        return tree;
+            BinTreeLeftCrumb => new Zipper(firstCrumb.Value, new BinTree(value, left, right), firstCrumb.Tree,
+                remainingCrumbs),
+            BinTreeRightCrumb => new Zipper(firstCrumb.Value, firstCrumb.Tree, new BinTree(value, left, right),
+                remainingCrumbs),
+            _ => null
+        };
     }
 
-    public bool Equals(Zipper other)
-    {
-        if (other == null)
-            return false;
+    public BinTree ToTree() =>
+        crumbs.Aggregate(new BinTree(value, left, right), (current, crumb) => crumb switch
+        {
+            BinTreeLeftCrumb => new BinTree(crumb.Value, current, crumb.Tree),
+            BinTreeRightCrumb => new BinTree(crumb.Value, crumb.Tree, current),
+            _ => current
+        });
 
-        return ToTree().Equals(other.ToTree());
-    }
+    public bool Equals(Zipper other) => other != null && ToTree().Equals(other.ToTree());
 
-    public static Zipper FromTree(BinTree tree) => new Zipper(tree.Value, tree.Left, tree.Right, new List<BinTreeCrumb>());
+    public static Zipper FromTree(BinTree tree) => new(tree.Value, tree.Left, tree.Right, new List<BinTreeCrumb>());
 }
