be1811e029b1f299eae60cf950a5d1a35b78fd6d
Erik Schierboom <erik_schierboom@hotmail.com>
2023-10-18 12:24:53 +0200

    csharp/accumulate: add iteration

--- csharp/accumulate/Accumulate.cs
+++ csharp/accumulate/Accumulate.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 public static class AccumulateExtensions
 {
-    public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> map)
+    public static IEnumerable<TResult> Accumulate<TSource, TResult>(this IEnumerable<TSource> collection, Func<TSource, TResult> map)
     {
         foreach (var element in collection)
             yield return map(element);
