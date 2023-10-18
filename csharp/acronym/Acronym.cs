64785b571362bf31a9415572fa25d5025c301bfc
Erik Schierboom <erik_schierboom@hotmail.com>
2023-10-18 12:55:56 +0200

    csharp/acronym: add iteration

--- csharp/acronym/Acronym.cs
+++ csharp/acronym/Acronym.cs
@@ -2,17 +2,14 @@
 using System.Linq;
 using System.Text.RegularExpressions;
 
-public static partial class Acronym
+public static class Acronym
 {
     public static string Abbreviate(string phrase) =>
         new(phrase.Words().Select(FirstLetter).ToArray());
 
     private static IEnumerable<string> Words(this string phrase) =>
-        WordsRegex().Matches(phrase).Select(m => m.Value);
+        Regex.Matches(phrase, @"[A-Za-z]+('[A-Za-z]+)?").Select(m => m.Value);
 
     private static char FirstLetter(string word) =>
         char.ToUpper(word[0]);
-
-    [GeneratedRegex("[A-Za-z]+('[A-Za-z]+)?")]
-    private static partial Regex WordsRegex();
 }
