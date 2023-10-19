9d236e2d279184c73daefd02ba17b8373ad88a53
Erik Schierboom <erik_schierboom@hotmail.com>
2023-10-19 17:41:04 +0200

    wasm/hello-world: add solution

---
+++ wasm/hello-world/hello-world.spec.js
@@ -0,0 +1,39 @@
+import { compileWat, WasmRunner } from "@exercism/wasm-lib";
+
+let wasmModule;
+let currentInstance;
+
+beforeAll(async () => {
+  try {
+    const watPath = new URL("./hello-world.wat", import.meta.url);
+    const { buffer } = await compileWat(watPath);
+    wasmModule = await WebAssembly.compile(buffer);
+  } catch (err) {
+    console.log(`Error compiling *.wat: ${err}`);
+  }
+});
+
+describe("Hello World", () => {
+  beforeEach(async () => {
+    currentInstance = null;
+
+    if (!wasmModule) {
+      return Promise.reject();
+    }
+    try {
+      currentInstance = await new WasmRunner(wasmModule);
+      return Promise.resolve();
+    } catch (err) {
+      console.log(`Error instantiating WebAssembly module: ${err}`);
+      return Promise.reject();
+    }
+  });
+
+  test("Say Hi!", () => {
+    expect(currentInstance).toBeTruthy();
+    const [offset, length] = currentInstance.exports.hello();
+    expect(length).toBe(13);
+    const greeting = currentInstance.get_mem_as_utf8(offset, length);
+    expect(greeting).toBe("Hello, World!");
+  });
+});
