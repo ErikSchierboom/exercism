2dddba74a99ec7ce0980848d27cddc99d4e93014
Erik Schierboom <erik_schierboom@hotmail.com>
2023-10-31 17:07:32 +0100

    wasm/difference-of-squares: add iteration

--- wasm/difference-of-squares/difference-of-squares.spec.js
+++ wasm/difference-of-squares/difference-of-squares.spec.js
@@ -35,39 +35,39 @@ describe("difference-of-squares", () => {
       expect(currentInstance.exports.squareOfSum(1)).toBe(1);
     });
 
-    test("square of sum 5", () => {
+    xtest("square of sum 5", () => {
       expect(currentInstance.exports.squareOfSum(5)).toBe(225);
     });
 
-    test("square of sum 100", () => {
+    xtest("square of sum 100", () => {
       expect(currentInstance.exports.squareOfSum(100)).toBe(25502500);
     });
   });
 
   describe("Sum the squares of the numbers up to the given number", () => {
-    test("sum of squares 1", () => {
+    xtest("sum of squares 1", () => {
       expect(currentInstance.exports.sumOfSquares(1)).toBe(1);
     });
 
-    test("sum of squares 5", () => {
+    xtest("sum of squares 5", () => {
       expect(currentInstance.exports.sumOfSquares(5)).toBe(55);
     });
 
-    test("sum of squares 100", () => {
+    xtest("sum of squares 100", () => {
       expect(currentInstance.exports.sumOfSquares(100)).toBe(338350);
     });
   });
 
   describe("Subtract sum of squares from square of sums", () => {
-    test("difference of squares 1", () => {
+    xtest("difference of squares 1", () => {
       expect(currentInstance.exports.difference(1)).toBe(0);
     });
 
-    test("difference of squares 5", () => {
+    xtest("difference of squares 5", () => {
       expect(currentInstance.exports.difference(5)).toBe(170);
     });
 
-    test("difference of squares 100", () => {
+    xtest("difference of squares 100", () => {
       expect(currentInstance.exports.difference(100)).toBe(25164150);
     });
   });
