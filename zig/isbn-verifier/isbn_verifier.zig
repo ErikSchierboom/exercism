1370dcc116123d15be2af7768e37bfda352e6827
Erik Schierboom <erik_schierboom@hotmail.com>
2023-11-21 17:32:31 +0100

    zig/isbn-verifier: add iteration

--- zig/isbn-verifier/isbn_verifier.zig
+++ zig/isbn-verifier/isbn_verifier.zig
@@ -5,21 +5,15 @@ pub fn isValidIsbn10(s: []const u8) bool {
     var checksum: u32 = 0;
 
     for (s) |c| {
-        var digit: u8 = switch (c) {
-            '-' => continue,
-            '0'...'9' => c - '0',
-            'X' => 10,
-            else => return false,
-        };
+        if (c == '-') continue;
 
-        if (multiplier < 1) return false;
-        if (digit == 10 and multiplier > 1) return false;
+        const digit: u8 = if (std.ascii.isDigit(c)) c - '0' else if (c == 'X') 10 else return false;
+
+        if (multiplier < 1 or digit == 10 and multiplier > 1) return false;
 
         checksum += (digit * multiplier);
         multiplier -= 1;
     }
 
-    if (multiplier > 0) return false;
-
-    return checksum % 11 == 0;
+    return multiplier == 0 and checksum % 11 == 0;
 }
