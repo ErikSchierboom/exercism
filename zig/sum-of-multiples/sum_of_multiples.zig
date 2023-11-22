cfd4d0c73198b747b136a394d749b9fbd4a31d86
Erik Schierboom <erik_schierboom@hotmail.com>
2023-11-22 10:35:43 +0100

    zig/sum-of-multiples: add iteration

--- zig/sum-of-multiples/sum_of_multiples.zig
+++ zig/sum-of-multiples/sum_of_multiples.zig
@@ -1,17 +1,20 @@
 const std = @import("std");
 const mem = std.mem;
 
-pub fn sum(_: mem.Allocator, factors: []const u32, limit: u32) !u64 {
-    var result: u64 = 0;
+pub fn sum(allocator: mem.Allocator, factors: []const u32, limit: u32) !u64 {
+    var bitSet = try std.bit_set.DynamicBitSet.initEmpty(allocator, limit);
+    defer bitSet.deinit();
 
-    for (1..limit) |n| {
-        for (factors) |factor| {
-            if (factor != 0 and n % factor == 0) {
-                result += n;
-                break;
-            }
+    for (factors) |factor| {
+        var multiple = factor;
+        while (factor > 0 and multiple < limit) : (multiple += factor) {
+            bitSet.set(multiple);
         }
     }
+
+    var result: u64 = 0;
+    var iterator = bitSet.iterator(.{});
+    while (iterator.next()) |i| result += i;
 
     return result;
 }
