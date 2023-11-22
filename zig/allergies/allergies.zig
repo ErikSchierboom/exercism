aa25dc08631e0df436b8cf84a45ef8028000f697
Erik Schierboom <erik_schierboom@hotmail.com>
2023-11-22 19:28:08 +0100

    zig/allergies: add solution

---
+++ zig/allergies/allergies.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+const EnumSet = std.EnumSet;
+
+pub const Allergen = enum {
+    eggs,
+    peanuts,
+    shellfish,
+    strawberries,
+    tomatoes,
+    chocolate,
+    pollen,
+    cats,
+};
+
+pub fn isAllergicTo(score: u8, allergen: Allergen) bool {
+    return initAllergenSet(score).contains(allergen);
+}
+
+pub fn initAllergenSet(score: usize) EnumSet(Allergen) {
+    var allergenSet = EnumSet(Allergen).initEmpty();
+    allergenSet.bits.mask = @intCast(score & 0b11111111);
+    return allergenSet;
+}
