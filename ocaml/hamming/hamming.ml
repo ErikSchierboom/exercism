7a8f97b9cf0c71576c4d8d4c4b034dee0604d1b8
Erik Schierboom <erik_schierboom@hotmail.com>
2023-11-27 09:11:24 +0100

    ocaml/hamming: add iteration

--- ocaml/hamming/hamming.ml
+++ ocaml/hamming/hamming.ml
@@ -1,11 +1,8 @@
 type nucleotide = A | C | G | T
 
-let hamming_distance first second =
-  if List.is_empty first && not (List.is_empty second) then
-    Error "left strand must not be empty"
-  else if List.is_empty second && not (List.is_empty first) then
-    Error "right strand must not be empty"
-  else if List.length first <> List.length second then
-    Error "left and right strands must be of equal length"
-  else
-    Ok (List.fold_left2 (fun distance l r -> if l <> r then distance + 1 else distance) 0 first second)
+let hamming_distance left right =
+  match List.length left, List.length right with
+  | x, y when x = y -> Ok (List.fold_left2 (fun distance l r -> if l <> r then distance + 1 else distance) 0 left right)
+  | 0, _ -> Error "left strand must not be empty"
+  | _, 0 -> Error "right strand must not be empty"
+  | _ -> Error "left and right strands must be of equal length"
