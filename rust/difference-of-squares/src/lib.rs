1dfddfd4494d12e425efb5549ed9661a1d88ab23
Erik Schierboom <erik_schierboom@hotmail.com>
2023-12-06 07:59:33 +0100

    rust/difference-of-squares: add iteration

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,15 +1,11 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    square((1..=n).sum())
+    ((n * (n + 1)) / 2).pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    (1..=n).map(|i| square(i)).sum()
+    (n * (n + 1) * (2 * n + 1)) / 6
 }
 
 pub fn difference(n: u32) -> u32 {
     square_of_sum(n) - sum_of_squares(n)
-}
-
-fn square(n: u32) -> u32 {
-    n * n
 }
