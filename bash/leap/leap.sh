3fc46ea28b1f41e285fd618258e80209df1f58db
Erik Schierboom <erik_schierboom@hotmail.com>
2024-01-16 10:17:45 +0100

    bash/leap: add iteration

--- bash/leap/leap.sh
+++ bash/leap/leap.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-if (( $1 % 4 == 0 && ($1 % 100 != 0 || $1 % 400 == 0) ));
+if [[ $# -ne 1 || $1 =~ [^0-9] ]]; then
+    echo "Usage: leap.sh <year>"
+    exit 1
+fi
+
+if (( $1 % 4 == 0 && ($1 % 100 != 0 || $1 % 400 == 0) )); then
     echo "true"
 else
     echo "false"
