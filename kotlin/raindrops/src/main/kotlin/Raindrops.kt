38b250a8219dd2ec1165249b897d80477b697576
Erik Schierboom <erik_schierboom@hotmail.com>
2024-01-23 09:43:34 +0100

    kotlin/raindrops: add iteration

--- kotlin/raindrops/src/main/kotlin/Raindrops.kt
+++ kotlin/raindrops/src/main/kotlin/Raindrops.kt
@@ -1,10 +1,9 @@
 object Raindrops {
     private val factors = mapOf(3 to "Pling", 5 to "Plang", 7 to "Plong")
 
-    fun convert(input: Int) =
-        factors
-            .filter { input % it.key == 0 }
-            .map { it.value }
-            .joinToString("")
-            .ifEmpty { input.toString() }
+    fun convert(n: Int) =
+        factors.entries
+            .filter { n % it.key == 0 }
+            .joinToString("") { it.value }
+            .ifEmpty { n.toString() }
 }
