9b89fc4a336784128d9155aaeda49d8e5346c424
Erik Schierboom <erik_schierboom@hotmail.com>
2024-01-31 15:01:48 +0100

    javascript/roman-numerals: add iteration

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -1,4 +1,4 @@
-function r(n, b, a, o, r) {
+export function toRoman(n, b, a, o, r) {
   for (r = b = "", a = 5; n; b++, a ^= 7)
     for (o = n % a, n = (n / a) ^ 0; o--; )
       r = "IVXLCDM".charAt(o > 2 ? b + n - (n &= ~1) + (o = 1) : b) + r;
@@ -6,4 +6,4 @@ function r(n, b, a, o, r) {
 }
 
 // Full out code golfing:
-// function r(n,b,a,o,r){for(r=b='',a=5;n;b++,a^=7)for(o=n%a,n=n/a^0;o--;)r='IVXLCDM'.charAt(o>2?b+n-(n&=~1)+(o=1):b)+r;return r}
+// export function toRoman(n,b,a,o,r){for(r=b='',a=5;n;b++,a^=7)for(o=n%a,n=n/a^0;o--;)r='IVXLCDM'.charAt(o>2?b+n-(n&=~1)+(o=1):b)+r;return r}
