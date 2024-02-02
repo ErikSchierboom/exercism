4d93f8242119f2d31d7b905995282e13150a7ea3
Erik Schierboom <erik_schierboom@hotmail.com>
2024-02-02 11:51:18 +0100

    fsharp/protein-translation: add iteration

--- fsharp/protein-translation/ProteinTranslation.fs
+++ fsharp/protein-translation/ProteinTranslation.fs
@@ -1,20 +1,16 @@
 ﻿module ProteinTranslation
 
-let private codonToProtein = 
-    function
-    | "AUG" -> "Methionine"
-    | "UGG" -> "Tryptophan"
-    | "UUC" | "UUU" -> "Phenylalanine"
-    | "UUA" | "UUG" -> "Leucine"    
-    | "UAU" | "UAC" -> "Tyrosine"
-    | "UGU" | "UGC" -> "Cysteine"    
-    | "UCU" | "UCC" | "UCA" | "UCG" -> "Serine"
-    | "UAA" | "UAG" | "UGA" -> "STOP"    
-    | _ -> failwith "Invalid codon"
+let proteins (rna: string): string list =
+    let doProteins (rna: string): (string * string) option =
+        match rna[0..2] with
+        | "AUG" ->                         Some ("Methionine",    rna[3..])
+        | "UGG" ->                         Some ("Tryptophan",    rna[3..])        
+        | "UUC" | "UUU" ->                 Some ("Phenylalanine", rna[3..])
+        | "UUA" | "UUG" ->                 Some ("Leucine",       rna[3..])
+        | "UAU" | "UAC" ->                 Some ("Tyrosine",      rna[3..])
+        | "UGU" | "UGC" ->                 Some ("Cysteine",      rna[3..])
+        | "UCU" | "UCC" | "UCA" | "UCG" -> Some ("Serine",        rna[3..])
+        | "UAA" | "UAG" | "UGA" | "" ->    None
+        | _ -> failwith "Unknown coding"
 
-let proteins (input: string) =
-    input 
-    |> Seq.chunkBySize 3 
-    |> Seq.map (System.String >> codonToProtein)
-    |> Seq.takeWhile (fun str -> str <> "STOP")
-    |> Seq.toList
+    List.unfold doProteins rna
