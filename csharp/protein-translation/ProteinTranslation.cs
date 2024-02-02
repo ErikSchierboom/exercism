d84c9d48a2d3adb0c37d7bd93c9a759d172bdd8e
Erik Schierboom <erik_schierboom@hotmail.com>
2024-02-02 12:00:47 +0100

    csharp/protein-translation: add iteration

--- csharp/protein-translation/ProteinTranslation.cs
+++ csharp/protein-translation/ProteinTranslation.cs
@@ -1,38 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 public static class ProteinTranslation
 {
     public static string[] Proteins(string strand) =>
-        strand.Chunked(3).Select(ToProtein).TakeWhile(protein => protein != "STOP").ToArray();
+        strand
+            .Chunk(3)
+            .Select(chunk => string.Concat(chunk))
+            .Select(ToProtein)
+            .TakeWhile(protein => protein != "STOP")
+            .ToArray();
 
     private static string ToProtein(string input) =>
         input switch
         {
             "AUG" => "Methionine",
-            "UUU" => "Phenylalanine",
-            "UUC" => "Phenylalanine",
-            "UUA" => "Leucine",
-            "UUG" => "Leucine",
-            "UCU" => "Serine",
-            "UCC" => "Serine",
-            "UCA" => "Serine",
-            "UCG" => "Serine",
-            "UAU" => "Tyrosine",
-            "UAC" => "Tyrosine",
-            "UGU" => "Cysteine",
-            "UGC" => "Cysteine",
             "UGG" => "Tryptophan",
-            "UAA" => "STOP",
-            "UAG" => "STOP",
-            "UGA" => "STOP",
+            "UUU" or "UUC" => "Phenylalanine",
+            "UUA" or "UUG" => "Leucine",
+            "UAU" or "UAC" => "Tyrosine",
+            "UGU" or "UGC" => "Cysteine",
+            "UCU" or "UCC" or "UCA" or "UCG" => "Serine",
+            "UAA" or "UAG" or "UGA" => "STOP",
             _ => throw new Exception("Invalid sequence")
         };
-
-    private static IEnumerable<string> Chunked(this string input, int size)
-    {
-        for (var i = 0; i < input.Length; i += size)
-            yield return input[i .. (i + size)];
-    }
 }
