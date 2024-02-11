aa418d3524234741084ff2af0c4bed1a29269417
Erik Schierboom <erik_schierboom@hotmail.com>
2024-02-11 12:47:38 +0100

    julia/raindrops: add iteration

--- julia/raindrops/raindrops.jl
+++ julia/raindrops/raindrops.jl
@@ -1,6 +1,6 @@
 DROPS = ((3, "Pling"), (5, "Plang"), (7, "Plong"))
 
 function raindrops(number)
-    sounds = [sound for (factor, sound) in DROPS if number % factor == 0]
-    isempty(sounds) ? string(number) : join(sounds)
+    sounds = join(sound for (factor, sound) in DROPS if number % factor == 0)
+    isempty(sounds) ? string(number) : sounds
 end
