e42ea438e2ac396b8902990e94c305d0d4a4626e
Erik Schierboom <erik_schierboom@hotmail.com>
2024-02-11 21:19:42 +0100

    julia/run-length-encoding: add solution

---
+++ julia/run-length-encoding/run-length-encoding.jl
@@ -0,0 +1,6 @@
+
+encode(s) = join(map(encode, eachmatch(r"(.)\1*", s)))
+encode(m::RegexMatch) = isone(length(m.match)) ? m[1] : string(length(m.match)) * m[1]
+
+decode(s) = join(map(decode, eachmatch(r"(\d*)(.)", s)))
+decode(m::RegexMatch) = m[2]^(isempty(m[1]) ? 1 : parse(Int32, m[1]))
