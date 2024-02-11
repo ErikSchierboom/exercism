19fa249ad4bb72860cd83e580752b27284cf7c76
Erik Schierboom <erik_schierboom@hotmail.com>
2024-02-11 14:16:21 +0100

    julia/scrabble-score: add iteration

--- julia/scrabble-score/scrabble-score.jl
+++ julia/scrabble-score/scrabble-score.jl
@@ -3,6 +3,4 @@ SCORES = Dict(
     for (letters, score) in ("AEIOULNRST" => 1, "DG" => 2, "BCMP" => 3, "FHVWY" => 4, "K" => 5, "JX" => 8, "QZ" => 10)
     for letter in letters)
 
-function score(str)
-    isempty(str) ? 0 : sum(get(SCORES, letter, 0) for letter in uppercase(str))
-end
+score(str) = sum(get(SCORES, letter, 0) for letter in uppercase(str); init=0)
