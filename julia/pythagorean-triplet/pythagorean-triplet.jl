64f7cf9a4b216b7a4938d9cee61e899c2e5fb521
Erik Schierboom <erik_schierboom@hotmail.com>
2024-02-14 19:01:51 +0100

    julia/pythagorean-triplet: add iteration

--- julia/pythagorean-triplet/pythagorean-triplet.jl
+++ julia/pythagorean-triplet/pythagorean-triplet.jl
@@ -1,1 +1,6 @@
-pythagorean_triplets(n) = [(a, b, c) for a in 1:n÷2 for b in a+1:n÷2 for c = n - b - a if a^2 + b^2 == c^2]
+pythagorean_triplets(n) =
+    [(a, b, c)
+     for a in 1:n÷3-1
+     for b in a+1:n÷2-1
+     for c = n - b - a
+     if a^2 + b^2 == c^2]
