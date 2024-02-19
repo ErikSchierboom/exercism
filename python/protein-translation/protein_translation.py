881770332f912de24709d5a2c3244091a65ea3a5
Erik Schierboom <erik_schierboom@hotmail.com>
2024-02-19 14:09:36 +0100

    python/protein-translation: add solution

---
+++ python/protein-translation/protein_translation.py
@@ -0,0 +1,21 @@
+MAPPING = {
+    'AUG': 'Methionine',
+    'UUU': 'Phenylalanine',
+    'UUC': 'Phenylalanine',
+    'UUA': 'Leucine', 'UUG': 'Leucine',
+    'UCU': 'Serine', 'UCC': 'Serine', 'UCA': 'Serine', 'UCG': 'Serine',
+    'UAU': 'Tyrosine', 'UAC': 'Tyrosine',
+    'UGU': 'Cysteine', 'UGC': 'Cysteine',
+    'UGG': 'Tryptophan',
+    'UAA': 'STOP', 'UAG': 'STOP', 'UGA': 'STOP'
+}
+
+def proteins(strand):
+    result = []
+
+    for i in range(0, len(strand), 3):
+        protein = MAPPING[strand[i:i+3]]
+        if protein == 'STOP': break
+        result.append(protein)
+
+    return result
