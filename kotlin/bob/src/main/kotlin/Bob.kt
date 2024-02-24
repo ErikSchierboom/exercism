1bd54a232f08156dcadf4c64b7f3fa1ba8d84231
Erik Schierboom <erik_schierboom@hotmail.com>
2024-02-24 19:38:49 +0100

    kotlin/bob: add iteration

--- kotlin/bob/src/main/kotlin/Bob.kt
+++ kotlin/bob/src/main/kotlin/Bob.kt
@@ -1,13 +1,13 @@
 object Bob {
     fun hey(response: String) = when {
-        response.isBlank() -> "Fine. Be that way!"
+        response.isSilent() -> "Fine. Be that way!"
         response.isYell() && response.isQuestion() -> "Calm down, I know what I'm doing!"
         response.isYell() -> "Whoa, chill out!"
         response.isQuestion() -> "Sure."
         else -> "Whatever."
     }
 
+    private fun String.isSilent() = isBlank()
     private fun String.isQuestion() = trimEnd().endsWith('?')
-
-    private fun String.isYell() = any(Char::isLetter) && uppercase() == this
+    private fun String.isYell() = any(Char::isLetter) && toUpperCase() == this
 }
