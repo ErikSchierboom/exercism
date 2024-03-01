7cd8d7986d4a1cd2d0e18ae513d4cf0e04dc7f72
Erik Schierboom <erik_schierboom@hotmail.com>
2024-03-01 11:39:21 +0100

    csharp/isogram: add iteration

--- csharp/isogram/Isogram.cs
+++ csharp/isogram/Isogram.cs
@@ -1,10 +1,20 @@
-﻿using System.Linq;
+﻿using System.Collections;
+using System.Linq;
 
 public static class Isogram
 {
     public static bool IsIsogram(string word)
     {
-        var lowerLetters = word.ToLower().Where(char.IsLetter).ToList();
-        return lowerLetters.Distinct().Count() == lowerLetters.Count;
+        var seen = new BitArray(26);
+
+        foreach (var c in word.Where(char.IsLetter).Select(char.ToLower))
+        {
+            if (seen.Get(c - 'a'))
+                return false;
+
+            seen.Set(c - 'a', true);
+        }
+
+        return true;
     }
 }
