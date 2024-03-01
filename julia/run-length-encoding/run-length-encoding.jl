f6ceb16c58e410a53127206bdbec7e0cf80e15e4
Erik Schierboom <erik_schierboom@hotmail.com>
2024-03-01 11:29:45 +0100

    julia/run-length-encoding: add iteration

--- julia/run-length-encoding/run-length-encoding.jl
+++ julia/run-length-encoding/run-length-encoding.jl
@@ -1,5 +1,5 @@
-encode(s) = join(map(encode, eachmatch(r"(.)\1*", s)))
-encode(m::RegexMatch) = isone(length(m.match)) ? m[1] : string(length(m.match)) * m[1]
+encode(s) = join(map(encode, eachmatch(r"(.)(\1*)", s)))
+encode(m::RegexMatch) = (isempty(m[2]) ? "" : string(length(m.match))) * m[1]
 
 decode(s) = join(map(decode, eachmatch(r"(\d*)(.)", s)))
 decode(m::RegexMatch) = m[2]^(isempty(m[1]) ? 1 : parse(Int32, m[1]))
