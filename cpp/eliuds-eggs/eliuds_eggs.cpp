481bcaccc161ca69154b4058070f07c1e8da3608
Erik Schierboom <erik_schierboom@hotmail.com>
2024-03-07 14:54:10 +0100

    cpp/eliuds-eggs: add iteration

--- cpp/eliuds-eggs/eliuds_eggs.cpp
+++ cpp/eliuds-eggs/eliuds_eggs.cpp
@@ -1,17 +1,11 @@
 #include "eliuds_eggs.h"
 
-#define MAGIC_MUL 0x1001001001001ULL
-#define MAGIC_AND 0x84210842108421ULL
-#define MAGIC_MOD 0x1f
-
 namespace chicken_coop
 {
     unsigned int positions_to_quantity(unsigned int v)
     {
-        unsigned int c = ((v & 0xfff) * MAGIC_MUL & MAGIC_AND) % MAGIC_MOD;
-        c += (((v & 0xfff000) >> 12) * MAGIC_MUL & MAGIC_AND) % MAGIC_MOD;
-        c += ((v >> 24) * MAGIC_MUL & MAGIC_AND) % MAGIC_MOD;
-
-        return c;
+        v = v - ((v >> 1) & 0x55555555);
+        v = (v & 0x33333333) + ((v >> 2) & 0x33333333);
+        return (((v + (v >> 4)) & 0xF0F0F0F) * 0x1010101) >> 24;
     }
 } // namespace chicken_coop
