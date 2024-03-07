f8e1167daa015a362b10f317c8b6452c93206bec
Erik Schierboom <erik_schierboom@hotmail.com>
2024-03-07 14:09:56 +0100

    csharp/binary-search: add iteration

--- csharp/binary-search/BinarySearch.cs
+++ csharp/binary-search/BinarySearch.cs
@@ -1,26 +1,20 @@
-﻿public static class BinarySearch
+﻿using System;
+
+public static class BinarySearch
 {
-    public static int Find(int[] array, int value)
+    public static int Find(Span<int> array, int value, int offset = 0)
     {
         if (array.Length == 0)
             return -1;
-
-        return FindBetweenIndices(0, array.Length - 1);
-
-        int FindBetweenIndices(int minIndex, int maxIndex)
-        {
-            if (minIndex > maxIndex)
-                return -1;
+        
+        var middleIndex = array.Length / 2;
+        var middleValue = array[middleIndex];
             
-            var middleIndex = (minIndex + maxIndex) / 2;
-            var valueAtMiddleIndex = array[middleIndex];
-            
-            if (valueAtMiddleIndex == value)
-                return middleIndex;
+        if (middleValue == value)
+            return offset + middleIndex;
 
-            return valueAtMiddleIndex > value
-                ? FindBetweenIndices(minIndex, middleIndex - 1)
-                : FindBetweenIndices(middleIndex + 1, maxIndex);
-        }
+        return middleValue > value
+            ? Find(array.Slice(0, middleIndex), value, offset)
+            : Find(array.Slice(middleIndex + 1), value, offset + middleIndex + 1);
     }
 }
