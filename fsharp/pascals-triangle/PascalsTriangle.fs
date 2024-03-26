8adcb119d4fbc4248a557e8a965c3691c994bb7b
Erik Schierboom <erik_schierboom@hotmail.com>
2024-03-26 14:43:47 +0100

    fsharp/pascals-triangle: add iteration

--- fsharp/pascals-triangle/PascalsTriangle.fs
+++ fsharp/pascals-triangle/PascalsTriangle.fs
@@ -4,10 +4,7 @@ let rows numberOfRows =
     let scanner previous row =
         match row with
         | 1 -> [1]
-        | _ ->
-            0 :: previous @ [0]
-            |> List.pairwise
-            |> List.map (fun (l,r) -> l+r) 
+        | _ -> 0 :: previous @ [0] |> List.pairwise |> List.map (fun (l,r) -> l+r) 
     
     [1 .. numberOfRows]
     |> List.scan scanner List.empty
