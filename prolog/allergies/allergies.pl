a4a0bb080730236ee75e41535b5ac96522368f5d
Erik Schierboom <erik_schierboom@hotmail.com>
2024-03-27 17:44:24 +0100

    prolog/allergies: add iteration

--- prolog/allergies/allergies.pl
+++ prolog/allergies/allergies.pl
@@ -2,15 +2,13 @@
 
 allergens([eggs, peanuts, shellfish, strawberries, tomatoes, chocolate, pollen, cats]).
 
-allergic_to(Score, eggs)         :- getbit(Score, 0) =:= 1, !.
-allergic_to(Score, peanuts)      :- getbit(Score, 1) =:= 1, !.
-allergic_to(Score, shellfish)    :- getbit(Score, 2) =:= 1, !.
-allergic_to(Score, strawberries) :- getbit(Score, 3) =:= 1, !.
-allergic_to(Score, tomatoes)     :- getbit(Score, 4) =:= 1, !.
-allergic_to(Score, chocolate)    :- getbit(Score, 5) =:= 1, !.
-allergic_to(Score, pollen)       :- getbit(Score, 6) =:= 1, !.
-allergic_to(Score, cats)         :- getbit(Score, 7) =:= 1, !.
+allergic_to(Score, eggs)         :- getbit(Score, 0) =:= 1.
+allergic_to(Score, peanuts)      :- getbit(Score, 1) =:= 1.
+allergic_to(Score, shellfish)    :- getbit(Score, 2) =:= 1.
+allergic_to(Score, strawberries) :- getbit(Score, 3) =:= 1.
+allergic_to(Score, tomatoes)     :- getbit(Score, 4) =:= 1.
+allergic_to(Score, chocolate)    :- getbit(Score, 5) =:= 1.
+allergic_to(Score, pollen)       :- getbit(Score, 6) =:= 1.
+allergic_to(Score, cats)         :- getbit(Score, 7) =:= 1.
 
-allergies(Score, Allergies) :-
-    allergens(Allergens),
-    include(allergic_to(Score), Allergens, Allergies).
+allergies(Score, Allergies) :- findall(Allergen, allergic_to(Score, Allergen), Allergies).
