98936ea391166d1254318ccae266fdd1e4e0abf3
Erik Schierboom <erik_schierboom@hotmail.com>
2024-03-28 08:57:53 +0100

    prolog/space-age: add iteration

--- prolog/space-age/space_age.pl
+++ prolog/space-age/space_age.pl
@@ -1,10 +1,10 @@
-space_age_period(Period, AgeSec, Years) :- Years is (AgeSec / Period) / 31557600.0.
+space_age_period(Period, Secs, Years) :- Years is (Secs / Period) / 31557600.0.
 
-space_age("Mercury", AgeSec, Years) :- space_age_period(0.2408467,  AgeSec, Years).
-space_age("Venus", AgeSec, Years)   :- space_age_period(0.61519726, AgeSec, Years).
-space_age("Earth", AgeSec, Years)   :- space_age_period(1.0,        AgeSec, Years).
-space_age("Mars", AgeSec, Years)    :- space_age_period(1.8808158,  AgeSec, Years).
-space_age("Jupiter", AgeSec, Years) :- space_age_period(11.862615,  AgeSec, Years).
-space_age("Saturn", AgeSec, Years)  :- space_age_period(29.447498,  AgeSec, Years).
-space_age("Uranus", AgeSec, Years)  :- space_age_period(84.016846,  AgeSec, Years).
-space_age("Neptune", AgeSec, Years) :- space_age_period(164.79132,  AgeSec, Years).
+space_age("Mercury", Secs, Years) :- space_age_period(0.2408467,  Secs, Years).
+space_age("Venus",   Secs, Years) :- space_age_period(0.61519726, Secs, Years).
+space_age("Earth",   Secs, Years) :- space_age_period(1.0,        Secs, Years).
+space_age("Mars",    Secs, Years) :- space_age_period(1.8808158,  Secs, Years).
+space_age("Jupiter", Secs, Years) :- space_age_period(11.862615,  Secs, Years).
+space_age("Saturn",  Secs, Years) :- space_age_period(29.447498,  Secs, Years).
+space_age("Uranus",  Secs, Years) :- space_age_period(84.016846,  Secs, Years).
+space_age("Neptune", Secs, Years) :- space_age_period(164.79132,  Secs, Years).
