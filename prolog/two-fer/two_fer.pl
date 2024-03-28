f6a07285013f586d91c4328ad40dca60af3ee742
Erik Schierboom <erik_schierboom@hotmail.com>
2024-03-28 08:14:54 +0100

    prolog/two-fer: add iteration

--- prolog/two-fer/two_fer.pl
+++ prolog/two-fer/two_fer.pl
@@ -1,5 +1,2 @@
-two_fer(Name, Dialogue) :-
-    string_concat("One for ", Name, Recipient),
-    string_concat(Recipient, ", one for me.", Dialogue).
-
 two_fer(Dialogue) :- two_fer("you", Dialogue).
+two_fer(Name, Dialogue) :- format(string(Dialogue), "One for ~s, one for me.", [Name]).
