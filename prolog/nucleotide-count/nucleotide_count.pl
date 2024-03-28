824bc6eec4bcfd7c25373843d17dafd579c4bf5b
Erik Schierboom <erik_schierboom@hotmail.com>
2024-03-28 10:49:36 +0100

    prolog/nucleotide-count: add iteration

--- prolog/nucleotide-count/nucleotide_count.pl
+++ prolog/nucleotide-count/nucleotide_count.pl
@@ -1,14 +1,14 @@
-nucleotide('A').
-nucleotide('C').
-nucleotide('G').
-nucleotide('T').
-
-letter_count(Letters, Letter, Mapping) :-
-    include(==(Letter), Letters, Matches),
-    length(Matches, Count),
-    Mapping = (Letter, Count).
+nucleotide(Nucleotide) :- member(Nucleotide, ['A', 'C', 'G', 'T']).
 
 nucleotide_count(Dna, Counts) :-
     string_chars(Dna, Letters),
     maplist(nucleotide, Letters),
-    maplist(letter_count(Letters), ['A','C','G', 'T'], Counts).
+    findall(
+        (Nucleotide, Count),
+        (   nucleotide(Nucleotide),
+            (   aggregate(count, member(Nucleotide, Letters), TempCount)
+            ->  Count = TempCount
+            ;   Count = 0
+            )
+        ),
+        Counts).
