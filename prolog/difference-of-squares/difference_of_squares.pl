9802f907f7f85fe4794ad6d0a78f3e66924c8ade
Erik Schierboom <erik_schierboom@hotmail.com>
2024-03-28 22:01:42 +0100

    prolog/difference-of-squares: add iteration

--- prolog/difference-of-squares/difference_of_squares.pl
+++ prolog/difference-of-squares/difference_of_squares.pl
@@ -1,11 +1,7 @@
 :- use_module(library(clpfd)).
 
-square_of_sum(N, Result) :-
-    Sum #= N * (N + 1) div 2,
-    Result #= Sum * Sum.
-
-sum_of_squares(N, Result) :-
-    Result #= (N * (N + 1) * (2 * N + 1)) div 6.
+square_of_sum(N, Result) :- Result #= (N * (N+1) // 2) ^ 2.
+sum_of_squares(N, Result) :- Result #= (N * (N+1) * (2*N+1)) // 6.
 
 difference(N, Result) :-
     square_of_sum(N, SquareOfSum),
