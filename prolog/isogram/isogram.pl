17325cc574fc67db5e5cc630592ce0cba1869247
Erik Schierboom <erik_schierboom@hotmail.com>
2024-03-29 19:28:48 +0100

    prolog/isogram: add iteration

--- prolog/isogram/isogram.pl
+++ prolog/isogram/isogram.pl
@@ -1,6 +1,7 @@
 isogram(Sentence) :-
-    string_lower(Sentence, Lower_sentence),
-    string_chars(Lower_sentence, Chars),
+    string_lower(Sentence, Lower),
+    string_chars(Lower, Chars),
     include([In]>>char_type(In, alpha), Chars, Letters),
-    list_to_set(Letters, Unique_letters),
-    length(Letters, X) == length(Unique_letters, X).
+    sort(Letters, UniqueLetters),
+    length(Letters, Length),
+    length(UniqueLetters, Length).
