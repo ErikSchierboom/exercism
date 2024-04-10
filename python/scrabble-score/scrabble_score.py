14f1525db11914e6278b5a5b57396b60a7db8412
Erik Schierboom <erik_schierboom@hotmail.com>
2024-04-10 21:53:18 +0200

    python/scrabble-score: add solution

---
+++ python/scrabble-score/scrabble_score.py
@@ -0,0 +1,13 @@
+from enum import IntEnum
+
+class LetterScore(IntEnum):
+    A = E = I = O = U = L = N = R = S = T = 1
+    D = G = 2
+    B = C = M = P = 3
+    F = H = V = W = Y = 4
+    K = 5
+    J = X = 8
+    Q = Z = 10
+
+def score(word):
+    return sum(LetterScore[letter] for letter in word.upper())
