cd6d8cd1e0f3aeac8ed46d7bd6f18c04631b8722
Erik Schierboom <erik_schierboom@hotmail.com>
2024-04-17 21:57:06 +0200

    csharp/pythagorean-triplet: add iteration

--- csharp/pythagorean-triplet/PythagoreanTriplet.cs
+++ csharp/pythagorean-triplet/PythagoreanTriplet.cs
@@ -1,17 +1,12 @@
 using System.Collections.Generic;
+using System.Linq;
 
 public static class PythagoreanTriplet
 {
-    public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
-    {
-        for (var a = 1; a <= sum / 3 - 1; ++a)
-        {
-            for (var b = a + 1; b <= (sum - a) / 2; ++b)
-            {
-                var c = sum - a - b;
-                if (a * a + b * b == c * c)
-                    yield return (a, b, c);
-            }
-        }
-    }
+    public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum) =>
+        from a in Enumerable.Range(2, sum / 3 - 2)
+        from b in Enumerable.Range(a + 1, (sum - a) / 2 - a)
+        let c = sum - a - b
+        where a * a + b * b == c * c
+        select (a, b, c);
 }
