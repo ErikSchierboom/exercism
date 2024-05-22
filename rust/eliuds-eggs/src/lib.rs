db0ff96a7b54532637d41111b03b2b71392961fa
Erik Schierboom <erik_schierboom@hotmail.com>
2024-05-22 19:27:52 +0200

    rust/eliuds-eggs: add iteration

--- rust/eliuds-eggs/src/lib.rs
+++ rust/eliuds-eggs/src/lib.rs
@@ -1,11 +1,11 @@
 pub fn egg_count(display_value: u32) -> usize {
-    let mut count = 0;
-    let mut remaining = display_value;
+    let mut count = 0usize;
+    let mut remaining = display_value as usize;
 
     while remaining > 0 {
         count += remaining & 1;
         remaining >>= 1;
     }
 
-    return count as usize;
+    count
 }
