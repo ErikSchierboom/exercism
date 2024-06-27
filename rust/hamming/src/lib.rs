e3975f564b997732865891ed01da4d914dc1da8b
Erik Schierboom <erik_schierboom@hotmail.com>
2024-06-27 20:39:25 +0100

    rust/hamming: add iteration

--- rust/hamming/src/lib.rs
+++ rust/hamming/src/lib.rs
@@ -1,5 +1,7 @@
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    if s1.len() != s2.len() { return None; }
+    if s1.len() != s2.len() {
+        return None;
+    }
 
     Some(s1.chars().zip(s2.chars()).filter(|(a, b)| a != b).count())
 }
