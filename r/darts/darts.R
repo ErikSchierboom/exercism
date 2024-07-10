f4a43951ca5e2e159aef6a7a2628a0cd29e94c9a
Erik Schierboom <erik_schierboom@hotmail.com>
2024-07-10 17:52:58 +0200

    r/darts: add iteration

--- r/darts/darts.R
+++ r/darts/darts.R
@@ -1,12 +1,7 @@
 score <- function(x, y) {
   distance <- sqrt(x^2 + y^2)
-  if (distance <= 1.0) { 
-    10
-  } else if (distance <= 5.0) { 
-    5
-  } else if (distance <= 10.0) { 
-    1
-  } else {
-    0
-  }
+  if (distance <= 1)  return(10)
+  if (distance <= 5)  return(5)
+  if (distance <= 10) return(1)
+  0
 }
