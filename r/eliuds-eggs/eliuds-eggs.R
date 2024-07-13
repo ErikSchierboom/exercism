5e58acfd0dfaa1656bdcaef436cb88a104b3e7f0
Erik Schierboom <erik_schierboom@hotmail.com>
2024-07-13 12:49:14 +0200

    r/eliuds-eggs: add iteration

--- r/eliuds-eggs/eliuds-eggs.R
+++ r/eliuds-eggs/eliuds-eggs.R
@@ -1,10 +1,3 @@
 egg_count <- function(display_value) {
-  count <- 0
-  
-  while (display_value > 0) {
-    count <- count + bitwAnd(display_value, 1)
-    display_value <- bitwShiftR(display_value, 1)
-  }
-  
-  count
+  sum(intToBits(display_value) > 0)
 }
