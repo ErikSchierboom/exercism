8290277950e109a1feb62411c5b1b0b957aeb28a
Erik Schierboom <erik_schierboom@hotmail.com>
2024-07-31 13:22:06 +0200

    rust/protein-translation: add iteration

--- rust/protein-translation/src/lib.rs
+++ rust/protein-translation/src/lib.rs
@@ -1,24 +1,19 @@
-use std::collections::HashMap;
-
-pub struct CodonsInfo<'a> {
-    codons: HashMap<&'a str, &'a str>
-}
-
-impl<'a> CodonsInfo<'a> {
-    pub fn name_for(&self, codon: &str) -> Option<&'a str> {
-        self.codons.get(&codon).cloned()
-    }
-
-    pub fn of_rna(&self, rna: &str) -> Option<Vec<&'a str>> {
-        rna.chars()
-            .collect::<Vec<char>>()
-            .chunks(3)
-            .map(|codon| self.name_for(&codon.iter().collect::<String>()))
-            .take_while(|protein| protein.is_none() || protein.unwrap() != "stop codon")
-            .collect()
-    }
-}
-
-pub fn parse<'a>(pairs: Vec<(&'a str, &'a str)>) -> CodonsInfo<'a> {
-    CodonsInfo { codons: pairs.into_iter().collect() }
+pub fn translate(rna: &str) -> Option<Vec<&str>> {
+    rna.as_bytes()
+        .chunks(3)
+        .map(|codon|
+            match codon {
+                b"AUG" => Some("Methionine"),
+                b"UUU" | b"UUC" => Some("Phenylalanine"),
+                b"UUA" | b"UUG" => Some("Leucine"),
+                b"UCU" | b"UCC" | b"UCA" | b"UCG" => Some("Serine"),
+                b"UAU" | b"UAC" => Some("Tyrosine"),
+                b"UGU" | b"UGC" => Some("Cysteine"),
+                b"UGG" => Some("Tryptophan"),
+                b"UAA" | b"UAG" | b"UGA" => Some("STOP"),
+                _ => None,
+            }
+        )
+        .take_while(|protein| protein.is_none() || protein.unwrap() != "STOP")
+        .collect()
 }
--- rust/protein-translation/tests/protein-translation.rs
+++ rust/protein-translation/tests/protein-translation.rs
@@ -1,297 +1,166 @@
 use protein_translation::*;
 
 #[test]
-fn methionine() {
-    let info = parse(make_pairs());
-    assert_eq!(info.name_for("AUG"), Some("methionine"));
-}
-
-#[test]
-fn cysteine_tgt() {
-    let info = parse(make_pairs());
-    assert_eq!(info.name_for("UGU"), Some("cysteine"));
-}
-
-#[test]
-fn stop() {
-    let info = parse(make_pairs());
-    assert_eq!(info.name_for("UAA"), Some("stop codon"));
-}
-
-#[test]
-fn valine() {
-    let info = parse(make_pairs());
-    assert_eq!(info.name_for("GUU"), Some("valine"));
-}
-
-#[test]
-fn isoleucine() {
-    let info = parse(make_pairs());
-    assert_eq!(info.name_for("AUU"), Some("isoleucine"));
-}
-
-#[test]
-fn arginine_cga() {
-    let info = parse(make_pairs());
-    assert_eq!(info.name_for("CGA"), Some("arginine"));
-}
-
-#[test]
-fn arginine_aga() {
-    let info = parse(make_pairs());
-    assert_eq!(info.name_for("AGA"), Some("arginine"));
-}
-
-#[test]
-fn arginine_agg() {
-    let info = parse(make_pairs());
-    assert_eq!(info.name_for("AGG"), Some("arginine"));
-}
-
-#[test]
-fn empty_is_invalid() {
-    let info = parse(make_pairs());
-    assert_eq!(info.name_for(""), None);
-}
-
-#[test]
-fn x_is_not_shorthand_so_is_invalid() {
-    let info = parse(make_pairs());
-    assert_eq!(info.name_for("VWX"), None);
-}
-
-#[test]
-fn too_short_is_invalid() {
-    let info = parse(make_pairs());
-    assert_eq!(info.name_for("AU"), None);
-}
-
-#[test]
-fn too_long_is_invalid() {
-    let info = parse(make_pairs());
-    assert_eq!(info.name_for("ATTA"), None);
-}
-
-#[test]
 fn empty_rna_sequence_results_in_no_proteins() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna(""), Some(vec![]),);
+    assert_eq!(translate(""), Some(vec![]),);
 }
 
 #[test]
 fn methionine_rna_sequence() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("AUG"), Some(vec!["methionine"]),);
+    assert_eq!(translate("AUG"), Some(vec!["Methionine"]),);
 }
 
 #[test]
 fn phenylalanine_rna_sequence_1() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UUU"), Some(vec!["phenylalanine"]),);
+    assert_eq!(translate("UUU"), Some(vec!["Phenylalanine"]),);
 }
 
 #[test]
 fn phenylalanine_rna_sequence_2() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UUC"), Some(vec!["phenylalanine"]),);
+    assert_eq!(translate("UUC"), Some(vec!["Phenylalanine"]),);
 }
 
 #[test]
 fn leucine_rna_sequence_1() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UUA"), Some(vec!["leucine"]),);
+    assert_eq!(translate("UUA"), Some(vec!["Leucine"]),);
 }
 
 #[test]
 fn leucine_rna_sequence_2() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UUG"), Some(vec!["leucine"]),);
+    assert_eq!(translate("UUG"), Some(vec!["Leucine"]),);
 }
 
 #[test]
 fn serine_rna_sequence_1() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UCU"), Some(vec!["serine"]),);
+    assert_eq!(translate("UCU"), Some(vec!["Serine"]),);
 }
 
 #[test]
 fn serine_rna_sequence_2() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UCC"), Some(vec!["serine"]),);
+    assert_eq!(translate("UCC"), Some(vec!["Serine"]),);
 }
 
 #[test]
 fn serine_rna_sequence_3() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UCA"), Some(vec!["serine"]),);
+    assert_eq!(translate("UCA"), Some(vec!["Serine"]),);
 }
 
 #[test]
 fn serine_rna_sequence_4() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UCG"), Some(vec!["serine"]),);
+    assert_eq!(translate("UCG"), Some(vec!["Serine"]),);
 }
 
 #[test]
 fn tyrosine_rna_sequence_1() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UAU"), Some(vec!["tyrosine"]),);
+    assert_eq!(translate("UAU"), Some(vec!["Tyrosine"]),);
 }
 
 #[test]
 fn tyrosine_rna_sequence_2() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UAC"), Some(vec!["tyrosine"]),);
+    assert_eq!(translate("UAC"), Some(vec!["Tyrosine"]),);
 }
 
 #[test]
 fn cysteine_rna_sequence_1() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UGU"), Some(vec!["cysteine"]),);
+    assert_eq!(translate("UGU"), Some(vec!["Cysteine"]),);
 }
 
 #[test]
 fn cysteine_rna_sequence_2() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UGC"), Some(vec!["cysteine"]),);
+    assert_eq!(translate("UGC"), Some(vec!["Cysteine"]),);
 }
 
 #[test]
 fn tryptophan_rna_sequence() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UGG"), Some(vec!["tryptophan"]),);
+    assert_eq!(translate("UGG"), Some(vec!["Tryptophan"]),);
 }
 
 #[test]
 fn stop_codon_rna_sequence_1() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UAA"), Some(vec![]),);
+    assert_eq!(translate("UAA"), Some(vec![]),);
 }
 
 #[test]
 fn stop_codon_rna_sequence_2() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UAG"), Some(vec![]),);
+    assert_eq!(translate("UAG"), Some(vec![]),);
 }
 
 #[test]
 fn stop_codon_rna_sequence_3() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UGA"), Some(vec![]),);
+    assert_eq!(translate("UGA"), Some(vec![]),);
 }
 
 #[test]
 fn sequence_of_two_protein_codons_translates_into_proteins() {
-    let info = parse(make_pairs());
     assert_eq!(
-        info.of_rna("UUUUUU"),
-        Some(vec!["phenylalanine", "phenylalanine"]),
+        translate("UUUUUU"),
+        Some(vec!["Phenylalanine", "Phenylalanine"]),
     );
 }
 
 #[test]
 fn sequence_of_two_different_protein_codons_translates_into_proteins() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UUAUUG"), Some(vec!["leucine", "leucine"]),);
+    assert_eq!(translate("UUAUUG"), Some(vec!["Leucine", "Leucine"]),);
 }
 
 #[test]
 fn translate_rna_strand_into_correct_protein_list() {
-    let info = parse(make_pairs());
     assert_eq!(
-        info.of_rna("AUGUUUUGG"),
-        Some(vec!["methionine", "phenylalanine", "tryptophan"]),
+        translate("AUGUUUUGG"),
+        Some(vec!["Methionine", "Phenylalanine", "Tryptophan"]),
     );
 }
 
 #[test]
 fn translation_stops_if_stop_codon_at_beginning_of_sequence() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UAGUGG"), Some(vec![]),);
+    assert_eq!(translate("UAGUGG"), Some(vec![]),);
 }
 
 #[test]
 fn translation_stops_if_stop_codon_at_end_of_two_codon_sequence() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UGGUAG"), Some(vec!["tryptophan"]),);
+    assert_eq!(translate("UGGUAG"), Some(vec!["Tryptophan"]),);
 }
 
 #[test]
 fn translation_stops_if_stop_codon_at_end_of_three_codon_sequence() {
-    let info = parse(make_pairs());
     assert_eq!(
-        info.of_rna("AUGUUUUAA"),
-        Some(vec!["methionine", "phenylalanine"]),
+        translate("AUGUUUUAA"),
+        Some(vec!["Methionine", "Phenylalanine"]),
     );
 }
 
 #[test]
 fn translation_stops_if_stop_codon_in_middle_of_three_codon_sequence() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("UGGUAGUGG"), Some(vec!["tryptophan"]),);
+    assert_eq!(translate("UGGUAGUGG"), Some(vec!["Tryptophan"]),);
 }
 
 #[test]
 fn translation_stops_if_stop_codon_in_middle_of_six_codon_sequence() {
-    let info = parse(make_pairs());
     assert_eq!(
-        info.of_rna("UGGUGUUAUUAAUGGUUU"),
-        Some(vec!["tryptophan", "cysteine", "tyrosine"]),
+        translate("UGGUGUUAUUAAUGGUUU"),
+        Some(vec!["Tryptophan", "Cysteine", "Tyrosine"]),
     );
+}
+
+#[test]
+fn sequence_of_two_non_stop_codons_does_not_translate_to_a_stop_codon() {
+    assert_eq!(translate("AUGAUG"), Some(vec!["Methionine", "Methionine"]),);
 }
 
 #[test]
 fn unknown_amino_acids_not_part_of_a_codon_can_t_translate() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("XYZ"), None,);
+    assert_eq!(translate("XYZ"), None,);
 }
 
 #[test]
 fn incomplete_rna_sequence_can_t_translate() {
-    let info = parse(make_pairs());
-    assert_eq!(info.of_rna("AUGU"), None,);
+    assert_eq!(translate("AUGU"), None,);
 }
 
 #[test]
 fn incomplete_rna_sequence_can_translate_if_valid_until_a_stop_codon() {
-    let info = parse(make_pairs());
     assert_eq!(
-        info.of_rna("UUCUUCUAAUGGU"),
-        Some(vec!["phenylalanine", "phenylalanine"]),
+        translate("UUCUUCUAAUGGU"),
+        Some(vec!["Phenylalanine", "Phenylalanine"]),
     );
-}
-
-// The input data constructor. Returns a list of codon, name pairs.
-fn make_pairs() -> Vec<(&'static str, &'static str)> {
-    let grouped = vec![
-        ("isoleucine", vec!["AUU", "AUC", "AUA"]),
-        ("valine", vec!["GUU", "GUC", "GUA", "GUG"]),
-        ("phenylalanine", vec!["UUU", "UUC"]),
-        ("methionine", vec!["AUG"]),
-        ("cysteine", vec!["UGU", "UGC"]),
-        ("alanine", vec!["GCU", "GCC", "GCA", "GCG"]),
-        ("glycine", vec!["GGU", "GGC", "GGA", "GGG"]),
-        ("proline", vec!["CCU", "CCC", "CCA", "CCG"]),
-        ("threonine", vec!["ACU", "ACC", "ACA", "ACG"]),
-        ("serine", vec!["UCU", "UCC", "UCA", "UCG"]),
-        ("tyrosine", vec!["UAU", "UAC"]),
-        ("tryptophan", vec!["UGG"]),
-        ("glutamine", vec!["CAA", "CAG"]),
-        ("asparagine", vec!["AAU", "AAC"]),
-        ("histidine", vec!["CAU", "CAC"]),
-        ("glutamic acid", vec!["GAA", "GAG"]),
-        ("aspartic acid", vec!["GAU", "GAC"]),
-        ("lysine", vec!["AAA", "AAG"]),
-        ("arginine", vec!["CGU", "CGC", "CGA", "CGG", "AGA", "AGG"]),
-        ("leucine", vec!["UUA", "UUG"]),
-        ("stop codon", vec!["UAA", "UAG", "UGA"]),
-    ];
-    let mut pairs = Vec::<(&'static str, &'static str)>::new();
-    for (name, codons) in grouped.into_iter() {
-        for codon in codons {
-            pairs.push((codon, name));
-        }
-    }
-    pairs.sort_by(|&(_, a), &(_, b)| a.cmp(b));
-    pairs
 }
