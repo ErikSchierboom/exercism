c6aaf910226e319960939587274d6e0e40f1e4d3
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-12 14:03:01 +0200

    r/luhn: add iteration

--- r/luhn/luhn.R
+++ r/luhn/luhn.R
@@ -1,12 +1,12 @@
 is_valid <- function(input) {
-  input <- gsub(" ", "", input)
+  digits <- gsub(" ", "", input)  |> strsplit("") |> unlist() |> as.numeric() |> suppressWarnings() |> rev()
   
-  if (!grepl("^\\d{2,}$", input))
+  if (length(digits) < 2 | any(is.na(digits)))
     return(FALSE)
   
-  digits <- rev(as.integer(strsplit(input, "")[[1]]))
   odd_numbers = seq(2, length(digits), by = 2)
   digits[odd_numbers] <- 2 * digits[odd_numbers]
   digits[digits > 9] <- digits[digits > 9] - 9
+  
   sum(digits) %% 10 == 0
 }
