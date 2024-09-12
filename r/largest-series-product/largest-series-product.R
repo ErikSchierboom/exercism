6295e3f6bc27ef1af5317422e6e5df982e69be7a
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-12 17:14:55 +0200

    r/largest-series-product: add iteration

--- r/largest-series-product/largest-series-product.R
+++ r/largest-series-product/largest-series-product.R
@@ -1,9 +1,8 @@
 largest_series_product <- function(sequence, span) {
   stopifnot(span <= nchar(sequence))
   
-  digits <- sequence |> strsplit("") |> unlist() |> as.numeric() |> suppressWarnings()
-  stopifnot(!any(is.na(digits)))
+  digits <- sequence |> strsplit("") |> unlist() |> as.integer()
+  stopifnot(!anyNA(digits))
   
-  lapply(1:(length(digits) - span + 1), function(i)
-    prod(digits[i:(i + span - 1)])) |> unlist() |> max()
+  max(sapply(1:(length(digits) - span + 1), function(i) prod(digits[i:(i + span - 1)])))
 }
