0067de8c7e378dc9ab9311ebdbd16366ffd16c26
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-13 16:12:37 +0200

    r/pascals-triangle: add iteration

--- r/pascals-triangle/pascals-triangle.R
+++ r/pascals-triangle/pascals-triangle.R
@@ -1,6 +1,7 @@
 pascals_triangle <- function(rows) {
-  stopifnot(is.numeric(rows) && rows >= 0)
+  stopifnot(rows >= 0)
   
-  if (rows == 0) return(list())
-  lapply(0:(rows-1), \(n) sapply(0:n, choose, n = n))
+  if (rows == 0) return (list())
+  
+  lapply(1:rows, \(n) choose(n - 1, 0:(n - 1)))
 }
