b92c7eaa5d2dfe566ae9380ab8c23ae3bed73b4b
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-13 15:14:02 +0200

    r/anagram: add iteration

--- r/anagram/anagram.R
+++ r/anagram/anagram.R
@@ -1,14 +1,13 @@
 anagram <- function(subject, candidates) {
-  str.sort <- \(str) sort(strsplit(tolower(str), "")[[1]])
+  sorted <- \(str) str |> tolower() |> strsplit("") |> unlist() |> sort()
   
-  is_anagram <- \(candidate)
+  is_anagram <- \(candidate) {
     !identical(tolower(subject), tolower(candidate)) &&
-      identical(str.sort(subject), str.sort(candidate))
+      identical(sorted(subject), sorted(candidate))
+  }
 
   anagrams <- candidates[sapply(candidates, is_anagram)]
-  if (length(anagrams) == 0) {
-    return(c())
-  } 
+  if (length(anagrams) == 0)return(c())
   
   anagrams
 }
