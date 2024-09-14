c92792f4c3d33fa1df6d387bfec8c170d2c71ab0
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-14 14:27:43 +0200

    r/phone-number: add iteration

--- r/phone-number/phone-number.R
+++ r/phone-number/phone-number.R
@@ -1,19 +1,6 @@
-library(stringr)
-
 parse_phone_number <- function(number_string) {
-  cleaned <- str_replace_all(number_string, "[\\(\\)\\.\\-\\s\\+]", "")
-  
-  digits <- cleaned |> strsplit("") |> unlist() |> as.numeric()
-  
-  if (any(is.na(digits))) return(NULL)
-  
-  if (length(digits) == 11) {
-    if (digits[1] != 1) return(NULL)
-
-    digits <- digits[-1]
-  }
-  
-  if (length(digits) != 10 || digits[1] < 2 || digits[4] < 2)  return(NULL)
-
-  digits |> paste(collapse = "")
+  cleaned <- gsub("\\D", "", number_string)
+  ifelse(grepl("^1?([2-9]\\d{2}){2}\\d{4}$", cleaned), 
+         gsub("^1", "", cleaned),
+         return(NULL))
 }
