8b6d82280807464d5cbe7f22ac3115367c8a4a43
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-15 14:18:09 +0200

    prolog/queen-attack: add iteration

--- prolog/queen-attack/queen_attack.pl
+++ prolog/queen-attack/queen_attack.pl
@@ -1,8 +1,5 @@
-create((Row, Col)) :-
-    between(0, 7, Row),
-    between(0, 7, Col).
+create((Row, Col)) :- between(0, 7, Row), between(0, 7, Col).
 
 attack((Row, _), (Row, _)) :- !.
 attack((_, Col), (_, Col)) :- !.
-attack((Row1, Col1), (Row2, Col2)) :-
-    abs(Row2-Row1)=:=abs(Col2-Col1).
+attack((Row1, Col1), (Row2, Col2)):- abs(Row1 - Row2) =:= abs(Col1 - Col2).
