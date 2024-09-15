29f0e3c075c8ae25a8339bc752735316728a03b8
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-15 14:33:02 +0200

    prolog/sublist: add iteration

--- prolog/sublist/sublist.pl
+++ prolog/sublist/sublist.pl
@@ -1,4 +1,4 @@
 sublist(Xs, Xs, equal) :- !.
-sublist(Xs, Ys, sublist) :- append(_, Rest, Ys), append(Xs, _, Rest), !.
+sublist(Xs, Ys, sublist) :- append(Prefix, _, Ys), append(_, Xs, Prefix), !.
 sublist(Xs, Ys, superlist) :- sublist(Ys, Xs, sublist), !.
 sublist(_, _, unequal).
