caf8f16c74d4392c6c43b4a093766fcd3b5d738d
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-29 08:48:06 +0200

    prolog/acronym: add iteration

--- prolog/acronym/acronym.pl
+++ prolog/acronym/acronym.pl
@@ -1,15 +1,13 @@
-words(Sentence, Words) :-
-    split_string(Sentence, " ", "", W0),
-    maplist([Word,Acc]>>(split_string(Word,"-", "",Acc)), W0, W1),
-    flatten(W1, W2),
-    maplist(string_upper, W2, W3),
-    delete(W3, "", Words).
-
-abbreviate_word(Word, Letter) :-
-    string_chars(Word, Letters),
-    delete(Letters, '_', [Letter|_]).
+letters([], _, []).
+letters([Letter|Rest], Previous, [Letter|Letters]) :-    
+    char_type(Letter, upper),
+    member(Previous, [' ', '-', '_']),
+    letters(Rest, Letter, Letters), !.
+letters([Letter|Rest], _, Letters) :-
+    letters(Rest, Letter, Letters).
 
 abbreviate(Sentence, Acromym) :-
-    words(Sentence, Words),
-    maplist(abbreviate_word, Words, Letters),
+    string_upper(Sentence, UpperSentence),
+    string_chars(UpperSentence, Chars),
+    letters(Chars, ' ', Letters),
     string_chars(Acromym, Letters).
