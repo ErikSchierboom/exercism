fb4030b3791dae78c1031cd0b626254a2c38a975
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-29 19:25:00 +0200

    r/allergies: add iteration

--- r/allergies/allergies.R
+++ r/allergies/allergies.R
@@ -1,15 +1,13 @@
-allergen_scores = c(eggs = 1, peanuts = 2, shellfish = 4, strawberries = 8, 
-                    tomatoes = 16, chocolate = 32, pollen = 64, cats = 128)
+allergens <- c("eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats")
 
 allergy <- function(num) {
-  num
+  allergens[as.logical(intToBits(num))] |> na.exclude()
 }
 
 allergic_to <- function(allergy_object, allergy) {
-  bitwAnd(allergy_object, allergen_scores[[allergy]]) != 0
+  allergy %in% allergy_object
 }
 
 list_allergies <- function(allergy_object) {
-  allergens = names(allergen_scores)
-  allergens[sapply(allergens, allergic_to, allergy_object = allergy_object)]
+  allergy_object
 }
