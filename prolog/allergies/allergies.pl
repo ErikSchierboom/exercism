fdd3e1aad579e030406e151c86d276920f36b206
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-29 08:56:33 +0200

    prolog/allergies: add iteration

--- prolog/allergies/allergies.pl
+++ prolog/allergies/allergies.pl
@@ -1,10 +1,15 @@
-allergic_to(Score, eggs)         :- getbit(Score, 0) =:= 1.
-allergic_to(Score, peanuts)      :- getbit(Score, 1) =:= 1.
-allergic_to(Score, shellfish)    :- getbit(Score, 2) =:= 1.
-allergic_to(Score, strawberries) :- getbit(Score, 3) =:= 1.
-allergic_to(Score, tomatoes)     :- getbit(Score, 4) =:= 1.
-allergic_to(Score, chocolate)    :- getbit(Score, 5) =:= 1.
-allergic_to(Score, pollen)       :- getbit(Score, 6) =:= 1.
-allergic_to(Score, cats)         :- getbit(Score, 7) =:= 1.
+allergic_to(Score, Allergen) :- 
+    score(Allergen, AllergenScore),
+    Score /\ AllergenScore =:= AllergenScore.
 
-allergies(Score, Allergies) :- findall(Allergen, allergic_to(Score, Allergen), Allergies).
+allergies(Score, Allergies) :-
+    findall(Allergen, allergic_to(Score, Allergen), Allergies).
+
+score(eggs, 1).
+score(peanuts, 2).
+score(shellfish, 4).
+score(strawberries, 8).
+score(tomatoes, 16).
+score(chocolate, 32).
+score(pollen, 64).
+score(cats, 128).
