27feb11a0dd4b7fb87b53c9d75865000fff54a4c
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-29 19:14:51 +0200

    prolog/flatten-array: add iteration

--- prolog/flatten-array/flatten_array.pl
+++ prolog/flatten-array/flatten_array.pl
@@ -1,17 +1,10 @@
 flatten_list([], []) :- !.
-
-flatten_list([X|Xs], Flattened) :-
-    X = nil,
-    flatten_list(Xs, Flattened),
-    !.
+flatten_list([nil|Xs], Flattened) :- flatten_list(Xs, Flattened), !.
 
 flatten_list([X|Xs], Flattened) :-
     is_list(X),
-    flatten_list(X, XFlattened),
-    flatten_list(Xs, XsFlattened),
-    append(XFlattened, XsFlattened, Flattened),
+    append(X, Xs, Appended),
+    flatten_list(Appended, Flattened),
     !.
 
-flatten_list([X|Xs], [X|Flattened]) :-
-    flatten_list(Xs, Flattened),
-    !.
+flatten_list([X|Xs], [X|Flattened]) :- flatten_list(Xs, Flattened), !.
