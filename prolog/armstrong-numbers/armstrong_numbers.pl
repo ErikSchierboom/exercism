ef97fdf6a265788af35c69bdd79815d1943f3393
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-29 09:10:57 +0200

    prolog/armstrong-numbers: add iteration

--- prolog/armstrong-numbers/armstrong_numbers.pl
+++ prolog/armstrong-numbers/armstrong_numbers.pl
@@ -1,10 +1,10 @@
-digits(N, Digits) :-
-    number_codes(N, Codes),
-    maplist([Code,Digit]>>number_codes(Digit, [Code]), Codes, Digits).
+digit(Code, Digit) :- number_codes(Digit, [Code]).
+digits(N, Digits) :- number_codes(N, Codes), maplist(digit, Codes, Digits).
 
 armstrong_sum(N, Sum) :-
     digits(N, Digits),
     length(Digits, NumDigits),
-    aggregate(sum(Pow), Digit^Pow^(member(Digit, Digits), Pow is Digit ^ NumDigits), Sum).
+    maplist([Digit, Pow]>>(Pow is Digit ^ NumDigits), Digits, Powers),
+    sum_list(Powers, Sum).
 
 armstrong_number(N) :- armstrong_sum(N, N).
