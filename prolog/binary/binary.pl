90ec945825a69ed488cec3ddba7baef162d84505
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-29 09:18:45 +0200

    prolog/binary: add iteration

--- prolog/binary/binary.pl
+++ prolog/binary/binary.pl
@@ -1,6 +1,4 @@
-shift('0', Acc, NewAcc) :- NewAcc is Acc << 1, !.
-shift('1', Acc, NewAcc) :- NewAcc is Acc << 1 + 1.
+binary(Str, Dec) :- string_chars(Str, Digits), foldl(binary, Digits, 0, Dec).
 
-binary(Str, Dec) :-
-    string_chars(Str, Digits),
-    foldl(shift, Digits, 0, Dec).
+binary('0', Acc, NewAcc) :- NewAcc is Acc << 1, !.
+binary('1', Acc, NewAcc) :- NewAcc is Acc << 1 + 1.
