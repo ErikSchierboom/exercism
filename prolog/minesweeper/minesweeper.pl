0fd861b14ad06658dd43df0c31324dde9d7abd31
Erik Schierboom <erik_schierboom@hotmail.com>
2024-10-19 16:35:36 +0200

    prolog/minesweeper: add iteration

--- prolog/minesweeper/minesweeper.pl
+++ prolog/minesweeper/minesweeper.pl
@@ -1,8 +1,6 @@
 :- use_module(library(clpfd)).
 
-cell(Rows, X, Y, Cell) :-
-    nth0(Y, Rows, Row),
-    nth0(X, Row, Cell).
+cell(Rows, X, Y, Cell) :- nth1(Y, Rows, Row), nth1(X, Row, Cell).
 
 adjacent_mine(Rows, X, Y) :-
     between(-1, 1, Dx),
@@ -11,25 +9,22 @@ adjacent_mine(Rows, X, Y) :-
     NeighborY #= Y + Dy,
     cell(Rows, NeighborX, NeighborY, '*').
 
-annotate_cell(_, '*', _, _, '*') :- !.
-annotate_cell(Rows, _, X, Y, AnnotatedCell) :-
+annotate_cell(_, _, '*', _, '*') :- !.
+annotate_cell(Rows, Y, _, X, AnnotatedCell) :-
     findall(1, adjacent_mine(Rows, X, Y), Mines),
     length(Mines, Count),
     (Count #= 0 -> AnnotatedCell = ' '; atom_number(AnnotatedCell, Count)).    
 
-annotate_row(_, [], _, _, []) :- !.
-annotate_row(Rows, [Cell|Cells], X, Y, [AnnotatedCell|AnnotatedCells]) :-
-    NextX #= X + 1,
-    annotate_cell(Rows, Cell, X, Y, AnnotatedCell),
-    annotate_row(Rows, Cells, NextX, Y, AnnotatedCells).
+annotate_row(Rows, Y, Row, AnnotatedRow) :-
+    length(Row, Length),
+    numlist(1, Length, Xs),
+    maplist(annotate_cell(Rows, Y), Row, Xs, AnnotatedCells),
+    string_chars(AnnotatedRow, AnnotatedCells).
 
-annotate_board(_, [], _, []) :- !.
-annotate_board(Rows, [Row|Rest], Y, [AnnotatedRow|AnnotatedRows]) :-
-    NextY #= Y + 1,
-    annotate_row(Rows, Row, 0, Y, AnnotatedRowChars),
-    string_chars(AnnotatedRow, AnnotatedRowChars),
-    annotate_board(Rows, Rest, NextY, AnnotatedRows), !.
-
+annotate([], []) :- !.
+annotate([""], [""]) :- !.
 annotate(Board, AnnotatedBoard) :-
     maplist(string_chars, Board, Rows),
-    annotate_board(Rows, Rows, 0, AnnotatedBoard).
+    length(Rows, Height),
+    numlist(1, Height, Ys),
+    maplist(annotate_row(Rows), Ys, Rows, AnnotatedBoard).
