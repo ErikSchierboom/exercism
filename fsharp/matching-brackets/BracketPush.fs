216ad9c19c581b6730c936c7a05877cecf3b7180
Erik Schierboom <erik_schierboom@hotmail.com>
2024-11-25 08:46:03 +0100

    fsharp/matching-brackets: add solution

---
+++ fsharp/matching-brackets/BracketPush.fs
@@ -0,0 +1,21 @@
+﻿module BracketPush
+
+let private delimiterPairs = Map.ofList [('[', ']'); ('{', '}'); ('(', ')')]
+
+let private isClosingDelimiter closeDelimiter =
+    Map.exists (fun _ value -> value = closeDelimiter) delimiterPairs
+
+let private isOpenDelimiter openDelimiter =
+    Map.containsKey openDelimiter delimiterPairs
+     
+let isPaired (input: string) =        
+    let rec loop unpaired unchecked =
+        match unpaired, unchecked with
+        | [], [] -> true
+        | _,  [] -> false
+        | _, y::ys when isOpenDelimiter y -> loop (y :: unpaired) ys
+        | x::xs, y::ys when isClosingDelimiter y && Map.find x delimiterPairs = y -> loop xs ys
+        | _, y::_ when isClosingDelimiter y -> false
+        | _, _::ys -> loop unpaired ys
+        
+    loop [] (List.ofSeq input)
---
+++ fsharp/matching-brackets/README.md
@@ -0,0 +1,28 @@
+# Matching Brackets
+
+Welcome to Matching Brackets on Exercism's F# Track.
+If you need help running the tests or submitting your code, check out `HELP.md`.
+
+## Instructions
+
+Given a string containing brackets `[]`, braces `{}`, parentheses `()`,
+or any combination thereof, verify that any and all pairs are matched
+and nested correctly.
+
+## Source
+
+### Created by
+
+- @ErikSchierboom
+
+### Contributed to by
+
+- @jrr
+- @lestephane
+- @robkeim
+- @valentin-p
+- @wolf99
+
+### Based on
+
+Ginna Baker
