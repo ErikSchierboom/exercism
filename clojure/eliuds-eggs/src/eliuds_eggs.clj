1dedc4ad78cbc624b432c911993cd5dc7db07ea1
Erik Schierboom <erik_schierboom@hotmail.com>
2025-01-04 20:06:10 +0100

    clojure/eliuds-eggs: add iteration

--- clojure/eliuds-eggs/src/eliuds_eggs.clj
+++ clojure/eliuds-eggs/src/eliuds_eggs.clj
@@ -1,8 +1,8 @@
 (ns eliuds-eggs)
 
 (defn egg-count [number]
-  (loop [n number
-         count 0]
-    (if (zero? n)
-      count
-      (recur (bit-shift-right n 1) (+ (bit-and n 1) count)))))
+  (->> number
+       (iterate #(bit-shift-right % 1))
+       (take-while pos?)
+       (map #(bit-and % 1))
+       (apply +)))
