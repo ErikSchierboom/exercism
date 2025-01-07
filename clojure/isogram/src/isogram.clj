89a7b64d3fa8fa33d10e0582af0d8e50f599725d
Erik Schierboom <erik_schierboom@hotmail.com>
2025-01-07 20:43:51 +0100

    clojure/isogram: add iteration

--- clojure/isogram/src/isogram.clj
+++ clojure/isogram/src/isogram.clj
@@ -1,5 +1,5 @@
 (ns isogram (:require [clojure.string :as str]))
 
 (defn isogram? [sentence]
-  (let [letters (filter #(Character/isLetter %) (str/lower-case sentence))]
+  (let [letters (filter Character/isLetter (str/lower-case sentence))]
     (= (distinct letters) letters)))
