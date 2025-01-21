2a15377dcfada911f9df0fecab0623ac1ee0916e
Erik Schierboom <erik_schierboom@hotmail.com>
2025-01-21 14:17:32 +0100

    csharp/run-length-encoding: add iteration

--- csharp/run-length-encoding/RunLengthEncoding.cs
+++ csharp/run-length-encoding/RunLengthEncoding.cs
@@ -1,59 +1,17 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
 public static class RunLengthEncoding
 {
-    public static string Encode(string input) => string.Join("", GroupConsecutive(input).Select(EncodeConsecutive));
+    public static string Encode(string input) => 
+        string.Join("", Regex.Matches(input, @"(.)\1*").Select(Encode));
 
-    private static string EncodeConsecutive(Tuple<char, int> group) => group.Item2 > 1 ? $"{group.Item2}{group.Item1}" : group.Item1.ToString();
+    private static string Encode(Match match) =>
+        match.Length == 1 ? match.Value : $"{match.Length}{match.Value[0]}";
 
-    private static IEnumerable<Tuple<char, int>> GroupConsecutive(string input)
-    {
-        if (input.Length == 0)
-        {
-            yield break;
-        }
+    public static string Decode(string input) => 
+        string.Join("", Regex.Matches(input, @"(\d+)?(.)").Select(Decode));
 
-        var current = input[0];
-        var count = 0;
-
-        foreach (var character in input)
-        {
-            if (character == current)
-            {
-                count++;
-            }
-            else
-            {
-                yield return Tuple.Create(current, count);
-                current = character;
-                count = 1;
-            }
-        }
-
-        yield return Tuple.Create(current, count);
-    }
-
-    public static string Decode(string input) => string.Join("", Regex.Matches(input, @"(\d+[^\d]|[^\d])").Cast<Match>().Select(Decode));
-
-    private static string Decode(Match match)
-    {
-        if (EncodedSingleCharacter(match))
-        {
-            return match.Value;
-        }
-
-        return DecodeConsecutiveCharacters(match);
-    }
-
-    private static bool EncodedSingleCharacter(Match match) => match.Value.Length == 1;
-
-    private static string DecodeConsecutiveCharacters(Match match)
-    {
-        var character = match.Value[match.Value.Length - 1];
-        var count = int.Parse(match.Value.Substring(0, match.Value.Length - 1));
-        return new string(character, count);
-    }
+    private static string Decode(Match match) =>
+        match.Length == 1 ? match.Value : new string(match.Groups[2].Value[0], int.Parse(match.Groups[1].Value));
 }
