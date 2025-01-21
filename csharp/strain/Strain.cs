59526794320b3174c24365789ae5584d2c734f4d
Erik Schierboom <erik_schierboom@hotmail.com>
2025-01-21 14:02:11 +0100

    csharp/strain: add iteration

--- csharp/strain/Strain.cs
+++ csharp/strain/Strain.cs
@@ -6,12 +6,10 @@ public static class Strain
     public static IEnumerable<T> Keep<T>(this IEnumerable<T> source, Predicate<T> predicate)
     {
         foreach (var element in source)
-        {
             if (predicate(element))
                 yield return element;
-        }
     }
 
-    public static IEnumerable<T> Discard<T>(this IEnumerable<T> source, Predicate<T> predicate) 
-        => source.Keep(element => !predicate(element));
+    public static IEnumerable<T> Discard<T>(this IEnumerable<T> source, Predicate<T> predicate) =>
+        source.Keep(element => !predicate(element));
 }
