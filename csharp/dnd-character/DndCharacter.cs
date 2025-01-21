3f8a95f1fe30676f079ff34f64ee14a567e90112
Erik Schierboom <erik_schierboom@hotmail.com>
2025-01-21 09:38:02 +0100

    csharp/dnd-character: add iteration

--- csharp/dnd-character/DndCharacter.cs
+++ csharp/dnd-character/DndCharacter.cs
@@ -5,20 +5,15 @@ public record DndCharacter(int Strength, int Dexterity, int Constitution, int In
 {
     public int Hitpoints { get; } = 10 + Modifier(Constitution);
     
-    public static int Modifier(int score) => (int)Math.Floor((score - 10) / 2.0);
+    public static int Modifier(int score) => -5 + score / 2;
 
     public static int Ability() =>
         Enumerable.Range(0, 4)
-            .Select(_ => Die.Roll())
-            .OrderByDescending(score => score).Take(3).Sum();
+            .Select(_ => Random.Shared.Next(1, 7))
+            .OrderDescending()
+            .Take(3)
+            .Sum();
 
     public static DndCharacter Generate() =>
         new(Ability(), Ability(), Ability(), Ability(), Ability(), Ability());
-}
-
-public static class Die
-{
-    private static readonly Random Random = new();
-
-    public static int Roll() => Random.Next(1, 7);
 }
