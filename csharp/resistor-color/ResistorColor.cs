e240e7706ebda2035172e015b529c6724a70af7b
Erik Schierboom <erik_schierboom@hotmail.com>
2025-01-21 14:41:13 +0100

    csharp/resistor-color: add iteration

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -2,7 +2,8 @@
 
 public static class ResistorColor
 {
-    private static readonly string[] ResistorColors = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+    private static readonly string[] ResistorColors = 
+        ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
 
     public static int ColorCode(string color) => Array.IndexOf(ResistorColors, color);
 
