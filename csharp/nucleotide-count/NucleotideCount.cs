d4c0d700d7e80dca523628f93da9972394c727ee
Erik Schierboom <erik_schierboom@hotmail.com>
2025-01-21 13:48:47 +0100

    csharp/nucleotide-count: add iteration

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -4,12 +4,12 @@ using System.Linq;
 
 public static class NucleotideCount
 {
-    private const string Nucleotides = "ACGT";
-
-    public static Dictionary<char, int> Count(string sequence)
-    {
-        if (!sequence.All(n => Nucleotides.Contains(n))) throw new ArgumentException();
-
-        return Nucleotides.ToDictionary(n => n, n => sequence.Count(c => c == n));
-    }
+    public static Dictionary<char, int> Count(string sequence) =>
+        sequence.Aggregate(
+            new Dictionary<char, int>{ ['A'] = 0, ['C'] = 0, ['G'] = 0, ['T'] = 0 }, 
+            (counts, c) =>
+            {
+                counts[c] = counts.TryGetValue(c, out var count) ? ++count : throw new ArgumentException();
+                return counts;
+            });
 }
