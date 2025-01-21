810cd365540539e539dcc47231bbecd2144f7fab
Erik Schierboom <erik_schierboom@hotmail.com>
2025-01-21 09:40:13 +0100

    csharp/high-scores: add iteration

--- csharp/high-scores/HighScores.cs
+++ csharp/high-scores/HighScores.cs
@@ -1,31 +1,13 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class HighScores
+public class HighScores(List<int> scores)
 {
-    private readonly List<int> _scores; 
+    public List<int> Scores() => scores;
 
-    public HighScores(List<int> scores) => _scores = scores;
+    public int Latest() => scores.Last();
 
-    public List<int> Scores() => _scores;
+    public int PersonalBest() => scores.Max();
 
-    public int Latest() => _scores.Last();
-
-    public int PersonalBest() => _scores.Max();
-
-    public List<int> PersonalTop() => _scores.OrderByDescending(score => score).Take(3).ToList();
-
-    public string Report()
-    {
-        var latestScoreReport = $"Your latest score was {Latest()}.";
-
-        var differenceOfLatestToPersonalBest = PersonalBest() - Latest();
-        var latestScoreComparedToPersonalBestReport = 
-            differenceOfLatestToPersonalBest == 0
-                ? "That's your personal best!"
-                : $"That's {differenceOfLatestToPersonalBest} short of your personal best!";
-
-        return $"{latestScoreReport} {latestScoreComparedToPersonalBestReport}";
-    }
+    public List<int> PersonalTopThree() => scores.OrderDescending().Take(3).ToList();
 }
